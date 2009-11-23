450add012ef8db47d94773bdf8f6da0b481841b7
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-11-23 08:25:40 +0000

    New Platformer StarterKit
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@34933 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ StarterKits/Platformer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aztec")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aztec")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6646564b-26f8-4949-88d2-c469a764f12d")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ StarterKits/Platformer/Tile.cs
@@ -0,0 +1,67 @@
+#region Using Clause
+using System;
+#if IPHONE
+using XnaTouch.Framework;
+using XnaTouch.Framework.Graphics;
+#else
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endif
+#endregion Using Clause
+
+namespace Platformer
+{
+    /// <summary>
+    /// Controls the collision detection and response behavior of a tile.
+    /// </summary>
+    enum TileCollision
+    {
+        /// <summary>
+        /// A passable tile is one which does not hinder player motion at all.
+        /// </summary>
+        Passable = 0,
+
+        /// <summary>
+        /// An impassable tile is one which does not allow the player to move through
+        /// it at all. It is completely solid.
+        /// </summary>
+        Impassable = 1,
+
+        /// <summary>
+        /// A platform tile is one which behaves like a passable tile except when the
+        /// player is above it. A player can jump up through a platform as well as move
+        /// past it to the left and right, but can not fall down through the top of it.
+        /// </summary>
+        Platform = 2,
+    }
+
+    /// <summary>
+    /// Stores the appearance and collision behavior of a tile.
+    /// </summary>
+    struct Tile
+    {
+        public Texture2D Texture;
+        public TileCollision Collision;
+
+#if ZUNE
+        public const int Width = 30;
+        public const int Height = 20;
+#elif IPHONE
+        public const int Width = 30;
+        public const int Height = 20;
+#else
+        public const int Width = 64;
+        public const int Height = 48;
+#endif
+        public static readonly Vector2 Size = new Vector2(Width, Height);
+
+        /// <summary>
+        /// Constructs a new tile.
+        /// </summary>
+        public Tile(Texture2D texture, TileCollision collision)
+        {
+            Texture = texture;
+            Collision = collision;
+        }
+    }
+}
