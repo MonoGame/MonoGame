7e116937be67f0121a42dfbcf11766d4211c113c
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-12-02 23:49:05 +0000

    Add ZuneLib to 3rd Party Repository.
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@35313 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ ThirdParty/ZuneLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZuneLib")]
+[assembly: AssemblyProduct("ZuneLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. Zune assemblies do not support COM.
+[assembly: ComVisible(false)]
+
+// If this assembly is the startup assembly, the following Guid is used to
+// uniquely identify the title storage container when deploying this assembly
+// to the Zune.
+[assembly: Guid("bfcde6d3-3cde-4906-8e8b-bea6d36f7413")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
