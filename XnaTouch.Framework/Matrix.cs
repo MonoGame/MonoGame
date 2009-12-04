8cd02180adbf5410201603e97db1e855acaa6ea1
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-12-04 15:59:13 +0000

    Added code to allow Farseer Physics to compile and run. Files Affected - * Curve.cs: * Matrix.cs: * CurveTangent.cs: * XnaTouch.Framework.csproj:
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@35369 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- XnaTouch.Framework/Matrix.cs
+++ XnaTouch.Framework/Matrix.cs
@@ -202,13 +202,45 @@ namespace XnaTouch.Framework
 
         public static Matrix Add(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            matrix1.M11 += matrix2.M11;
+            matrix1.M12 += matrix2.M12;
+            matrix1.M13 += matrix2.M13;
+            matrix1.M14 += matrix2.M14;
+            matrix1.M21 += matrix2.M21;
+            matrix1.M22 += matrix2.M22;
+            matrix1.M23 += matrix2.M23;
+            matrix1.M24 += matrix2.M24;
+            matrix1.M31 += matrix2.M31;
+            matrix1.M32 += matrix2.M32;
+            matrix1.M33 += matrix2.M33;
+            matrix1.M34 += matrix2.M34;
+            matrix1.M41 += matrix2.M41;
+            matrix1.M42 += matrix2.M42;
+            matrix1.M43 += matrix2.M43;
+            matrix1.M44 += matrix2.M44;
+            return matrix1;
         }
 
 
         public static void Add(ref Matrix matrix1, ref Matrix matrix2, out Matrix result)
         {
-            throw new NotImplementedException();
+            result.M11 = matrix1.M11 + matrix2.M11;
+            result.M12 = matrix1.M12 + matrix2.M12;
+            result.M13 = matrix1.M13 + matrix2.M13;
+            result.M14 = matrix1.M14 + matrix2.M14;
+            result.M21 = matrix1.M21 + matrix2.M21;
+            result.M22 = matrix1.M22 + matrix2.M22;
+            result.M23 = matrix1.M23 + matrix2.M23;
+            result.M24 = matrix1.M24 + matrix2.M24;
+            result.M31 = matrix1.M31 + matrix2.M31;
+            result.M32 = matrix1.M32 + matrix2.M32;
+            result.M33 = matrix1.M33 + matrix2.M33;
+            result.M34 = matrix1.M34 + matrix2.M34;
+            result.M41 = matrix1.M41 + matrix2.M41;
+            result.M42 = matrix1.M42 + matrix2.M42;
+            result.M43 = matrix1.M43 + matrix2.M43;
+            result.M44 = matrix1.M44 + matrix2.M44;
+
         }
 
         
@@ -339,44 +371,75 @@ namespace XnaTouch.Framework
 
         public static Matrix CreateRotationX(float radians)
         {
-            throw new NotImplementedException();
+            Matrix returnMatrix = Matrix.Identity;
+
+            returnMatrix.M22 = (float)Math.Cos(radians);
+            returnMatrix.M23 = (float)Math.Sin(radians);
+            returnMatrix.M32 = -returnMatrix.M23;
+            returnMatrix.M33 = returnMatrix.M22;
+
+            return returnMatrix;
+
         }
 
 
         public static void CreateRotationX(float radians, out Matrix result)
         {
-            throw new NotImplementedException();
+            result = Matrix.Identity;
+
+            result.M22 = (float)Math.Cos(radians);
+            result.M23 = (float)Math.Sin(radians);
+            result.M32 = -result.M23;
+            result.M33 = result.M22;
+
         }
 
 
         public static Matrix CreateRotationY(float radians)
         {
-            throw new NotImplementedException();
+            Matrix returnMatrix = Matrix.Identity;
+
+            returnMatrix.M11 = (float)Math.Cos(radians);
+            returnMatrix.M13 = (float)Math.Sin(radians);
+            returnMatrix.M31 = -returnMatrix.M13;
+            returnMatrix.M33 = returnMatrix.M11;
+
+            return returnMatrix;
         }
 
 
         public static void CreateRotationY(float radians, out Matrix result)
         {
-            throw new NotImplementedException();
+            result = Matrix.Identity;
+
+            result.M11 = (float)Math.Cos(radians);
+            result.M13 = (float)Math.Sin(radians);
+            result.M31 = -result.M13;
+            result.M33 = result.M11;
         }
 
 
         public static Matrix CreateRotationZ(float radians)
         {
-            Matrix matrix = new Matrix();
-            matrix.M11 = (float)Math.Cos((double)radians);
-            matrix.M12 = (float)Math.Sin((double)radians);
-            matrix.M21 = -matrix.M12;
-            matrix.M22 = matrix.M11;
-            matrix.M33 = 1f;
-            matrix.M44 = 1f;
-            return matrix;
+            Matrix returnMatrix = Matrix.Identity;
+
+            returnMatrix.M11 = (float)Math.Cos(radians);
+            returnMatrix.M12 = (float)Math.Sin(radians);
+            returnMatrix.M21 = -returnMatrix.M12;
+            returnMatrix.M22 = returnMatrix.M11;
+
+            return returnMatrix;
         }
 
 
         public static void CreateRotationZ(float radians, out Matrix result)
         {
-            result = CreateRotationZ(radians);
+            result = Matrix.Identity;
+
+            result.M11 = (float)Math.Cos(radians);
+            result.M12 = (float)Math.Sin(radians);
+            result.M21 = -result.M12;
+            result.M22 = result.M11;
         }
 
 
@@ -430,7 +493,11 @@ namespace XnaTouch.Framework
 
         public static void CreateTranslation(ref Vector3 position, out Matrix result)
         {
-            throw new NotImplementedException();
+            result = Matrix.Identity;
+
+            result.M41 = position.X;
+            result.M42 = position.Y;
+            result.M43 = position.Z;
         }
 
 
@@ -442,7 +509,11 @@ namespace XnaTouch.Framework
 
         public static void CreateTranslation(float xPosition, float yPosition, float zPosition, out Matrix result)
         {
-            throw new NotImplementedException();
+            result = Matrix.Identity;
+
+            result.M41 = xPosition;
+            result.M42 = yPosition;
+            result.M43 = zPosition;
         }
 
 
@@ -494,15 +565,50 @@ namespace XnaTouch.Framework
         }
 
 
-        public static Matrix Invert(Matrix m)
+        public static Matrix Invert(Matrix matrix)
         {
-            throw new NotImplementedException();
+            Invert(ref matrix, out matrix);
+            return matrix;
+
         }
 
 
         public static void Invert(ref Matrix matrix, out Matrix result)
         {
-            throw new NotImplementedException();
+            ///
+            // Use Laplace expansion theorem to calculate the inverse of a 4x4 matrix
+            // 
+            // 1. Calculate the 2x2 determinants needed the 4x4 determinant based on the 2x2 determinants 
+            // 3. Create the adjugate matrix, which satisfies: A * adj(A) = det(A) * I
+            // 4. Divide adjugate matrix with the determinant to find the inverse
+            
+            float det1, det2, det3, det4, det5, det6, det7, det8, det9, det10, det11, det12;
+            float detMatrix;
+            findDeterminants(ref matrix, out detMatrix, out det1, out det2, out det3, out det4, out det5, out det6, 
+                             out det7, out det8, out det9, out det10, out det11, out det12);
+            
+            float invDetMatrix = 1f / detMatrix;
+            
+            Matrix ret; // Allow for matrix and result to point to the same structure
+            
+            ret.M11 = (matrix.M22*det12 - matrix.M23*det11 + matrix.M24*det10) * invDetMatrix;
+            ret.M12 = (-matrix.M12*det12 + matrix.M13*det11 - matrix.M14*det10) * invDetMatrix;
+            ret.M13 = (matrix.M42*det6 - matrix.M43*det5 + matrix.M44*det4) * invDetMatrix;
+            ret.M14 = (-matrix.M32*det6 + matrix.M33*det5 - matrix.M34*det4) * invDetMatrix;
+            ret.M21 = (-matrix.M21*det12 + matrix.M23*det9 - matrix.M24*det8) * invDetMatrix;
+            ret.M22 = (matrix.M11*det12 - matrix.M13*det9 + matrix.M14*det8) * invDetMatrix;
+            ret.M23 = (-matrix.M41*det6 + matrix.M43*det3 - matrix.M44*det2) * invDetMatrix;
+            ret.M24 = (matrix.M31*det6 - matrix.M33*det3 + matrix.M34*det2) * invDetMatrix;
+            ret.M31 = (matrix.M21*det11 - matrix.M22*det9 + matrix.M24*det7) * invDetMatrix;
+            ret.M32 = (-matrix.M11*det11 + matrix.M12*det9 - matrix.M14*det7) * invDetMatrix;
+            ret.M33 = (matrix.M41*det5 - matrix.M42*det3 + matrix.M44*det1) * invDetMatrix;
+            ret.M34 = (-matrix.M31*det5 + matrix.M32*det3 - matrix.M34*det1) * invDetMatrix;
+            ret.M41 = (-matrix.M21*det10 + matrix.M22*det8 - matrix.M23*det7) * invDetMatrix;
+            ret.M42 = (matrix.M11*det10 - matrix.M12*det8 + matrix.M13*det7) * invDetMatrix;
+            ret.M43 = (-matrix.M41*det4 + matrix.M42*det2 - matrix.M43*det1) * invDetMatrix;
+            ret.M44 = (matrix.M31*det4 - matrix.M32*det2 + matrix.M33*det1) * invDetMatrix;
+            
+            result = ret;
         }
 
 
@@ -519,25 +625,77 @@ namespace XnaTouch.Framework
 
         public static Matrix Multiply(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            Matrix ret;
+            Multiply(ref matrix1, ref matrix2, out ret);
+            return ret;
         }
 
 
         public static void Multiply(ref Matrix matrix1, ref Matrix matrix2, out Matrix result)
         {
-            throw new NotImplementedException();
+            result.M11 = matrix1.M11 * matrix2.M11 + matrix1.M12 * matrix2.M21 + matrix1.M13 * matrix2.M31 + matrix1.M14 * matrix2.M41;
+                        result.M12 = matrix1.M11 * matrix2.M12 + matrix1.M12 * matrix2.M22 + matrix1.M13 * matrix2.M32 + matrix1.M14 * matrix2.M42;
+                        result.M13 = matrix1.M11 * matrix2.M13 + matrix1.M12 * matrix2.M23 + matrix1.M13 * matrix2.M33 + matrix1.M14 * matrix2.M43;
+                        result.M14 = matrix1.M11 * matrix2.M14 + matrix1.M12 * matrix2.M24 + matrix1.M13 * matrix2.M34 + matrix1.M14 * matrix2.M44;
+                        
+            result.M21 = matrix1.M21 * matrix2.M11 + matrix1.M22 * matrix2.M21 + matrix1.M23 * matrix2.M31 + matrix1.M24 * matrix2.M41;
+                        result.M22 = matrix1.M21 * matrix2.M12 + matrix1.M22 * matrix2.M22 + matrix1.M23 * matrix2.M32 + matrix1.M24 * matrix2.M42;
+                        result.M23 = matrix1.M21 * matrix2.M13 + matrix1.M22 * matrix2.M23 + matrix1.M23 * matrix2.M33 + matrix1.M24 * matrix2.M43;
+                        result.M24 = matrix1.M21 * matrix2.M14 + matrix1.M22 * matrix2.M24 + matrix1.M23 * matrix2.M34 + matrix1.M24 * matrix2.M44;
+            
+                        result.M31 = matrix1.M31 * matrix2.M11 + matrix1.M32 * matrix2.M21 + matrix1.M33 * matrix2.M31 + matrix1.M34 * matrix2.M41;
+                        result.M32 = matrix1.M31 * matrix2.M12 + matrix1.M32 * matrix2.M22 + matrix1.M33 * matrix2.M32 + matrix1.M34 * matrix2.M42;
+                        result.M33 = matrix1.M31 * matrix2.M13 + matrix1.M32 * matrix2.M23 + matrix1.M33 * matrix2.M33 + matrix1.M34 * matrix2.M43;
+                        result.M34 = matrix1.M31 * matrix2.M14 + matrix1.M32 * matrix2.M24 + matrix1.M33 * matrix2.M34 + matrix1.M34 * matrix2.M44;
+            
+                        result.M41 = matrix1.M41 * matrix2.M11 + matrix1.M42 * matrix2.M21 + matrix1.M43 * matrix2.M31 + matrix1.M44 * matrix2.M41;
+                        result.M42 = matrix1.M41 * matrix2.M12 + matrix1.M42 * matrix2.M22 + matrix1.M43 * matrix2.M32 + matrix1.M44 * matrix2.M42;
+                        result.M43 = matrix1.M41 * matrix2.M13 + matrix1.M42 * matrix2.M23 + matrix1.M43 * matrix2.M33 + matrix1.M44 * matrix2.M43;
+            result.M44 = matrix1.M41 * matrix2.M14 + matrix1.M42 * matrix2.M24 + matrix1.M43 * matrix2.M34 + matrix1.M44 * matrix2.M44; 
         }
 
 
         public static Matrix Multiply(Matrix matrix1, float factor)
         {
-            throw new NotImplementedException();
+            matrix1.M11 *= factor;
+            matrix1.M12 *= factor;
+            matrix1.M13 *= factor;
+            matrix1.M14 *= factor;
+            matrix1.M21 *= factor;
+            matrix1.M22 *= factor;
+            matrix1.M23 *= factor;
+            matrix1.M24 *= factor;
+            matrix1.M31 *= factor;
+            matrix1.M32 *= factor;
+            matrix1.M33 *= factor;
+            matrix1.M34 *= factor;
+            matrix1.M41 *= factor;
+            matrix1.M42 *= factor;
+            matrix1.M43 *= factor;
+            matrix1.M44 *= factor;
+            return matrix1;
         }
 
 
         public static void Multiply(ref Matrix matrix1, float factor, out Matrix result)
         {
-            throw new NotImplementedException();
+            result.M11 = matrix1.M11 * factor;
+            result.M12 = matrix1.M12 * factor;
+            result.M13 = matrix1.M13 * factor;
+            result.M14 = matrix1.M14 * factor;
+            result.M21 = matrix1.M21 * factor;
+            result.M22 = matrix1.M22 * factor;
+            result.M23 = matrix1.M23 * factor;
+            result.M24 = matrix1.M24 * factor;
+            result.M31 = matrix1.M31 * factor;
+            result.M32 = matrix1.M32 * factor;
+            result.M33 = matrix1.M33 * factor;
+            result.M34 = matrix1.M34 * factor;
+            result.M41 = matrix1.M41 * factor;
+            result.M42 = matrix1.M42 * factor;
+            result.M43 = matrix1.M43 * factor;
+            result.M44 = matrix1.M44 * factor;
+
         }
 
 
@@ -555,7 +713,8 @@ namespace XnaTouch.Framework
 
         public static Matrix operator +(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            Matrix.Add(ref matrix1, ref matrix2, out matrix1);
+            return matrix1;
         }
 
 
@@ -661,14 +820,75 @@ namespace XnaTouch.Framework
 
         public static Matrix Transpose(Matrix matrix)
         {
-            throw new NotImplementedException();
+            Matrix ret;
+            Transpose(ref matrix, out ret);
+            return ret;
         }
 
         
         public static void Transpose(ref Matrix matrix, out Matrix result)
         {
-            throw new NotImplementedException();
+            result.M11 = matrix.M11;
+            result.M12 = matrix.M21;
+            result.M13 = matrix.M31;
+            result.M14 = matrix.M41;
+
+            result.M21 = matrix.M12;
+            result.M22 = matrix.M22;
+            result.M23 = matrix.M32;
+            result.M24 = matrix.M42;
+
+            result.M31 = matrix.M13;
+            result.M32 = matrix.M23;
+            result.M33 = matrix.M33;
+            result.M34 = matrix.M43;
+
+            result.M41 = matrix.M14;
+            result.M42 = matrix.M24;
+            result.M43 = matrix.M34;
+            result.M44 = matrix.M44;
         }
         #endregion Public Methods
+		
+		#region Private Static Methods
+        
+        /// <summary>
+        /// Helper method for using the Laplace expansion theorem using two rows expansions to calculate major and 
+        /// minor determinants of a 4x4 matrix. This method is used for inverting a matrix.
+        /// </summary>
+        private static void findDeterminants(ref Matrix matrix, out float major, 
+                                             out float minor1, out float minor2, out float minor3, out float minor4, out float minor5, out float minor6,
+                                             out float minor7, out float minor8, out float minor9, out float minor10, out float minor11, out float minor12)
+        {
+                double det1 = (double)matrix.M11 * (double)matrix.M22 - (double)matrix.M12 * (double)matrix.M21;
+                double det2 = (double)matrix.M11 * (double)matrix.M23 - (double)matrix.M13 * (double)matrix.M21;
+                double det3 = (double)matrix.M11 * (double)matrix.M24 - (double)matrix.M14 * (double)matrix.M21;
+                double det4 = (double)matrix.M12 * (double)matrix.M23 - (double)matrix.M13 * (double)matrix.M22;
+                double det5 = (double)matrix.M12 * (double)matrix.M24 - (double)matrix.M14 * (double)matrix.M22;
+                double det6 = (double)matrix.M13 * (double)matrix.M24 - (double)matrix.M14 * (double)matrix.M23;
+                double det7 = (double)matrix.M31 * (double)matrix.M42 - (double)matrix.M32 * (double)matrix.M41;
+                double det8 = (double)matrix.M31 * (double)matrix.M43 - (double)matrix.M33 * (double)matrix.M41;
+                double det9 = (double)matrix.M31 * (double)matrix.M44 - (double)matrix.M34 * (double)matrix.M41;
+                double det10 = (double)matrix.M32 * (double)matrix.M43 - (double)matrix.M33 * (double)matrix.M42;
+                double det11 = (double)matrix.M32 * (double)matrix.M44 - (double)matrix.M34 * (double)matrix.M42;
+                double det12 = (double)matrix.M33 * (double)matrix.M44 - (double)matrix.M34 * (double)matrix.M43;
+                
+                major = (float)(det1*det12 - det2*det11 + det3*det10 + det4*det9 - det5*det8 + det6*det7);
+                minor1 = (float)det1;
+                minor2 = (float)det2;
+                minor3 = (float)det3;
+                minor4 = (float)det4;
+                minor5 = (float)det5;
+                minor6 = (float)det6;
+                minor7 = (float)det7;
+                minor8 = (float)det8;
+                minor9 = (float)det9;
+                minor10 = (float)det10;
+                minor11 = (float)det11;
+                minor12 = (float)det12;
+        }
+        
+        #endregion Private Static Methods
+
     }
 }
