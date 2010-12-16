6727c7b08f9369927d3b687973d691ac986484cc
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2010-12-16 11:55:36 +0000

    JDIndia's changes to speed up SpriteBatch rendering. Thanks again JDIndia!
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@55031 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ XnaTouch.Framework/Graphics/VertexColorTexture.cs
@@ -0,0 +1,22 @@
+using System;
+
+using XnaTouch.Framework;
+using XnaTouch.Framework.Graphics;
+
+namespace XnaTouch.Framework.Graphics
+{
+	//[StructLayout(LayoutKind.Sequential)]
+	internal struct VertexColorTexture
+	{
+		public Vector2 Vertex;
+		public uint Color;
+		public Vector2 TexCoord;
+		public VertexColorTexture ( Vector2 vertex, Color color, Vector2 texCoord )
+		{
+			Vertex = vertex;
+			Color = color.PackedValue;
+			TexCoord = texCoord;
+		}
+	}
+}
+
---
+++ XnaTouch.Framework/Graphics/VertexPosition2ColorTexture.cs
@@ -0,0 +1,24 @@
+using System;
+
+using XnaTouch.Framework;
+using XnaTouch.Framework.Graphics;
+
+namespace XnaTouch.Framework.Graphics
+{
+	//[StructLayout(LayoutKind.Sequential)]
+	// This should really be XNA's VertexPositionColorTexture
+	// but I'm not sure we want to use Vector3s if we don't have to.
+	internal struct VertexPosition2ColorTexture
+	{
+		public Vector2 Position;
+		public uint Color;
+		public Vector2 TextureCoordinate;
+		public VertexPosition2ColorTexture ( Vector2 position, Color color, Vector2 texCoord )
+		{
+			Position = position;
+			Color = color.PackedValue;
+			TextureCoordinate = texCoord;
+		}
+	}
+}
+
