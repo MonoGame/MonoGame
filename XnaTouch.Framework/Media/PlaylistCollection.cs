549b0616f0e65ed3bae556d4903d8b80246b2806
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-01-29 01:25:40 +0000

    * Media/MediaLibrary.cs: * Media/SongCollection.cs: * Media/PlaylistCollection.cs:
    Media API improvements
    
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@56886 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- XnaTouch.Framework/Media/PlaylistCollection.cs
+++ XnaTouch.Framework/Media/PlaylistCollection.cs
@@ -37,40 +37,108 @@ permitted under your local laws, the contributors exclude the implied warranties
 purpose and non-infringement.
 */
 #endregion License
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
 ﻿
 namespace XnaTouch.Framework.Media
 {
 
-    public sealed class PlaylistCollection : IEnumerable<Playlist>, IEnumerable, IDisposable
+    public sealed class PlaylistCollection : ICollection<Playlist>, IEnumerable<Playlist>, IEnumerable, IDisposable
     {
+		private bool isReadOnly = false;
+		private List<Playlist> innerlist;
+		
         public void Dispose()
-        {
-        }
-
-        protected override void Finalize()
         {
         }
 
         public IEnumerator<Playlist> GetEnumerator()
         {
+            return innerlist.GetEnumerator();
         }
-
+		
         IEnumerator IEnumerable.GetEnumerator()
         {
+            return innerlist.GetEnumerator();
         }
 
         public int Count
         {
             get
             {
+				return innerlist.Count;
             }
+        }
+		
+		public bool IsReadOnly
+        {
+            get { return this.isReadOnly; }
         }
 
         public Playlist this[int index]
         {
             get
             {
+				return this.innerlist[index];
             }
+        }
+		
+		public void Add(Playlist item)
+        {
+            if (item == null)
+                throw new ArgumentNullException();
+
+            if (innerlist.Count == 0)
+            {
+                this.innerlist.Add(item);
+                return;
+            }
+
+            for (int i = 0; i < this.innerlist.Count; i++)
+            {
+                if (item.Duration < this.innerlist[i].Duration)
+                {
+                    this.innerlist.Insert(i, item);
+                    return;
+                }
+            }
+
+            this.innerlist.Add(item);
+        }
+		
+		public void Clear()
+        {
+            innerlist.Clear();
+        }
+        
+        public PlaylistCollection Clone()
+        {
+            PlaylistCollection plc = new PlaylistCollection();
+            foreach (Playlist playlist in this.innerlist)
+                plc.Add(playlist);
+            return plc;
+        }
+        
+        public bool Contains(Playlist item)
+        {
+            return innerlist.Contains(item);
+        }
+        
+        public void CopyTo(Playlist[] array, int arrayIndex)
+        {
+            innerlist.CopyTo(array, arrayIndex);
+        }
+		
+		public int IndexOf(Playlist item)
+        {
+            return innerlist.IndexOf(item);
+        }
+        
+        public bool Remove(Playlist item)
+        {
+            return innerlist.Remove(item);
         }
     }
 }
