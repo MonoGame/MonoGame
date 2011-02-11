325f9200f3ec1609fc549d61f7815ee9336fcff6
SND\kelthar_cp <SND\kelthar_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-02-11 14:50:52 +0000

    git-svn-id: https://xnatouch.svn.codeplex.com/svn@57609 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ XnaTouch.Framework/Graphics/CompareFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XnaTouch.Framework.Graphics
+{
+    public enum CompareFunction
+    {
+        Always,
+        Never,
+        Less,
+        LessEqual,
+        Equal,
+        GreaterEqual,
+        Greater,
+        NotEqual
+    }
+}
