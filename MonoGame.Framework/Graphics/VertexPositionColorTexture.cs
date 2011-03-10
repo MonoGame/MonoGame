b42329d992ed95a17acb34c6d6fc59977ec76e9c
SND\taskbit_cp <SND\taskbit_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-03-10 02:18:58 +0000

    Initial Windows OS support using OpenGL
    XNA 4: Added IVertexType, VertextPositionColorTexture
    
    
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@59043 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ MonoGame.Framework/Graphics/VertexPositionColorTexture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    [SerializableAttribute]
+    public struct VertexPositionColorTexture : IVertexType
+    {
+        public static readonly VertexDeclaration VertexDeclaration;
+
+        public Color Color;
+        public Vector3 Position;
+        public Vector2 TextureCoordinate;
+
+        public VertexPositionColorTexture(Vector3 position, Color color, Vector2 textureCoordinate)
+        {
+            Color = color;
+            Position = position;
+            TextureCoordinate = textureCoordinate;
+        }
+       
+        public static bool operator !=(VertexPositionColorTexture left, VertexPositionColorTexture right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(VertexPositionColorTexture left, VertexPositionColorTexture right)
+        {
+            return left.Color == right.Color && left.Position == right.Position &&
+                   left.TextureCoordinate == right.TextureCoordinate;
+        }
+
+
+        VertexDeclaration IVertexType.VertexDeclaration
+        {
+            get { return VertexDeclaration; }
+        }
+    }
+}
