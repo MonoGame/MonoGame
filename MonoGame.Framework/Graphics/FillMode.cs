a90f61799aaa84bf5d8f5e17b6933801eaff5d18
SND\CartBlanche_cp <SND\CartBlanche_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-03-15 01:22:02 +0000

    Updated and added missing files.
    git-svn-id: https://xnatouch.svn.codeplex.com/svn@59304 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- MonoGame.Framework/Graphics/FillMode.cs
+++ MonoGame.Framework/Graphics/FillMode.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace XnaTouch.Framework.Graphics
+namespace Microsoft.Xna.Framework.Graphics
 {
     public enum FillMode
     {
