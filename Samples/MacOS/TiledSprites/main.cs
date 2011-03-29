bd38f4a5aa39c26c2fc42f28b4a8d42e21a8465b
Kenneth Pouncey <kjpou@pt.lu>
2011-03-29 12:09:29 +0200

    Add Tiled Sprites Sample

---
+++ Samples/MacOS/TiledSprites/main.cs
@@ -0,0 +1,34 @@
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+
+namespace TiledSprites
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate();
+				NSApplication.Main(args);
+			}
+		}
+	}
+	
+	class AppDelegate : NSApplicationDelegate
+	{
+		private TiledSpritesSample game;
+		
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			game = new TiledSpritesSample ();
+			game.Run ();
+		}
+		
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}	
+}
--- Samples/iOS/TiledSprites/AnimatedSprite.cs
+++ Samples/iOS/TiledSprites/AnimatedSprite.cs
@@ -29,155 +29,140 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Media;
+
 #endif
 
 #endregion
 
 namespace TiledSprites
 {
-    public class AnimatedSprite
-    {
-        #region Fields
-        private SpriteSheet sheet;
-        private Vector2 positionValue;
-        private Vector2 originValue;
-        private Vector2 scaleValue;
-        private float rotationValue;
-        private int currentFrameValue;
-        private int numFrames;
-        #endregion
+	public class AnimatedSprite
+	{
+	#region Fields
+		private SpriteSheet sheet;
+		private Vector2 positionValue;
+		private Vector2 originValue;
+		private Vector2 scaleValue;
+		private float rotationValue;
+		private int currentFrameValue;
+		private int numFrames;
 
-        #region Public Properties
-        public Vector2 Position
-        {
-            set
-            {
-                positionValue = value;
-            }
-            get
-            {
-                return positionValue;
-            }
-        }
+	#endregion
 
-        public Vector2 Origin
-        {
-            set
-            {
-                originValue = value;
-            }
-            get
-            {
-                return originValue;
-            }
-        }
+	#region Public Properties
+		public Vector2 Position {
+			set {
+				positionValue = value;
+			}
+			get {
+				return positionValue;
+			}
+		}
 
-        public float Rotation
-        {
-            set
-            {
-                rotationValue = value;
-            }
-            get
-            {
-                return rotationValue;
-            }
-        }
+		public Vector2 Origin {
+			set {
+				originValue = value;
+			}
+			get {
+				return originValue;
+			}
+		}
 
-        public Vector2 ScaleValue
-        {
-            set
-            {
-                scaleValue = value;
-            }
-            get
-            {
-                return scaleValue;
-            }
-        }
-        public int CurrentFrame
-        {
-            set
-            {
-                if (value > (numFrames - 1))
-                {
-                    string message =
-                        string.Format("{0} is an invalid value for CurrentFrame.  " +
-                        "Valid values are from 0 to numFrames - 1 ({1})",
-                        value, numFrames - 1);
-                    throw new ArgumentOutOfRangeException("value", message);
-                }
-                currentFrameValue = value;
-            }
-            get
-            {
-                return currentFrameValue;
-            }
+		public float Rotation {
+			set {
+				rotationValue = value;
+			}
+			get {
+				return rotationValue;
+			}
+		}
 
-        }
-        #endregion
+		public Vector2 ScaleValue {
+			set {
+				scaleValue = value;
+			}
+			get {
+				return scaleValue;
+			}
+		}
 
-        #region Contructors
-        public AnimatedSprite(SpriteSheet spriteSheet, int frameWidth, 
-            int frameHeight, int padding, int rows, int columns,
-            Point startFrame, int frames)
-        {
-            if (spriteSheet == null)
-            {
-                throw new ArgumentNullException("spriteSheet");
-            }
-            int spriteAreaHeight = (frameHeight + padding) * rows - padding;
-            int spriteAreaWidth = (frameWidth + padding) * columns - padding;
+		public int CurrentFrame {
+			set {
+				if (value > (numFrames - 1)) {
+					string message = 
+			string.Format ("{0} is an invalid value for CurrentFrame.  " + 
+			"Valid values are from 0 to numFrames - 1 ({1})", 
+			value, numFrames - 1);
+					throw new ArgumentOutOfRangeException ("value", message);
+				}
+				currentFrameValue = value;
+			}
+			get {
+				return currentFrameValue;
+			}
 
-            //first, make sure the sheet is possible
-            if ((spriteAreaWidth > spriteSheet.Texture.Width) ||
-                (spriteAreaHeight > spriteSheet.Texture.Height))
-            {
-                throw new ArgumentException(
-                    "The layout specified is too large for the SpriteSheet."
-                    );
-            }
+		}
 
-            sheet = spriteSheet;
-            numFrames = frames;
-           
-            int startFrameIndex = startFrame.Y * columns + startFrame.X;
-            
+	#endregion
 
-            //now auto-generate the animation data,
-            //left to right, top to bottom.
-            int frameIndex = 0;
-            for (int i = startFrameIndex; i < (numFrames+startFrameIndex); i++)
-            {
-                int x =(i % columns);
-                int y = (i / columns);
-                int left = ( x * (frameWidth+ padding));
-                int top = (y * (frameHeight + padding));
+	#region Contructors
+		public AnimatedSprite (SpriteSheet spriteSheet, int frameWidth, 
+		int frameHeight,int padding,int rows,int columns,
+		Point startFrame,int frames)		{
+			if (spriteSheet == null) {
+				throw new ArgumentNullException ("spriteSheet");
+			}
+			int spriteAreaHeight = (frameHeight + padding) * rows - padding;
+			int spriteAreaWidth = (frameWidth + padding) * columns - padding;
 
-                top = top % spriteAreaHeight;
+			//first, make sure the sheet is possible
+			if ((spriteAreaWidth > spriteSheet.Texture.Width) || 
+		(spriteAreaHeight > spriteSheet.Texture.Height)) {
+				throw new ArgumentException (
+			"The layout specified is too large for the SpriteSheet."
+			);
+			}
 
-                sheet.AddSourceSprite(frameIndex,
-                    new Rectangle(left, top, frameWidth, frameHeight));
-                frameIndex++;
-            }
-        }
-        #endregion
+			sheet = spriteSheet;
+			numFrames = frames;
 
-        #region Methods
-        public void IncrementAnimationFrame()
-        {
-            currentFrameValue = (currentFrameValue + 1) % numFrames;
-        }
+			int startFrameIndex = startFrame.Y * columns + startFrame.X;
 
-        public void Draw(SpriteBatch batch, Color color, SpriteBlendMode blendMode)
-        {
-            batch.Begin(blendMode, SpriteSortMode.Immediate, 
-                SaveStateMode.None);
-            batch.Draw(sheet.Texture, positionValue, sheet[currentFrameValue],
-                color, rotationValue, originValue, scaleValue,
-                SpriteEffects.None, 0f);
-            batch.End();
-        }
-        #endregion
-    }
+
+			//now auto-generate the animation data,
+			//left to right, top to bottom.
+			int frameIndex = 0;
+			for (int i = startFrameIndex; i < (numFrames+startFrameIndex); i++) {
+				int x = (i % columns);
+				int y = (i / columns);
+				int left = (x * (frameWidth + padding));
+				int top = (y * (frameHeight + padding));
+
+				top = top % spriteAreaHeight;
+
+				sheet.AddSourceSprite (frameIndex, 
+			new Rectangle (left, top, frameWidth, frameHeight));
+				frameIndex++;
+			}
+		}
+
+	#endregion
+
+	#region Methods
+		public void IncrementAnimationFrame ()
+		{
+			currentFrameValue = (currentFrameValue + 1) % numFrames;
+		}
+
+		public void Draw (SpriteBatch batch, Color color, SpriteBlendMode blendMode)
+		{
+			batch.Begin (SpriteSortMode.Immediate, blendMode, 
+				SaveStateMode.None);
+			batch.Draw (sheet.Texture, positionValue, sheet [currentFrameValue], 
+		color, rotationValue, originValue, scaleValue, 
+		SpriteEffects.None, 0f);
+			batch.End ();
+		}
+	#endregion
+	}
 }
