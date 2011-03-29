60e7af0e09f29d4e852393c7b9345cae70d993f0
Kenneth Pouncey <kjpou@pt.lu>
2011-03-29 07:36:13 +0200

    Add bouncing box sample for MacOS

---
+++ Samples/MacOS/BouncingBox/main.cs
@@ -0,0 +1,34 @@
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+
+namespace Microsoft.Xna.Samples.BouncingBox
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate();
+				NSApplication.Main(args);
+			}
+		}
+	}
+	
+	class AppDelegate : NSApplicationDelegate
+	{
+		private Game1 game;
+		
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			game = new Game1 ();
+			game.Run ();
+		}
+		
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}	
+}
