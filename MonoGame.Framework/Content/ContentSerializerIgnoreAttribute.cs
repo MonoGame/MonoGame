7161c24d5ba3d6288da0f459bf3e117d6e8070c1
Kenneth Pouncey <kjpou@pt.lu>
2011-04-12 06:40:12 +0200

    Move the ContentSerializer* files to the Content directory where they should have been

--- MonoGame.Framework/Content/ContentSerializerIgnoreAttribute.cs
+++ MonoGame.Framework/Content/ContentSerializerIgnoreAttribute.cs
@@ -37,13 +37,21 @@
 // purpose and non-infringement.
 // */
 // #endregion License
-// 
+//
+// Author: Kenneth James Pouncey
+//
 using System;
 
-namespace Microsoft.Xna.Framework.Content
+namespace Microsoft.Xna.Framework
 {
+	// http://msdn.microsoft.com/en-us/library/bb195465.aspx
+	// The class definition on msdn site shows: [AttributeUsageAttribute(384)]
+	// The following code var ff = (AttributeTargets)384; shows that ff is Field | Property
+	//  so that is what we use.
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 	public sealed class ContentSerializerIgnoreAttribute : Attribute
 	{
 	}
+
 }
 
