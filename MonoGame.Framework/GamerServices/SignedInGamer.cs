3c36db8c78d33d058dc399415df37c280e199059
Dominique Louis <Dominique@SavageSoftwareSolutions.com>
2011-06-30 00:04:21 +0100

    Even better version  parsing to ensure GameCenter works even past iOS 10.x - Thanks Clancey.

--- MonoGame.Framework/GamerServices/SignedInGamer.cs
+++ MonoGame.Framework/GamerServices/SignedInGamer.cs
@@ -76,9 +76,17 @@ namespace Microsoft.Xna.Framework.GamerServices
 		
 		private void DoAuthentication()
 		{
-			try 
+			try 				
 			{
-				if (double.Parse(UIDevice.CurrentDevice.SystemVersion) > 4.1)
+				var osVersion = UIDevice.CurrentDevice.SystemVersion;
+				if(osVersion.Contains("."))
+				if(osVersion.IndexOf(".") != osVersion.LastIndexOf("."))
+				{
+					var parts = osVersion.Split(char.Parse("."));
+					osVersion = parts[0] + "." + parts[1];
+				}
+				
+				if (double.Parse(osVersion) > 4.1)
 				{
 					
 					lp = GKLocalPlayer.LocalPlayer;
