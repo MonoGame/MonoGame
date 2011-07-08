7d6494e2f1869cd330e73098799ba7eeb7d5772d
Kenneth Pouncey <kjpou@pt.lu>
2011-07-08 11:44:03 +0200

    Add new modules to source base

---
+++ MonoGame.Framework/Net/GamerStates.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Microsoft.Xna.Framework.Net
+{
+	[Flags]
+	public enum GamerStates
+	{
+		Local 		= 0x000001,
+		Host 		= 0x000010,
+		HasVoice 	= 0x000100,
+		Guest 		= 0x001000,
+		MutedByLocalUser = 0x010000,
+		PrivateSlot	=  0x100000
+		
+	}
+}
+
