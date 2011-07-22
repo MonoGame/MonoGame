fc8bf54989e3b198d3db881ef74219a204cac495
Kenneth Pouncey <kjpou@pt.lu>
2011-07-23 00:05:05 +0200

    Added Asteroid Belt Assault which is the second coding example from Chapters 4 and 5 from the book XNA 4.0 Game Development by Example.

---
+++ Samples/MacOS/BookSourceCode/XNAGameDevelopmentbyExampleCode/AsteroidBeltAssault/CollisionManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Asteroid_Belt_Assault
+{
+    class CollisionManager
+    {
+        private AsteroidManager asteroidManager;
+        private PlayerManager playerManager;
+        private EnemyManager enemyManager;
+        private ExplosionManager explosionManager;
+        private Vector2 offScreen = new Vector2(-500, -500);
+        private Vector2 shotToAsteroidImpact = new Vector2(0, -20);
+        private int enemyPointValue = 100;
+
+        public CollisionManager(
+            AsteroidManager asteroidManager,
+            PlayerManager playerManager,
+            EnemyManager enemyManager,
+            ExplosionManager explosionManager)
+        {
+            this.asteroidManager = asteroidManager;
+            this.playerManager = playerManager;
+            this.enemyManager = enemyManager;
+            this.explosionManager = explosionManager;
+        }
+
+        private void checkShotToEnemyCollisions()
+        {
+            foreach (Sprite shot in playerManager.PlayerShotManager.Shots)
+            {
+                foreach (Enemy enemy in enemyManager.Enemies)
+                {
+                    if (shot.IsCircleColliding(
+                        enemy.EnemySprite.Center,
+                        enemy.EnemySprite.CollisionRadius))
+                    {
+                        shot.Location = offScreen;
+                        enemy.Destroyed = true;
+                        playerManager.PlayerScore += enemyPointValue;
+                        explosionManager.AddExplosion(
+                            enemy.EnemySprite.Center,
+                            enemy.EnemySprite.Velocity / 10);
+                    }
+
+                }
+            }
+        }
+
+        private void checkShotToAsteroidCollisions()
+        {
+            foreach (Sprite shot in playerManager.PlayerShotManager.Shots)
+            {
+                foreach (Sprite asteroid in asteroidManager.Asteroids)
+                {
+                    if (shot.IsCircleColliding(
+                        asteroid.Center,
+                        asteroid.CollisionRadius))
+                    {
+                        shot.Location = offScreen;
+                        asteroid.Velocity += shotToAsteroidImpact;
+                    }
+                }
+            }
+        }
+
+        private void checkShotToPlayerCollisions()
+        {
+            foreach (Sprite shot in enemyManager.EnemyShotManager.Shots)
+            {
+                if (shot.IsCircleColliding(
+                    playerManager.playerSprite.Center,
+                    playerManager.playerSprite.CollisionRadius))
+                {
+                    shot.Location = offScreen;
+                    playerManager.Destroyed = true;
+                    explosionManager.AddExplosion(
+                        playerManager.playerSprite.Center,
+                        Vector2.Zero);
+                }
+            }
+        }
+
+        private void checkEnemyToPlayerCollisions()
+        {
+            foreach (Enemy enemy in enemyManager.Enemies)
+            {
+                if (enemy.EnemySprite.IsCircleColliding(
+                    playerManager.playerSprite.Center,
+                    playerManager.playerSprite.CollisionRadius))
+                {
+                    enemy.Destroyed = true;
+                    explosionManager.AddExplosion(
+                        enemy.EnemySprite.Center,
+                        enemy.EnemySprite.Velocity / 10);
+
+                    playerManager.Destroyed = true;
+
+                    explosionManager.AddExplosion(
+                        playerManager.playerSprite.Center,
+                        Vector2.Zero);
+                }
+            }
+        }
+
+        private void checkAsteroidToPlayerCollisions()
+        {
+            foreach (Sprite asteroid in asteroidManager.Asteroids)
+            {
+                if (asteroid.IsCircleColliding(
+                    playerManager.playerSprite.Center,
+                    playerManager.playerSprite.CollisionRadius))
+                {
+                    explosionManager.AddExplosion(
+                        asteroid.Center,
+                        asteroid.Velocity / 10);
+
+                    asteroid.Location = offScreen;
+
+                    playerManager.Destroyed = true;
+                    explosionManager.AddExplosion(
+                        playerManager.playerSprite.Center,
+                        Vector2.Zero);
+                }
+            }
+        }
+
+        public void CheckCollisions()
+        {
+            checkShotToEnemyCollisions();
+            checkShotToAsteroidCollisions();
+            if (!playerManager.Destroyed)
+            {
+                checkShotToPlayerCollisions();
+                checkEnemyToPlayerCollisions();
+                checkAsteroidToPlayerCollisions();
+            }
+        }
+
+    }
+}
---
+++ Samples/MacOS/BookSourceCode/XNAGameDevelopmentbyExampleCode/AsteroidBeltAssault/Enemy.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Asteroid_Belt_Assault
+{
+    class Enemy
+    {
+        public Sprite EnemySprite;
+        public Vector2 gunOffset = new Vector2(25, 25);
+        private Queue<Vector2> waypoints = new Queue<Vector2>();
+        private Vector2 currentWaypoint = Vector2.Zero;
+        private float speed = 120f;
+        public bool Destroyed = false;
+        private int enemyRadius = 15;
+        private Vector2 previousLocation = Vector2.Zero;
+
+        public Enemy(
+            Texture2D texture,
+            Vector2 location,
+            Rectangle initialFrame,
+            int frameCount)
+        {
+            EnemySprite = new Sprite(
+                location,
+                texture,
+                initialFrame,
+                Vector2.Zero);
+
+            for (int x = 1; x < frameCount; x++)
+            {
+                EnemySprite.AddFrame(
+                    new Rectangle(
+                    initialFrame.X = (initialFrame.Width * x),
+                    initialFrame.Y,
+                    initialFrame.Width,
+                    initialFrame.Height));
+            }
+            previousLocation = location;
+            currentWaypoint = location;
+            EnemySprite.CollisionRadius = enemyRadius;
+        }
+
+        public void AddWaypoint(Vector2 waypoint)
+        {
+            waypoints.Enqueue(waypoint);
+        }
+
+        public bool WaypointReached()
+        {
+            if (Vector2.Distance(EnemySprite.Location, currentWaypoint) <
+                (float)EnemySprite.Source.Width / 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsActive()
+        {
+            if (Destroyed)
+            {
+                return false;
+            }
+
+            if (waypoints.Count > 0)
+            {
+                return true;
+            }
+
+            if (WaypointReached())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (IsActive())
+            {
+                Vector2 heading = currentWaypoint - EnemySprite.Location;
+                if (heading != Vector2.Zero)
+                {
+                    heading.Normalize();
+                }
+                heading *= speed;
+                EnemySprite.Velocity = heading;
+                previousLocation = EnemySprite.Location;
+                EnemySprite.Update(gameTime);
+                EnemySprite.Rotation =
+                    (float)Math.Atan2(
+                    EnemySprite.Location.Y - previousLocation.Y,
+                    EnemySprite.Location.X - previousLocation.X);
+
+                if (WaypointReached())
+                {
+                    if (waypoints.Count > 0)
+                    {
+                        currentWaypoint = waypoints.Dequeue();
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (IsActive())
+            {
+                EnemySprite.Draw(spriteBatch);
+            }
+        }
+
+
+    }
+}
