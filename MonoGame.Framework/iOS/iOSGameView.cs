ecd3993a70fcd0eda4e061511d54e37a475f6aab
Dominique Louis <Dominique@SavageSoftwareSolutions.com>
2011-07-31 00:49:35 +0100

    Added iGameWindow methods

--- MonoGame.Framework/iOS/iOSGameView.cs
+++ MonoGame.Framework/iOS/iOSGameView.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 using MonoTouch.UIKit;
 
+using OpenTK;
+using OpenTK.Platform;
+
 namespace Microsoft.Xna.Framework
 {
-    public class iOSGameView : UIViewController
+    public class iOSGameView : UIViewController//, IGameWindow
     {
 		public iOSGameView()
 		{
@@ -75,5 +79,205 @@ namespace Microsoft.Xna.Framework
 			return true;
 			
 		}
+
+		#region IGameWindow implementation
+		public event EventHandler<EventArgs> Load;
+
+		public event EventHandler<EventArgs> Unload;
+
+		public event EventHandler<FrameEventArgs> UpdateFrame;
+
+		public event EventHandler<FrameEventArgs> RenderFrame;
+
+		public void Run ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Run (double updateRate)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void MakeCurrent ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void SwapBuffers ()
+		{
+			throw new NotImplementedException ();
+		}
+		#endregion
+
+		#region INativeWindow implementation
+		public event EventHandler<EventArgs> Move;
+
+		public event EventHandler<EventArgs> Resize;
+
+		//public event EventHandler<CancelEventArgs> Closing;
+
+		public event EventHandler<EventArgs> Closed;
+
+		public event EventHandler<EventArgs> Disposed;
+
+		public event EventHandler<EventArgs> TitleChanged;
+
+		public event EventHandler<EventArgs> VisibleChanged;
+
+		public event EventHandler<EventArgs> FocusedChanged;
+
+		public event EventHandler<EventArgs> WindowBorderChanged;
+
+		public event EventHandler<EventArgs> WindowStateChanged;
+
+		public event EventHandler<KeyPressEventArgs> KeyPress;
+
+		public void Close ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void ProcessEvents ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public System.Drawing.Point PointToClient (System.Drawing.Point point)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public System.Drawing.Point PointToScreen (System.Drawing.Point point)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool Focused {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public bool Visible {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public bool Exists {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public IWindowInfo WindowInfo {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public OpenTK.WindowState WindowState {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public OpenTK.WindowBorder WindowBorder {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public System.Drawing.Rectangle Bounds {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public System.Drawing.Point Location {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public System.Drawing.Size Size {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int X {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int Y {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int Width {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int Height {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public System.Drawing.Rectangle ClientRectangle {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public System.Drawing.Size ClientSize {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+		#endregion
     }
 }
