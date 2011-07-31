af471c6e67544df6507681f6adb75818f13f2442
Kenneth Pouncey <kjpou@pt.lu>
2011-07-31 09:30:18 +0200

    Changed DepthFormat to be 4.0 compliant.  Implemented Texture2D SetData methods - Still missing one method implementation because needing example

--- MonoGame.Framework/Audio/NoAudioHardwareException.cs
+++ MonoGame.Framework/Audio/NoAudioHardwareException.cs
@@ -1,7 +1,7 @@
-#region License
+// #region License
 // /*
 // Microsoft Public License (Ms-PL)
-// MonoGame - Copyright © 2009-2010 The MonoGame Team
+// MonoGame - Copyright © 2009 The MonoGame Team
 // 
 // All rights reserved.
 // 
@@ -36,23 +36,16 @@
 // permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular
 // purpose and non-infringement.
 // */
-#endregion License
-
-#region Using clause
+// #endregion License
+// 
 using System;
+using System.Runtime.InteropServices;
 
-#endregion Using clause
-
-namespace Microsoft.Xna.Framework.Net
+namespace Microsoft.Xna.Framework.Audio
 {
-	public enum NetworkSessionType
+	[SerializableAttribute]
+	public sealed class NoAudioHardwareException : ExternalException
 	{
-		Local,    		// Does not involve any networking traffic, but can be used for split-screen gaming on a single Xbox 360 console. Creating a local network session may also make it easier to share code between local and online game modes. 
-
-		SystemLink,	// Connect multiple Xbox 360 consoles or computers over a local subnet. These machines do not require a connection to Xbox LIVE or any LIVE accounts. However, connection to machines on different subnets is not allowed.
-				// If you are a Creators Club developer testing your game, you can use this type to connect an Xbox 360 console to a computer. However, cross-platform networking is not supported in games distributed to non-–Creators Club community players.
-
-		PlayerMatch,	// Uses the Xbox LIVE servers. This enables connection to other machines over the Internet. It requires a LIVE Silver Membership for Windows-based games or a LIVE Gold membership for Xbox 360 games. Games in development will also require an XNA Creators Club premium membership. While in trial mode, Indie games downloaded from Xbox LIVE Markeplace will not have access to LIVE matchmaking.
-		Ranked,		// All session matches are ranked. This option is available only for commercial games that have passed Xbox LIVE certification. Due to the competitive nature of the gameplay, this session type does not support join-in-progress.
 	}
 }
+
