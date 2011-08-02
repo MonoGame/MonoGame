eec56975c4809b7d97ee8cff6b5f81da2a02ac60
Andre Esteve <andreesteve@gmail.com>
2011-08-02 18:13:28 -0300

    Audio partialy implemented (unstable)

---
+++ Samples/Linux/Sound/Game1.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+
+#if ANDROID
+using Android.App;
+#endif
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Microsoft.Xna.Samples.Sound
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;	
+		KeyboardState oldSate;
+		SoundEffect sound;
+		
+#if ANDROID 
+		public Game1 (Activity activity) : base (activity)
+#else 
+        public Game1 ()  
+#endif
+		{
+			graphics = new GraphicsDeviceManager (this);
+			
+			Content.RootDirectory = "Content";
+			
+			graphics.PreferMultiSampling = true;
+			graphics.IsFullScreen = false;	
+
+			Window.AllowUserResizing = false;
+
+			graphics.SupportedOrientations = DisplayOrientation.Portrait | DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight | DisplayOrientation.PortraitUpsideDown;
+		}
+		
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			// TODO: Add your initialization logic here
+			
+			base.Initialize ();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+
+			sound = Content.Load<SoundEffect>("Explosion1");
+			
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			KeyboardState ks = Keyboard.GetState();
+			
+			
+			if (ks[Keys.Escape] == KeyState.Down)
+				base.Exit();
+			
+			if (ks.IsKeyDown(Keys.A) && oldSate.IsKeyUp(Keys.A))
+				sound.Play();
+			
+			oldSate = ks;
+			    
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			base.Draw(gameTime);
+		}
+	}
+}
