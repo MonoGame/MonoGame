6adb260fbdca8b28d7d2531644495a8ef473f5f0
dellis1972 <dellis1972@googlemail.com>
2011-08-04 00:31:59 +0100

    Changes to get NetRumble working under windows. Sound does not work.

--- MonoGame.Framework/Windows/Graphics/Effect.cs
+++ MonoGame.Framework/Windows/Graphics/Effect.cs
@@ -38,7 +38,6 @@
 // */
 // #endregion License
 // 
-
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -48,48 +47,55 @@ using OpenTK.Graphics.OpenGL;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-	public class Effect : IDisposable
-	{
+    public class Effect : GraphicsResource
+    {
         public EffectParameterCollection Parameters { get; set; }
+
         public EffectTechniqueCollection Techniques { get; set; }
-		private GraphicsDevice graphicsDevice;
-		private int fragment_handle;
+
+        internal List<EffectParameter> _textureMappings = new List<EffectParameter>();
+
+        private int fragment_handle;
         private int vertex_handle;
         private bool fragment;
         private bool vertex;
+        private string filename;
 
-		public Effect (
-         GraphicsDevice graphicsDevice,
-         byte[] effectCode,
-         CompilerOptions options,
-         EffectPool pool)
-		{
-			
-			if (graphicsDevice == null)
+        internal List<int> vertexShaders = new List<int>();
+        internal List<int> fragmentShaders = new List<int>();
+
+        public Effect(
+            GraphicsDevice graphicsDevice,
+            byte[] effectCode,
+            CompilerOptions options,
+            EffectPool pool)
+        {
+
+            if (graphicsDevice == null)
             {
                 throw new ArgumentNullException("Graphics Device Cannot Be Null");
             }
-			this.graphicsDevice = graphicsDevice;
-			
-			if (pool == null)
-            { 
-				return;
+            this.graphicsDevice = graphicsDevice;
+
+            if (pool == null)
+            {
+                return;
                 // TODO throw new ArgumentNullException("Effect Pool Cannot Be Null");
             }
-			
-			int fragmentblocklength = BitConverter.ToInt32(effectCode, 0);
+
+            int fragmentblocklength = BitConverter.ToInt32(effectCode, 0);
 
             int vertexblocklength = BitConverter.ToInt32(effectCode, fragmentblocklength + 4);
 
             if (fragmentblocklength != 0)
             {
-                fragment_handle = GL.CreateShader( ShaderType.FragmentShader );
+                fragment_handle = GL.CreateShader(ShaderType.FragmentShader);
                 fragment = true;
             }
 
             if (vertexblocklength != 0)
             {
-                vertex_handle = GL.CreateShader( ShaderType.VertexShader );
+                vertex_handle = GL.CreateShader(ShaderType.VertexShader);
                 vertex = true;
             }
 
@@ -97,7 +103,7 @@ namespace Microsoft.Xna.Framework.Graphics
             {
                 string[] fragmentstring = new string[1] { Encoding.UTF8.GetString(effectCode, 4, fragmentblocklength) };
                 //int[] fragmentLength = new int[1] { fragmentstring[0].Length };
-				int fragmentLength = fragmentstring[0].Length;
+                int fragmentLength = fragmentstring[0].Length;
                 GL.ShaderSource(fragment_handle, 1, fragmentstring, ref fragmentLength);
             }
 
@@ -105,40 +111,37 @@ namespace Microsoft.Xna.Framework.Graphics
             {
                 string[] vertexstring = new string[1] { Encoding.UTF8.GetString(effectCode, fragmentblocklength + 8, vertexblocklength) };
                 // int[] vertexLength = new int[1] { vertexstring[0].Length };
-				int vertexLength = vertexstring[0].Length;
+                int vertexLength = vertexstring[0].Length;
                 GL.ShaderSource(vertex_handle, 1, vertexstring, ref vertexLength);
             }
-			
-			int compiled = 0;
+
+            int compiled = 0;
 
             if (fragment)
             {
                 GL.CompileShader(fragment_handle);
-				
-				GL.GetShader(fragment_handle, ShaderParameter.CompileStatus, out compiled );
-				if (compiled == (int)All.False)
-				{
-					Console.Write("Fragment Compilation Failed!");
-				}
+
+                GL.GetShader(fragment_handle, ShaderParameter.CompileStatus, out compiled);
+                if (compiled == (int)All.False)
+                {
+                    Console.Write("Fragment Compilation Failed!");
+                }
             }
 
             if (vertex)
             {
                 GL.CompileShader(vertex_handle);
-				GL.GetShader(vertex_handle, ShaderParameter.CompileStatus, out compiled );
-				if (compiled == (int)All.False)
-				{
-					Console.Write("Vertex Compilation Failed!");
-				}
+                GL.GetShader(vertex_handle, ShaderParameter.CompileStatus, out compiled);
+                if (compiled == (int)All.False)
+                {
+                    Console.Write("Vertex Compilation Failed!");
+                }
             }
 
-		}
+        }
 
         protected Effect(GraphicsDevice graphicsDevice, Effect cloneSource)
         {
-            Parameters = new EffectParameterCollection();
-            Techniques = new EffectTechniqueCollection();
-
             if (graphicsDevice == null)
             {
                 throw new ArgumentNullException("Graphics Device Cannot Be Null");
@@ -146,65 +149,237 @@ namespace Microsoft.Xna.Framework.Graphics
             this.graphicsDevice = graphicsDevice;
         }
 
+        protected Effect(GraphicsDevice graphicsDevice)
+        {
+            if (graphicsDevice == null)
+            {
+                throw new ArgumentNullException("Graphics Device Cannot Be Null");
+            }
+            this.graphicsDevice = graphicsDevice;
+            Techniques = new EffectTechniqueCollection();
+            Parameters = new EffectParameterCollection();
+        }
+
+        internal Effect(GraphicsDevice aGraphicsDevice, string aFileName)
+            : this(aGraphicsDevice)
+        {
+            StreamReader streamReader = new StreamReader(aFileName);
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            if (aFileName.ToLower().Contains("fsh"))
+            {
+                CreateFragmentShaderFromSource(text);
+            }
+            else
+            {
+                CreateVertexShaderFromSource(text);
+            }
+
+            DefineTechnique("Technique1", "Pass1", 0, 0);
+            CurrentTechnique = Techniques["Technique1"];
+        }
+
+        protected Effect(Effect cloneSource)
+        {
+
+        }
+
+        public Effect(
+            GraphicsDevice graphicsDevice,
+            byte[] effectCode)
+        {
+
+            if (graphicsDevice == null)
+            {
+                throw new ArgumentNullException("Graphics Device Cannot Be Null");
+            }
+            this.graphicsDevice = graphicsDevice;
+
+            int fragmentblocklength = BitConverter.ToInt32(effectCode, 0);
+
+            int vertexblocklength = BitConverter.ToInt32(effectCode, fragmentblocklength + 4);
+
+            if (fragmentblocklength != 0)
+            {
+                fragment_handle = GL.CreateShader(ShaderType.FragmentShader);
+                fragment = true;
+            }
+
+            if (vertexblocklength != 0)
+            {
+                vertex_handle = GL.CreateShader(ShaderType.VertexShader);
+                vertex = true;
+            }
+
+            if (fragment)
+            {
+                string[] fragmentstring = new string[1] { Encoding.UTF8.GetString(effectCode, 4, fragmentblocklength) };
+                //int[] fragmentLength = new int[1] { fragmentstring[0].Length };
+                int fragmentLength = fragmentstring[0].Length;
+                GL.ShaderSource(fragment_handle, 1, fragmentstring, ref fragmentLength);
+            }
+
+            if (vertex)
+            {
+                string[] vertexstring = new string[1] { Encoding.UTF8.GetString(effectCode, fragmentblocklength + 8, vertexblocklength) };
+                // int[] vertexLength = new int[1] { vertexstring[0].Length };
+                int vertexLength = vertexstring[0].Length;
+                GL.ShaderSource(vertex_handle, 1, vertexstring, ref vertexLength);
+            }
+
+            int compiled = 0;
+
+            if (fragment)
+            {
+                GL.CompileShader(fragment_handle);
+
+                GL.GetShader(fragment_handle, ShaderParameter.CompileStatus, out compiled);
+                if (compiled == (int)All.False)
+                {
+                    Console.Write("Fragment Compilation Failed!");
+                }
+            }
+
+            if (vertex)
+            {
+                GL.CompileShader(vertex_handle);
+                GL.GetShader(vertex_handle, ShaderParameter.CompileStatus, out compiled);
+                if (compiled == (int)All.False)
+                {
+                    Console.Write("Vertex Compilation Failed!");
+                }
+            }
+
+        }
+
+        public void Begin()
+        {
+        }
+
+        public void Begin(SaveStateMode saveStateMode)
+        {
+
+        }
+
+        public virtual Effect Clone(GraphicsDevice device)
+        {
+            Effect f = new Effect(graphicsDevice, this);
+            return f;
+        }
+
+        public virtual Effect Clone()
+        {
+            return Clone(graphicsDevice);
+        }
+
+        public void End()
+        {
+        }
+
+        internal static string Normalize(string FileName)
+        {
+            if (File.Exists(FileName))
+                return FileName;
+
+            // Check the file extension
+            if (!string.IsNullOrEmpty(Path.GetExtension(FileName)))
+            {
+                return null;
+            }
+
+            // Concat the file name with valid extensions
+            if (File.Exists(FileName + ".fsh"))
+                return FileName + ".fsh";
+            if (File.Exists(FileName + ".vsh"))
+                return FileName + ".vsh";
+
+            return null;
+        }
+
+        public EffectTechnique CurrentTechnique
+        {
+            get;
+            set;
+        }
+
         internal virtual void Apply()
         {
-            GLStateManager.Cull(graphicsDevice.RasterizerState.CullMode.OpenGL11());
-            // TODO: This is prolly not right (DepthBuffer, etc)
-            GLStateManager.DepthTest(graphicsDevice.DepthStencilState.DepthBufferEnable);
+            //if (graphicsDevice.Textures.)
+            Techniques[0].Passes[0].Apply();
         }
-		
-		public void Begin()
-		{
-		}
-		
-		public void Begin(SaveStateMode saveStateMode)
-		{
-			
-		}
-		
-		public virtual Effect Clone(GraphicsDevice device)
-		{
-			Effect f = new Effect( graphicsDevice, this );
-			return f;
-		}
-		
-		public void Dispose()
-		{
-		}
-		
-		public void End()
-		{
-		}
-		
-		internal static string Normalize(string FileName)
-		{
-			if (File.Exists(FileName))
-				return FileName;
-			
-			// Check the file extension
-			if (!string.IsNullOrEmpty(Path.GetExtension(FileName)))
-			{
-				return null;
-			}
-			
-			// Concat the file name with valid extensions
-			if (File.Exists(FileName+".fsh"))
-				return FileName+".fsh";
-			if (File.Exists(FileName+".vsh"))
-				return FileName+".vsh";
-			
-			return null;
-		}
 
-        public EffectTechnique CurrentTechnique { get; set; }
-
-        internal Effect(GraphicsDevice device)
+        protected virtual void OnApply()
         {
-            graphicsDevice = device;
-            Parameters = new EffectParameterCollection();
-            Techniques = new EffectTechniqueCollection();
-            CurrentTechnique = new EffectTechnique(this);
+
         }
 
-	}
+        protected void CreateVertexShaderFromSource(string source)
+        {
+            int shader = GL.CreateShader(ShaderType.VertexShader);
+            // Attach the loaded source string to the shader object
+            GL.ShaderSource(shader, source);
+            // Compile the shader
+            GL.CompileShader(shader);
+
+            vertexShaders.Add(shader);
+        }
+
+
+        protected void CreateFragmentShaderFromSource(string source)
+        {
+            int shader = GL.CreateShader(ShaderType.FragmentShader);
+            // Attach the loaded source string to the shader object
+            GL.ShaderSource(shader, source);
+            // Compile the shader
+            GL.CompileShader(shader);
+
+            fragmentShaders.Add(shader);
+        }
+
+        protected void DefineTechnique(string techniqueName, string passName, int vertexIndex, int fragmentIndex)
+        {
+            EffectTechnique tech = new EffectTechnique(this);
+            tech.Name = techniqueName;
+            EffectPass pass = new EffectPass(tech);
+            pass.Name = passName;
+            pass.VertexIndex = vertexIndex;
+            pass.FragmentIndex = fragmentIndex;
+            pass.ApplyPass();
+            tech.Passes._passes.Add(pass);
+            Techniques._techniques.Add(tech);
+            LogShaderParameters(String.Format("Technique {0} - Pass {1} :", tech.Name, pass.Name), pass.shaderProgram);
+
+        }
+
+        // Output the log of an object
+        private void LogShaderParameters(string whichObj, int obj)
+        {
+            int actUnis = 0;
+            Parameters._parameters.Clear();
+
+            GL.GetProgram(obj, ProgramParameter.ActiveUniforms, out actUnis);
+
+            Console.WriteLine("{0} {1}", whichObj, actUnis);
+
+            int size;
+            ActiveUniformType type;
+            StringBuilder name = new StringBuilder(100);
+            int length;
+
+            for (int x = 0; x < actUnis; x++)
+            {
+                name = new StringBuilder(100);
+                GL.GetActiveUniform(obj, x, 100, out length, out size, out type, name);
+                Console.WriteLine("{0}: {1} {2} {3}", x, name, type, length);
+                EffectParameter efp = new EffectParameter(this, name.ToString(), x, type.ToString(), length);
+                Parameters._parameters.Add(efp.Name, efp);
+                if (efp.ParameterType == EffectParameterType.Texture2D)
+                {
+                    _textureMappings.Add(efp);
+                }
+            }
+
+        }
+    }
 }
