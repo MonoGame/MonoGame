aa57e593a6aeab459d9e25b6bdc5f95c930bedae
Dominique Louis <Dominique@SavageSoftwareSolutions.com>
2011-08-12 21:22:14 +0100

    Changes to allow Marblets to work a bit more nicely.

--- StarterKits/iOS/Marblets/GameScreen.cs
+++ StarterKits/iOS/Marblets/GameScreen.cs
@@ -17,81 +17,81 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Marblets
 {
-    /// <summary>
-    /// GameScreen is the screen that is loaded to play Marblets
-    /// </summary>
-    class GameScreen : Screen
-    {
-        private GameBoard board;
-        private Texture2D gameOver;
+	/// <summary>
+	/// GameScreen is the screen that is loaded to play Marblets
+	/// </summary>
+	class GameScreen : Screen
+	{
+		private GameBoard board;
+		private Texture2D gameOver;
 
-        public GameScreen(Game game, string backgroundImage/*TODO , SoundEntry backgroundMusic*/)
-            : base(game, backgroundImage/* TODO , backgroundMusic*/)
-        {
+		public GameScreen(Game game, string backgroundImage, SoundEntry backgroundMusic)
+			: base(game, backgroundImage, backgroundMusic)
+		{
 
-            //Remove the define at the top of the file to play the tutorial version
-            bool tutorialVersion = false;
+			//Remove the define at the top of the file to play the tutorial version
+			bool tutorialVersion = false;
 #if TutorialVersion
             tutorialVersion = true;
 #endif
-            if (tutorialVersion)
-            {
-                board = new TutorialGameBoard(this.Game);
-            }
-            else
-            {
-                board = new GameBoard(this.Game);
-            }
-        }
+			if(tutorialVersion)
+			{
+				board = new TutorialGameBoard(this.Game);
+			}
+			else
+			{
+				board = new GameBoard(this.Game);
+			}
+		}
 
-        public override void Initialize()
-        {
-            base.Initialize();
-            board.Initialize();
-        }
+		public override void Initialize()
+		{
+			base.Initialize();
+			board.Initialize();
+		}
 
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            board.Update(gameTime);
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			board.Update(gameTime);
 
-            if (board.GameOver && InputHelper.GamePads[PlayerIndex.One].APressed)
-            {
-                MarbletsGame.NextGameState = GameState.Started;
-            }
-        }
+			if(board.GameOver && (InputHelper.GamePads[PlayerIndex.One].APressed || Clicked))
+			{
+				MarbletsGame.NextGameState = GameState.Started;
+			}
+		}
 
-        protected override void LoadContent()
-        {
-            gameOver = MarbletsGame.Content.Load<Texture2D>("Textures/game_over_frame");
-            base.LoadContent();
-        }
+		protected override void LoadContent()
+		{
+			gameOver = MarbletsGame.Content.Load<Texture2D>("Textures/game_over_frame");
+			base.LoadContent();
+		}
 
 
-        public override void Draw(GameTime gameTime)
-        {
-            base.Draw(gameTime);
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
 
-            SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+			SpriteBatch.Begin();
 
-            board.Draw(SpriteBatch);
+			board.Draw(SpriteBatch);
 
-            //Draw Score
-            Font.Draw(SpriteBatch, FontStyle.Small, 946, 140,
-                      String.Format("{0:###,##0}", MarbletsGame.Score));
+			//Draw Score
+			Font.Draw(SpriteBatch, FontStyle.Small, 20, 440,
+					  String.Format("{0:###,##0}", MarbletsGame.Score));
 
-            if (board.GameOver)
-            {
-                SpriteBatch.Draw(gameOver, new Vector2(329, 264), Color.White);
-            }
+			if(board.GameOver)
+			{
+				SpriteBatch.Draw(gameOver, new Vector2(0, 60), Color.White);
+			}
 
-            SpriteBatch.End();
+			SpriteBatch.End();
 
-        }
+		}
 
-        public void NewGame()
-        {
-            board.NewGame();
-        }
-    }
+		public void NewGame()
+		{
+			board.NewGame();
+		}
+	}
 }
--- StarterKits/iOS/Marblets/Settings.cs
+++ StarterKits/iOS/Marblets/Settings.cs
@@ -17,73 +17,45 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Marblets
 {
-    /// <summary>
-    /// The Setting class handles loading and saving of global application settings.
-    /// The normal .Net classes (System.Configuration) for doing this are not available
-    /// on the Xbox 360.
-    /// </summary>
-    public class Settings
-    {
-        #region General App Settings
+	/// <summary>
+	/// The Setting class handles loading and saving of global application settings.
+	/// The normal .Net classes (System.Configuration) for doing this are not available
+	/// on the Xbox 360.
+	/// </summary>
+	public class Settings
+	{
+		#region General App Settings
 
-        /// <summary>
-        /// The path to look for all media in
-        /// </summary>
-        public string MediaPath = @"Content\";
+		/// <summary>
+		/// The path to look for all media in
+		/// </summary>
+		public string MediaPath = @"Content\";
 
-        /// <summary>
-        /// The name of the window when running in windowed mode
-        /// </summary>
-        public string WindowTitle = "Marblets";
+		/// <summary>
+		/// The name of the window when running in windowed mode
+		/// </summary>
+		public string WindowTitle = "Marblets";
 
-        #endregion
+		#endregion
 
-        #region MarbleColors
+		#region MarbleColors
 
-        /// <summary>
-        /// Default marble colors to use
-        /// </summary>
-        public Color[] MarbleColors = new Color[] { new Color(255, 0, 0), 
+		/// <summary>
+		/// Default marble colors to use
+		/// </summary>
+		public Color[] MarbleColors = new Color[] { new Color(255, 0, 0), 
                                                     new Color(40, 175, 255),
                                                     new Color(40, 255, 20),
                                                     new Color(255, 255, 0),
                                                     new Color(255, 20, 230) };
 
-        //monochrome
-        //public Color[] MarbleColors = new Color[] { new Color(255, 255, 255), 
-        //                                            new Color(205, 205, 205), 
-        //                                            new Color(155, 155, 155), 
-        //                                            new Color(105, 105, 105), 
-        //                                            new Color(55, 55, 55) };
+		//monochrome
+		//public Color[] MarbleColors = new Color[] { new Color(255, 255, 255), 
+		//                                            new Color(205, 205, 205), 
+		//                                            new Color(155, 155, 155), 
+		//                                            new Color(105, 105, 105), 
+		//                                            new Color(55, 55, 55) };
 
-        #endregion
-
-        #region Load/Save code
-
-        /// <summary>
-        /// Saves the current settings
-        /// </summary>
-        /// <param name="filename">The filename to save to</param>
-        public void Save(string filename)
-        {
-            Stream stream = File.Create(filename);
-
-            XmlSerializer serializer = new XmlSerializer(typeof(Settings));
-            serializer.Serialize(stream, this);
-            stream.Close();
-        }
-
-        /// <summary>
-        /// Loads settings from a file
-        /// </summary>
-        /// <param name="filename">The filename to load</param>
-        public static Settings Load(string filename)
-        {
-            Stream stream = File.OpenRead(filename);
-            XmlSerializer serializer = new XmlSerializer(typeof(Settings));
-            return (Settings)serializer.Deserialize(stream);
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
