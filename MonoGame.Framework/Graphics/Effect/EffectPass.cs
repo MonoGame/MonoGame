71020a6857d3a2cdd0843e1e0a0c32d6ac53248a
CartBlanche <Dominique@SavageSoftwareSolutions.com>
2011-11-13 13:58:48 +0000

    Changes to support DrawUserPrimitives.

--- MonoGame.Framework/Graphics/Effect/EffectPass.cs
+++ MonoGame.Framework/Graphics/Effect/EffectPass.cs
@@ -14,9 +14,13 @@ namespace Microsoft.Xna.Framework.Graphics
     public class EffectPass
     {
         EffectTechnique _technique = null;
+		internal int shaderProgram;
 
         public void Apply()
         {
+			// Tell the GL Context to use the program
+			GL20.UseProgram (shaderProgram);
+			
             _technique._effect.Apply();
         }
 
@@ -24,6 +28,54 @@ namespace Microsoft.Xna.Framework.Graphics
         {
             _technique = technique;
         }
-		public string Name { get; set; }		
+		
+		internal void ApplyPass ()
+		{
+			
+			// Create a Program object
+			shaderProgram = GL20.CreateProgram ();
+
+			// Attach our compiled shaders
+			if ( VertexIndex < _technique._effect.vertexShaders.Count)
+				GL20.AttachShader (shaderProgram, _technique._effect.vertexShaders[VertexIndex]);
+			if ( FragmentIndex < _technique._effect.fragmentShaders.Count)			
+				GL20.AttachShader (shaderProgram, _technique._effect.fragmentShaders[FragmentIndex]);
+
+			// Set the parameters
+			// TODO GL20.ProgramParameter (shaderProgram, AssemblyProgramParameterArb.GeometryInputType, (int)All.Lines);	
+			// TODO GL20.ProgramParameter (shaderProgram, AssemblyProgramParameterArb.GeometryOutputType, (int)All.Line);
+			
+			// Set the max vertices
+			int maxVertices;
+			// TODO GL20.GetInteger (GetPName.MaxGeometryOutputVertices, out maxVertices);
+			// TODO GL20.ProgramParameter (shaderProgram, AssemblyProgramParameterArb.GeometryVerticesOut, maxVertices);
+
+			// Link the program
+			GL20.LinkProgram (shaderProgram);
+			string name = String.Format("Technique {0} - Pass {1}: ",_technique.Name, Name);
+			ShaderLog(name,shaderProgram);				
+			
+		}
+		
+		public string Name { get; set; }	
+		
+		// internal for now until I figure out what I can do with this mess
+		internal int VertexIndex { get; set; }
+		internal int FragmentIndex { get; set; }
+		
+		// Output the log of an object
+		private void ShaderLog (string whichObj, int obj)
+		{
+			int infoLogLen = 0;
+			var infoLog = "Is good to go.";
+
+			// TODO GL.GetProgram (obj, ProgramParameter.InfoLogLength, out infoLogLen);
+
+			/* TODO if (infoLogLen > 0)
+				infoLog = GL.GetProgramInfoLog (obj); */
+
+			Console.WriteLine ("{0} {1}", whichObj, infoLog);
+
+		}	
     }
 }
