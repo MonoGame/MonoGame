04dcb0d081097e01d87fce436f23077541d71cc0
espes <nielkie2@gmail.com>
2011-12-05 23:21:09 +1000

    MS's SpriteEffect

---
+++ MonoGame.Framework/Graphics/Effect/SpriteEffect.cs
@@ -0,0 +1,85 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// SpriteEffect.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    /// <summary>
+    /// The default effect used by SpriteBatch.
+    /// </summary>
+    public class SpriteEffect : Effect
+    {
+        #region Effect Parameters
+
+        EffectParameter matrixParam;
+
+        #endregion
+
+        #region Methods
+
+
+        /// <summary>
+        /// Creates a new SpriteEffect.
+        /// </summary>
+        public SpriteEffect(GraphicsDevice device)
+            : base(device, new byte[] {} /*Resources.SpriteEffect*/)
+        {
+            CacheEffectParameters();
+        }
+
+
+        /// <summary>
+        /// Creates a new SpriteEffect by cloning parameter settings from an existing instance.
+        /// </summary>
+        protected SpriteEffect(SpriteEffect cloneSource)
+            : base(cloneSource)
+        {
+            CacheEffectParameters();
+        }
+
+
+        /// <summary>
+        /// Creates a clone of the current SpriteEffect instance.
+        /// </summary>
+        public override Effect Clone()
+        {
+            return new SpriteEffect(this);
+        }
+
+
+        /// <summary>
+        /// Looks up shortcut references to our effect parameters.
+        /// </summary>
+        void CacheEffectParameters()
+        {
+            matrixParam = Parameters["MatrixTransform"];
+        }
+
+
+        /// <summary>
+        /// Lazily computes derived parameter values immediately before applying the effect.
+        /// </summary>
+        protected internal override void OnApply()
+        {
+            Viewport viewport = GraphicsDevice.Viewport;
+
+            Matrix projection = Matrix.CreateOrthographicOffCenter(0, viewport.Width, viewport.Height, 0, 0, 1);
+            Matrix halfPixelOffset = Matrix.CreateTranslation(-0.5f, -0.5f, 0);
+
+            matrixParam.SetValue(halfPixelOffset * projection);
+        }
+
+
+        #endregion
+    }
+}
