8c048800954e9b97e62ca0c5279398cabaccb416
Kenneth Pouncey <kjpou@pt.lu>
2012-01-25 07:30:26 +0100

    Mod to make sure Game.cs compiles

--- MonoGame.Framework/Game.cs
+++ MonoGame.Framework/Game.cs
@@ -77,7 +77,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
-using MonoGame.Framework;
+using Microsoft.Xna.Framework;
 
 namespace Microsoft.Xna.Framework
 {
