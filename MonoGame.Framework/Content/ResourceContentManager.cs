3d36ac1db7172445f3b3890f1692fd6459c86cdd
Thiago Pastor <thiagodiaspastor@gmail.com>
2012-02-08 23:01:45 -0200

    Some Changes to 3D

---
+++ MonoGame.Framework/Content/ResourceContentManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Resources;
+
+namespace Microsoft.Xna.Framework.Content
+{
+    public class ResourceContentManager : ContentManager
+    {
+        private ResourceManager resource;
+
+        public ResourceContentManager(IServiceProvider servicesProvider, ResourceManager resource)
+            : base(servicesProvider)
+        {
+            if (resource == null)
+            {
+                throw new ArgumentNullException("resource");
+            }
+            this.resource = resource;
+        }
+
+        protected override System.IO.Stream OpenStream(string assetName)
+        {
+            object obj = this.resource.GetObject(assetName);
+            if (obj == null)
+            {
+                throw new ContentLoadException("Resource not found");
+            }
+            if (!(obj is byte[]))
+            {
+                throw new ContentLoadException("Resource is not in binary format");
+            }
+            return new MemoryStream(obj as byte[]);
+        }
+    }
+}
