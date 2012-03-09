f6d6665fee536a5c748d1876a0d1517ed260e810
Christopher Chambers <chris@peanutcode.com>
2012-03-08 20:18:24 -0600

    Adds stubs to get develop3d-only tests compiling
    Adds Model and several ancillary classes as stubs. Adds a few stub
    properties to BasicEffect and Graphics device.

---
+++ MonoGame.Framework/Graphics/ModelBone.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+	[Obsolete ("ModelBone is currently a stub")]
+	public sealed class ModelBone
+	{
+		// FIXME: ModelBoneCollection
+		[Obsolete ("Children is currently a stub")]
+		public ICollection<ModelBone> Children { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("Index is currently a stub")]
+		public int Index { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("Name is currently a stub")]
+		public string Name { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("Parent is currently a stub")]
+		public ModelBone Parent { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("Transform is currently a stub")]
+		public Matrix Transform 
+		{
+			get { throw new NotImplementedException (); }
+			set { throw new NotImplementedException (); }
+		}
+	}
+}
---
+++ MonoGame.Framework/Graphics/ModelMesh.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+	[Obsolete ("ModelEffect is currently a stub")]
+	public class ModelEffect : BasicEffect {
+		private ModelEffect (GraphicsDevice device)
+			: base (device)
+		{
+		}
+	}
+
+	[Obsolete ("ModelMeshPart is currently a stub")]
+	public class ModelMeshPart { }
+
+	[Obsolete ("ModelMesh is currently a stub")]
+	public sealed class ModelMesh {
+		private ModelMesh()
+		{
+			throw new NotImplementedException ();
+		}
+
+		[Obsolete ("BoundingSphere is currently a stub")]
+		public BoundingSphere BoundingSphere { get { throw new NotImplementedException (); } }
+
+		// FIXME: ModelEffectCollection
+		[Obsolete ("Effects is currently a stub")]
+		public ICollection<ModelEffect> Effects { get { throw new NotImplementedException (); } }
+
+		// FIXME: ModelMeshPartCollection
+		[Obsolete ("MeshParts is currently a stub")]
+		public ICollection<ModelMeshPart> MeshParts { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("Name is currently a stub")]
+		public string Name { get { throw new NotImplementedException (); } }
+
+		[Obsolete ("ParentBone is currently a stub")]
+		public ModelBone ParentBone { get { throw new NotImplementedException (); } }
+
+		public object Tag { get; set; }
+
+		[Obsolete ("Draw is currently a stub")]
+		public void Draw()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
