7a5bb4beb66a12a913c9949735a5df2c657037ec
Tom Spilman <tom@sickheadgames.com>
2012-04-27 18:52:59 -0500

    Fixes to the DXEffectObject for generating the newst revision of the MGFX format.

--- MonoGame.Framework/Graphics/Effect/DXEffectObject.writer.cs
+++ MonoGame.Framework/Graphics/Effect/DXEffectObject.writer.cs
@@ -16,111 +16,85 @@ namespace Microsoft.Xna.Framework.Graphics
             writer.Write(Header.ToCharArray());
             writer.Write((byte)Version);
 
-            // Write the shared objects... we will write the index to
-            // the object later in the file.
-            writer.Write((byte)Objects.Length);
-            for (var o = 0; o < Objects.Length; o++)
-            {
-                var param = Objects[o];
-                writer.Write(param != null);
-                if (param == null)
-                    continue;
-
-                WriteParameter(writer, param);
-
-                // Shove a index in here for later.
-                param.object_id = o;
-            }
+            // Write all the shared shaders.
+            writer.Write((byte)Shaders.Count);
+            foreach (var shader in Shaders)
+                shader.Write(writer);
 
             // Write the parameters.
-            writer.Write((byte)Parameters.Length);
-            foreach (var param in Parameters)
-                WriteParameter(writer, param);
+            WriteParameters(writer, Parameters, Parameters.Length);
 
             // Write the techniques.
             writer.Write((byte)Techniques.Length);
             foreach (var technique in Techniques)
-                WriteTechnique(writer, technique);
+            {
+                writer.Write(technique.name);
+                WriteAnnotations(writer, technique.annotation_handles);
+
+                // Write the passes.
+                writer.Write((byte)technique.pass_count);
+                for (var p = 0; p < technique.pass_count; p++)
+                {
+                    var pass = technique.pass_handles[p];
+
+                    writer.Write(pass.name);
+                    WriteAnnotations(writer, pass.annotation_handles);
+
+                    // Write the index for the vertex and pixel shaders.
+                    var vertexShader = GetShaderIndex(STATE_CLASS.VERTEXSHADER, pass.states);
+                    var pixelShader = GetShaderIndex(STATE_CLASS.PIXELSHADER, pass.states);
+                    writer.Write((byte)vertexShader);
+                    writer.Write((byte)pixelShader);
+
+                    // Write the state objects too!
+                }
+            }
+        }
+
+        private static void WriteParameters(BinaryWriter writer, d3dx_parameter[] parameters, int count)
+        {
+            writer.Write((byte)count);
+            for (var i = 0; i < count; i++)
+                WriteParameter(writer, parameters[i]);
         }
 
         private static void WriteParameter(BinaryWriter writer, d3dx_parameter param)
         {
-            writer.Write((byte)param.object_id);
-            if (param.object_id != -1)
-                return;
+            var class_ = ToParameterClass(param.class_);
+            var type = ToParameterType(param.type);
+            writer.Write((byte)class_);
+            writer.Write((byte)type);
 
-            writer.Write((byte)param.class_);
             writer.Write(param.name);
-            writer.Write((byte)param.type);
+            writer.Write(param.semantic);
+            WriteAnnotations(writer, param.annotation_handles);
+
             writer.Write((byte)param.rows);
             writer.Write((byte)param.columns);
-            writer.Write(param.semantic);
 
-            WriteAnnotations(writer, param.annotation_handles, param.annotation_count);
+            // Write the elements or struct members.
+            WriteParameters(writer, param.member_handles, (int)param.element_count);
+            WriteParameters(writer, param.member_handles, (int)param.member_count);
 
-            // Note if this is a struct or not.
-            var isStruct = param.member_count > 0;
-            writer.Write(isStruct);
-
-            // Write the correct number of elements/members.
-            var count = isStruct ? param.member_count : param.element_count;
-            writer.Write((byte)count);
-            for(var i=0; i < count; i++)
-                WriteParameter(writer, param.member_handles[i]);
-            
-            // Write the data payload.
-            if (param.data is byte[])
+            if (param.element_count == 0 && param.member_count == 0)
             {
-                writer.Write((byte)0);
-                writer.Write((short)param.bytes);
-                writer.Write((byte[])param.data);
-            }
-            else if (param.data is DXExpression)
-            {
-                writer.Write((byte)1);
-                var preshader = (DXExpression)param.data;
-                preshader.Write(writer);
-            }
-            else if (param.data is DXShader)
-            {
-                writer.Write((byte)2);
-                var shader = (DXShader)param.data;
-                shader.Write(writer);
-            }
-            else if (param.data is d3dx_sampler)
-            {
-                writer.Write((byte)3);
-                var sampler = (d3dx_sampler)param.data;
-                WriteStates(writer, sampler.states, sampler.state_count);
-            }
-            else
-            {
-                throw new Exception("Unknown data type!");
+                switch (type)
+                {
+                    case EffectParameterType.Bool:
+                    case EffectParameterType.Int32:
+                    case EffectParameterType.Single:
+                        writer.Write((byte[])param.data);
+                        break;
+                }
             }
         }
 
-        private static void WriteAnnotations(BinaryWriter writer, d3dx_parameter[] annotations, uint count)
+        private static void WriteAnnotations(BinaryWriter writer, d3dx_parameter[] annotations)
         {
+            var count = annotations == null ? 0 : annotations.Length;
             writer.Write((byte)count);
             for (var i = 0; i < count; i++)
                 WriteParameter(writer, annotations[i]);
-        }
-
-        private static void WriteTechnique(BinaryWriter writer, d3dx_technique technique)
-        {
-            writer.Write(technique.name);
-            WriteAnnotations(writer, technique.annotation_handles, technique.annotation_count);
-
-            // Write the passes.
-            writer.Write((byte)technique.pass_count);
-            for (var p = 0; p < technique.pass_count; p++)
-            {
-                var pass = technique.pass_handles[p];
-
-                writer.Write(pass.name);
-                WriteAnnotations(writer, pass.annotation_handles, pass.annotation_count);
-                WriteStates(writer, pass.states, pass.state_count);
-            }
         }
 
         private static void WriteStates(BinaryWriter writer, d3dx_state [] states, uint count)
--- Tools/2MGFX/DXShader.cs
+++ Tools/2MGFX/DXShader.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
 	public class DXShader
 	{
-        public readonly bool IsVertexShader;
+        public static bool IsDirectX = false;
+
+        // The index of the shader in the shared list.
+        public int SharedIndex { get; private set; }
+
+        private bool IsVertexShader;
 
         private readonly DXPreshader _preshader;
 
@@ -16,12 +22,43 @@ namespace Microsoft.Xna.Framework.Graphics
         private readonly int _uniforms_int4_count = 0;
         private readonly int _uniforms_bool_count = 0;
 
+        private struct Sampler
+        {
+            public MojoShader.MOJOSHADER_samplerType type;
+            public int index;
+            public string name;
+            public string parameter;
+        }
+
+        private struct Attribute
+        {
+            public VertexElementUsage usage;
+            public int index;
+            public string name;
+        }
+
         private readonly MojoShader.MOJOSHADER_symbol[] _symbols;
-        private readonly MojoShader.MOJOSHADER_sampler[] _samplers;
-        private readonly MojoShader.MOJOSHADER_attribute[] _attributes;
-		
-		public DXShader(byte[] byteCode)
+        private readonly Sampler[] _samplers;
+        private readonly Attribute[] _attributes;
+
+        public byte[] Bytecode { get; private set; }
+
+        public DXShader(byte[] byteCode, bool isVertexShader, int sharedIndex)
+        {
+            IsVertexShader = isVertexShader;
+            SharedIndex = sharedIndex;
+            Bytecode = byteCode;
+
+            _symbols = new MojoShader.MOJOSHADER_symbol[0];
+            _samplers = new Sampler[0];
+            _attributes = new Attribute[0];
+        }
+
+        public DXShader(byte[] byteCode, int sharedIndex)
 		{
+            SharedIndex = sharedIndex;
+            Bytecode = byteCode;
+
 			var parseDataPtr = MojoShader.NativeMethods.MOJOSHADER_parse(
 					"glsl",
                     byteCode,
@@ -73,24 +110,75 @@ namespace Microsoft.Xna.Framework.Graphics
 				return va.CompareTo(vb);
 			});//(a, b) => ((int)(a.info.elements > 1))a.register_index.CompareTo(b.register_index));
 			
-			_samplers = DXHelper.UnmarshalArray<MojoShader.MOJOSHADER_sampler>(
-					parseData.samplers, parseData.sampler_count);
-			
-			_attributes = DXHelper.UnmarshalArray<MojoShader.MOJOSHADER_attribute>(
-					parseData.attributes, parseData.attribute_count);
+            // Convert the attributes.
+            {
+			    var samplers = DXHelper.UnmarshalArray<MojoShader.MOJOSHADER_sampler>(
+					    parseData.samplers, parseData.sampler_count);
+                _samplers = new Sampler[samplers.Length];
+                for (var i=0; i < samplers.Length; i++)
+                {
+                    _samplers[i].name = samplers[i].name;
+                    _samplers[i].parameter = string.Empty;
+                    _samplers[i].type = samplers[i].type;
+                    _samplers[i].index = samplers[i].index;
+                }
+            }
+
+            // Conver the attributes.
+            {
+			    var attributes = DXHelper.UnmarshalArray<MojoShader.MOJOSHADER_attribute>(
+				    	parseData.attributes, parseData.attribute_count);
+
+                _attributes = new Attribute[attributes.Length];
+                for (var i = 0; i < attributes.Length; i++)
+                {
+                    _attributes[i].name = attributes[i].name;
+                    _attributes[i].index = attributes[i].index;
+                    _attributes[i].usage = DXEffectObject.ToVertexElementUsage(attributes[i].usage);
+                }
+            }
+
+            // For whatever reason the register indexing is 
+            // incorrect from MojoShader.
+            {
+                uint bool_index = 0;
+                uint float4_index = 0;
+                uint int4_index = 0;
+
+                for (var i = 0; i < _symbols.Length; i++)
+                {
+                    switch (_symbols[i].register_set)
+                    {
+                        case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_BOOL:
+                            _symbols[i].register_index = bool_index;
+                            bool_index += _symbols[i].register_count;
+                            break;
+
+                        case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_FLOAT4:
+                            _symbols[i].register_index = float4_index;
+                            float4_index += _symbols[i].register_count;
+                            break;
+
+                        case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_INT4:
+                            _symbols[i].register_index = int4_index;
+                            int4_index += _symbols[i].register_count;
+                            break;
+                    }
+                }
+            }
 
             foreach (var symbol in _symbols)
             {
                 switch (symbol.register_set)
                 {
                     case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_BOOL:
-                        _uniforms_bool_count += (int)symbol.register_count;
+                        _uniforms_bool_count = Math.Max(_uniforms_bool_count, (int)(symbol.register_index + symbol.register_count) );
                         break;
                     case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_FLOAT4:
-                        _uniforms_float4_count += (int)symbol.register_count;
+                        _uniforms_float4_count = Math.Max(_uniforms_float4_count, (int)(symbol.register_index + symbol.register_count) );
                         break;
                     case MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_INT4:
-                        _uniforms_int4_count += (int)symbol.register_count;
+                        _uniforms_int4_count = Math.Max(_uniforms_int4_count, (int)(symbol.register_index + symbol.register_count));
                         break;
                     default:
                         break;
@@ -117,15 +205,50 @@ namespace Microsoft.Xna.Framework.Graphics
                         _glslCode;
 		}
 
+        public void SetSamplerParameters(Dictionary<string, DXEffectObject.d3dx_parameter> parameters)
+        {
+            for (var i = 0; i < _samplers.Length; i++ )
+            {
+                MojoShader.MOJOSHADER_symbol? samplerSymbol = null;
+                foreach (var symbol in _symbols)
+                {
+                    if (symbol.register_set ==
+                            MojoShader.MOJOSHADER_symbolRegisterSet.MOJOSHADER_SYMREGSET_SAMPLER &&
+                        symbol.register_index == _samplers[i].index)
+                    {
+                        samplerSymbol = symbol;
+                        break;
+                    }
+                }
+
+                DXEffectObject.d3dx_parameter param;
+                if (samplerSymbol.HasValue && parameters.TryGetValue(samplerSymbol.Value.name, out param))
+                {
+                    var samplerState = (DXEffectObject.d3dx_sampler)param.data;
+                    if (samplerState != null && samplerState.state_count > 0)
+                    {
+                        var textureName = samplerState.states[0].parameter.name;
+                        _samplers[i].parameter = textureName;
+                    }
+                }
+            }
+        }
+
         public void Write(BinaryWriter writer)
         {
             writer.Write(IsVertexShader);
 
-            writer.Write(_preshader != null);
-            if (_preshader != null)
-                _preshader.Write(writer);
+            //writer.Write(_preshader != null);
+            //if (_preshader != null)
+                //_preshader.Write(writer);
 
-            writer.Write(_glslCode);
+            if (IsDirectX)
+            {
+                writer.Write((ushort)Bytecode.Length);
+                writer.Write(Bytecode);
+            }
+            else
+                writer.Write(_glslCode);
 
             writer.Write((byte)_uniforms_bool_count);
             writer.Write((byte)_uniforms_int4_count);
@@ -141,11 +264,12 @@ namespace Microsoft.Xna.Framework.Graphics
             }
 
             writer.Write((byte)_samplers.Length);
-            foreach (var samples in _samplers)
+            foreach (var sampler in _samplers)
             {
-                writer.Write(samples.name);
-                writer.Write((byte)samples.type);
-                writer.Write((byte)samples.index);
+                writer.Write(sampler.name);
+                writer.Write(sampler.parameter);
+                writer.Write((byte)sampler.type);
+                writer.Write((byte)sampler.index);
             }
 
             writer.Write((byte)_attributes.Length);
