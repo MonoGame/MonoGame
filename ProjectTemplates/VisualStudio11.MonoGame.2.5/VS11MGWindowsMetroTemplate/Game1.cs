edd209d42daae3b712f822efe0924543d95f454d
Dan Rigby <dan@danrigby.com>
2012-05-01 10:34:57 -0400

    Removed reference to GamePad in Game1.cs for now since it currently doesn't exist in MonoGame for Windows 8 Metro.

--- ProjectTemplates/VisualStudio11.MonoGame.2.5/VS11MGWindowsMetroTemplate/Game1.cs
+++ ProjectTemplates/VisualStudio11.MonoGame.2.5/VS11MGWindowsMetroTemplate/Game1.cs
@@ -66,7 +66,7 @@ namespace $safeprojectname$
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
