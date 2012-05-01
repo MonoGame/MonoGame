80fd1fb57d81799daa12330be62e9d83d7c4f154
Dan Rigby <dan@danrigby.com>
2012-04-30 20:52:42 -0400

    Initial VS11 project templates for MonoGame.

---
+++ ProjectTemplates/VisualStudio2012.MonoGame.2.5/VS2012MGWindowsMetroTemplate/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        private static Game1 game;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<Game1>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
