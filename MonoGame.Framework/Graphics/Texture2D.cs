3fbc4c29c6dacb69ec0ea63ac98f8d43b2e6c6fb
Tom Spilman <tom@sickheadgames.com>
2012-05-01 16:28:18 -0500

    Fixed Texture2D creation for DX11.

--- MonoGame.Framework/Graphics/Texture2D.cs
+++ MonoGame.Framework/Graphics/Texture2D.cs
@@ -126,14 +126,13 @@ namespace Microsoft.Xna.Framework.Graphics
             var desc = new SharpDX.Direct3D11.Texture2DDescription();
             desc.Width = width;
             desc.Height = height;
-            desc.MipLevels = 0;// mipmap ? 10 : 1;
+            desc.MipLevels = mipmap ? 0 : 1;
             desc.ArraySize = 1;
-            desc.Format = SharpDXHelper.ToFormat(SurfaceFormat.Color);
+            desc.Format = SharpDXHelper.ToFormat(format);
             desc.BindFlags = SharpDX.Direct3D11.BindFlags.ShaderResource;
-            desc.CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.Write;
+            desc.CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.None;
             desc.SampleDescription.Count = 1;
             desc.SampleDescription.Quality = 0;
-            desc.Usage = SharpDX.Direct3D11.ResourceUsage.Dynamic;
             desc.Usage = SharpDX.Direct3D11.ResourceUsage.Default;
             desc.OptionFlags = SharpDX.Direct3D11.ResourceOptionFlags.None;
 
