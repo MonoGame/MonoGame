cf6c7660013b986fee8250d0b2bbd8d47130aa15
Tom Spilman <tom@sickheadgames.com>
2012-05-01 16:30:03 -0500

    Removed internal field in TextureCollection.

--- MonoGame.Framework/Graphics/TextureCollection.cs
+++ MonoGame.Framework/Graphics/TextureCollection.cs
@@ -7,12 +7,17 @@ namespace Microsoft.Xna.Framework.Graphics
 {
     public class TextureCollection
     {
-        internal Dictionary<int, Texture> _textures = new Dictionary<int, Texture>();
+        private Dictionary<int, Texture> _textures = new Dictionary<int, Texture>();
 
         public Texture this[int index]
         {
             get { return _textures[index]; }
             set { _textures[index] = value; }
+        }
+
+        internal void Clear()
+        {
+            _textures.Clear();
         }
     }
 }
