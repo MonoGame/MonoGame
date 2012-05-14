326b9c5f9ff91247a845f541321b7d022e5be64d
Ray Batts <Ray@Sickheadgames.com>
2012-05-14 16:43:11 -0500

    Fixing a typo.

--- MonoGame.Framework/Content/ContentManager.cs
+++ MonoGame.Framework/Content/ContentManager.cs
@@ -223,7 +223,7 @@ namespace Microsoft.Xna.Framework.Content
 			}
 			
 			var lastPathSeparatorIndex = Math.Max(assetName.LastIndexOf('\\'), assetName.LastIndexOf('/'));
-			CurrentAssetDirectory = lastPathSeparatorIndex == 1 ? RootDirectory : assetName.Substring(0, lastPathSeparatorIndex);
+			CurrentAssetDirectory = lastPathSeparatorIndex == -1 ? RootDirectory : assetName.Substring(0, lastPathSeparatorIndex);
 			
 			string originalAssetName = assetName;
 			object result = null;
