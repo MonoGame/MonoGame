2b85f8ab2dd771ef48b63fbb2dff1d5e536a5ab2
tomspilman <tom@sickheadgames.com>
2012-06-03 21:01:09 -0500

    Moved effect parsing out of Main() so that it can be reused from other tools.

--- Tools/2MGFX/Program.cs
+++ Tools/2MGFX/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -30,22 +27,11 @@ namespace TwoMGFX
             //
             // For now we assume we're going right to a compiled MGFXO file.
 
-            var macros = new List<SharpDX.Direct3D.ShaderMacro>();
-            macros.Add(new SharpDX.Direct3D.ShaderMacro("MGFX", 1));
-
-            // Under the DX11 profile we pass a few more macros.
-            if (options.DX11Profile)
-            {
-                macros.Add(new SharpDX.Direct3D.ShaderMacro("HLSL", 1));
-                macros.Add(new SharpDX.Direct3D.ShaderMacro("SM4", 1));
-            }
-
             // Parse the MGFX file expanding includes, macros, and returning the techniques.
             ShaderInfo shaderInfo;
             try
             {
-                shaderInfo = ParseFile(options.SourceFile, macros);
-                shaderInfo.DX11Profile = options.DX11Profile;
+                shaderInfo = ShaderInfo.FromFile(options.SourceFile, options);
             }
             catch (Exception ex)
             {
@@ -89,75 +75,5 @@ namespace TwoMGFX
             Console.WriteLine("Compiled '{0}' to '{1}'.", options.SourceFile, options.OutputFile);
             return 0;
         }
-
-        static private ShaderInfo ParseFile(string path, List<SharpDX.Direct3D.ShaderMacro> macros)
-        {
-            // Use the D3DCompiler to pre-process the file resolving 
-            // all #includes and macros.... this even works for GLSL.
-            string newFile;
-            using (var includer = new CompilerInclude())
-                newFile = SharpDX.D3DCompiler.ShaderBytecode.PreprocessFromFile(path, macros.ToArray(), includer);
-
-            // Parse the resulting file for techniques and passes.
-            var tree = new Parser(new Scanner()).Parse(newFile);
-            if (tree.Errors.Count > 0)
-            {
-                // TODO: Make the error info pretty!
-                var errors = String.Empty;
-                foreach (var error in tree.Errors)
-                {
-                    int line, col;
-                    FindLineAndCol(newFile, error.Position, out line, out col);
-                    errors += string.Format("{0}({1},{2}) : {3}\r\n", path, line, col, error.Message);
-                }
-
-                throw new Exception(errors);
-            }
-
-            // Evaluate the results of the parse tree.
-            var result = tree.Eval() as ShaderInfo;
-            result.fileName = path;
-            result.fileContent = newFile;
-
-            // Finally remove the techniques from the file.
-            //
-            // TODO: Do we really need to do this, or will the HLSL 
-            // compiler just ignore it as we compile shaders?
-            //
-            /*
-            var extra = 2;
-            var offset = 0;
-            foreach (var tech in result.Techniques)
-            {
-                // Remove the technique from the file.
-                newFile = newFile.Remove(tech.startPos + offset, tech.length + extra);
-                offset -= tech.length + extra;
-
-                techniques.Add(tech);
-            }
-            */
-
-            return result;
-        }
-
-        public static void FindLineAndCol(string src, int pos, out int line, out int col)
-        {
-            line = 1;
-            col = 1;
-
-            for (var i = 0; i < pos; i++)
-            {
-                if (src[i] == '\n')
-                {
-                    line++;
-                    col = 1;
-                }
-                else
-                {
-                    col++;
-                }
-            }
-        }
-
     }
 }
