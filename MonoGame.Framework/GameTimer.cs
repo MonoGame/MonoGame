202f9acc13748ec90195e7cfc1a1d9dcadd4f51b
tomspilman <tom@sickheadgames.com>
2012-06-18 01:21:04 -0500

    Initial classes for WinRT XAML app support in MonoGame.

---
+++ MonoGame.Framework/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xna.Framework
+{
+    public sealed class GameTimer : IDisposable
+    {
+        static List<GameTimer> _activeTimers = new List<GameTimer>();
+
+        int DrawOrder { get; set; }
+
+        public int FrameActionOrder { get; set; }
+
+        public TimeSpan UpdateInterval { get; set; }
+
+        public int UpdateOrder { get; set; }
+
+        public event EventHandler<EventArgs> FrameAction;
+
+        public event EventHandler<GameTimerEventArgs> Draw;
+
+        public event EventHandler<GameTimerEventArgs> Update;
+
+        public void Start()
+        {
+            if ( !_activeTimers.Contains(this) )
+                _activeTimers.Add(this);
+        }
+
+        public void Stop()
+        {
+            _activeTimers.Remove(this);
+        }
+
+        public static void ResetElapsedTime()
+        {
+        }
+
+        public static void SuppressFrame()
+        {
+        }
+
+        public void Dispose()
+        {
+            _activeTimers.Remove(this);
+        }
+    }
+}
---
+++ MonoGame.Framework/SharedGraphicsDeviceManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Microsoft.Xna.Framework
+{
+    public class SharedGraphicsDeviceManager : IGraphicsDeviceService, IDisposable
+    {
+        public GraphicsDevice GraphicsDevice
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> DeviceCreated;
+
+        public event EventHandler<EventArgs> DeviceDisposing;
+
+        public event EventHandler<EventArgs> DeviceReset;
+
+        public event EventHandler<EventArgs> DeviceResetting;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
