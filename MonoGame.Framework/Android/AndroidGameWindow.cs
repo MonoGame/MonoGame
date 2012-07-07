1a91e4bf0893f8a288c0bac5d3558435b33b5646
Aranda Morrison <aranda_morrison@yahoo.com.au>
2012-07-08 00:38:53 +0800

    Fixed GetEffectiveSupportedOrientations to handle Default according to preferred aspect ratio (PreferredBackBuffer Width/Height).

--- MonoGame.Framework/Android/AndroidGameWindow.cs
+++ MonoGame.Framework/Android/AndroidGameWindow.cs
@@ -187,14 +187,27 @@ namespace Microsoft.Xna.Framework
         }
 
         /// <summary>
-        /// In Xna, DisplayOrientation.Default has the same effect as (DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight)
+        /// In Xna, setting SupportedOrientations = DisplayOrientation.Default (which is the default value)
+        /// has the effect of setting SupportedOrientations to landscape only or portrait only, based on the
+        /// aspect ratio of PreferredBackBufferWidth / PreferredBackBufferHeight
         /// </summary>
         /// <returns></returns>
         internal DisplayOrientation GetEffectiveSupportedOrientations()
         {
             if (supportedOrientations == DisplayOrientation.Default)
             {
-                return DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+                var deviceManager = (_game.Services.GetService(typeof(IGraphicsDeviceManager)) as GraphicsDeviceManager);
+                if (deviceManager == null)
+                    return DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+
+                if (deviceManager.PreferredBackBufferWidth > deviceManager.PreferredBackBufferHeight)
+                {
+                    return DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+                }
+                else
+                {
+                    return DisplayOrientation.Portrait | DisplayOrientation.PortraitUpsideDown;
+                }
             }
             else
             {
