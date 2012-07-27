493b11ce2bb6b576b6ae4a16b1624f61a1b42004
RayBatts <Ray@sickheadgames.com>
2012-07-26 21:02:40 -0500

    Stubbed out a bunch of classes needed by the incoming font processor.

---
+++ MonoGame.Framework.Content.Pipeline/ContentProcessors/TextureProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using System.ComponentModel;
+
+namespace Microsoft.Xna.Framework.Content.Pipeline.Processors
+{
+    public enum TextureProcessorOutputFormat
+    {
+        Color,
+        DXTCompressed,
+        NoChange
+    }
+
+    [ContentProcessorAttribute]
+    public class TextureProcessor : ContentProcessor<TextureContent, TextureContent>
+    {
+        public TextureProcessor()
+        {
+
+        }
+
+        public virtual Color ColorKeyColor { get; set; }
+
+        public virtual bool ColorKeyEnabled { get; set; }
+
+        public virtual bool GenerateMipmaps { get; set; }
+
+        [DefaultValueAttribute(true)]
+        public virtual bool PremultiplyAlpha { get; set; }
+
+        public virtual bool ResizeToPowerOfTwo { get; set; }
+
+        public virtual TextureProcessorOutputFormat TextureFormat { get; set; }
+
+        public override TextureContent Process(TextureContent input, ContentProcessorContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ MonoGame.Framework.Content.Pipeline/ContentWriters/ContentTypeWriterAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ContentTypeWriterAttribute : Attribute { }
+}
