53575c7cf0b4e984b35d89f7740abe60c5ebf388
Nezz <bloodsource@vipmail.hu>
2012-08-26 22:30:24 +0200

    Windows Phone's interface for sending emails and opening web pages implemented for iOS

--- MonoGame.Framework/iOS/GamerServices/EmailComposeTask.cs
+++ MonoGame.Framework/iOS/GamerServices/EmailComposeTask.cs
@@ -5,33 +5,85 @@ using MonoTouch.UIKit;
 
 namespace Microsoft.Xna.Framework.GamerServices
 {
-    public static class EmailComposeTask
+    public class EmailComposeTask
     {
+        #region Fields
         private static Game game;
+        private MFMailComposeViewController mail;
+        #endregion
 
+        #region Properties
+        public string To
+        {
+            set
+            {
+                if (this.mail != null && !string.IsNullOrEmpty(value))
+                    mail.SetToRecipients(new string[]{value});
+            }
+        }
+        public string Cc
+        {
+            set
+            {
+                if (this.mail != null && !string.IsNullOrEmpty(value))
+                    mail.SetCcRecipients(new string[]{value});
+            }
+        }
+        public string Bcc
+        {
+            set
+            {
+                if (this.mail != null && !string.IsNullOrEmpty(value))
+                    mail.SetBccRecipients(new string[]{value});
+            }
+        }
+        public string Subject
+        {
+            set
+            {
+                if (this.mail != null && this.mail != null && !string.IsNullOrEmpty(value))
+                    mail.SetSubject(value);
+            }
+        }
+        public string Body
+        {
+            set
+            {
+                if (this.mail != null && !string.IsNullOrEmpty(value))
+                    mail.SetMessageBody(value, false);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public EmailComposeTask()
+        {
+            if (MFMailComposeViewController.CanSendMail)
+            {
+                this.mail = new MFMailComposeViewController ();
+                this.mail.Finished += Mail_Dismiss;
+            }
+        }
+        #endregion
+
+        #region Methods
         public static void Initialise(Game game)
         {
             EmailComposeTask.game = game;
         }
 
-        public static void Show(string to, string cc, string bcc, string subject, string body)
+        public void Show()
         {
             if (MFMailComposeViewController.CanSendMail)
             {
-                MFMailComposeViewController mail = new MFMailComposeViewController();
-                if (!string.IsNullOrEmpty(to))
-                    mail.SetToRecipients(new string[]{to});
-                if (!string.IsNullOrEmpty(cc))
-                    mail.SetCcRecipients(new string[]{cc});
-                if (!string.IsNullOrEmpty(bcc))
-                    mail.SetBccRecipients(new string[]{bcc});
-                if (!string.IsNullOrEmpty(subject))
-                    mail.SetSubject(subject);
-                if (!string.IsNullOrEmpty(body))
-                    mail.SetMessageBody(body, false);
-                mail.Finished += (object sender, MFComposeResultEventArgs args) => { args.Controller.DismissModalViewControllerAnimated (true); };
-                (EmailComposeTask.game.Platform as iOSGamePlatform).ViewController.PresentModalViewController(mail, true);
+                (EmailComposeTask.game.Platform as iOSGamePlatform).ViewController.PresentModalViewController(this.mail, true);
             }
         }
+
+        private void Mail_Dismiss(object sender, MFComposeResultEventArgs args)
+        {
+            args.Controller.DismissModalViewControllerAnimated(true);
+        }
+        #endregion
     }
 }
--- MonoGame.Framework/iOS/GamerServices/WebBrowserTask.cs
+++ MonoGame.Framework/iOS/GamerServices/WebBrowserTask.cs
@@ -1,18 +1,38 @@
+using System;
+
 using Microsoft.Xna.Framework;
 
 using MonoTouch.Foundation;
-using MonoTouch.MessageUI;
 using MonoTouch.UIKit;
 
 namespace Microsoft.Xna.Framework.GamerServices
 {
-    public static class WebBrowserTask
+    public  class WebBrowserTask
     {
-        public static void Show(string url)
+        #region Fields
+        private NSUrl nsurl;
+        #endregion
+
+        #region Properties
+        public Uri Uri
         {
-            NSUrl nsurl = new NSUrl(url);
-            UIApplication.SharedApplication.OpenUrl(nsurl);
+            set
+            {
+                if (value.IsAbsoluteUri && (value.Scheme == Uri.UriSchemeHttp || value.Scheme == Uri.UriSchemeHttps))
+                    this.nsurl = new NSUrl(value.OriginalString);
+                else
+                    throw new ArgumentOutOfRangeException("Uri", "This property supports only absolute URIs that use the HTTP or HTTPS protocol.");
+            }
         }
+        #endregion
+
+        #region Methods
+        public void Show()
+        {
+            if (this.nsurl != null)
+                UIApplication.SharedApplication.OpenUrl(nsurl);
+        }
+        #endregion
     }
 }
 
