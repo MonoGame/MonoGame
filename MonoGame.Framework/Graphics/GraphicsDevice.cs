4f0805e8c224439b0e7ea6afd5117d4a2a95ce79
Dean Ellis <dellis1972@googlemail.com>
2012-09-06 20:32:50 +0100

    Fix for #636 - ScissorRectangle now only updated if RenderTarget is not set Added a fix to sort out the Viewport problem when a RenderTarget is set.

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -280,6 +280,14 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
+        public bool IsRenderTargetBound
+        {
+            get
+            {
+                return _currentRenderTargetBindings == null || _currentRenderTargetBindings.Length == 0;
+            }
+        }
+
         public GraphicsDevice ()
 		{
 			// Initialize the main viewport
@@ -1000,7 +1008,14 @@ namespace Microsoft.Xna.Framework.Graphics
                 lock (_d3dContext) 
                     _d3dContext.Rasterizer.SetViewports(viewport);
 #elif OPENGL
-                GL.Viewport (value.X, value.Y, value.Width, value.Height);
+                if (IsRenderTargetBound)
+                {
+                       GL.Viewport(value.X, value.Y, value.Width, value.Height);
+                }
+                else
+                {
+                    GL.Viewport(value.X, PresentationParameters.BackBufferHeight - value.Y - value.Height, value.Width, value.Height);
+                }
 #if GLES
                 GL.DepthRange(value.MinDepth, value.MaxDepth);
 #else
@@ -1029,7 +1044,10 @@ namespace Microsoft.Xna.Framework.Graphics
 
 #if OPENGL
                 var glScissorRectangle = _scissorRectangle;
-                glScissorRectangle.Y = _viewport.Height - glScissorRectangle.Y - glScissorRectangle.Height;
+                if (!IsRenderTargetBound)
+                {
+                    glScissorRectangle.Y = _viewport.Height - glScissorRectangle.Y - glScissorRectangle.Height;
+                }                
                 GLStateManager.SetScissor(glScissorRectangle);				
 #endif
             }
@@ -1211,7 +1229,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
 		public RenderTargetBinding[] GetRenderTargets()
 		{
-            if (_currentRenderTargetBindings == null)
+            if (!IsRenderTargetBound)
                 return EmptyRenderTargetBinding;
 
             return _currentRenderTargetBindings;
