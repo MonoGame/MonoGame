d9e9ef6ed8f12ac875198f4b448dfa36739317de
Dean Ellis <dellis1972@googlemail.com>
2012-09-08 19:39:36 +0100

    Added a new glTextureUnit field to the Texture class. Made a call to GL.ActiveTexture in the Texture.Activate method as it makes sense to keep these together. Updated the Shader.ogl.cs to use the new glTExtureUnit field rather than calling GL.ActiveTexture itself. Removed the opengl/directx specific code from the SpriteBatcher but had to add a call to activate the Texture before calling drawUserPrimitives to get round a problem with the texture not being set. removed a retundant call to graphicsDevice.Textures [0] = texture; in SpriteBatch as it is handled by the SpriteBatcher now

--- MonoGame.Framework/Graphics/Texture.cs
+++ MonoGame.Framework/Graphics/Texture.cs
@@ -69,6 +69,7 @@ namespace Microsoft.Xna.Framework.Graphics
 #elif OPENGL
 		internal int glTexture = -1;
 		internal TextureTarget glTarget;
+        internal TextureUnit glTextureUnit = TextureUnit.Texture0;
 #endif
 		
 		public SurfaceFormat Format
@@ -146,6 +147,7 @@ namespace Microsoft.Xna.Framework.Graphics
 #if OPENGL
 		internal virtual void Activate()
         {
+            GL.ActiveTexture(glTextureUnit);
 			GL.BindTexture(glTarget, this.glTexture);
         }
 #endif
