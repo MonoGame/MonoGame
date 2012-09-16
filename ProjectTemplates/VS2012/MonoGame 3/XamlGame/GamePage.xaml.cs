7c3b21abe26e81756999dfd9b13fa9ab954e3880
Tom Spilman <tom@sickheadgames.com>
2012-09-16 16:39:19 -0500

    Added new VS2012 templates for both the Game and Game+XAML projects.

---
+++ ProjectTemplates/VS2012/MonoGame 3/XamlGame/GamePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// The root page used to display the game.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly $safeprojectname$Game _game;
+
+        public GamePage(string launchArguments)
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+            _game = XamlGame<$safeprojectname$Game>.Create(launchArguments, Window.Current.CoreWindow, this);
+        }
+    }
+}
