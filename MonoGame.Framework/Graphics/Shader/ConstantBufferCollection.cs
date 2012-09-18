f4ca6cbeed8f75704e62b4bb745d8f59b4786889
Dominique Louis <Dominique@SavageSoftwareSolutions.com>
2012-09-18 13:50:46 +0100

    Changes to allow PlayStation Mobile to compile again, after recent changes.

--- MonoGame.Framework/Graphics/Shader/ConstantBufferCollection.cs
+++ MonoGame.Framework/Graphics/Shader/ConstantBufferCollection.cs
@@ -4,6 +4,8 @@
 using MonoMac.OpenGL;
 #elif WINDOWS || LINUX
 using OpenTK.Graphics.OpenGL;
+#elif PSS
+using Sce.PlayStation.Core.Graphics;
 #elif GLES
 using OpenTK.Graphics.ES20;
 #endif
@@ -56,7 +58,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
 #if DIRECTX
         internal void SetConstantBuffers(GraphicsDevice device)
-#elif OPENGL
+#elif OPENGL || PSS
         internal void SetConstantBuffers(GraphicsDevice device, int shaderProgram)
 #endif
         {
@@ -73,7 +75,7 @@ namespace Microsoft.Xna.Framework.Graphics
                 {
 #if DIRECTX
                     buffer.Apply(device, _stage, i);
-#elif OPENGL
+#elif OPENGL || PSS
                     buffer.Apply(device, shaderProgram);
 #endif
                 }
