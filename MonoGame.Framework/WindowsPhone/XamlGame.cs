10c9c5f129ffc9f3887f073ffedad33b7dc31b1f
Tom Spilman <tom@sickheadgames.com>
2012-11-06 13:12:03 -0600

    For some reason an object cannot be a DrawingSurfaceBackgroundContentProviderNativeBase and a IDrawingSurfaceManipulationHandler.

--- MonoGame.Framework/WindowsPhone/XamlGame.cs
+++ MonoGame.Framework/WindowsPhone/XamlGame.cs
@@ -18,60 +18,8 @@ namespace MonoGame.Framework.WindowsPhone
     public static class XamlGame<T>
         where T : Game, new()
     {
-        class SurfaceHost : DrawingSurfaceBackgroundContentProviderNativeBase, 
-                            IDrawingSurfaceManipulationHandler
+        class SurfaceTouchHandler : IDrawingSurfaceManipulationHandler
         {
-            private Device _device;
-            private DeviceContext _context;
-            private readonly T _game;
-            DrawingSurfaceRuntimeHost _host;
-
-            public SurfaceHost(T game)
-            {
-                _game = game;
-            }
-
-            public override void Connect(DrawingSurfaceRuntimeHost host, Device device)
-            {
-                _host = host;
-            }
-
-            public override void Disconnect()
-            {
-                _host = null;
-            }
-
-            public override void Draw(Device device, DeviceContext context, RenderTargetView renderTargetView)
-            {
-                var deviceChanged = _device != device || _context != context;
-                _device = device;
-                _context = context;
-
-                if (!_game.Initialized)
-                {
-                    DrawingSurfaceState.Device = _device;
-                    DrawingSurfaceState.Context = _context;
-                    DrawingSurfaceState.RenderTargetView = renderTargetView;
-                    deviceChanged = false;
-
-                    // Start running the game.
-                    _game.Run(GameRunBehavior.Asynchronous);
-                }
-
-                if (deviceChanged)
-                    _game.GraphicsDevice.UpdateDevice(device, context);
-                _game.GraphicsDevice.UpdateTarget(renderTargetView);
-                _game.Tick();
-
-                _host.RequestAdditionalFrame();
-            }
-
-            public override void PrepareResources(DateTime presentTargetTime, DrawingSizeF desiredRenderTargetSize)
-            {
-                WindowsPhoneGameWindow.Width = desiredRenderTargetSize.Width;
-                WindowsPhoneGameWindow.Height = desiredRenderTargetSize.Height;
-            }
-
             public void SetManipulationHost(DrawingSurfaceManipulationHost manipulationHost)
             {
                 manipulationHost.PointerPressed += OnPointerPressed;
@@ -110,6 +58,61 @@ namespace MonoGame.Framework.WindowsPhone
             }
         }
 
+        class SurfaceUpdateHandler : DrawingSurfaceBackgroundContentProviderNativeBase                                                        
+        {
+            private Device _device;
+            private DeviceContext _context;
+            private readonly T _game;
+            DrawingSurfaceRuntimeHost _host;
+
+            public SurfaceUpdateHandler(T game)
+            {
+                _game = game;
+            }
+
+            public override void Connect(DrawingSurfaceRuntimeHost host, Device device)
+            {
+                _host = host;
+            }
+
+            public override void Disconnect()
+            {
+                // TODO: Do we deal with this as a device lost case?
+                _host = null;
+            }
+
+            public override void Draw(Device device, DeviceContext context, RenderTargetView renderTargetView)
+            {
+                var deviceChanged = _device != device || _context != context;
+                _device = device;
+                _context = context;
+
+                if (!_game.Initialized)
+                {
+                    DrawingSurfaceState.Device = _device;
+                    DrawingSurfaceState.Context = _context;
+                    DrawingSurfaceState.RenderTargetView = renderTargetView;
+                    deviceChanged = false;
+
+                    // Start running the game.
+                    _game.Run(GameRunBehavior.Asynchronous);
+                }
+
+                if (deviceChanged)
+                    _game.GraphicsDevice.UpdateDevice(device, context);
+                _game.GraphicsDevice.UpdateTarget(renderTargetView);
+                _game.Tick();
+
+                _host.RequestAdditionalFrame();
+            }
+
+            public override void PrepareResources(DateTime presentTargetTime, DrawingSizeF desiredRenderTargetSize)
+            {
+                WindowsPhoneGameWindow.Width = desiredRenderTargetSize.Width;
+                WindowsPhoneGameWindow.Height = desiredRenderTargetSize.Height;
+            }
+        }
+
  
         /// <summary>
         /// Creates your Game class initializing it to worth within a XAML application window.
@@ -131,11 +134,9 @@ namespace MonoGame.Framework.WindowsPhone
             if (game.graphicsDeviceManager == null)
                 throw new NullReferenceException("You must create the GraphicsDeviceManager in the Game constructor!");
 
-            // Hookup the surface host which will finish creating
-            // the game once a device and context are available.
-            var host = new SurfaceHost(game);
-            drawingSurface.SetBackgroundContentProvider(host);
-            drawingSurface.SetBackgroundManipulationHandler(host);
+            // Hookup the handlers for updates and touch.
+            drawingSurface.SetBackgroundContentProvider(new SurfaceUpdateHandler(game));
+            drawingSurface.SetBackgroundManipulationHandler(new SurfaceTouchHandler());
 
             // Return the constructed, but not initialized game.
             return game;
