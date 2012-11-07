4499cb5a9a0bb1a649085dc5779aa90c1bf7271e
Steve Williams <slygamer@gmail.com>
2012-11-07 23:07:45 +1000

    License headers for all current content pipeline source files changed to refer to LICENSE.TXT.

--- MonoGame.Framework.Content.Pipeline/TargetPlatform.cs
+++ MonoGame.Framework.Content.Pipeline/TargetPlatform.cs
@@ -1,42 +1,6 @@
-﻿#region License
-/*
- Microsoft Public License (Ms-PL)
- MonoGame - Copyright © 2012 The MonoGame Team
- 
- All rights reserved.
- 
- This license governs use of the accompanying software. If you use the software, you accept this license. If you do not
- accept the license, do not use the software.
- 
- 1. Definitions
- The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under 
- U.S. copyright law.
- 
- A "contribution" is the original software, or any additions or changes to the software.
- A "contributor" is any person that distributes its contribution under this license.
- "Licensed patents" are a contributor's patent claims that read directly on its contribution.
- 
- 2. Grant of Rights
- (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, 
- each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
- (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, 
- each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
- 
- 3. Conditions and Limitations
- (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
- (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, 
- your patent license from such contributor to the software ends automatically.
- (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution 
- notices that are present in the software.
- (D) If you distribute any portion of the software in source code form, you may do so only under this license by including 
- a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object 
- code form, you may only do so under a license that complies with this license.
- (E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees
- or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent
- permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular
- purpose and non-infringement.
- */
-#endregion License
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
 
 namespace Microsoft.Xna.Framework.Content.Pipeline
 {
@@ -49,10 +13,12 @@ namespace Microsoft.Xna.Framework.Content.Pipeline
         /// All supported versions of Windows (excluding Windows Phone)
         /// </summary>
         Windows,
+
         /// <summary>
         /// Xbox 360 video game and entertainment system
         /// </summary>
         Xbox360,
+
         /// <summary>
         /// Windows Phone
         /// </summary>
@@ -65,40 +31,53 @@ namespace Microsoft.Xna.Framework.Content.Pipeline
         /// (MonoGame)
         /// </summary>
         iOS,
+
         /// <summary>
         /// Android-based devices
         /// (MonoGame)
         /// </summary>
         Android,
+
         /// <summary>
         /// Linux-based PCs
         /// (MonoGame)
         /// </summary>
         Linux,
+
         /// <summary>
         /// Apple Mac OSX-based devices (iMac, MacBook, MacBook Air, etc)
         /// (MonoGame)
         /// </summary>
         MacOSX,
+
         /// <summary>
-        /// Windows 8 Metro apps
+        /// Windows Store App
         /// (MonoGame)
         /// </summary>
-        Metro,
+        WindowsStoreApp,
+
         /// <summary>
         /// Google Chrome Native Client
         /// (MonoGame)
         /// </summary>
         NativeClient,
+
         /// <summary>
         /// Ouya video game console, a variation of Android
         /// (MonoGame)
         /// </summary>
         Ouya,
+
         /// <summary>
         /// Sony PlayStation Mobile (PS Vita)
         /// (MonoGame)
         /// </summary>
         PlayStationMobile,
+
+        /// <summary>
+        /// Windows Phone 8
+        /// (MonoGame)
+        /// </summary>
+        WindowsPhone8,
     }
 }
