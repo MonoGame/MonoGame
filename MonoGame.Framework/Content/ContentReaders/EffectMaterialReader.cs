cda7fcf4432f486980c12f5bd17dd45fb1250791
KakCAT_TGN <support@ru-zero.com>
2012-11-11 15:52:22 +0100

    Revert "* Removed #if IPHONE"
    This reverts commit 263118fc3b6b8a43186a5e993f99437b129c133a.

--- MonoGame.Framework/Content/ContentReaders/EffectMaterialReader.cs
+++ MonoGame.Framework/Content/ContentReaders/EffectMaterialReader.cs
@@ -40,9 +40,6 @@ purpose and non-infringement.
 
 using System;
 using System.Collections.Generic;
-#if WINRT
-using System.Reflection;
-#endif
 
 using Microsoft.Xna.Framework.Graphics;
 
@@ -59,14 +56,8 @@ namespace Microsoft.Xna.Framework.Content
 
 			foreach (KeyValuePair<string, object> item in dict) {
 				var parameter = effectMaterial.Parameters [item.Key];
-				if (parameter != null) 
-				{
-#if WINRT
-					if (item.Value.GetType().GetTypeInfo().IsAssignableFrom (typeof (Texture).GetTypeInfo()))
-#else
-					if (typeof(Texture).IsAssignableFrom (item.Value.GetType ())) 
-#endif
-					{
+				if (parameter != null) {
+					if (typeof(Texture).IsAssignableFrom (item.Value.GetType ())) {
 						parameter.SetValue ((Texture)item.Value);
 					} else {
 						throw new NotImplementedException ();
