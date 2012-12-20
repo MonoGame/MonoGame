8a1d4395911f9e1a5ce96f07b7867a93bb951b6d
Andrea Magnorsky <roundcrisis@gmail.com>
2012-12-20 15:54:35 +0000

    Not building on windows because "CursorVisible " was not available in OpenTKGameWindow

--- MonoGame.Framework/Desktop/OpenTKGamePlatform.cs
+++ MonoGame.Framework/Desktop/OpenTKGamePlatform.cs
@@ -251,13 +251,15 @@ namespace Microsoft.Xna.Framework
         }
   
         protected override void OnIsMouseVisibleChanged()
-        {
+		{
+#if LINUX
             MouseState oldState = Mouse.GetState();
             _view.Window.CursorVisible = IsMouseVisible;
             // IsMouseVisible changes the location of the cursor on Linux (and Windows?) and we have to manually set it back to the correct position
             System.Drawing.Point mousePos = _view.Window.PointToScreen(new System.Drawing.Point(oldState.X, oldState.Y));
             OpenTK.Input.Mouse.SetPosition(mousePos.X, mousePos.Y);
-        }
+#endif
+		}
         
         public override void Log(string Message)
         {
