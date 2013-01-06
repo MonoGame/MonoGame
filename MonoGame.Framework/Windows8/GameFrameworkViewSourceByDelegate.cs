b47de09d6493f45886e5570c751d5eb3a3000faf
Thiago Pastor <thiagodiaspastor@gmail.com>
2013-01-06 16:10:16 -0200

    Extending the way WinRt Game Class can be created
    Instead of providing just the type of the class extending Xna Game, you
    can provide a factory method that you provide the customizated
    implementation.
    With this change, you will be able to create a Winrt Game as follows:
    
    static void Main()
    {
    var factory = new
    
    MonoGame.Framework.GameFrameworkViewSourceByDelegate<EngineStuff>(GameFactory);
    ///you provide a factory method
    Windows.ApplicationModel.Core.CoreApplication.Run(factory);
    }
    
    ///the method that provides the Game implementation
    static EngineStuff GameFactory(CoreApplicationView voceview,
    IActivatedEventArgs args)
    {
    InitialEngineDescription desc = InitialEngineDescription.Default();
    desc.useMipMapWhenPossible = true;
    EngineStuff engine = new EngineStuff(ref desc, LoadScreen);
    return engine;
    }

---
+++ MonoGame.Framework/Windows8/GameFrameworkViewSourceByDelegate.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Core;
+using Microsoft.Xna.Framework;
+using System;
+using Windows.ApplicationModel.Activation;
+
+namespace MonoGame.Framework
+{    
+    public class GameFrameworkViewSourceByDelegate<T> : IFrameworkViewSource
+        where T : Game
+    {
+        private Func<CoreApplicationView, IActivatedEventArgs, T> createGameInstanceDelegate;
+        public GameFrameworkViewSourceByDelegate(Func<CoreApplicationView, IActivatedEventArgs, T> createGameInstanceDelegate)
+        {
+            this.createGameInstanceDelegate = createGameInstanceDelegate;
+        }
+
+        #region IFrameworkViewSource Members
+
+        public IFrameworkView CreateView()
+        {
+            return new MetroFrameworkViewByDelegate<T>(this.createGameInstanceDelegate);
+        }
+
+        #endregion
+    }
+}
---
+++ MonoGame.Framework/Windows8/MetroFrameworkViewByDelegate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.ApplicationModel.Activation;
+
+namespace Microsoft.Xna.Framework
+{
+    class MetroFrameworkViewByDelegate<T> : IFrameworkView
+        where T : Game
+    {
+
+        public MetroFrameworkViewByDelegate(Func<CoreApplicationView, IActivatedEventArgs, T> createGameInstanceDelegate)
+        {
+            this.createGameInstanceDelegate = createGameInstanceDelegate;
+        }
+
+        private Func<CoreApplicationView, IActivatedEventArgs, T> createGameInstanceDelegate;
+        private CoreApplicationView _applicationView;
+        private T _game;
+
+        public void Initialize(CoreApplicationView applicationView)
+        {
+            _applicationView = applicationView;
+
+            _applicationView.Activated += ViewActivated;
+        }
+
+        private void ViewActivated(CoreApplicationView sender, IActivatedEventArgs args)
+        {
+            if (args.Kind == ActivationKind.Launch)
+            {
+                // Save any launch parameters to be parsed by the platform.
+                MetroGamePlatform.LaunchParameters = ((LaunchActivatedEventArgs)args).Arguments;
+                MetroGamePlatform.PreviousExecutionState = ((LaunchActivatedEventArgs)args).PreviousExecutionState;
+
+                // Construct the game.
+                _game = createGameInstanceDelegate(sender,args);
+            }
+            else if (args.Kind == ActivationKind.Protocol)
+            {
+                // Save any protocol launch parameters to be parsed by the platform.
+                var protocolArgs = args as ProtocolActivatedEventArgs;                
+                MetroGamePlatform.LaunchParameters = protocolArgs.Uri.AbsoluteUri;
+                MetroGamePlatform.PreviousExecutionState = protocolArgs.PreviousExecutionState;
+
+                // Construct the game.
+                _game = createGameInstanceDelegate(sender, args);
+            }
+        }
+
+        public void Load(string entryPoint)
+        {
+        }
+
+        public void Run()
+        {
+            // Initialize and run the game.
+            _game.Run();
+        }
+       
+        public void SetWindow(CoreWindow window)
+        {
+            // Initialize the singleton window.
+            MetroGameWindow.Instance.Initialize(window, null);
+        }
+
+        public void Uninitialize()
+        {
+            // TODO: I have no idea when and if this is
+            // called... as of Win8 build 8250 this seems 
+            // like its never called.
+        }
+    }
+}
