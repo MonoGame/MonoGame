ee60155053cd3fdec20a5ac9f2f057c37b2548f6
Jordan S <xanather@hotmail.com>
2013-02-02 17:13:32 +0800

    [WinDX]: IsMouseVisible + IsBorderless
    code for IsMouseVisible and IsBorderless properties

--- MonoGame.Framework/Windows/WinFormsGameWindow.cs
+++ MonoGame.Framework/Windows/WinFormsGameWindow.cs
@@ -1,4 +1,4 @@
-﻿#region License
+#region License
 /*
 Microsoft Public License (Ms-PL)
 XnaTouch - Copyright © 2009 The XnaTouch Team
@@ -60,6 +60,12 @@ namespace MonoGame.Framework
 
         private WinFormsGamePlatform _platform;
 
+        private bool _isResizable;
+
+        private bool _isBorderless;
+
+        private bool _isMouseHidden;
+
         #region Internal Properties
 
         internal Game Game { get; private set; }
@@ -83,11 +89,19 @@ namespace MonoGame.Framework
 
         public override bool AllowUserResizing
         {
-            get { return _form.FormBorderStyle == FormBorderStyle.Sizable; }
+            get { return _isResizable; }
             set
             {
-                _form.FormBorderStyle = value ? FormBorderStyle.Sizable : FormBorderStyle.Fixed3D;
-                _form.MaximizeBox = value;
+                if (_isResizable != value)
+                {
+                    _isResizable = value;
+                    _form.MaximizeBox = _isResizable;
+                }
+                else
+                    return;
+                if (_isBorderless)
+                    return;
+                _form.FormBorderStyle = _isResizable ? FormBorderStyle.Sizable : FormBorderStyle.FixedSingle;
             }
         }
 
@@ -98,6 +112,22 @@ namespace MonoGame.Framework
 
         protected internal override void SetSupportedOrientations(DisplayOrientation orientations)
         {
+        }
+
+        public override bool IsBorderless
+        {
+            get { return _isBorderless; }
+            set
+            {
+                if (_isBorderless != value)
+                    _isBorderless = value;
+                else
+                    return;
+                if (_isBorderless)
+                    _form.FormBorderStyle = FormBorderStyle.None;
+                else
+                    _form.FormBorderStyle = _isResizable ? FormBorderStyle.Sizable : FormBorderStyle.FixedSingle;
+            }
         }
 
         #endregion
@@ -122,6 +152,8 @@ namespace MonoGame.Framework
             _form.MouseWheel += OnMouseState;
             _form.KeyDown += OnKeyDown;
             _form.KeyUp += OnKeyUp;
+            _form.MouseEnter += OnMouseEnter;
+            _form.MouseLeave += OnMouseLeave;
             Keyboard.SetKeys(_keyState);
 
             _form.Activated += OnActivated;
@@ -161,6 +193,24 @@ namespace MonoGame.Framework
         {
             var key = (XnaKey)keyEventArgs.KeyCode;
             _keyState.Remove(key);
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            if (!_platform.IsMouseVisible && !_isMouseHidden)
+            {
+                _isMouseHidden = true;
+                Cursor.Hide();
+            }
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            if (_isMouseHidden)
+            {
+                _isMouseHidden = false;
+                Cursor.Show();
+            }
         }
 
         internal void Initialize()
