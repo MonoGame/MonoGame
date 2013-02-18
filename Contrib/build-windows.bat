e7519ed6b8ea096f09954b36239cc50e7bd6c1d9
Huseyin Uslu <shalafiraistlin@gmail.com>
2013-02-18 12:47:50 +0200

    Added build script for windows platform, that can build WindowsDX, WindowsGL, Windows8, Android and WindowsPhone targets. Also added build-macos.sh and build-linux.sh but they are just placeholders atm.

---
+++ Contrib/build-windows.bat
@@ -0,0 +1,19 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /version
+:: WindowsDX
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /t:Rebuild ../MonoGame.Framework.Windows.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /t:Rebuild ../MonoGame.Framework.Windows.sln
+:: WindowsGL
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /t:Rebuild ../MonoGame.Framework.WindowsGL.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /t:Rebuild ../MonoGame.Framework.WindowsGL.sln
+:: Windows8
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /t:Rebuild ../MonoGame.Framework.Windows8.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /t:Rebuild ../MonoGame.Framework.Windows8.sln
+:: WindowsPhone - ARM
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /p:Platform=ARM /t:Rebuild "../MonoGame.Framework.WindowsPhone.sln"
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /p:Platform=ARM /t:Rebuild ../MonoGame.Framework.WindowsPhone.sln
+:: WindowsPhone - x86
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /p:Platform=x86 /t:Rebuild ../MonoGame.Framework.WindowsPhone.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /p:Platform=x86 /t:Rebuild ../MonoGame.Framework.WindowsPhone.sln
+:: Android
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Release /t:Rebuild ../MonoGame.Framework.Android.sln
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /ds /p:Configuration=Debug /t:Rebuild ../MonoGame.Framework.Android.sln
