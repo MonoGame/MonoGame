e3bc4f565facd9c7c7afe56db84ebb264e4afa7e
dbyzero <cyril@dbyzero.com>
2013-03-06 21:35:01 +0100

    Update SpriteBatch.cs
    Update indentation

--- MonoGame.Framework/Graphics/SpriteBatch.cs
+++ MonoGame.Framework/Graphics/SpriteBatch.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Xna.Framework.Graphics
 		DepthStencilState _depthStencilState; 
 		RasterizerState _rasterizerState;		
 		Effect _effect;
-        bool _beginCalled;
+       		bool _beginCalled;
 
 		Effect _spriteEffect;
 
@@ -30,23 +30,23 @@ namespace Microsoft.Xna.Framework.Graphics
 
 			this.GraphicsDevice = graphicsDevice;
 
-            // Use a custom SpriteEffect so we can control the transformation matrix
-            _spriteEffect = new Effect(graphicsDevice, SpriteEffect.Bytecode);
+            		// Use a custom SpriteEffect so we can control the transformation matrix
+            		_spriteEffect = new Effect(graphicsDevice, SpriteEffect.Bytecode);
 
-            _batcher = new SpriteBatcher(graphicsDevice);
+           		 _batcher = new SpriteBatcher(graphicsDevice);
 
-            _beginCalled = false;
+            		_beginCalled = false;
 		}
 
 		public void Begin ()
 		{
-            Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Matrix.Identity);	
+            		Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Matrix.Identity);	
 		}
 
 		public void Begin (SpriteSortMode sortMode, BlendState blendState, SamplerState samplerState, DepthStencilState depthStencilState, RasterizerState rasterizerState, Effect effect, Matrix transformMatrix)
 		{
-            if (_beginCalled)
-                throw new InvalidOperationException("Begin cannot be called again until End has been successfully called.");
+           		 if (_beginCalled)
+               			throw new InvalidOperationException("Begin cannot be called again until End has been successfully called.");
 
 			// defaults
 			_sortMode = sortMode;
@@ -59,11 +59,11 @@ namespace Microsoft.Xna.Framework.Graphics
 			
 			_matrix = transformMatrix;
 
-            // Setup things now so a user can chage them.
-            if (sortMode == SpriteSortMode.Immediate)
+            		// Setup things now so a user can chage them.
+           		 if (sortMode == SpriteSortMode.Immediate)
 				Setup();
 
-            _beginCalled = true;
+            		_beginCalled = true;
 		}
 
 		public void Begin (SpriteSortMode sortMode, BlendState blendState)
@@ -87,103 +87,108 @@ namespace Microsoft.Xna.Framework.Graphics
 
 			if (_sortMode != SpriteSortMode.Immediate)
 				Setup();
-#if PSM   
-            GraphicsDevice.BlendState = _blendState;
-            _blendState.ApplyState(GraphicsDevice);
-#endif
+			#if PSM   
+            		GraphicsDevice.BlendState = _blendState;
+            		_blendState.ApplyState(GraphicsDevice);
+			#endif
             
-            _batcher.DrawBatch(_sortMode);
-        }
+            		_batcher.DrawBatch(_sortMode);
+       		}
 		
 		void Setup() 
-        {
-            GraphicsDevice gd = GraphicsDevice;
+        	{
+            		GraphicsDevice gd = GraphicsDevice;
 			gd.BlendState = _blendState;
 			gd.DepthStencilState = _depthStencilState;
 			gd.RasterizerState = _rasterizerState;
 			gd.SamplerStates[0] = _samplerState;
 			
-            // Setup the default sprite effect.
+            		// Setup the default sprite effect.
 			var vp = gd.Viewport;
 
-            // GL requires a half pixel offset where as DirectX and PSS does not.
-#if PSM || DIRECTX
-            var projection = Matrix.CreateOrthographicOffCenter(0, vp.Width, vp.Height, 0, -1, 0);
-            var transform = _matrix * projection;
-#else
-            var projection = Matrix.CreateOrthographicOffCenter(0, vp.Width, vp.Height, 0, 0, 1);
+            		// GL requires a half pixel offset where as DirectX and PSS does not.
+			#if PSM || DIRECTX
+            		var projection = Matrix.CreateOrthographicOffCenter(0, vp.Width, vp.Height, 0, -1, 0);
+            		var transform = _matrix * projection;
+			#else
+            		var projection = Matrix.CreateOrthographicOffCenter(0, vp.Width, vp.Height, 0, 0, 1);
 			var halfPixelOffset = Matrix.CreateTranslation(-0.5f, -0.5f, 0);
 			var transform = _matrix * (halfPixelOffset * projection);
-#endif
+			#endif
 
 			_spriteEffect.Parameters["MatrixTransform"].SetValue(transform);				                
 			_spriteEffect.CurrentTechnique.Passes[0].Apply();
 
 			// If the user supplied a custom effect then apply
-            // it now to override the sprite effect.
-            if (_effect != null)
-			    _effect.CurrentTechnique.Passes[0].Apply();
+            		// it now to override the sprite effect.
+            		if (_effect != null)
+			    	_effect.CurrentTechnique.Passes[0].Apply();
 		}
 		
-        void CheckValid(Texture2D texture)
-        {
-            if (texture == null)
-                throw new ArgumentNullException("texture");
-            if (!_beginCalled)
-                throw new InvalidOperationException("Draw was called, but Begin has not yet been called. Begin must be called successfully before you can call Draw.");
-        }
+        	void CheckValid(Texture2D texture)
+        	{
+            		if (texture == null)
+                		throw new ArgumentNullException("texture");
+            		if (!_beginCalled)
+                		throw new InvalidOperationException("Draw was called, but Begin has not yet been called. Begin must be called successfully before you can call Draw.");
+        	}
 
-        void CheckValid(SpriteFont spriteFont, string text)
-        {
-            if (spriteFont == null)
-                throw new ArgumentNullException("spriteFont");
-            if (text == null)
-                throw new ArgumentNullException("text");
-            if (!_beginCalled)
-                throw new InvalidOperationException("DrawString was called, but Begin has not yet been called. Begin must be called successfully before you can call DrawString.");
-        }
+        	void CheckValid(SpriteFont spriteFont, string text)
+        	{
+            		if (spriteFont == null)
+                		throw new ArgumentNullException("spriteFont");
+            		if (text == null)
+                		throw new ArgumentNullException("text");
+            		if (!_beginCalled)
+               			 throw new InvalidOperationException("DrawString was called, but Begin has not yet been called. Begin must be called successfully before you can call DrawString.");
+        	}
 
-        void CheckValid(SpriteFont spriteFont, StringBuilder text)
-        {
-            if (spriteFont == null)
-                throw new ArgumentNullException("spriteFont");
-            if (text == null)
-                throw new ArgumentNullException("text");
-            if (!_beginCalled)
-                throw new InvalidOperationException("DrawString was called, but Begin has not yet been called. Begin must be called successfully before you can call DrawString.");
-        }
+        	void CheckValid(SpriteFont spriteFont, StringBuilder text)
+        	{
+            		if (spriteFont == null)
+	                	throw new ArgumentNullException("spriteFont");
+	            	if (text == null)
+	                	throw new ArgumentNullException("text");
+	           	 if (!_beginCalled)
+	                	throw new InvalidOperationException("DrawString was called, but Begin has not yet been called. Begin must be called successfully before you can call DrawString.");
+	        }
 
-		public void Draw (Texture2D texture,
-				Vector2 position,
-				Rectangle? sourceRectangle,
-				Color color,
-				float rotation,
-				Vector2 origin,
-				Vector2 scale,
-				SpriteEffects effect,
-				float depth)
+		public void Draw (
+			Texture2D texture,
+			Vector2 position,
+			Rectangle? sourceRectangle,
+			Color color,
+			float rotation,
+			Vector2 origin,
+			Vector2 scale,
+			SpriteEffects effect,
+			float depth
+		)
 		{
-            CheckValid(texture);
+            		CheckValid(texture);
 
-            var w = texture.Width * scale.X;
-            var h = texture.Height * scale.Y;
+	        	var w = texture.Width * scale.X;
+	        	var h = texture.Height * scale.Y;
 			if (sourceRectangle.HasValue)
-            {
+            		{
 				w = sourceRectangle.Value.Width*scale.X;
 				h = sourceRectangle.Value.Height*scale.Y;
 			}
 
-            DrawInternal(texture,
+            		DrawInternal(
+            			texture,
 				new Vector4(position.X, position.Y, w, h),
 				sourceRectangle,
 				color,
 				rotation,
 				origin * scale,
 				effect,
-				depth);
+				depth
+			);
 		}
 
-		public void Draw (Texture2D texture,
+		public void Draw (
+				Texture2D texture,
 				Vector2 position,
 				Rectangle? sourceRectangle,
 				Color color,
@@ -191,29 +196,33 @@ namespace Microsoft.Xna.Framework.Graphics
 				Vector2 origin,
 				float scale,
 				SpriteEffects effect,
-				float depth)
+				float depth
+			)
 		{
-            CheckValid(texture);
+            		CheckValid(texture);
 
-            var w = texture.Width * scale;
-            var h = texture.Height * scale;
-            if (sourceRectangle.HasValue)
-            {
-                w = sourceRectangle.Value.Width * scale;
-                h = sourceRectangle.Value.Height * scale;
-            }
+			var w = texture.Width * scale;
+			var h = texture.Height * scale;
+			if (sourceRectangle.HasValue)
+			{
+				w = sourceRectangle.Value.Width * scale;
+				h = sourceRectangle.Value.Height * scale;
+			}
 
-            DrawInternal(texture,
-                new Vector4(position.X, position.Y, w, h),
+            		DrawInternal(texture,
+	                new Vector4(
+	                	position.X, position.Y, w, h),
 				sourceRectangle,
 				color,
 				rotation,
 				origin * scale,
 				effect,
-				depth);
+				depth
+			);
 		}
 
-		public void Draw (Texture2D texture,
+		public void Draw (
+			Texture2D texture,
 			Rectangle destinationRectangle,
 			Rectangle? sourceRectangle,
 			Color color,
@@ -222,20 +231,22 @@ namespace Microsoft.Xna.Framework.Graphics
 			SpriteEffects effect,
 			float depth)
 		{
-            CheckValid(texture);
-
-            DrawInternal(texture,
-			      new Vector4(destinationRectangle.X,
-			                  destinationRectangle.Y,
-			                  destinationRectangle.Width,
-			                  destinationRectangle.Height),
-			      sourceRectangle,
-			      color,
-			      rotation,
-			      new Vector2(origin.X * ((float)destinationRectangle.Width / (float)( (sourceRectangle.HasValue && sourceRectangle.Value.Width != 0) ? sourceRectangle.Value.Width : texture.Width)),
-                        			origin.Y * ((float)destinationRectangle.Height) / (float)( (sourceRectangle.HasValue && sourceRectangle.Value.Height != 0) ? sourceRectangle.Value.Height : texture.Height)),
-			      effect,
-			      depth);
+			CheckValid(texture);
+			
+			DrawInternal(texture,
+			new Vector4(
+				destinationRectangle.X,
+				destinationRectangle.Y,
+				destinationRectangle.Width,
+				destinationRectangle.Height),
+				sourceRectangle,
+				color,
+				rotation,
+				new Vector2(origin.X * ((float)destinationRectangle.Width / (float)( (sourceRectangle.HasValue && sourceRectangle.Value.Width != 0) ? sourceRectangle.Value.Width : texture.Width)),
+				origin.Y * ((float)destinationRectangle.Height) / (float)( (sourceRectangle.HasValue && sourceRectangle.Value.Height != 0) ? sourceRectangle.Value.Height : texture.Height)),
+				effect,
+				depth
+			);
 		}
 
 		internal void DrawInternal (Texture2D texture,
@@ -267,30 +278,32 @@ namespace Microsoft.Xna.Framework.Graphics
 			_texCoordBR.Y = (_tempRect.Y + _tempRect.Height) / (float)texture.Height;
 
 			if ((effect & SpriteEffects.FlipVertically) != 0) {
-                var temp = _texCoordBR.Y;
+                		var temp = _texCoordBR.Y;
 				_texCoordBR.Y = _texCoordTL.Y;
 				_texCoordTL.Y = temp;
 			}
 			if ((effect & SpriteEffects.FlipHorizontally) != 0) {
-                var temp = _texCoordBR.X;
+                		var temp = _texCoordBR.X;
 				_texCoordBR.X = _texCoordTL.X;
 				_texCoordTL.X = temp;
 			}
 
-			item.Set (destinationRectangle.X,
-					destinationRectangle.Y, 
-					-origin.X, 
-					-origin.Y,
-					destinationRectangle.Z,
-					destinationRectangle.W,
-					(float)Math.Sin (rotation), 
-					(float)Math.Cos (rotation), 
-					color, 
-					_texCoordTL, 
-					_texCoordBR);			
+			item.Set (
+				destinationRectangle.X,
+				destinationRectangle.Y, 
+				-origin.X, 
+				-origin.Y,
+				destinationRectangle.Z,
+				destinationRectangle.W,
+				(float)Math.Sin (rotation), 
+				(float)Math.Cos (rotation), 
+				color, 
+				_texCoordTL, 
+				_texCoordBR
+			);			
 			
 			if (_sortMode == SpriteSortMode.Immediate)
-                _batcher.DrawBatch(_sortMode);
+                		_batcher.DrawBatch(_sortMode);
 		}
 
 		public void Draw (Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color)
@@ -315,78 +328,78 @@ namespace Microsoft.Xna.Framework.Graphics
 
 		public void DrawString (SpriteFont spriteFont, string text, Vector2 position, Color color)
 		{
-            CheckValid(spriteFont, text);
+           		 CheckValid(spriteFont, text);
 
-            var source = new SpriteFont.CharacterSource(text);
+            		var source = new SpriteFont.CharacterSource(text);
 			spriteFont.DrawInto (
-                this, ref source, position, color, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
+                		this, ref source, position, color, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
 		}
 
 		public void DrawString (
-			SpriteFont spriteFont, string text, Vector2 position, Color color,
-			float rotation, Vector2 origin, float scale, SpriteEffects effects, float depth)
+		SpriteFont spriteFont, string text, Vector2 position, Color color,
+		float rotation, Vector2 origin, float scale, SpriteEffects effects, float depth)
 		{
-            CheckValid(spriteFont, text);
-
+			CheckValid(spriteFont, text);
+		
 			var scaleVec = new Vector2(scale, scale);
-            var source = new SpriteFont.CharacterSource(text);
-            spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scaleVec, effects, depth);
+			var source = new SpriteFont.CharacterSource(text);
+			spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scaleVec, effects, depth);
 		}
-
+		
 		public void DrawString (
 			SpriteFont spriteFont, string text, Vector2 position, Color color,
 			float rotation, Vector2 origin, Vector2 scale, SpriteEffects effect, float depth)
 		{
-            CheckValid(spriteFont, text);
-
-            var source = new SpriteFont.CharacterSource(text);
-            spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scale, effect, depth);
+			CheckValid(spriteFont, text);
+			
+			var source = new SpriteFont.CharacterSource(text);
+			spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scale, effect, depth);
 		}
-
+		
 		public void DrawString (SpriteFont spriteFont, StringBuilder text, Vector2 position, Color color)
 		{
-            CheckValid(spriteFont, text);
-
-            var source = new SpriteFont.CharacterSource(text);
+			CheckValid(spriteFont, text);
+			
+			var source = new SpriteFont.CharacterSource(text);
 			spriteFont.DrawInto(this, ref source, position, color, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0f);
 		}
-
+		
 		public void DrawString (
 			SpriteFont spriteFont, StringBuilder text, Vector2 position, Color color,
 			float rotation, Vector2 origin, float scale, SpriteEffects effects, float depth)
 		{
-            CheckValid(spriteFont, text);
-
+			CheckValid(spriteFont, text);
+			
 			var scaleVec = new Vector2 (scale, scale);
-            var source = new SpriteFont.CharacterSource(text);
-            spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scaleVec, effects, depth);
+			var source = new SpriteFont.CharacterSource(text);
+			spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scaleVec, effects, depth);
 		}
-
+		
 		public void DrawString (
 			SpriteFont spriteFont, StringBuilder text, Vector2 position, Color color,
 			float rotation, Vector2 origin, Vector2 scale, SpriteEffects effect, float depth)
 		{
-            CheckValid(spriteFont, text);
-
-            var source = new SpriteFont.CharacterSource(text);
-            spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scale, effect, depth);
+			CheckValid(spriteFont, text);
+			
+			var source = new SpriteFont.CharacterSource(text);
+			spriteFont.DrawInto(this, ref source, position, color, rotation, origin, scale, effect, depth);
 		}
-
-        protected override void Dispose(bool disposing)
-        {
-            if (!IsDisposed)
-            {
-                if (disposing)
-                {
-                    if (_spriteEffect != null)
-                    {
-                        _spriteEffect.Dispose();
-                        _spriteEffect = null;
-                    }
-                }
-            }
-            base.Dispose(disposing);
-        }
+		
+		protected override void Dispose(bool disposing)
+		{
+			if (!IsDisposed)
+			{
+				if (disposing)
+				{
+					if (_spriteEffect != null)
+					{
+						_spriteEffect.Dispose();
+						_spriteEffect = null;
+					}
+				}
+			}
+			base.Dispose(disposing);
+		}
 	}
 }
 
