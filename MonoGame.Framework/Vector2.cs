d04c27c595e9aa3c7e94d529f51a180b02c6d928
Simon (darkside) Jackson <darkside@zenithmoon.com>
2013-04-09 12:17:16 +0100

    Changed the Serialization attributes on all files employing them to new DataContract Attributes including enabling the DataMember attributes where existing (no new additions, although these should be reviewed where ever DataContract is used)
    Also updated project files to include the System.Runtime.Serialisation namespace if not already existing as this is needed to reference the DataContract Attribute

--- MonoGame.Framework/Vector2.cs
+++ MonoGame.Framework/Vector2.cs
@@ -28,17 +28,11 @@ SOFTWARE.
 using System;
 using System.Text;
 using System.Globalization;
-#if WINRT
 using System.Runtime.Serialization;
-#endif
 
 namespace Microsoft.Xna.Framework
 {
-    #if WINRT
     [DataContract]
-    #else
-    [Serializable]
-    #endif
     public struct Vector2 : IEquatable<Vector2>
     {
         #region Private Fields
@@ -52,13 +46,11 @@ namespace Microsoft.Xna.Framework
 
 
         #region Public Fields
-#if WINRT
+      
         [DataMember]
-#endif
         public float X;
-#if WINRT
+        
         [DataMember]
-#endif
         public float Y;
 
         #endregion Public Fields
