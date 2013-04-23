9d955c2699aa6f3d0f723f5ada5fb1f5300c7982
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-04-23 18:59:00 +0100

    Added Data Contract attributes to types that did not have them previously

--- MonoGame.Framework/Graphics/Viewport.cs
+++ MonoGame.Framework/Graphics/Viewport.cs
@@ -58,7 +58,9 @@ namespace Microsoft.Xna.Framework.Graphics
 		private float maxDepth;
 		
 		#region Properties
-		public int Height {
+        [DataMember]
+        public int Height
+        {
 			get {
 				return this.height;
 			}
@@ -67,7 +69,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
-		public float MaxDepth {
+        [DataMember]
+        public float MaxDepth
+        {
 			get {
 				return this.maxDepth;
 			}
@@ -76,7 +80,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
-		public float MinDepth {
+        [DataMember]
+        public float MinDepth
+        {
 			get {
 				return this.minDepth;
 			}
@@ -85,7 +91,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
-		public int Width {
+        [DataMember]
+        public int Width
+        {
 			get {
 				return this.width;
 			}
@@ -94,7 +102,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 		}
 
-		public int Y {
+        [DataMember]
+        public int Y
+        {
 			get {
 				return this.y;
 
@@ -103,7 +113,9 @@ namespace Microsoft.Xna.Framework.Graphics
 				y = value;
 			}
 		}
-		public int X 
+
+        [DataMember]
+        public int X 
 		{
 			get{ return x;}
 			set{ x = value;}
