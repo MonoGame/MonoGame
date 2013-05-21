25c9f21ca4b2f4a2db634b04e473a2465d8f5f76
kjpou1 <kjpou@pt.lu>
2013-05-21 06:48:22 +0200

    Use PresentationParameters.RenderTargetUsage to control back buffer clearing or not.

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -1623,6 +1623,7 @@ namespace Microsoft.Xna.Framework.Graphics
 #elif PSM
                 _graphics.SetFrameBuffer(_graphics.Screen);
 #endif
+                clearTarget = PresentationParameters.RenderTargetUsage == RenderTargetUsage.DiscardContents;
 
                 Viewport = new Viewport(0, 0,
 					PresentationParameters.BackBufferWidth, 
