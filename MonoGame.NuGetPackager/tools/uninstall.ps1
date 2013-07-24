bdd5f62ac2a594934479c6622510a50d8eb3f41e
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-07-24 18:17:45 +0100

    Initial check in of NuGet packager for MonoGame

---
+++ MonoGame.NuGetPackager/tools/uninstall.ps1
@@ -0,0 +1,8 @@
+﻿# Runs every time a package is uninstalled
+
+param($installPath, $toolsPath, $package, $project)
+
+# $installPath is the path to the folder where the package is installed.
+# $toolsPath is the path to the tools directory in the folder where the package is installed.
+# $package is a reference to the package object.
+# $project is a reference to the project the package was installed to.
