e02e6b52deb5938e384ce09deda1b4ea827d9e22
Shmuel Englard <shmueli.yosef@englard.net>
2013-08-07 08:50:12 -0400

    Added ability to load mage where image is just byte array

--- MonoGame.Framework/AGENT/ContentManager.cs
+++ MonoGame.Framework/AGENT/ContentManager.cs
@@ -16,7 +16,25 @@ namespace Microsoft.Xna.Framework
 
         public Texture2D LoadTexture(Enum name)
         {
-            Bitmap bitmap = (Bitmap)ResourceUtility.GetObject(manager, name);
+            Bitmap bitmap = null;
+            object data = ResourceUtility.GetObject(manager, name);
+            string dataType = data.GetType().AssemblyQualifiedName;
+            string bitmapType = typeof(Bitmap).AssemblyQualifiedName;
+            if (dataType == bitmapType)
+            {
+                bitmap = (Bitmap)data;
+            }
+            else
+            {
+                try
+                {
+                    bitmap = new Bitmap((byte[])data, Bitmap.BitmapImageType.Bmp);
+                }
+                catch
+                {
+                    bitmap = new Bitmap((byte[])data, Bitmap.BitmapImageType.Jpeg);
+                }
+            }
             Texture2D texture2D = new Texture2D();
             texture2D.Bitmap = bitmap;
             return texture2D;
