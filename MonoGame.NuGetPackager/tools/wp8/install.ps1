0cf4df4efec281bccd49df19aed08f49b07359cc
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-08-13 16:45:01 +0100

    1st round of WP tests

--- MonoGame.NuGetPackager/tools/wp8/install.ps1
+++
@@ -1,8 +0,0 @@
-﻿# Runs every time a package is installed in a project
-
-param($installPath, $toolsPath, $package, $project)
-
-# $installPath is the path to the folder where the package is installed.
-# $toolsPath is the path to the tools directory in the folder where the package is installed.
-# $package is a reference to the package object.
-# $project is a reference to the project the package was installed to.
