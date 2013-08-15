c20cd9c5098137435d887f02e7c7cbb2a8ea0f75
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-08-15 21:46:28 +1000

    Added shell script to determine definition file contents based on C# projects.

---
+++ MonoGame.Framework/find-platform-dependent-files.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+CSFILES=$(find ./ -name \*.cs | sort)
+PLATFORMS="Android iOS Linux MacOS Ouya PSMobile Windows8 Windows WindowsGL WindowsPhone"
+
+for ocs in $CSFILES; do
+  ocs=${ocs:2}
+  cs=$ocs
+  is=$ocs
+  while [[ "$cs" == *"/"* ]]; do
+    cs=${cs/\//\\\\}
+  done
+  while [[ "$is" == *"/"* ]]; do
+    is=${is/\//\\}
+  done
+  notin=""
+  for platform in $PLATFORMS; do
+    result=$(grep "\"$cs" MonoGame.Framework.$platform.csproj)
+    if [ "$result" == "" ]; then
+      notin="$notin$platform "
+    fi
+  done
+  if [ "$notin" == "" ]; then
+    echo "<Compile Include=\"${is}\" />"
+  else
+    platresult=""
+    for platform in $PLATFORMS; do
+      if [[ "$notin" == *"$platform"* ]]; then
+        # do nothing
+        true
+      else
+        if [ "$platresult" == "" ]; then
+          platresult="$platform"
+        else
+          platresult="$platresult,$platform"
+        fi
+      fi
+    done
+    echo "<Compile Include=\"${is}\">"
+    echo "  <Platforms>$platresult</Platforms>"
+    echo "</Compile>"
+  fi
+done
