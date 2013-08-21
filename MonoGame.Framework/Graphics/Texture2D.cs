3773837d81dcedb28195bc43647f27c8c029e05b
RC Game <rc.game.studio@gmail.com>
2013-08-21 12:27:01 +1000

    Implement Texture2D.FromStream() for Windows Phone 8

--- MonoGame.Framework/Graphics/Texture2D.cs
+++ MonoGame.Framework/Graphics/Texture2D.cs
@@ -702,7 +702,7 @@ namespace Microsoft.Xna.Framework.Graphics
 		{
 			this.GetData(0, null, data, 0, data.Length);
 		}
-		
+
 		public static Texture2D FromStream(GraphicsDevice graphicsDevice, Stream stream)
 		{
             //todo: partial classes would be cleaner
@@ -791,8 +791,17 @@ namespace Microsoft.Xna.Framework.Graphics
                 return texture;
             }
 #elif WINDOWS_PHONE
-            throw new NotImplementedException();
-
+            var writableBitmap = Microsoft.Phone.PictureDecoder.DecodeJpeg(stream);
+            // Convert from ARGB to ABGR
+            int[] pixels = writableBitmap.Pixels;
+            for (int i = 0; i < writableBitmap.PixelWidth * writableBitmap.PixelHeight; ++i)
+            {
+                uint pixel = (uint)pixels[i];
+                pixels[i] = (int)((pixel & 0xFF00FF00) | ((pixel & 0x00FF0000) >> 16) | ((pixel & 0x000000FF) << 16));
+            }
+            Texture2D texture = new Texture2D(graphicsDevice, writableBitmap.PixelWidth, writableBitmap.PixelHeight, false, SurfaceFormat.Color);
+            texture.SetData<int>(pixels);
+            return texture;
 #elif WINDOWS_STOREAPP || DIRECTX
 
             // For reference this implementation was ultimately found through this post:
