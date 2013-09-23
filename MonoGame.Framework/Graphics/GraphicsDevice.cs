ebc5e960955de44d51544a3efad4b8562492a772
Paul Cunningham <paul.cunningham@uk.medgate.com>
2013-09-23 13:54:20 +0100

    Some fixes to PSM rendering
    Added TexCoord to BasicEffect shaders
    Removed default BlendState for BasicEffect pass
    Renamed reserved word Texture effect parameter in BasicEffect
    Look for EXACT vertex buffer match (vertex and index array sizes must match request)

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -2036,12 +2036,14 @@ namespace Microsoft.Xna.Framework.Graphics
                 }
 #endif
             }
-
+   
+#if !PSM
             if (_vertexShader == null)
                 throw new InvalidOperationException("A vertex shader must be set!");
             if (_pixelShader == null)
-                throw new InvalidOperationException("A pixel shader must not set!");
-
+                throw new InvalidOperationException("A pixel shader must be set!");
+#endif
+            
 #if DIRECTX 
 
             if (_vertexShaderDirty)
@@ -2226,6 +2228,7 @@ namespace Microsoft.Xna.Framework.Graphics
             GraphicsExtensions.CheckGLError();
 #elif PSM
             BindVertexBuffer(true);
+            ApplyState(true);
             _graphics.DrawArrays(PSSHelper.ToDrawMode(primitiveType), startIndex, GetElementCountArray(primitiveType, primitiveCount));
 #endif
         }
@@ -2447,11 +2450,11 @@ namespace Microsoft.Xna.Framework.Graphics
                 var buf = _availableVertexBuffers[i];
                 
 #region Check there is enough space
-                if (buf.VertexCount < requiredVertexLength)
+                if (buf.VertexCount != requiredVertexLength)
                     continue;
                 if (requiredIndexLength == 0 && buf.IndexCount != 0)
                     continue;
-                if (requiredIndexLength > 0 && buf.IndexCount < requiredIndexLength)
+                if (requiredIndexLength > 0 && buf.IndexCount != requiredIndexLength)
                     continue;
 #endregion
                 
@@ -2484,7 +2487,7 @@ namespace Microsoft.Xna.Framework.Graphics
             
             if (bestMatch != null)
             {
-                _availableVertexBuffers.RemoveAt(bestMatchIndex);
+                return bestMatch;
             }
             else
             {
