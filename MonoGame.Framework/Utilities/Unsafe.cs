bff309553790a1365ec2530cdcc2c992f5e4dd61
K. Gadd <kg@luminance.org>
2013-09-24 23:20:01 -0700

    .NET 3.5 unsafe pointer fallback for PSM (PR #2030)

--- MonoGame.Framework/Utilities/Unsafe.cs
+++ MonoGame.Framework/Utilities/Unsafe.cs
@@ -7,9 +7,17 @@ using System.Text;
 namespace Microsoft.Xna.Framework.Internal {
     public static class UnsafeExtensions {
         public static IntPtr AddressWithOffset (this GCHandle handle, int offsetInBytes) {
+#if PSM
+            // HACK: PSM doesn't support .NET 4
+            var address = handle.AddrOfPinnedObject().ToInt64();
+            // FIXME: Will this work if the Int64 form of the pointer is negative?
+            address += offsetInBytes;
+            return (IntPtr)address;
+#else
             var address = handle.AddrOfPinnedObject();
             address += offsetInBytes;
             return address;
+#endif
         }
     }
 }
