fb16e1686a4e49c8a981289da01dc8739ad6767b
Dean Ellis <dellis1972@googlemail.com>
2013-09-27 17:56:06 +0100

    A few fixes for the ContentWriter

--- MonoGame.Framework.Content.Pipeline/Serialization/Compiler/ContentWriter.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Compiler/ContentWriter.cs
@@ -294,10 +294,15 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
         /// <remarks>This method can be called recursively with a null value.</remarks>
         public void WriteObject<T>(T value)
         {
-            if (value == null)
-                Write7BitEncodedInt(0);
-            else
-                WriteObject<T>(value, GetTypeWriter(value.GetType()));
+		if (value == null)
+			Write7BitEncodedInt (0);
+		else {
+			var elementWriter =  GetTypeWriter (value.GetType ());
+			var index = typeWriterMap[elementWriter.GetType ()];
+			// Because zero means null object, we add one to the index before writing it to the file
+			Write7BitEncodedInt (index + 1);
+			WriteObject<T> (value, elementWriter);
+		}
         }
 
         /// <summary>
@@ -322,15 +327,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
             }
             else
             {
-                Type objectType = typeof (T);
-                if (objectType.IsValueType) {
-                    typeWriter.Write (this, value);
-                } else {
-                    var index = typeWriterMap[typeWriter.GetType ()];
-                    // Because zero means null object, we add one to the index before writing it to the file
-                    Write7BitEncodedInt (index + 1);
-                    typeWriter.Write (this, value);
-                }
+                typeWriter.Write (this, value);
             }
         }
 
@@ -393,7 +390,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
             }
         }
 
-        /// <summary>
+	/// <summary>
         /// Writes a Color value.
         /// </summary>
         /// <param name="value">Value of a color using Red, Green, Blue, and Alpha values to write.</param>
@@ -479,5 +476,13 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
             Write(value.Center);
             Write(value.Radius);
         }
+
+	internal void Write (Rectangle value)
+	{
+		Write (value.X);
+		Write (value.Y);
+		Write (value.Width);
+		Write (value.Height);
+	}
     }
 }
