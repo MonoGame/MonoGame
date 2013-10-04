0ca73852215b7e45b00b1bfd5bfbf5949b71656c
David Leaver <danzel@localhost.geek.nz>
2013-10-05 09:55:42 +1300

    Make SoundEffect.Play not throw an exception on iOS if it fails to play, return false instead as we are meant to.

--- MonoGame.Framework/Desktop/Audio/SoundEffectInstance.cs
+++ MonoGame.Framework/Desktop/Audio/SoundEffectInstance.cs
@@ -287,22 +287,32 @@ namespace Microsoft.Xna.Framework.Audio
         /// </summary>
 		public virtual void Play ()
 		{
-			if (hasSourceId) {
-				return;
-			}
-			bool isSourceAvailable = controller.ReserveSource (soundBuffer);
-            if (!isSourceAvailable)
+            if (!TryPlay())
                 throw new InstancePlayLimitException();
+		}
+
+        /// <summary>
+        /// Internal implementation of Play that returns false on failure. See comments on Play for workings.
+        /// </summary>
+        internal bool TryPlay()
+        {
+            if (hasSourceId) {
+                return true;
+            }
+            bool isSourceAvailable = controller.ReserveSource (soundBuffer);
+            if (!isSourceAvailable)
+                return false;
 
             int bufferId = soundBuffer.OpenALDataBuffer;
             AL.Source(soundBuffer.SourceId, ALSourcei.Buffer, bufferId);
-			ApplyState ();
+            ApplyState ();
 
-			controller.PlaySound (soundBuffer);            
-			//Console.WriteLine ("playing: " + sourceId + " : " + soundEffect.Name);
-			soundState = SoundState.Playing;
-		}
+            controller.PlaySound (soundBuffer);            
+            //Console.WriteLine ("playing: " + sourceId + " : " + soundEffect.Name);
+            soundState = SoundState.Playing;
 
+            return true;
+        }
         /// <summary>
         /// When the sound state is paused, and the source is available, then the sound
         /// is played using the ResumeSound method from the OpenALSoundController. Otherwise,
