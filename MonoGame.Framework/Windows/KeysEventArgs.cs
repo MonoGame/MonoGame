c3d212cf60ed517a45a7e1ede4f23cdb33890178
John Beshir <john@beshir.org>
2013-11-19 12:34:08 +0000

    Add missed file used in text input on Windows.

---
+++ MonoGame.Framework/Windows/KeysEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Microsoft.Xna.Framework
+{
+    // Amendment to OpenTK code to allow us to use our existing key processing logic (with a few slight alterations).
+    public class KeysEventArgs : EventArgs
+    {
+        public Keys Keys { get; private set; }
+
+        public KeysEventArgs(Microsoft.Xna.Framework.Input.Keys keys)
+        {
+            this.Keys = keys;
+        }
+    }
+}
