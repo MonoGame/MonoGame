fb04dbcdaea2e0c1a1b65bb163dc2f32075bbd3e
Tom Spilman <tom@sickheadgames.com>
2014-01-19 00:00:56 -0600

    Added tests for NormalizedByte4.

---
+++ Test/Framework/PackedVectorTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Framework
+{
+    class PackedVectorTest
+    {
+        [Test]
+        public void NormalizedByte4()
+        {
+            Assert.AreEqual(0x0, new NormalizedByte4(Vector4.Zero).PackedValue);
+            Assert.AreEqual(0x7F7F7F7F, new NormalizedByte4(Vector4.One).PackedValue);
+            Assert.AreEqual(0x81818181, new NormalizedByte4(-Vector4.One).PackedValue);
+
+            Assert.AreEqual(Vector4.One, new NormalizedByte4(Vector4.One).ToVector4());
+            Assert.AreEqual(Vector4.Zero, new NormalizedByte4(Vector4.Zero).ToVector4());
+            Assert.AreEqual(-Vector4.One, new NormalizedByte4(-Vector4.One).ToVector4());
+            Assert.AreEqual(Vector4.One, new NormalizedByte4(Vector4.One * 1234.0f).ToVector4());
+            Assert.AreEqual(-Vector4.One, new NormalizedByte4(Vector4.One * -1234.0f).ToVector4());
+        }
+    }
+}
