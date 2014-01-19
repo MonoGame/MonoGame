e5205360ebc582ed786a57d2190304ffc475b8b9
Tom Spilman <tom@sickheadgames.com>
2014-01-19 00:32:30 -0600

    Implemented NormalizedByte2.

--- Test/Framework/PackedVectorTest.cs
+++ Test/Framework/PackedVectorTest.cs
@@ -19,5 +19,22 @@ namespace MonoGame.Tests.Framework
             Assert.AreEqual(Vector4.One, new NormalizedByte4(Vector4.One * 1234.0f).ToVector4());
             Assert.AreEqual(-Vector4.One, new NormalizedByte4(Vector4.One * -1234.0f).ToVector4());
         }
+
+        [Test]
+        public void NormalizedByte2()
+        {
+            Assert.AreEqual(0x0, new NormalizedByte2(Vector2.Zero).PackedValue);
+            Assert.AreEqual(0x7F7F, new NormalizedByte2(Vector2.One).PackedValue);
+            Assert.AreEqual(0x8181, new NormalizedByte2(-Vector2.One).PackedValue);
+
+            Assert.AreEqual(Vector2.One, new NormalizedByte2(Vector2.One).ToVector2());
+            Assert.AreEqual(Vector2.Zero, new NormalizedByte2(Vector2.Zero).ToVector2());
+            Assert.AreEqual(-Vector2.One, new NormalizedByte2(-Vector2.One).ToVector2());
+            Assert.AreEqual(Vector2.One, new NormalizedByte2(Vector2.One * 1234.0f).ToVector2());
+            Assert.AreEqual(-Vector2.One, new NormalizedByte2(Vector2.One * -1234.0f).ToVector2());
+
+            Assert.AreEqual(new Vector4(1,1,0,1), ((IPackedVector)new NormalizedByte2(Vector2.One)).ToVector4());
+            Assert.AreEqual(new Vector4(0,0,0,1), ((IPackedVector)new NormalizedByte2(Vector2.Zero)).ToVector4());
+        }
     }
 }
