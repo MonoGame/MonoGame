3329a68339ff7bbdfeea2d490ce7603c3a4aedfc
Tom Spilman <tom@sickheadgames.com>
2014-01-20 03:26:53 -0600

    We now try to build a full path for assembly references.

--- Tools/MGCB/BuildContent.cs
+++ Tools/MGCB/BuildContent.cs
@@ -168,7 +168,12 @@ namespace MGCB
             // Feed all the assembly references to the pipeline manager
             // so it can resolve importers, processors, writers, and types.
             foreach (var r in References)
-                _manager.AddAssembly(r);
+            {
+                var assembly = r;
+                if (!Path.IsPathRooted(assembly))
+                    assembly = Path.GetFullPath(Path.Combine(projectDirectory, assembly));
+                _manager.AddAssembly(assembly);
+            }
 
             // Load the previously serialized list of built content.
             var contentFile = Path.Combine(intermediatePath, PipelineBuildEvent.Extension);
