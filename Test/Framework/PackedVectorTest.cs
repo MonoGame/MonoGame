34aac611964678d2482cbdb6aeeec58dd9c45daa
Tom Spilman <tom@sickheadgames.com>
2014-01-21 02:27:11 -0600

    Added NormalizedShort4 and NormalizedShort2 to test suite.

--- Test/Framework/PackedVectorTest.cs
+++ Test/Framework/PackedVectorTest.cs
@@ -36,5 +36,36 @@ namespace MonoGame.Tests.Framework
             Assert.AreEqual(new Vector4(1,1,0,1), ((IPackedVector)new NormalizedByte2(Vector2.One)).ToVector4());
             Assert.AreEqual(new Vector4(0,0,0,1), ((IPackedVector)new NormalizedByte2(Vector2.Zero)).ToVector4());
         }
+
+        [Test]
+        public void NormalizedShort4()
+        {
+            Assert.AreEqual(0x0, new NormalizedShort4(Vector4.Zero).PackedValue);
+            Assert.AreEqual(0x7FFF7FFF7FFF7FFF, new NormalizedShort4(Vector4.One).PackedValue);
+            Assert.AreEqual(0x8001800180018001, new NormalizedShort4(-Vector4.One).PackedValue);
+
+            Assert.AreEqual(Vector4.One, new NormalizedShort4(Vector4.One).ToVector4());
+            Assert.AreEqual(Vector4.Zero, new NormalizedShort4(Vector4.Zero).ToVector4());
+            Assert.AreEqual(-Vector4.One, new NormalizedShort4(-Vector4.One).ToVector4());
+            Assert.AreEqual(Vector4.One, new NormalizedShort4(Vector4.One * 1234.0f).ToVector4());
+            Assert.AreEqual(-Vector4.One, new NormalizedShort4(Vector4.One * -1234.0f).ToVector4());
+        }
+
+        [Test]
+        public void NormalizedShort2()
+        {
+            Assert.AreEqual(0x0, new NormalizedShort2(Vector2.Zero).PackedValue);
+            Assert.AreEqual(0x7FFF7FFF, new NormalizedShort2(Vector2.One).PackedValue);
+            Assert.AreEqual(0x80018001, new NormalizedShort2(-Vector2.One).PackedValue);
+
+            Assert.AreEqual(Vector2.One, new NormalizedShort2(Vector2.One).ToVector2());
+            Assert.AreEqual(Vector2.Zero, new NormalizedShort2(Vector2.Zero).ToVector2());
+            Assert.AreEqual(-Vector2.One, new NormalizedShort2(-Vector2.One).ToVector2());
+            Assert.AreEqual(Vector2.One, new NormalizedShort2(Vector2.One * 1234.0f).ToVector2());
+            Assert.AreEqual(-Vector2.One, new NormalizedShort2(Vector2.One * -1234.0f).ToVector2());
+
+            Assert.AreEqual(new Vector4(1, 1, 0, 1), ((IPackedVector)new NormalizedShort2(Vector2.One)).ToVector4());
+            Assert.AreEqual(new Vector4(0, 0, 0, 1), ((IPackedVector)new NormalizedShort2(Vector2.Zero)).ToVector4());
+        }
     }
 }
