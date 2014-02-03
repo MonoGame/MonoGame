2cc09e5f7a0ea8da4aa2a7192c7e994901d264d8
James Lupiani <lupiani@sickheadgames.com>
2014-02-03 16:32:50 -0600

    Flattened inheritance chain.

--- MonoGame.Framework.Content.Pipeline/FbxImporter.cs
+++ MonoGame.Framework.Content.Pipeline/FbxImporter.cs
@@ -2,11 +2,6 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Assimp;
-using Assimp.Configs;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline
@@ -15,11 +10,12 @@ namespace Microsoft.Xna.Framework.Content.Pipeline
     /// Provides methods for reading AutoDesk (.fbx) files for use in the Content Pipeline.
     /// </summary>
     [ContentImporter(".fbx", DisplayName = "Fbx Importer - MonoGame", DefaultProcessor = "ModelProcessor")]
-    public class FbxImporter : OpenAssetImporter
+    public class FbxImporter : ContentImporter<NodeContent>
     {
         public override NodeContent Import(string filename, ContentImporterContext context)
         {
-            return base.Import(filename, context);
+            var importer = new OpenAssetImporter();
+            return importer.Import(filename, context);
         }
     }
 }
