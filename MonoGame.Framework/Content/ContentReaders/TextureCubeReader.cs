adaa12cc909ec1525875d652003b4f1eef0e679f
Nikos Kastellanos <nkastellanos@gmail.com>
2014-03-02 19:54:13 +0200

    TextureCubeReader ignore existingInstance
    TextureCubeReader ignore existingInstance. As a result of this on WP8
    when the app is deactivated/activated the ReloadAsset fails to reload
    the Cube.
    
    I applied the same fix to Texture3DReader although I didn't test it. The
    fix is quite simple and should work.
    
    I tried to make the code resemble as much the style of the
    Texture3DReader.Read(...)

--- MonoGame.Framework/Content/ContentReaders/TextureCubeReader.cs
+++ MonoGame.Framework/Content/ContentReaders/TextureCubeReader.cs
@@ -9,17 +9,27 @@ namespace Microsoft.Xna.Framework.Content
 		
 		protected internal override TextureCube Read(ContentReader reader, TextureCube existingInstance)
 		{
-			SurfaceFormat surfaceFormat = (SurfaceFormat)reader.ReadInt32 ();
+            TextureCube textureCube = null;
+
+			SurfaceFormat surfaceFormat = (SurfaceFormat)reader.ReadInt32();
 			int size = reader.ReadInt32 ();
 			int levels = reader.ReadInt32 ();
-			TextureCube textureCube = new TextureCube(reader.GraphicsDevice, size, levels > 1, surfaceFormat);
-			for (int face = 0; face < 6; face++) {
-				for (int i=0; i<levels; i++) {
+
+            if (existingInstance == null)
+                textureCube = new TextureCube(reader.GraphicsDevice, size, levels > 1, surfaceFormat);
+            else
+                textureCube = existingInstance;
+
+			for (int face = 0; face < 6; face++) 
+            {
+				for (int i=0; i<levels; i++) 
+                {
 					int faceSize = reader.ReadInt32();
 					byte[] faceData = reader.ReadBytes(faceSize);
 					textureCube.SetData<byte>((CubeMapFace)face, i, null, faceData, 0, faceSize);
 				}
 			}
+
 			return textureCube;
 		}
 	}
