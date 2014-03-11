e8b196c002f0033ce201d14414475a59553169bf
Nezz <bloodsource@vipmail.hu>
2014-03-11 01:54:07 +0100

    Fixed measure string to return correct width if spacing is used

--- MonoGame.Framework/Graphics/SpriteFont.cs
+++ MonoGame.Framework/Graphics/SpriteFont.cs
@@ -239,7 +239,7 @@ namespace Microsoft.Xna.Framework.Graphics
                     hasCurrentGlyph = true;                        
                 }
 
-                var proposedWidth = offset.X + currentGlyph.WidthIncludingBearings;
+                var proposedWidth = offset.X + currentGlyph.WidthIncludingBearings + Spacing;
                 if (proposedWidth > width)
                     width = proposedWidth;
 
