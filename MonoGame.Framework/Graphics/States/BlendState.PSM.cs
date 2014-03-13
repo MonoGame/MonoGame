0818cf740dae9757ba9842956fec5308fc92f085
James Lupiani <lupiani@sickheadgames.com>
2014-03-13 15:59:21 -0500

    PSM build fix

--- MonoGame.Framework/Graphics/States/BlendState.PSM.cs
+++ MonoGame.Framework/Graphics/States/BlendState.PSM.cs
@@ -12,41 +12,13 @@ namespace Microsoft.Xna.Framework.Graphics
     {
         internal void PlatformApplyState(GraphicsDevice device)
         {
-            var blendEnabled = !(this.ColorSourceBlend == Blend.One && 
-                                 this.ColorDestinationBlend == Blend.Zero &&
-                                 this.AlphaSourceBlend == Blend.One &&
-                                 this.AlphaDestinationBlend == Blend.Zero);
-            if (blendEnabled)
-                GL.Enable(EnableCap.Blend);
-            else
-                GL.Disable(EnableCap.Blend);
-            GraphicsExtensions.CheckGLError();
-
-            GL.BlendColor(
-                this.BlendFactor.R / 255.0f,      
-                this.BlendFactor.G / 255.0f, 
-                this.BlendFactor.B / 255.0f, 
-                this.BlendFactor.A / 255.0f);
-            GraphicsExtensions.CheckGLError();
-
-            GL.BlendEquationSeparate(
-                this.ColorBlendFunction.GetBlendEquationMode(),
-                this.AlphaBlendFunction.GetBlendEquationMode());
-            GraphicsExtensions.CheckGLError();
-
-            GL.BlendFuncSeparate(
-                this.ColorSourceBlend.GetBlendFactorSrc(), 
-                this.ColorDestinationBlend.GetBlendFactorDest(), 
-                this.AlphaSourceBlend.GetBlendFactorSrc(), 
-                this.AlphaDestinationBlend.GetBlendFactorDest());
-            GraphicsExtensions.CheckGLError();
-
-            GL.ColorMask(
-                (this.ColorWriteChannels & ColorWriteChannels.Red) != 0,
-                (this.ColorWriteChannels & ColorWriteChannels.Green) != 0,
-                (this.ColorWriteChannels & ColorWriteChannels.Blue) != 0,
-                (this.ColorWriteChannels & ColorWriteChannels.Alpha) != 0);
-            GraphicsExtensions.CheckGLError();
+            device._graphics.Enable(EnableMode.Blend);
+            device._graphics.SetBlendFuncAlpha(PSSHelper.ToBlendFuncMode(device.BlendState.AlphaBlendFunction),
+                                          PSSHelper.ToBlendFuncFactor(device.BlendState.AlphaSourceBlend),
+                                          PSSHelper.ToBlendFuncFactor(device.BlendState.AlphaDestinationBlend));
+            device._graphics.SetBlendFuncRgb(PSSHelper.ToBlendFuncMode(device.BlendState.ColorBlendFunction),
+                                          PSSHelper.ToBlendFuncFactor(device.BlendState.ColorSourceBlend),
+                                          PSSHelper.ToBlendFuncFactor(device.BlendState.ColorDestinationBlend));
         }
 
         private void PlatformDispose()
