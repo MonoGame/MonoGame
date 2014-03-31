8652eae63e2ed24c2eedd4131d7367ae58d07f2f
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2014-03-31 12:07:19 -0300

    Fixes SoundEffect/SoundEffectInstance - Android

--- MonoGame.Framework/Content/ContentManager.cs
+++ MonoGame.Framework/Content/ContentManager.cs
@@ -408,8 +408,12 @@ namespace Microsoft.Xna.Framework.Content
             }
             else if ((typeof(T) == typeof(SoundEffect)))
             {
+#if ANDROID
+                return new SoundEffect(assetName);
+#else
                 using (Stream s = TitleContainer.OpenStream(assetName))
                     return SoundEffect.FromStream(s);
+#endif
             }
             else if ((typeof(T) == typeof(Video)))
             {
