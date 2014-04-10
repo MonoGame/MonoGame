c8d59d306664d401d8a9cab89f7a589d2677ba8f
Tom Spilman <tom@sickheadgames.com>
2014-04-10 02:06:06 -0500

    Moved EffectObject to TwoMGFX namespace. Moved HLSL compile step to CompileHLSL. Cleaned up a bunch of redundant namespace qualifications. Added placeholder for CompilePSSL.

--- Tools/2MGFX/EffectObject.hlsl.cs
+++ Tools/2MGFX/EffectObject.hlsl.cs
@@ -1,272 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace Microsoft.Xna.Framework.Graphics
+namespace TwoMGFX
 {
     partial class EffectObject
     {
-        private EffectObject()
+        private static byte[] CompileHLSL(ShaderInfo shaderInfo, string shaderFunction, string shaderProfile)
         {
-        }
-
-        static public EffectObject FromShaderInfo(TwoMGFX.ShaderInfo shaderInfo)
-        {
-            var effect = new EffectObject();
-
-            // These are filled out as we process stuff.
-            effect.ConstantBuffers = new List<ConstantBufferData>();
-            effect.Shaders = new List<ShaderData>();
-
-            // Go thru the techniques and that will find all the 
-            // shaders and constant buffers.
-            effect.Techniques = new d3dx_technique[shaderInfo.Techniques.Count];
-            for (var t = 0; t < shaderInfo.Techniques.Count; t++)
+            SharpDX.D3DCompiler.ShaderBytecode shaderByteCode;
+            try
             {
-                var tinfo = shaderInfo.Techniques[t]; ;
+                SharpDX.D3DCompiler.ShaderFlags shaderFlags = 0;
 
-                var technique = new d3dx_technique();
-                technique.name = tinfo.name;
-                technique.pass_count = (uint)tinfo.Passes.Count;
-                technique.pass_handles = new d3dx_pass[tinfo.Passes.Count];
+                // While we never allow preshaders, this flag is invalid for
+                // the DX11 shader compiler which doesn't allow preshaders
+                // in the first place.
+                //shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.NoPreshader;
 
-                for (var p = 0; p < tinfo.Passes.Count; p++)
+                if (shaderInfo.Profile == ShaderProfile.DirectX_11)
+                    shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.EnableBackwardsCompatibility;
+
+                if (shaderInfo.Debug)
                 {
-                    var pinfo = tinfo.Passes[p];
-
-                    var pass = new d3dx_pass();
-                    pass.name = pinfo.name ?? string.Empty;
-
-					pass.blendState = pinfo.blendState;
-					pass.depthStencilState = pinfo.depthStencilState;
-					pass.rasterizerState = pinfo.rasterizerState;
-
-                    pass.state_count = 0;
-                    var tempstate = new d3dx_state[2];
-
-                    pinfo.ValidateShaderModels(shaderInfo.Profile);
-
-                    if (!string.IsNullOrEmpty(pinfo.psFunction))
-                    {
-                        pass.state_count += 1;
-                        tempstate[pass.state_count -1] = effect.CreateShader(shaderInfo, pinfo.psFunction, pinfo.psModel, false);
-                    }
-
-                    if (!string.IsNullOrEmpty(pinfo.vsFunction))
-                    {
-                        pass.state_count += 1;
-                        tempstate[pass.state_count - 1] = effect.CreateShader(shaderInfo, pinfo.vsFunction, pinfo.vsModel, true);
-                    }
-
-                    pass.states = new d3dx_state[pass.state_count];
-                    for (var s = 0; s < pass.state_count; s++)
-                        pass.states[s] = tempstate[s];
-
-                    technique.pass_handles[p] = pass;
+                    shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.SkipOptimization;
+                    shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.Debug;
+                }
+                else
+                {
+                    shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.OptimizationLevel3;
                 }
 
-                effect.Techniques[t] = technique;
+                // Compile the shader into bytecode.                
+                var result = SharpDX.D3DCompiler.ShaderBytecode.Compile(
+                    shaderInfo.fileContent,
+                    shaderFunction,
+                    shaderProfile,
+                    shaderFlags,
+                    0,
+                    null,
+                    null,
+                    shaderInfo.fileName);
+
+                if (result.HasErrors)
+                    throw new Exception(result.Message);
+
+                shaderByteCode = result.Bytecode;
+                //var source = shaderByteCode.Disassemble();
             }
-                
-            // Make the list of parameters by combining all the
-            // constant buffers ignoring the buffer offsets.
-            var parameters = new List<d3dx_parameter>();
-            for (var c = 0; c < effect.ConstantBuffers.Count; c++ )
+            catch (Exception ex)
             {
-                var cb = effect.ConstantBuffers[c];
-
-                for (var i = 0; i < cb.Parameters.Count; i++ )
-                {
-                    var param = cb.Parameters[i];
-
-                    var match = parameters.FindIndex(e => e.name == param.name);
-                    if (match == -1)
-                    {
-                        cb.ParameterIndex.Add(parameters.Count);
-                        parameters.Add(param);
-                    }
-                    else
-                    {
-                        // TODO: Make sure the type and size of 
-                        // the parameter match up!
-                        cb.ParameterIndex.Add(match);
-                    }
-                }
+                throw ex;
             }
 
-            // Add the texture parameters from the samplers.
-            foreach (var shader in effect.Shaders)
-            {
-                for (var s = 0; s < shader._samplers.Length; s++)
-                {
-                    var sampler = shader._samplers[s];
-
-                    var match = parameters.FindIndex(e => e.name == sampler.parameterName);
-                    if (match == -1)
-                    {
-                        // Store the index for runtime lookup.
-                        shader._samplers[s].parameter = parameters.Count;
-
-                        var param = new d3dx_parameter();
-                        param.class_ = D3DXPARAMETER_CLASS.OBJECT;
-                        param.name = sampler.parameterName;
-                        param.semantic = string.Empty;
-
-                        switch (sampler.type)
-                        {
-                            case MojoShader.MOJOSHADER_samplerType.MOJOSHADER_SAMPLER_1D:
-                                param.type = EffectObject.D3DXPARAMETER_TYPE.TEXTURE1D;
-                                break;
-
-                            case MojoShader.MOJOSHADER_samplerType.MOJOSHADER_SAMPLER_2D:
-                                param.type = EffectObject.D3DXPARAMETER_TYPE.TEXTURE2D;
-                                break;
-
-                            case MojoShader.MOJOSHADER_samplerType.MOJOSHADER_SAMPLER_VOLUME:
-                                param.type = EffectObject.D3DXPARAMETER_TYPE.TEXTURE3D;
-                                break;
-
-                            case MojoShader.MOJOSHADER_samplerType.MOJOSHADER_SAMPLER_CUBE:
-                                param.type = EffectObject.D3DXPARAMETER_TYPE.TEXTURECUBE;
-                                break;
-                        }
-
-                        parameters.Add(param);
-                    }
-                    else
-                    {
-                        // TODO: Make sure the type and size of 
-                        // the parameter match up!
-
-                        shader._samplers[s].parameter = match;
-                    }
-                }
-            }
-
-            // TODO: Annotations are part of the .FX format and
-            // not a part of shaders... we need to implement them
-            // in our mgfx parser if we want them back.
-            /*
-            // Now find the things we could not in the constant
-            // buffer reflection interface...  semantics and annotations.
-            for (var i = 0; i < parameters.Count; i++)
-            {
-                var vdesc = dxeffect.GetVariableByName(parameters[i].name);
-                if (!vdesc.IsValid)
-                    continue;
-
-                parameters[i].semantic = vdesc.Description.Semantic ?? string.Empty;
-
-                // TODO: Annotations!
-            }
-            */
-
-            effect.Parameters = parameters.ToArray();
-
-            return effect;
-        }
-        
-        private d3dx_state CreateShader(TwoMGFX.ShaderInfo shaderInfo, string shaderFunction, string shaderProfile, bool isVertexShader)
-        {
-            // Compile the shader.
-            byte[] bytecode;
-            if (shaderInfo.Profile == TwoMGFX.ShaderProfile.PlayStation4)
-            {
-                bytecode = null;
-            }
-            else
-            {
-                SharpDX.D3DCompiler.ShaderBytecode shaderByteCode;
-                try
-                {
-                    SharpDX.D3DCompiler.ShaderFlags shaderFlags = 0;
-
-                    // While we never allow preshaders, this flag is invalid for
-                    // the DX11 shader compiler which doesn't allow preshaders
-                    // in the first place.
-                    //shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.NoPreshader;
-
-                    if (shaderInfo.Profile == TwoMGFX.ShaderProfile.DirectX_11)
-                        shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.EnableBackwardsCompatibility;
-
-                    if (shaderInfo.Debug)
-                    {
-                        shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.SkipOptimization;
-                        shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.Debug;
-                    }
-                    else
-                    {
-                        shaderFlags |= SharpDX.D3DCompiler.ShaderFlags.OptimizationLevel3;
-                    }
-
-                    // Compile the shader into bytecode.                
-                    var result = SharpDX.D3DCompiler.ShaderBytecode.Compile(
-                        shaderInfo.fileContent,
-                        shaderFunction,
-                        shaderProfile,
-                        shaderFlags,
-                        0,
-                        null,
-                        null,
-                        shaderInfo.fileName);
-
-                    if (result.HasErrors)
-                        throw new Exception(result.Message);
-
-                    shaderByteCode = result.Bytecode;
-                    //var source = shaderByteCode.Disassemble();
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-
-                // Get a copy of the shader bytecode.
-                bytecode = shaderByteCode.Data.ToArray();
-            }
-
-            // First look to see if we already created this same shader.
-            ShaderData shaderData = null;
-            foreach (var shader in Shaders)
-            {
-                if (bytecode.SequenceEqual(shader.Bytecode))
-                {
-                    shaderData = shader;
-                    break;
-                }
-            }
-
-            // Create a new shader.
-            if (shaderData == null)
-            {
-                if (shaderInfo.Profile == TwoMGFX.ShaderProfile.DirectX_11)
-                    shaderData = ShaderData.CreateHLSL(bytecode, isVertexShader, ConstantBuffers, Shaders.Count, shaderInfo.SamplerStates, shaderInfo.Debug);
-                else  if (shaderInfo.Profile == TwoMGFX.ShaderProfile.OpenGL)
-                    shaderData = ShaderData.CreateGLSL(bytecode, ConstantBuffers, Shaders.Count, shaderInfo.SamplerStates);
-                else if (shaderInfo.Profile == TwoMGFX.ShaderProfile.PlayStation4)
-                    throw new NotSupportedException("Unknown shader profile!");
-
-                Shaders.Add(shaderData);
-            }
-            
-            //var assmbly = desc.Bytecode.Disassemble();
-            //var buffer = reflection.GetConstantBuffer(0);
-
-            var state = new d3dx_state();
-            state.index = 0;
-            state.type = STATE_TYPE.CONSTANT;
-            state.operation = isVertexShader ? (uint)146 : (uint)147;
-
-            state.parameter = new d3dx_parameter();
-            state.parameter.name = string.Empty;
-            state.parameter.semantic = string.Empty;
-            state.parameter.class_ = D3DXPARAMETER_CLASS.OBJECT;
-            state.parameter.type = isVertexShader ? D3DXPARAMETER_TYPE.VERTEXSHADER : D3DXPARAMETER_TYPE.PIXELSHADER;
-            state.parameter.rows = 0;
-            state.parameter.columns = 0;
-            state.parameter.data = shaderData.SharedIndex;
-
-            return state;
+            // Return a copy of the shader bytecode.
+            return shaderByteCode.Data.ToArray();
         }
     }
 }
