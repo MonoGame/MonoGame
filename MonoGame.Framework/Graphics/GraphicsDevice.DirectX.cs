3f4bd732094f013c55b5339316940897bee103bc
David Leaver <danzel@localhost.geek.nz>
2014-04-20 14:34:31 +1200

    GetBackBufferData Windows Phone support

--- MonoGame.Framework/Graphics/GraphicsDevice.DirectX.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.DirectX.cs
@@ -1206,14 +1206,20 @@ namespace Microsoft.Xna.Framework.Graphics
 
             lock (_d3dContext)
             {
+#if WINDOWS_PHONE
+                using (var backBufferTexture = new SharpDX.Direct3D11.Texture2D(_renderTargetView.Resource.NativePointer))
+#endif
+#if WINDOWS || WINDOWS_STOREAPP
                 //You can't Map the BackBuffer surface, so we copy to another texture
                 using (var dxgiBackBuffer = _swapChain.GetBackBuffer<Surface>(0))
                 using (var backBufferTexture = dxgiBackBuffer.QueryInterface<SharpDX.Direct3D11.Texture2D>())
+#endif
                 {
                     var desc = backBufferTexture.Description;
                     desc.BindFlags = SharpDX.Direct3D11.BindFlags.None;
                     desc.CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.Read;
                     desc.Usage = SharpDX.Direct3D11.ResourceUsage.Staging;
+                    desc.OptionFlags = SharpDX.Direct3D11.ResourceOptionFlags.None;
 
                     using (var texture = new SharpDX.Direct3D11.Texture2D(_d3dDevice, desc))
                     {
