49c0c791470751aa7ffaa7066ba29ed16342227c
David Leaver <danzel@localhost.geek.nz>
2014-04-25 15:06:30 +1200

    using this in rather than complicating the code a bit

--- MonoGame.Framework/Graphics/Vertices/VertexDeclaration.OpenGL.cs
+++ MonoGame.Framework/Graphics/Vertices/VertexDeclaration.OpenGL.cs
@@ -11,6 +11,7 @@ using MonoMac.OpenGL;
 using OpenTK.Graphics.OpenGL;
 #else
 using OpenTK.Graphics.ES20;
+using VertexAttribPointerType = OpenTK.Graphics.ES20.All;
 #endif
 
 namespace Microsoft.Xna.Framework.Graphics
@@ -75,11 +76,7 @@ namespace Microsoft.Xna.Framework.Graphics
                 public int Offset;
                 public int AttributeLocation;
                 public int NumberOfElements;
-#if GLES
-                public All VertexAttribPointerType;
-#elif OPENGL
                 public VertexAttribPointerType VertexAttribPointerType;
-#endif
                 public bool Normalized;
             }
 
