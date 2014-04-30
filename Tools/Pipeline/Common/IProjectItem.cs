c76d38257efdbdd986a849e8f1a5772cb3f18293
Tom Spilman <tom@sickheadgames.com>
2014-04-30 02:58:46 -0500

    Cleaned up usings. Added header comments.

--- Tools/Pipeline/Common/IProjectItem.cs
+++ Tools/Pipeline/Common/IProjectItem.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel;
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System.ComponentModel;
 
 namespace MonoGame.Tools.Pipeline
 {
--- Tools/Pipeline/Windows/MainView.cs
+++ Tools/Pipeline/Windows/MainView.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace MonoGame.Tools.Pipeline
