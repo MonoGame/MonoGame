438b7fdee19d68a786802cdc45b4fd4c6c7fb02b
Ray Batts <Ray@Sickheadgames.com>
2014-05-01 18:52:04 -0500

    Update InstancePlayLimitException.cs

--- MonoGame.Framework/Audio/InstancePlayLimitException.cs
+++ MonoGame.Framework/Audio/InstancePlayLimitException.cs
@@ -9,7 +9,7 @@ using System.Runtime.Serialization;
 namespace Microsoft.Xna.Framework.Audio
 {
     /// <summary>
-    /// The exception thrown when the system attempts to play more SoundEffectInstances than alloted.
+    /// The exception thrown when the system attempts to play more SoundEffectInstances than allotted.
     /// </summary>
     /// <remarks>
     /// Most platforms have a hard limit on how many sounds can be played simultaneously. This exception is thrown when that limit is exceeded.
