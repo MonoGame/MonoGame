c13d74277acd7d51dca9976acc247004e6afbf29
James <james@sickheadgames.com>
2014-05-07 17:21:02 -0500

    Implemented color to string converter methods. Without this method the pipelinetool can serialize a color type processor parameter.

--- MonoGame.Framework.Content.Pipeline/Builder/Convertors/StringToColorConverter.cs
+++ MonoGame.Framework.Content.Pipeline/Builder/Convertors/StringToColorConverter.cs
@@ -8,6 +8,23 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Builder.Convertors
 {
 	class StringToColorConverter : TypeConverter
 	{
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+                return true;
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType != typeof (string))            
+                return base.ConvertTo(context, culture, value, destinationType);
+
+            var color = (Color)value;
+            return string.Format("{0},{1},{2},{3}", color.R, color.G, color.B, color.A);
+        }
+
 		public override bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
 		{
 			if (sourceType == typeof (string))
@@ -20,13 +37,18 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Builder.Convertors
 		{
 			if (value.GetType () == typeof (string)) {
 				string[] values = ((string)value).Split(new char[] {','},StringSplitOptions.None);
-				if (values.Length == 4) {
-					var r = int.Parse(values[0].Trim());
-					var g = int.Parse(values[1].Trim());
-					var b =	int.Parse(values[2].Trim());
-					var a =	int.Parse (values[3].Trim ());
-					return new Microsoft.Xna.Framework.Color (r,g,b,a);
-				}
+                if (values.Length == 4)
+                {
+                    var r = int.Parse(values[0].Trim());
+                    var g = int.Parse(values[1].Trim());
+                    var b = int.Parse(values[2].Trim());
+                    var a = int.Parse(values[3].Trim());
+                    return new Microsoft.Xna.Framework.Color(r, g, b, a);
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("Could not convert from string({0}) to Color, expected format is 'r,g,b,a'", value));                    
+                }
 			}
 
 			return base.ConvertFrom (context, culture, value);
