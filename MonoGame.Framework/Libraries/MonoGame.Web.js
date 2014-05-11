03b3fb3f41d18db74dc5b4dc9d03fa539da72bb5
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-05-11 21:20:50 +1000

    Hook up Javascript implementations to JSAPI class
    To enable this to work, developers need to modify the default index.htm generated by Protobuild to have:
    
            manifests: [
              "MonoGame.Framework",
              "JSILPort.exe"
            ],
    
    This will cause the appropriate MonoGame libraries to load.

---
+++ MonoGame.Framework/Libraries/MonoGame.Web.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+if (typeof (JSIL) === "undefined")
+    throw new Error("JSIL.Core required");
+
+var $jsilmg = JSIL.DeclareAssembly("MonoGame.Web");
+
+var $sdlasms = new JSIL.AssemblyCollection({
+    2: "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+    4: "System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+});
+
+JSIL.DeclareNamespace("MonoGame");
+JSIL.DeclareNamespace("MonoGame.Web");
+
+JSIL.ImplementExternals("MonoGame.Web.JSAPI", function ($interfaceBuilder) {
+    var $ = $interfaceBuilder;
+
+    $.Method({ Static: false, Public: true, Virtual: true }, "Test",
+      new JSIL.MethodSignature($.String, [], []),
+      function Test() {
+          return "hello";
+      }
+    );
+});
