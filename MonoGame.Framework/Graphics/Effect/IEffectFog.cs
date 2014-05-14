2fa04b36ddfd8ddf917b6200b2629cc3b89ca454
Tom Spilman <tom@sickheadgames.com>
2014-05-14 02:24:30 -0500

    Moved docs to interface definitions and improved them.

--- MonoGame.Framework/Graphics/Effect/IEffectFog.cs
+++ MonoGame.Framework/Graphics/Effect/IEffectFog.cs
@@ -42,11 +42,37 @@ using System;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
+    /// <summary>
+    /// The common effect fog rendering parameters.
+    /// </summary>
 	public interface IEffectFog
 	{
+        /// <summary>
+        /// The floating point fog color.
+        /// </summary>
 		Vector3 FogColor { get; set; }
+
+        /// <summary>
+        /// Used to toggle the rendering of fog.
+        /// </summary>
 		bool FogEnabled { get; set; }
+
+        /// <summary>
+        /// The world space distance from the camera at which fogging is fully applied.
+        /// </summary>
+        /// <remarks>
+        /// FogEnd should be greater than FogStart.  If FogEnd and FogStart 
+        /// are the same value everything is fully fogged.
+        /// </remarks>
 		float FogEnd { get; set; }
+
+        /// <summary>
+        /// The world space distance from the camera at which fogging begins.
+        /// </summary>
+        /// <remarks>
+        /// FogStart should be less than FogEnd.  If FogEnd and FogStart are the
+        /// same value everything is fully fogged.
+        /// </remarks>
 		float FogStart { get; set; }
 	}
 }
