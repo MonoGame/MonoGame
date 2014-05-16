78a0688366b6eb692bcab64f2522dfe99424c890
James Lupiani <lupiani@sickheadgames.com>
2014-05-16 14:50:50 -0500

    Added missing effect content writers.

---
+++ MonoGame.Framework.Content.Pipeline/Serialization/Compiler/DualTextureEffectWriter.cs
@@ -0,0 +1,21 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+
+namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
+{
+    [ContentTypeWriter]
+    class DualTextureEffectWriter : BuiltInContentWriter<DualTextureMaterialContent>
+    {
+        protected internal override void Write(ContentWriter output, DualTextureMaterialContent value)
+        {
+            output.WriteExternalReference(value.Texture);
+            output.WriteExternalReference(value.Texture2);
+            output.Write(value.DiffuseColor.HasValue ? value.DiffuseColor.Value : Vector3.One);
+            output.Write(value.Alpha.HasValue ? value.Alpha.Value : 1.0f);
+            output.Write(value.VertexColorEnabled.HasValue ? value.VertexColorEnabled.Value : false);
+        }
+    }
+}
