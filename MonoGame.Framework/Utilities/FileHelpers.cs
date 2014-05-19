da49c7f657a67e770a01218ecb9b80f4ad0d0a5b
danzel <danzel@localhost.geek.nz>
2014-05-20 09:57:10 +1200

    Whitespace and license since I just nuked most of this file.

--- MonoGame.Framework/Utilities/FileHelpers.cs
+++ MonoGame.Framework/Utilities/FileHelpers.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.IO;
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
 
 namespace Microsoft.Xna.Framework.Utilities
 {
-	internal static class FileHelpers
-	{
+    internal static class FileHelpers
+    {
 #if WINRT
-		public static char notSeparator = '/';
-		public static char separator = '\\';
+        public static char notSeparator = '/';
+        public static char separator = '\\';
 #else
-		public static char notSeparator = '\\';
-		public static char separator = Path.DirectorySeparatorChar;
+        public static char notSeparator = '\\';
+        public static char separator = Path.DirectorySeparatorChar;
 #endif
 
-		public static string NormalizeFilePathSeparators(string name)
-		{
+        public static string NormalizeFilePathSeparators(string name)
+        {
             return name.Replace(notSeparator, separator);
-		}
-	}
+        }
+    }
 }
