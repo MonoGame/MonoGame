3ea79a258dbe06893ce10af3d6a8fe46cd12698b
David Leaver <danzel@localhost.geek.nz>
2014-06-07 18:05:30 +1200

    Tidy up TouchPanelState creation in TouchPanelTests. Add a few more tests (one even fails!)

--- Test/Framework/TouchPanelTest.cs
+++ Test/Framework/TouchPanelTest.cs
@@ -11,12 +11,18 @@ namespace MonoGame.Tests.Framework
     [TestFixture]
     internal class TouchPanelTest
     {
+        private TouchPanelState _tps;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tps = new TouchPanelState(new MockWindow());
+        }
+
         [Test]
         public void InitiallyHasNoTouches()
         {
-            var tps = new TouchPanelState(new MockWindow());
-
-            var state = tps.GetState();
+            var state = _tps.GetState();
 
             Assert.AreEqual(0, state.Count);
             Assert.AreEqual(true, state.IsReadOnly);
@@ -25,18 +31,34 @@ namespace MonoGame.Tests.Framework
         [Test]
         public void PressedStartsATouch()
         {
-            var tps = new TouchPanelState(new MockWindow());
-
             var pos = new Vector2(100, 50);
-            tps.AddEvent(1, TouchLocationState.Pressed, pos);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos);
 
-            var state = tps.GetState();
+            var state = _tps.GetState();
 
             Assert.AreEqual(1, state.Count);
 
             var touch = state[0];
             Assert.AreEqual(TouchLocationState.Pressed, touch.State);
             Assert.AreEqual(pos, touch.Position);
+        }
+
+        [Test]
+        [Description("In XNA if you press then move your finger before GetState is called, the initial touch position is the latest position of the touch")]
+        public void PressedThenMoveStartsATouchAtNewPosition()
+        {
+            var pos = new Vector2(100, 50);
+            var pos2 = new Vector2(100, 100);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos);
+            _tps.AddEvent(1, TouchLocationState.Moved, pos2);
+
+            var state = _tps.GetState();
+
+            Assert.AreEqual(1, state.Count);
+
+            var touch = state[0];
+            Assert.AreEqual(TouchLocationState.Pressed, touch.State);
+            Assert.AreEqual(pos2, touch.Position);
         }
 
         [Test]
@@ -45,11 +67,9 @@ namespace MonoGame.Tests.Framework
         [TestCase(TouchLocationState.Released)]
         public void NonPressedDoesntStartATouch(TouchLocationState providedState)
         {
-            var tps = new TouchPanelState(new MockWindow());
+            _tps.AddEvent(1, providedState, new Vector2(100, 50));
 
-            tps.AddEvent(1, providedState, new Vector2(100, 50));
-
-            var state = tps.GetState();
+            var state = _tps.GetState();
 
             Assert.AreEqual(0, state.Count);
         }
@@ -57,30 +77,29 @@ namespace MonoGame.Tests.Framework
         [Test]
         public void PressedAgesToMovedAfterGetState()
         {
-            var tps = new TouchPanelState(new MockWindow());
-
             var pos = new Vector2(100, 50);
-            tps.AddEvent(1, TouchLocationState.Pressed, pos);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos);
 
-            tps.GetState(); //Throw the first one away
-            var state = tps.GetState();
+            var initialState = _tps.GetState();
+            var initialTouch = initialState[0];
+
+            var state = _tps.GetState();
 
             Assert.AreEqual(1, state.Count);
 
             var touch = state[0];
             Assert.AreEqual(TouchLocationState.Moved, touch.State);
             Assert.AreEqual(pos, touch.Position);
+            Assert.AreEqual(initialTouch.Id, touch.Id);
         }
 
         [Test]
         public void MovingTouchUpdatesPosition()
         {
-            var tps = new TouchPanelState(new MockWindow());
-
             var pos1 = new Vector2(100, 50);
-            tps.AddEvent(1, TouchLocationState.Pressed, pos1);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos1);
 
-            var state = tps.GetState();
+            var state = _tps.GetState();
             Assert.AreEqual(1, state.Count);
 
             var touch = state[0];
@@ -88,9 +107,9 @@ namespace MonoGame.Tests.Framework
             Assert.AreEqual(pos1, touch.Position);
 
             var pos2 = new Vector2(100, 50);
-            tps.AddEvent(1, TouchLocationState.Moved, pos2);
+            _tps.AddEvent(1, TouchLocationState.Moved, pos2);
 
-            state = tps.GetState();
+            state = _tps.GetState();
             Assert.AreEqual(1, state.Count);
 
             touch = state[0];
@@ -99,18 +118,66 @@ namespace MonoGame.Tests.Framework
         }
 
         [Test]
-        public void TouchBetweenGetStateCallsMakesNoTouch()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ReleasingTouchMakesItGoAway(bool moveInBetween)
         {
-            var tps = new TouchPanelState(new MockWindow());
-
-            var state = tps.GetState();
-            Assert.AreEqual(0, state.Count); //No touches to start with
-
+            //Touch the screen, we should get one touch with the given location in the pressed state
             var pos = new Vector2(100, 50);
-            tps.AddEvent(1, TouchLocationState.Pressed, pos);
-            tps.AddEvent(1, TouchLocationState.Released, pos);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos);
+            
+            var state = _tps.GetState();
+            Assert.AreEqual(1, state.Count);
 
-            state = tps.GetState();
+            var initialTouch = state[0];
+            Assert.AreEqual(TouchLocationState.Pressed, initialTouch.State);
+            Assert.AreEqual(pos, initialTouch.Position);
+
+            //Now optionally move the touch, should give the same touch in the new location in the moved state
+            if (moveInBetween)
+            {
+                var pos2 = new Vector2(100, 100);
+                _tps.AddEvent(1, TouchLocationState.Moved, pos2);
+
+                var movedState = _tps.GetState();
+                Assert.AreEqual(1, movedState.Count);
+
+                var touch = movedState[0];
+                Assert.AreEqual(TouchLocationState.Moved, touch.State);
+                Assert.AreEqual(pos2, touch.Position);
+                Assert.AreEqual(initialTouch.Id, touch.Id);
+            }
+
+            //Release the touch, it should then show up as released touch
+            var pos3 = new Vector2(100, 150);
+            _tps.AddEvent(1, TouchLocationState.Released, pos3);
+
+            var endState = _tps.GetState();
+            Assert.AreEqual(1, endState.Count);
+
+            var endTouch = endState[0];
+            Assert.AreEqual(TouchLocationState.Released, endTouch.State);
+            Assert.AreEqual(pos3, endTouch.Position);
+            Assert.AreEqual(initialTouch.Id, endTouch.Id);
+
+            //Finally get the TouchState again, we should now have no touches
+            var finalState = _tps.GetState();
+            Assert.AreEqual(0, finalState.Count);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        [Description("In XNA if you press and release your finger before GetState is called, the touch never shows up")]
+        public void TouchBetweenGetStateCallsMakesNoTouch(bool moveInBetween)
+        {
+            var pos = new Vector2(100, 50);
+            _tps.AddEvent(1, TouchLocationState.Pressed, pos);
+            if (moveInBetween) //Moving shouldn't change the behavior
+                _tps.AddEvent(1, TouchLocationState.Moved, pos);
+            _tps.AddEvent(1, TouchLocationState.Released, pos);
+
+            var state = _tps.GetState();
             Assert.AreEqual(0, state.Count); //Should miss the touch that happened between
         }
 
