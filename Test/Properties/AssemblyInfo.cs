3cd30aaca1c8fb248fa8ebf81a80bc93f5c4fa1a
Tom Spilman <tom@sickheadgames.com>
2014-07-09 04:26:48 -0500

    Fixed assembly info for Tests.

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoGame.Framework.Tests")]
+[assembly: AssemblyTitle("MonoGameTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonoGame.Framework.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyProduct("MonoGameTests")]
+[assembly: AssemblyCopyright("Copyright © MonoGame Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
