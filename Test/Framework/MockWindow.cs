453937deb7235b4cc87b7756570c0512d4e764c3
Tom Spilman <tom@sickheadgames.com>
2014-07-09 04:34:46 -0500

    Working around a difference in the WindowsDX build.

--- Test/Framework/MockWindow.cs
+++ Test/Framework/MockWindow.cs
@@ -18,7 +18,7 @@ namespace MonoGame.Tests.Framework
         }
 
 // TODO: Make this common so that all platforms have it!
-#if !IOS
+#if WINDOWS && DIRECTX
         public override Point Position { get; set; }
 #endif
 
