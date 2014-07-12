39bfe9843181421ff5a27be58e3b7c24bbc46d65
Daniel Neel <dneelyep@gmail.com>
2014-07-12 15:52:52 -0400

    Added namespace documentation for Microsoft.Xna.Framework.Input.

---
+++ MonoGame.Framework/Input/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Xna.Framework.Input
+{
+    /// <summary>
+    /// Provides functionality to handle input from keyboards, mice, gamepads, etc.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+}
