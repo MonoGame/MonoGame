f1d29ca3297f7b0b31483947b754889dffc294ee
Tom Spilman <tom@sickheadgames.com>
2014-07-13 14:58:54 -0500

    Removed unused using.

--- MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/ArraySerializer.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/ArraySerializer.cs
@@ -3,7 +3,6 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
-using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
 {
