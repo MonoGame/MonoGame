2475ab64473f7a2928c0f33207576b0740b51734
Daniel Neel <dneelyep@gmail.com>
2014-07-12 23:06:58 -0400

    Moved namespace documentation into a single file (NamespaceDocs), and included this file in the project definition file.

---
+++ MonoGame.Framework/NamespaceDocs.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Xna.Framework.Input
+{
+    /// <summary>
+    /// Provides functionality to handle input from keyboards, mice, gamepads, etc.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+}
+
+namespace Microsoft.Xna.Framework.Audio
+{
+    /// <summary>
+    /// Support for playing sound effects and XACT audio.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+}
+
+namespace Microsoft.Xna.Framework.Content
+{
+    /// <summary>
+    /// The runtime support for loading content pipeline content.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+}
