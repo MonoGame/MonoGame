54e35fa3679cbae673b71c8a1cf3c897403dd1f6
Tom Spilman <tom@sickheadgames.com>
2014-07-13 20:58:00 -0500

    Refactored to support flatten content cases.

--- MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/ReflectiveSerializer.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/ReflectiveSerializer.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
             public Func<object, object> Getter;
         };
 
-        private readonly Dictionary<string, ElementInfo> _elements = new Dictionary<string, ElementInfo>();
+        private readonly List<ElementInfo> _elements = new List<ElementInfo>();
 
         private bool GetElementInfo(IntermediateSerializer serializer, MemberInfo member, out ElementInfo info)
         {
@@ -102,7 +102,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
             {
                 ElementInfo info;
                 if (GetElementInfo(serializer, prop, out info))
-                    _elements.Add(info.Attribute.ElementName, info);
+                    _elements.Add(info);
             }
 
             var fields = TargetType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
@@ -110,7 +110,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
             {
                 ElementInfo info;
                 if (GetElementInfo(serializer, field, out info))
-                    _elements.Add(info.Attribute.ElementName, info);                
+                    _elements.Add(info);                
             }
         }
 
@@ -129,52 +129,29 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
                 }                
             }
 
-            var reader = input.Xml;
-            var depth = reader.Depth;
-
-            // Read the next node.
-            while (reader.Read())
+            foreach (var info in _elements)
             {
-                // Skip over any whitespace.
-                if (reader.NodeType == XmlNodeType.Whitespace)
-                    continue;
-
-                // Did we reach the end of this object?
-                if (reader.NodeType == XmlNodeType.EndElement)
-                    break;
-
-                Debug.Assert(reader.Depth == depth, "We are not at the right depth!");
-
-                if (reader.NodeType == XmlNodeType.Element)
+                if (!info.Attribute.FlattenContent)
                 {
-                    var elementName = reader.Name;
-
-                    ElementInfo info;
-                    if (!_elements.TryGetValue(elementName, out info))
-                        throw new InvalidContentException(string.Format("Element `{0}` was not found in type `{1}`.", elementName, TargetType));
-
-                    if (info.Attribute.SharedResource)
-                    {
-                        Action<object> fixup = (o) => info.Setter(result, o);
-                        input.ReadSharedResource(info.Attribute, fixup);
-                    }
-                    else if (info.Setter == null)
-                    {
-                        var value = info.Getter(result);
-                        input.ReadObject(info.Attribute, info.Serializer, value);
-                    }
-                    else
-                    {
-                        var value = input.ReadObject<object>(info.Attribute, info.Serializer, null);
-                        info.Setter(result, value);
-                    }
-
-                    continue;
+                    if (!input.MoveToElement(info.Attribute.ElementName))
+                        continue;
                 }
 
-                // If we got here then we were not interested 
-                // in this node... so skip its children.
-                reader.Skip();
+                if (info.Attribute.SharedResource)
+                {
+                    Action<object> fixup = (o) => info.Setter(result, o);
+                    input.ReadSharedResource(info.Attribute, fixup);
+                }
+                else if (info.Setter == null)
+                {
+                    var value = info.Getter(result);
+                    input.ReadObject(info.Attribute, info.Serializer, value);
+                }
+                else
+                {
+                    var value = input.ReadObject<object>(info.Attribute, info.Serializer);
+                    info.Setter(result, value);
+                }
             }
 
             return result;
