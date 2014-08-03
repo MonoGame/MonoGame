b3c87adf0711e4b185999f65c93e1036cf9d97be
Tom Spilman <tom@sickheadgames.com>
2014-08-03 16:05:47 -0500

    Cleanup of ComputeHash(byte[]).

--- MonoGame.Framework/Utilities/Hash.cs
+++ MonoGame.Framework/Utilities/Hash.cs
@@ -8,16 +8,21 @@ namespace MonoGame.Utilities
 {
     internal static class Hash
     {
-        // Modified FNV Hash in C#
-        // http://stackoverflow.com/a/468084
+        /// <summary>
+        /// Compute a hash from a byte array.
+        /// </summary>
+        /// <remarks>
+        /// Modified FNV Hash in C#
+        /// http://stackoverflow.com/a/468084
+        /// </remarks>
         internal static int ComputeHash(params byte[] data)
         {
             unchecked
             {
                 const int p = 16777619;
-                int hash = (int)2166136261;
+                var hash = (int)2166136261;
 
-                for (int i = 0; i < data.Length; i++)
+                for (var i = 0; i < data.Length; i++)
                     hash = (hash ^ data[i]) * p;
 
                 hash += hash << 13;
