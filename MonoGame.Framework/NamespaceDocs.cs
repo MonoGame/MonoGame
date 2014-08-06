3894f9da85be5447712e1abdd751ee76210e0b6c
Shqrdx <chaosdev@mail.ru>
2014-08-06 08:44:36 +0400

    Fix license for all basic types

--- MonoGame.Framework/NamespaceDocs.cs
+++ MonoGame.Framework/NamespaceDocs.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Xna.Framework.Input
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework.Input
 {
     /// <summary>
     /// Provides functionality to handle input from keyboards, mice, gamepads, etc.
