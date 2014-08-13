55220277f048321b6b7e41dfc546bd566725f81d
David Leaver <danzel@localhost.geek.nz>
2014-08-13 19:59:17 +1200

    Test and fix for null reference error when empty constructing a TouchCollection.
    Fixes #2898

---
+++ Test/Framework/TouchCollectionTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Input.Touch;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Framework
+{
+    [TestFixture]
+    class TouchCollectionTest
+    {
+        [Test]
+        public void WorksWhenConstructedEmpty()
+        {
+            TouchCollection collection = new TouchCollection();
+
+            Assert.AreEqual(0, collection.Count);
+            foreach (var touch in collection)
+                Assert.Fail("Shouldn't have any touches in an empty collection");
+
+            Assert.AreEqual(-1, collection.IndexOf(new TouchLocation()));
+
+            TouchLocation touchLocation;
+            Assert.False(collection.FindById(1, out touchLocation));
+        }
+    }
+}
