8ad544d6ee7d6459deeaaa0c6f76f3dfad26e21d
wmartin129 <wayne@flumps.net>
2014-09-26 14:35:56 +0100

    Added microphone support

---
+++ MonoGame.Framework/Audio/MicrophoneState.cs
@@ -0,0 +1,18 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+
+namespace Microsoft.Xna.Framework.Audio
+{
+	/// <summary>Describes the state of a Microphone instance.</summary>
+	public enum MicrophoneState
+	{
+		/// <summary>The Microphone audio capture has started.</summary>
+		Started,
+		/// <summary>The Microphone audio capture has stopped.</summary>
+		Stopped
+	}
+}
+
