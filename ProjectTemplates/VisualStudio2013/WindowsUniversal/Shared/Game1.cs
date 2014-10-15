cb0bd7c7d376fc0b6bd313069b9b1dea1bbd467e
Alejandro Jimenez Martinez <aljim@microsoft.com>
2014-10-15 00:19:26 -0700

    Fix identation

--- ProjectTemplates/VisualStudio2013/WindowsUniversal/Shared/Game1.cs
+++ ProjectTemplates/VisualStudio2013/WindowsUniversal/Shared/Game1.cs
@@ -25,25 +25,25 @@ namespace $ext_safeprojectname$
         VertexPositionColor[] cube;
         float cubeRotation = 0.0f;
         static ushort []cubeIndices =
-		{
-			0,2,1, // -x
-			1,2,3,
+    		{
+    			0,2,1, // -x
+    			1,2,3,
 
-			4,5,6, // +x
-			5,7,6,
+    			4,5,6, // +x
+    			5,7,6,
 
-			0,1,5, // -y
-			0,5,4,
+    			0,1,5, // -y
+    			0,5,4,
 
-			2,6,7, // +y
-			2,7,3,
+    			2,6,7, // +y
+    			2,7,3,
 
-			0,4,6, // -z
-			0,6,2,
+    			0,4,6, // -z
+    			0,6,2,
 
-			1,3,7, // +z
-			1,7,5,
-		};
+    			1,3,7, // +z
+    			1,7,5,
+    		};
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
