4c4c150fb634b6f2a54059bf5bde01c61afd33fa
Shqrdx <chaosdev@mail.ru>
2014-10-26 06:57:43 +0300

    Replace operator's block

--- MonoGame.Framework/Vector2.cs
+++ MonoGame.Framework/Vector2.cs
@@ -116,6 +116,136 @@ namespace Microsoft.Xna.Framework
 
         #endregion
 
+        #region Operators
+
+        /// <summary>
+        /// Inverts values in the specified <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="value">Source <see cref="Vector2"/> on the right of the sub sign.</param>
+        /// <returns>Result of the inversion.</returns>
+        public static Vector2 operator -(Vector2 value)
+        {
+            value.X = -value.X;
+            value.Y = -value.Y;
+            return value;
+        }
+
+        /// <summary>
+        /// Adds two vectors.
+        /// </summary>
+        /// <param name="value1">Source <see cref="Vector2"/> on the left of the add sign.</param>
+        /// <param name="value2">Source <see cref="Vector2"/> on the right of the add sign.</param>
+        /// <returns>Sum of the vectors.</returns>
+        public static Vector2 operator +(Vector2 value1, Vector2 value2)
+        {
+            value1.X += value2.X;
+            value1.Y += value2.Y;
+            return value1;
+        }
+
+        /// <summary>
+        /// Subtracts a <see cref="Vector2"/> from a <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="value1">Source <see cref="Vector2"/> on the left of the sub sign.</param>
+        /// <param name="value2">Source <see cref="Vector2"/> on the right of the sub sign.</param>
+        /// <returns>Result of the vector subtraction.</returns>
+        public static Vector2 operator -(Vector2 value1, Vector2 value2)
+        {
+            value1.X -= value2.X;
+            value1.Y -= value2.Y;
+            return value1;
+        }
+
+        /// <summary>
+        /// Multiplies the components of two vectors by each other.
+        /// </summary>
+        /// <param name="value1">Source <see cref="Vector2"/> on the left of the mul sign.</param>
+        /// <param name="value2">Source <see cref="Vector2"/> on the right of the mul sign.</param>
+        /// <returns>Result of the vector multiplication.</returns>
+        public static Vector2 operator *(Vector2 value1, Vector2 value2)
+        {
+            value1.X *= value2.X;
+            value1.Y *= value2.Y;
+            return value1;
+        }
+
+        /// <summary>
+        /// Multiplies the components of vector by a scalar.
+        /// </summary>
+        /// <param name="value">Source <see cref="Vector2"/> on the left of the mul sign.</param>
+        /// <param name="scaleFactor">Scalar value on the right of the mul sign.</param>
+        /// <returns>Result of the vector multiplication with a scalar.</returns>
+        public static Vector2 operator *(Vector2 value, float scaleFactor)
+        {
+            value.X *= scaleFactor;
+            value.Y *= scaleFactor;
+            return value;
+        }
+
+        /// <summary>
+        /// Multiplies the components of vector by a scalar.
+        /// </summary>
+        /// <param name="scaleFactor">Scalar value on the left of the mul sign.</param>
+        /// <param name="value">Source <see cref="Vector2"/> on the right of the mul sign.</param>
+        /// <returns>Result of the vector multiplication with a scalar.</returns>
+        public static Vector2 operator *(float scaleFactor, Vector2 value)
+        {
+            value.X *= scaleFactor;
+            value.Y *= scaleFactor;
+            return value;
+        }
+
+        /// <summary>
+        /// Divides the components of a <see cref="Vector2"/> by the components of another <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="value1">Source <see cref="Vector2"/> on the left of the div sign.</param>
+        /// <param name="value2">Divisor <see cref="Vector2"/> on the right of the div sign.</param>
+        /// <returns>The result of dividing the vectors.</returns>
+        public static Vector2 operator /(Vector2 value1, Vector2 value2)
+        {
+            value1.X /= value2.X;
+            value1.Y /= value2.Y;
+            return value1;
+        }
+
+        /// <summary>
+        /// Divides the components of a <see cref="Vector2"/> by a scalar.
+        /// </summary>
+        /// <param name="value1">Source <see cref="Vector2"/> on the left of the div sign.</param>
+        /// <param name="divider">Divisor scalar on the right of the div sign.</param>
+        /// <returns>The result of dividing a vector by a scalar.</returns>
+        public static Vector2 operator /(Vector2 value1, float divider)
+        {
+            float factor = 1 / divider;
+            value1.X *= factor;
+            value1.Y *= factor;
+            return value1;
+        }
+
+        /// <summary>
+        /// Compares whether two <see cref="Vector2"/> instances are equal.
+        /// </summary>
+        /// <param name="value1"><see cref="Vector2"/> instance on the left of the equal sign.</param>
+        /// <param name="value2"><see cref="Vector2"/> instance on the right of the equal sign.</param>
+        /// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+        public static bool operator ==(Vector2 value1, Vector2 value2)
+        {
+            return value1.X == value2.X && value1.Y == value2.Y;
+        }
+
+        /// <summary>
+        /// Compares whether two <see cref="Vector2"/> instances are not equal.
+        /// </summary>
+        /// <param name="value1"><see cref="Vector2"/> instance on the left of the not equal sign.</param>
+        /// <param name="value2"><see cref="Vector2"/> instance on the right of the not equal sign.</param>
+        /// <returns><c>true</c> if the instances are not equal; <c>false</c> otherwise.</returns>	
+        public static bool operator !=(Vector2 value1, Vector2 value2)
+        {
+            return value1.X != value2.X || value1.Y != value2.Y;
+        }
+
+        #endregion
+
         #region Public Methods
 
         /// <summary>
@@ -938,136 +1068,6 @@ namespace Microsoft.Xna.Framework
         )
         {
             TransformNormal(sourceArray, 0, ref matrix, destinationArray, 0, sourceArray.Length);
-        }
-
-        #endregion
-
-        #region Operators
-
-        /// <summary>
-        /// Inverts values in the specified <see cref="Vector2"/>.
-        /// </summary>
-        /// <param name="value">Source <see cref="Vector2"/> on the right of the sub sign.</param>
-        /// <returns>Result of the inversion.</returns>
-        public static Vector2 operator -(Vector2 value)
-        {
-            value.X = -value.X;
-            value.Y = -value.Y;
-            return value;
-        }
-
-        /// <summary>
-        /// Adds two vectors.
-        /// </summary>
-        /// <param name="value1">Source <see cref="Vector2"/> on the left of the add sign.</param>
-        /// <param name="value2">Source <see cref="Vector2"/> on the right of the add sign.</param>
-        /// <returns>Sum of the vectors.</returns>
-        public static Vector2 operator +(Vector2 value1, Vector2 value2)
-        {
-            value1.X += value2.X;
-            value1.Y += value2.Y;
-            return value1;
-        }
-
-        /// <summary>
-        /// Subtracts a <see cref="Vector2"/> from a <see cref="Vector2"/>.
-        /// </summary>
-        /// <param name="value1">Source <see cref="Vector2"/> on the left of the sub sign.</param>
-        /// <param name="value2">Source <see cref="Vector2"/> on the right of the sub sign.</param>
-        /// <returns>Result of the vector subtraction.</returns>
-        public static Vector2 operator -(Vector2 value1, Vector2 value2)
-        {
-            value1.X -= value2.X;
-            value1.Y -= value2.Y;
-            return value1;
-        }
-
-        /// <summary>
-        /// Multiplies the components of two vectors by each other.
-        /// </summary>
-        /// <param name="value1">Source <see cref="Vector2"/> on the left of the mul sign.</param>
-        /// <param name="value2">Source <see cref="Vector2"/> on the right of the mul sign.</param>
-        /// <returns>Result of the vector multiplication.</returns>
-        public static Vector2 operator *(Vector2 value1, Vector2 value2)
-        {
-            value1.X *= value2.X;
-            value1.Y *= value2.Y;
-            return value1;
-        }
-
-        /// <summary>
-        /// Multiplies the components of vector by a scalar.
-        /// </summary>
-        /// <param name="value">Source <see cref="Vector2"/> on the left of the mul sign.</param>
-        /// <param name="scaleFactor">Scalar value on the right of the mul sign.</param>
-        /// <returns>Result of the vector multiplication with a scalar.</returns>
-        public static Vector2 operator *(Vector2 value, float scaleFactor)
-        {
-            value.X *= scaleFactor;
-            value.Y *= scaleFactor;
-            return value;
-        }
-
-        /// <summary>
-        /// Multiplies the components of vector by a scalar.
-        /// </summary>
-        /// <param name="scaleFactor">Scalar value on the left of the mul sign.</param>
-        /// <param name="value">Source <see cref="Vector2"/> on the right of the mul sign.</param>
-        /// <returns>Result of the vector multiplication with a scalar.</returns>
-        public static Vector2 operator *(float scaleFactor, Vector2 value)
-        {
-            value.X *= scaleFactor;
-            value.Y *= scaleFactor;
-            return value;
-        }
-
-        /// <summary>
-        /// Divides the components of a <see cref="Vector2"/> by the components of another <see cref="Vector2"/>.
-        /// </summary>
-        /// <param name="value1">Source <see cref="Vector2"/> on the left of the div sign.</param>
-        /// <param name="value2">Divisor <see cref="Vector2"/> on the right of the div sign.</param>
-        /// <returns>The result of dividing the vectors.</returns>
-        public static Vector2 operator /(Vector2 value1, Vector2 value2)
-        {
-            value1.X /= value2.X;
-            value1.Y /= value2.Y;
-            return value1;
-        }
-
-        /// <summary>
-        /// Divides the components of a <see cref="Vector2"/> by a scalar.
-        /// </summary>
-        /// <param name="value1">Source <see cref="Vector2"/> on the left of the div sign.</param>
-        /// <param name="divider">Divisor scalar on the right of the div sign.</param>
-        /// <returns>The result of dividing a vector by a scalar.</returns>
-        public static Vector2 operator /(Vector2 value1, float divider)
-        {
-            float factor = 1 / divider;
-            value1.X *= factor;
-            value1.Y *= factor;
-            return value1;
-        }
-
-        /// <summary>
-        /// Compares whether two <see cref="Vector2"/> instances are equal.
-        /// </summary>
-        /// <param name="value1"><see cref="Vector2"/> instance on the left of the equal sign.</param>
-        /// <param name="value2"><see cref="Vector2"/> instance on the right of the equal sign.</param>
-        /// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
-        public static bool operator ==(Vector2 value1, Vector2 value2)
-        {
-            return value1.X == value2.X && value1.Y == value2.Y;
-        }
-
-        /// <summary>
-        /// Compares whether two <see cref="Vector2"/> instances are not equal.
-        /// </summary>
-        /// <param name="value1"><see cref="Vector2"/> instance on the left of the not equal sign.</param>
-        /// <param name="value2"><see cref="Vector2"/> instance on the right of the not equal sign.</param>
-        /// <returns><c>true</c> if the instances are not equal; <c>false</c> otherwise.</returns>	
-        public static bool operator !=(Vector2 value1, Vector2 value2)
-        {
-            return value1.X != value2.X || value1.Y != value2.Y;
         }
 
         #endregion
