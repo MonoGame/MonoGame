3f26bf50451d2a621ee3df2e8585e764648f6bd8
David Leaver <danzel@localhost.geek.nz>
2014-11-26 20:56:45 +1300

    Use a queue for touches on WindowsPhone as the touch events are dispatched asynchonously (while user code is running).

---
+++ MonoGame.Framework/Input/Touch/TouchQueue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Xna.Framework.Input.Touch
+{
+	/// <summary>
+	/// Stores touches to apply them once a frame for platforms that dispatch touches asynchronously
+	/// while user code is running.
+	/// </summary>
+	internal class TouchQueue
+	{
+		private readonly List<TouchEvent> _queue = new List<TouchEvent>(); 
+
+		public void Enqueue(int id, TouchLocationState state, Vector2 pos)
+		{
+			lock (_queue)
+			{
+				_queue.Add(new TouchEvent(id, state, pos));
+			}
+		}
+
+		public void ProcessQueued()
+		{
+			lock (_queue)
+			{
+				for (var i = 0; i < _queue.Count; i++)
+				{
+					var ev = _queue[i];
+					TouchPanel.AddEvent(ev.Id, ev.State, ev.Pos);
+				}
+				_queue.Clear();
+			}
+		}
+
+		private struct TouchEvent
+		{
+			public readonly int Id;
+			public readonly TouchLocationState State;
+			public readonly Vector2 Pos;
+
+			public TouchEvent(int id, TouchLocationState state, Vector2 pos)
+			{
+				Id = id;
+				State = state;
+				Pos = pos;
+			}
+		}
+
+	}
+}
