58533768515b9090e8d26db010752dddff60f069
cra0zy <cra0zy@gmail.com>
2014-12-17 23:14:29 +0100

    updated most of the projectview stuff

---
+++ Tools/Pipeline/Dialogs/YesNoCancelDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MonoGame.Tools.Pipeline
+{
+	public partial class YesNoCancelDialog : Gtk.Dialog
+	{
+		public YesNoCancelDialog ()
+		{
+			this.Build ();
+		}
+	}
+}
+
---
+++ Tools/Pipeline/gtk-gui/MonoGame.Tools.Pipeline.YesNoCancelDialog.cs
@@ -0,0 +1,10 @@
+
+namespace MonoGame.Tools.Pipeline
+{
+	public partial class YesNoCancelDialog
+	{
+		private Gtk.Button buttonCancel;
+		
+		private Gtk.Button buttonOk;
+	}
+}
