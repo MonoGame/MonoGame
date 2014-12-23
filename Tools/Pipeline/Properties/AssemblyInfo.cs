f3686be3d655e7d64884d796538163f92f0d98da
Shqrdx <chaosdev@mail.ru>
2014-12-23 12:01:50 +0300

    Fix

--- Tools/Pipeline/Properties/AssemblyInfo.cs
+++ Tools/Pipeline/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
