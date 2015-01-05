39bf27f21c54d290b1ce8b49eb4f5c0e6c4501fb
Dean Ellis <dellis1972@googlemail.com>
2015-01-05 23:18:35 +0000

    Initial Commit of the rework on the Xamarin Studio/MonoDevelop Addin Includes new templates and new Shared Projects support Includes Pipeline Tool support Removes old ProjectTemplates Removed call to old MonoDevelop ProjectTemplate default.build Fixed nuget download

---
+++ IDE/MonoDevelop/MonoDevelop.MonoGame.Android/MonoGameAndroidProject.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MonoDevelop.MonoGame.Android
+{
+	public class MonoGameAndroidProject : MonoDevelop.MonoDroid.MonoDroidProject
+	{
+		public MonoGameAndroidProject () : base()
+		{
+		}
+
+		public override MonoDevelop.Projects.SolutionItemConfiguration CreateConfiguration (string name)
+		{
+			return base.CreateConfiguration (name);
+		}
+
+		protected override void PopulateSupportFileList (MonoDevelop.Projects.FileCopySet list, MonoDevelop.Projects.ConfigurationSelector configuration)
+		{
+			base.PopulateSupportFileList (list, configuration);
+		}
+
+		public override string ProjectType {
+			get {
+				return "MonoGameAndroid";
+			}
+		}
+	}
+}
+
---
+++ IDE/MonoDevelop/MonoDevelop.MonoGame/PipelineDisplayBinding.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using MonoDevelop.Ide.Gui;
+using System.IO;
+using System.Diagnostics;
+using MonoDevelop.Ide.Gui.Content;
+using Gtk;
+
+namespace MonoDevelop.MonoGame
+{
+	#if BUILDINEDITOR
+	public class PipelineDisplayBinding: IViewDisplayBinding
+	{
+		#region IViewDisplayBinding implementation
+
+		public IViewContent CreateContent (MonoDevelop.Core.FilePath fileName, string mimeType, MonoDevelop.Projects.Project ownerProject)
+		{
+			return new MonoGameContentEditorViewContent (fileName, ownerProject);
+		}
+
+		public string Name {
+			get {
+				return "MonoGame Content Builder";
+			}
+		}
+
+		#endregion
+
+		#region IDisplayBinding implementation
+
+		public bool CanHandle (MonoDevelop.Core.FilePath fileName, string mimeType, MonoDevelop.Projects.Project ownerProject)
+		{
+			return mimeType == "text/x-mgcb";
+		}
+
+		public bool CanUseAsDefault {
+			get {
+				return true;
+			}
+		}
+
+		#endregion
+	}
+	#else
+	public class PipelineDisplayBinding : IExternalDisplayBinding {
+		#region IExternalDisplayBinding implementation
+		public MonoDevelop.Ide.Desktop.DesktopApplication GetApplication (MonoDevelop.Core.FilePath fileName, string mimeType, MonoDevelop.Projects.Project ownerProject)
+		{
+			return new PipelineDesktopApplication (fileName.FileName,ownerProject);
+		}
+		#endregion
+		#region IDisplayBinding implementation
+		public bool CanHandle (MonoDevelop.Core.FilePath fileName, string mimeType, MonoDevelop.Projects.Project ownerProject)
+		{
+			return mimeType == "text/x-mgcb";
+		}
+		public bool CanUseAsDefault {
+			get {
+				return true;
+			}
+		}
+		#endregion
+		
+	}
+
+	class PipelineDesktopApplication : MonoDevelop.Ide.Desktop.DesktopApplication
+	{
+		MonoDevelop.Projects.Project project;
+		string filename;
+
+		public PipelineDesktopApplication (string filename, MonoDevelop.Projects.Project ownerProject)
+			: base ("MonoGamePipelineTool", "MonoGame Pipeline Tool", true)
+		{
+			this.project = ownerProject;
+			this.filename = filename;
+		}
+
+		public override void Launch (params string[] files)
+		{
+			var process = new Process ();
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+			process.StartInfo.FileName = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.ProgramFilesX86), @"MonoGame\Tools", "Pipeline.exe");;
+				process.StartInfo.Arguments = string.Format ("\"{0}\"", filename);
+			} else {
+				if (File.Exists ("/Applications/Pipeline.app/Contents/MacOS/Pipeline")) {
+					process.StartInfo.FileName = "/Applications/Pipeline.app/Contents/MacOS/Pipeline";
+					process.StartInfo.Arguments = string.Format ("\"{0}\"", filename);
+				} else {
+					// figure out linix 
+					process.StartInfo.FileName = "mono";
+					process.StartInfo.Arguments = string.Format ("\"{0}\" \"{2}\"", "/usr/local/bin/Pipeline.exe", filename);
+				}
+			}
+			process.StartInfo.CreateNoWindow = true;
+			process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			process.StartInfo.UseShellExecute = false;
+			
+			// Fire off the process.
+			process.Start();
+		}
+	}
+	#endif
+}
+
