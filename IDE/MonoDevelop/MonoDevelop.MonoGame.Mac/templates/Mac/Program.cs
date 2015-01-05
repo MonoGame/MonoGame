6f856a7622c3f8110570429974374c9013620c09
Dean Ellis <dellis1972@googlemail.com>
2015-01-05 23:18:35 +0000

    Includes new templates and new Shared Projects support Includes Pipeline Tool support Removes old ProjectTemplates Removed call to old MonoDevelop ProjectTemplate default.build Added New XamarinStudio/MonoDevelop Addin Added new Mac OS Installer

---
+++ IDE/MonoDevelop/MonoDevelop.MonoGame.Mac/templates/Mac/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+#endregion
+
+namespace ${Namespace}
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate();
+				NSApplication.Main(args);
+			}
+		}
+	}
+
+	class AppDelegate : NSApplicationDelegate
+	{
+		private static Game1 game;
+
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			// Handle a Xamarin.Mac Upgrade
+			AppDomain.CurrentDomain.AssemblyResolve += (object sender, ResolveEventArgs a) =>  {
+			if (a.Name.StartsWith("MonoMac")) {
+				return typeof(MonoMac.AppKit.AppKitFramework).Assembly;
+			}
+			return null;
+			};
+			game = new Game1();
+			game.Run();
+		}
+
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}  
+
+}
+
