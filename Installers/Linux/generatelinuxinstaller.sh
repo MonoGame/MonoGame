67ab7e16ec3b0158d0ab2fa8700f45c8f401db37
marko <cra0zy@gmail.com>
2015-01-10 03:05:01 +0100

    updated dependencies

--- Installers/Linux/generatelinuxinstaller.sh
+++ Installers/Linux/generatelinuxinstaller.sh
@@ -10,5 +10,5 @@ cp ../../Tools/Pipeline/bin/Linux/AnyCPU/Release/. $DIR/ -R
 cp ../monogame.ico $DIR
 cp postinstall.sh $DIR
 cp uninstall.sh $DIR
-./makeself.sh Data/ ../../monogame-pipeline-linux.run "Monogame Pipeline Installer" ./postinstall.sh
+./../../ThirdParty/Dependencies/makeself/makeself.sh Data/ ../../monogame-linux.run "Monogame Pipeline Installer" ./postinstall.sh
 rm -rf Data
