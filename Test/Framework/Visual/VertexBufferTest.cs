MonoGame/MonoGame

commit d8483b0b8a00e9e37ed6fada9557dd57e33c2ed4

Implement vertexStride in VertexBuffer.SetData for DirectX platform.
This code roughly mirrors the equivalent path in GetData, although it is
only used when vertexStride != elementSize, so it doesn't affect
existing usage. An alternative implementation (which I actually tried
first), which avoids the need for a staging buffer, would call
UpdateSubresource once for each element, but I guess that would be very
slow for large arrays.

Note that this only implements the vertexStride parameter for
non-dynamic VertexBuffers (which is actually harder than it would be for
dynamic VertexBuffers, since you wouldn't need the staging buffer for a
dynamic VertexBuffer).

I changed Game.RunOneFrame() to Game.Run() for these tests, because
Game.RunOneFrame() doesn't seem to call Draw() when running tests in
XNA, so the tests gave false positives.

MonoGame.Framework/Graphics/Vertices/VertexBuffer.DirectX.cs
@@ -138,18 +138,52 @@ namespace Microsoft.Xna.Framework.Graphics
                 var startBytes = startIndex * elementSizeInBytes;
                 var dataPtr = (IntPtr)(dataHandle.AddrOfPinnedObject().ToInt64() + startBytes);
 
-                var box = new SharpDX.DataBox(dataPtr, 1, 0);
+                var d3dContext = GraphicsDevice._d3dContext;
+                lock (d3dContext)
+                {
+                    if (vertexStride == elementSizeInBytes)
+                    {
+                        var box = new SharpDX.DataBox(dataPtr, 1, 0);
 
-                var region = new SharpDX.Direct3D11.ResourceRegion();
-                region.Top = 0;
-                region.Front = 0;
-                region.Back = 1;
-                region.Bottom = 1;
-                region.Left = offsetInBytes;
-                region.Right = offsetInBytes + (elementCount * elementSizeInBytes);
+                        var region = new SharpDX.Direct3D11.ResourceRegion();
+                        region.Top = 0;
+                        region.Front = 0;
+                        region.Back = 1;
+                        region.Bottom = 1;
+                        region.Left = offsetInBytes;
+                        region.Right = offsetInBytes + (elementCount * elementSizeInBytes);
 
-                lock (GraphicsDevice._d3dContext)
-                    GraphicsDevice._d3dContext.UpdateSubresource(box, _buffer, 0, region);
+                        d3dContext.UpdateSubresource(box, _buffer, 0, region);
+                    }
+                    else
+                    {
+                        // Copy the buffer to a staging resource, so that any elements we don't write to will still be correct.
+                        var stagingDesc = _buffer.Description;
+                        stagingDesc.BindFlags = SharpDX.Direct3D11.BindFlags.None;
+                        stagingDesc.CpuAccessFlags = SharpDX.Direct3D11.CpuAccessFlags.Read | SharpDX.Direct3D11.CpuAccessFlags.Write;
+                        stagingDesc.Usage = SharpDX.Direct3D11.ResourceUsage.Staging;
+                        stagingDesc.OptionFlags = SharpDX.Direct3D11.ResourceOptionFlags.None;
+                        var stagingBuffer = new SharpDX.Direct3D11.Buffer(GraphicsDevice._d3dDevice, stagingDesc);
+
+                        lock (GraphicsDevice._d3dContext)
+                            d3dContext.CopyResource(_buffer, stagingBuffer);
+
+                        // Map the staging resource to a CPU accessible memory
+                        var box = d3dContext.MapSubresource(stagingBuffer, 0, SharpDX.Direct3D11.MapMode.Read, SharpDX.Direct3D11.MapFlags.None);
+
+                        for (int i = 0; i < data.Length; i++)
+                            SharpDX.Utilities.CopyMemory(box.DataPointer + i * vertexStride + offsetInBytes, dataPtr + i * elementSizeInBytes, elementSizeInBytes);
+
+                        // Make sure that we unmap the resource in case of an exception
+                        d3dContext.UnmapSubresource(stagingBuffer, 0);
+
+                        // Copy back from staging resource to real buffer.
+                        lock (GraphicsDevice._d3dContext)
+                            d3dContext.CopyResource(stagingBuffer, _buffer);
+
+                        stagingBuffer.Dispose();
+                    }
+                }
 
                 dataHandle.Free();
             }

MonoGame.Framework/Graphics/Vertices/VertexBuffer.cs
@@ -80,7 +80,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
         public void SetData<T>(int offsetInBytes, T[] data, int startIndex, int elementCount, int vertexStride) where T : struct
         {
-            SetDataInternal<T>(offsetInBytes, data, startIndex, elementCount, VertexDeclaration.VertexStride, SetDataOptions.None);
+            SetDataInternal<T>(offsetInBytes, data, startIndex, elementCount, vertexStride, SetDataOptions.None);
         }
         		
 		public void SetData<T>(T[] data, int startIndex, int elementCount) where T : struct

Test/Framework/Visual/VertexBufferTest.cs
@@ -121,7 +121,36 @@ namespace MonoGame.Tests.Visual
                 Assert.AreEqual(savedData[2].Position, readData[2]);
                 Assert.AreEqual(savedData[3].Position, readData[3]);
             };
-            Game.RunOneFrame();
+            Game.Run();
+        }
+
+        //[TestCase(true)]
+        [TestCase(false)]
+        public void SetPosition(bool dynamic)
+        {
+            Game.DrawWith += (sender, e) =>
+            {
+                var vertexBuffer = (dynamic)
+                    ? new DynamicVertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None)
+                    : new VertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None);
+                var positions = new[]
+                {
+                    savedData[0].Position,
+                    savedData[1].Position,
+                    savedData[2].Position,
+                    savedData[3].Position
+                };
+                var vertexStride = VertexPositionTexture.VertexDeclaration.VertexStride;
+                vertexBuffer.SetData(0, positions, 0, 4, vertexStride);
+
+                var readData = new Vector3[4];
+                vertexBuffer.GetData(0, readData, 0, 4, vertexStride);
+                Assert.AreEqual(savedData[0].Position, readData[0]);
+                Assert.AreEqual(savedData[1].Position, readData[1]);
+                Assert.AreEqual(savedData[2].Position, readData[2]);
+                Assert.AreEqual(savedData[3].Position, readData[3]);
+            };
+            Game.Run();
         }
 
         //[TestCase(true)]
@@ -144,8 +173,37 @@ namespace MonoGame.Tests.Visual
                 Assert.AreEqual(savedData[2].TextureCoordinate, readData[2]);
                 Assert.AreEqual(savedData[3].TextureCoordinate, readData[3]);
             };
-            Game.RunOneFrame();
+            Game.Run();
         }
-                
+
+        //[TestCase(true)]
+        [TestCase(false)]
+        public void SetTextureCoordinate(bool dynamic)
+        {
+            Game.DrawWith += (sender, e) =>
+            {
+                var vertexBuffer = (dynamic)
+                    ? new DynamicVertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None)
+                    : new VertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None);
+                var texCoords = new[]
+                {
+                    savedData[0].TextureCoordinate,
+                    savedData[1].TextureCoordinate,
+                    savedData[2].TextureCoordinate,
+                    savedData[3].TextureCoordinate
+                };
+                var vertexStride = VertexPositionTexture.VertexDeclaration.VertexStride;
+                var offsetInBytes = VertexPositionTexture.VertexDeclaration.GetVertexElements()[1].Offset;
+                vertexBuffer.SetData(offsetInBytes, texCoords, 0, 4, vertexStride);
+
+                var readData = new Vector2[4];
+                vertexBuffer.GetData(offsetInBytes, readData, 0, 4, vertexStride);
+                Assert.AreEqual(savedData[0].TextureCoordinate, readData[0]);
+                Assert.AreEqual(savedData[1].TextureCoordinate, readData[1]);
+                Assert.AreEqual(savedData[2].TextureCoordinate, readData[2]);
+                Assert.AreEqual(savedData[3].TextureCoordinate, readData[3]);
+            };
+            Game.Run();
+        }
     }
 }

commit 39e5616825ad4a247e2954e87ede98943d870818

Fix VertexBuffer.SetData bug introduced in d8483b0b8a00e9e37ed6fada9557dd57e33c2ed4
and add more SetData/GetData tests to tease out XNA behaviour surround
startIndex/elementCount/vertexStride parameters.

MonoGame.Framework/Graphics/Vertices/VertexBuffer.DirectX.cs
@@ -173,7 +173,7 @@ namespace Microsoft.Xna.Framework.Graphics
                         var box = d3dContext.MapSubresource(stagingBuffer, 0, SharpDX.Direct3D11.MapMode.Read,
                             SharpDX.Direct3D11.MapFlags.None);
 
-                        for (int i = 0; i < data.Length; i++)
+                        for (int i = 0; i < elementCount; i++)
                             SharpDX.Utilities.CopyMemory(
                                 box.DataPointer + i*vertexStride + offsetInBytes,
                                 dataPtr + i*elementSizeInBytes, elementSizeInBytes);

MonoGame.Framework/Graphics/Vertices/VertexBuffer.cs
@@ -54,13 +54,18 @@ namespace Microsoft.Xna.Framework.Graphics
 
         public void GetData<T> (int offsetInBytes, T[] data, int startIndex, int elementCount, int vertexStride) where T : struct
         {
+            var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+
+            if (vertexStride == 0)
+                vertexStride = elementSizeInBytes;
+
             if (data == null)
                 throw new ArgumentNullException("data", "This method does not accept null for this parameter.");
             if (data.Length < (startIndex + elementCount))
                 throw new ArgumentOutOfRangeException("elementCount", "This parameter must be a valid index within the array.");
             if (BufferUsage == BufferUsage.WriteOnly)
                 throw new NotSupportedException("Calling GetData on a resource that was created with BufferUsage.WriteOnly is not supported.");
-			if ((elementCount * vertexStride) > (VertexCount * VertexDeclaration.VertexStride))
+			if (elementCount > 1 && (elementCount * vertexStride) > (VertexCount * VertexDeclaration.VertexStride))
                 throw new InvalidOperationException("The array is not the correct size for the amount of data requested.");
 
             PlatformGetData<T>(offsetInBytes, data, startIndex, elementCount, vertexStride);
@@ -85,26 +90,34 @@ namespace Microsoft.Xna.Framework.Graphics
         		
 		public void SetData<T>(T[] data, int startIndex, int elementCount) where T : struct
         {
-            SetDataInternal<T>(0, data, startIndex, elementCount, VertexDeclaration.VertexStride, SetDataOptions.None);
+            var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+            SetDataInternal<T>(0, data, startIndex, elementCount, elementSizeInBytes, SetDataOptions.None);
 		}
 		
         public void SetData<T>(T[] data) where T : struct
         {
-            SetDataInternal<T>(0, data, 0, data.Length, VertexDeclaration.VertexStride, SetDataOptions.None);
+            var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+            SetDataInternal<T>(0, data, 0, data.Length, elementSizeInBytes, SetDataOptions.None);
         }
 
         protected void SetDataInternal<T>(int offsetInBytes, T[] data, int startIndex, int elementCount, int vertexStride, SetDataOptions options) where T : struct
         {
             if (data == null)
-                throw new ArgumentNullException("data is null");
-            if (data.Length < (startIndex + elementCount))
-                throw new InvalidOperationException("The array specified in the data parameter is not the correct size for the amount of data requested.");
-
-            var bufferSize = VertexCount * VertexDeclaration.VertexStride;
-            if ((vertexStride > bufferSize) || (vertexStride < VertexDeclaration.VertexStride))
-                throw new ArgumentOutOfRangeException("One of the following conditions is true:\nThe vertex stride is larger than the vertex buffer.\nThe vertex stride is too small for the type of data requested.");
+                throw new ArgumentNullException("data", "This method does not accept null for this parameter.");
 
             var elementSizeInBytes = Marshal.SizeOf(typeof(T));
+            var bufferSize = VertexCount * VertexDeclaration.VertexStride;
+
+            if (vertexStride == 0)
+                vertexStride = elementSizeInBytes;
+
+            if (startIndex + elementCount > data.Length || elementCount <= 0)
+                throw new ArgumentOutOfRangeException("The array specified in the data parameter is not the correct size for the amount of data requested.");
+            if (elementCount > 1 && (elementCount * vertexStride > bufferSize))
+                throw new InvalidOperationException("The vertex stride is larger than the vertex buffer.");
+            if (vertexStride < elementSizeInBytes)
+                throw new ArgumentOutOfRangeException("The vertex stride must be greater than or equal to the size of the specified data (" + elementSizeInBytes + ").");            
+
             PlatformSetDataInternal<T>(offsetInBytes, data, startIndex, elementCount, vertexStride, options, bufferSize, elementSizeInBytes);
         }
     }

Test/Framework/Visual/VertexBufferTest.cs
@@ -3,9 +3,12 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using System.CodeDom;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using NUnit.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using NUnit.Framework.Constraints;
 
 namespace MonoGame.Tests.Visual
 {
@@ -98,6 +101,105 @@ namespace MonoGame.Tests.Visual
                 vertexBuffer.GetData(offsetInBytes, readData, 0, 2, vertexStride);
                 Assert.AreEqual(savedData[2], readData[0]);
                 Assert.AreEqual(savedData[3], readData[1]);
+            };
+            Game.RunOneFrame();
+        }
+
+        //[TestCase(true)]
+        [TestCase(false)]
+        public void ShouldSetAndGetDataBytes(bool dynamic)
+        {
+            Game.DrawWith += (sender, e) =>
+            {
+                var vertexBuffer = (dynamic)
+                    ? new DynamicVertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None)
+                    : new VertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length, BufferUsage.None);
+                var savedDataBytes = ArrayUtil.ConvertFrom(savedData);
+                vertexBuffer.SetData(savedDataBytes);
+
+                var readData = new VertexPositionTexture[4];
+                vertexBuffer.GetData(readData, 0, 4);
+                Assert.AreEqual(savedData, readData);
+            };
+            Game.RunOneFrame();
+        }
+
+        //[TestCase(true)]
+        [TestCase(false, -1, 0, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 0, 0, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 0, 1, true, null)]
+        [TestCase(false, 0, -1, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 0, 80, true, null)]
+        [TestCase(false, 0, 81, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 1, 0, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 1, 1, true, null)]
+        [TestCase(false, 1, 79, true, null)]
+        [TestCase(false, 1, 80, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 79, 1, true, null)]
+        [TestCase(false, 79, 2, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 80, 0, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 80, 1, false, typeof(ArgumentOutOfRangeException))]
+        public void SetDataWithElementCount(bool dynamic, int startIndex, int elementCount, bool shouldSucceed, Type expectedExceptionType)
+        {
+            Game.DrawWith += (sender, e) =>
+            {
+                var vertexBuffer = (dynamic)
+                    ? new DynamicVertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length,
+                        BufferUsage.None)
+                    : new VertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length,
+                        BufferUsage.None);
+                var savedDataBytes = ArrayUtil.ConvertFrom(savedData);
+
+                if (!shouldSucceed)
+                    Assert.Throws(expectedExceptionType, () => vertexBuffer.SetData(savedDataBytes, startIndex, elementCount));
+                else
+                {
+                    vertexBuffer.SetData(savedDataBytes, startIndex, elementCount);
+
+                    var readDataBytes = new byte[savedDataBytes.Length];
+                    vertexBuffer.GetData(readDataBytes, startIndex, elementCount);
+                    Assert.AreEqual(
+                        savedDataBytes.Skip(startIndex).Take(elementCount).ToArray(),
+                        readDataBytes.Skip(startIndex).Take(elementCount).ToArray());
+                }
+            };
+            Game.RunOneFrame();
+        }
+
+        //[TestCase(true)]
+        [TestCase(false, 1, -1, false, typeof(ArgumentOutOfRangeException))]
+        [TestCase(false, 80, 0, true, null)]
+        [TestCase(false, 80, 1, true, null)]
+        [TestCase(false, 1, 2, true, null)]
+        [TestCase(false, 1, 40, true, null)]
+        [TestCase(false, 2, 40, true, null)]
+        [TestCase(false, 2, 80, false, typeof(InvalidOperationException))]
+        [TestCase(false, 1, 80, true, null)]
+        [TestCase(false, 1, 81, true, null)]
+        [TestCase(false, 2, 81, false, typeof(InvalidOperationException))]
+        public void SetDataWithElementCountAndVertexStride(bool dynamic, int elementCount, int vertexStride, bool shouldSucceed, Type expectedExceptionType)
+        {
+            Game.DrawWith += (sender, e) =>
+            {
+                var vertexBuffer = (dynamic)
+                    ? new DynamicVertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length,
+                        BufferUsage.None)
+                    : new VertexBuffer(Game.GraphicsDevice, typeof(VertexPositionTexture), savedData.Length,
+                        BufferUsage.None);
+                var savedDataBytes = ArrayUtil.ConvertFrom(savedData);
+
+                if (!shouldSucceed)
+                    Assert.Throws(expectedExceptionType, () => vertexBuffer.SetData(0, savedDataBytes, 0, elementCount, vertexStride));
+                else
+                {
+                    vertexBuffer.SetData(0, savedDataBytes, 0, elementCount, vertexStride);
+
+                    var readDataBytes = new byte[savedDataBytes.Length];
+                    vertexBuffer.GetData(0, readDataBytes, 0, elementCount, vertexStride);
+                    Assert.AreEqual(
+                        savedDataBytes.Take(elementCount).ToArray(), 
+                        readDataBytes.Take(elementCount).ToArray());
+                }
             };
             Game.RunOneFrame();
         }

Test/Runner/Utility.cs
@@ -115,6 +115,23 @@ namespace MonoGame.Tests {
 
             return dest;
         }
+
+        public static byte[] ConvertFrom<T>(T[] source) where T : struct
+        {
+            var sizeOfSource = Marshal.SizeOf(typeof(T));
+            var count = source.Length;
+            var dest = new byte[sizeOfSource * count];
+
+            var pinned = GCHandle.Alloc(dest, GCHandleType.Pinned);
+            var pointer = pinned.AddrOfPinnedObject();
+
+            for (var i = 0; i < count; i++, pointer += sizeOfSource)
+                Marshal.StructureToPtr(source[i], pointer, true);
+
+            pinned.Free();
+
+            return dest;
+        }
     }
 
 	static class MathUtility 