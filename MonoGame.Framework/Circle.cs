c297a49bb8edba33a6a33e756e1a991462436682
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-01-17 00:51:39 -0500

    Added Circle

---
+++ MonoGame.Framework/Circle.cs
@@ -0,0 +1,483 @@
+// MIT License - Copyright (C) The Mono.Xna Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+
+namespace Microsoft.Xna.Framework
+{
+	/// <summary>
+	/// Describes a 2D-circle. 
+	/// </summary>
+	[DataContract]
+	[DebuggerDisplay("{DebugDisplayString,nq}")]
+	public struct Circle : IEquatable<Circle>
+	{
+		#region Private Fields
+
+		private static Circle emptyCircle = new Circle();
+
+		#endregion
+
+		#region Public Fields
+
+		/// <summary>
+		/// The point representing the center of this <see cref="Circle"/>.
+		/// </summary>
+		[DataMember]
+		public Vector2 Center;
+
+		/// <summary>
+		/// The radius from the center of this <see cref="Circle"/>.
+		/// </summary>
+		[DataMember]
+		public float Radius;
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Returns a <see cref="Circle"/> with Point = Vector2.Zero and Radius= 0.
+		/// </summary>
+		public static Circle Empty
+		{
+			get { return emptyCircle; }
+		}
+
+		/// <summary>
+		/// Returns the x coordinate of the far left point of this <see cref="Circle"/>.
+		/// </summary>
+		public float Left
+		{
+			get { return (this.Center.X - this.Radius); }
+		}
+
+		/// <summary>
+		/// Returns the x coordinate of the far right point of this <see cref="Circle"/>.
+		/// </summary>
+		public float Right
+		{
+			get { return (this.Center.X + this.Radius); }
+		}
+
+		/// <summary>
+		/// Returns the y coordinate of the far top point of this <see cref="Circle"/>.
+		/// </summary>
+		public float Top
+		{
+			get { return (this.Center.Y - this.Radius); }
+		}
+
+		/// <summary>
+		/// Returns the y coordinate of the far bottom point of this <see cref="Circle"/>.
+		/// </summary>
+		public float Bottom
+		{
+			get { return (this.Center.Y + this.Radius); }
+		}
+
+		/// <summary>
+		/// The center coordinates of this <see cref="Circle"/>.
+		/// </summary>
+		public Point Location
+		{
+			get
+			{
+				return this.Center.ToPoint();
+			}
+			set
+			{
+				this.Center.X = value.X;
+				this.Center.Y = value.Y;
+			}
+		}
+
+		/// <summary>
+		/// Returns the diameter of this <see cref="Circle"/>
+		/// </summary>
+		public float Diameter
+		{
+			get { return (this.Radius * 2.0f); }
+		}
+
+		/// <summary>
+		/// Returns the Circumference of this <see cref="Circle"/>
+		/// </summary>
+		public float Circumference
+		{
+			get { return (2.0f * MathHelper.Pi * Radius); }
+		}
+
+		/// <summary>
+		/// Whether or not this <see cref="Circle"/> has a <see cref="Center"/> and
+		/// <see cref="Radius"/> of 0.
+		/// </summary>
+		public bool IsEmpty
+		{
+			get
+			{
+				return ((this.Radius == 0) && (this.Center == Vector2.Zero));
+			}
+		}
+
+		#endregion
+
+		#region Internal Properties
+
+		internal string DebugDisplayString
+		{
+			get
+			{
+				return string.Concat(
+					this.Center.ToString(), "  ",
+					this.Radius
+					);
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Creates a new instance of <see cref="Circle"/> struct, with the specified
+		/// position, and radius
+		/// </summary>
+		/// <param name="center">The position of the center of the created <see cref="Circle"/>.</param>
+		/// <param name="radius">The radius of the created <see cref="Circle"/>.</param>
+		public Circle(Vector2 center, float radius)
+		{
+			this.Center = center;
+			this.Radius = radius;
+		}
+
+		#endregion
+
+		#region Operators
+
+		/// <summary>
+		/// Compares whether two <see cref="Circle"/> instances are equal.
+		/// </summary>
+		/// <param name="a"><see cref="Circle"/> instance on the left of the equal sign.</param>
+		/// <param name="b"><see cref="Circle"/> instance on the right of the equal sign.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public static bool operator ==(Circle a, Circle b)
+		{
+			return ((a.Center == b.Center) && (a.Radius == b.Radius));
+		}
+
+		/// <summary>
+		/// Compares whether two <see cref="Circle"/> instances are not equal.
+		/// </summary>
+		/// <param name="a"><see cref="Circle"/> instance on the left of the not equal sign.</param>
+		/// <param name="b"><see cref="Circle"/> instance on the right of the not equal sign.</param>
+		/// <returns><c>true</c> if the instances are not equal; <c>false</c> otherwise.</returns>
+		public static bool operator !=(Circle a, Circle b)
+		{
+			return !(a == b);
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Gets the point at the edge of this <see cref="Circle"/> from the provided angle
+		/// </summary>
+		/// <param name="angle">an angle in radians</param>
+		/// <returns><see cref="Vector2"/> representing the point on this <see cref="Circle"/>'s surface at the specified angle</returns>
+		public Vector2 GetPointAlongEdge(float angle)
+		{
+			return new Vector2(Center.X + (Radius * (float)Math.Cos((double)angle)),
+							   Center.Y + (Radius * (float)Math.Sin((double)angle)));
+		}
+
+
+		/// <summary>
+		/// Gets whether or not the provided coordinates lie within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="x">The x coordinate of the point to check for containment.</param>
+		/// <param name="y">The y coordinate of the point to check for containment.</param>
+		/// <returns><c>true</c> if the provided coordinates lie inside this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Contains(float x, float y)
+		{
+			return ((new Vector2(x, y) - Center).Length() <= Radius);
+		}
+		
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Point"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The coordinates to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <returns><c>true</c> if the provided <see cref="Point"/> lies inside this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Contains(Point value)
+		{
+			return ((value.ToVector2() - Center).Length() <= Radius);
+		}
+
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Point"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The coordinates to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <param name="result"><c>true</c> if the provided <see cref="Point"/> lies inside this <see cref="Circle"/>; <c>false</c> otherwise. As an output parameter.</param>
+		public void Contains(ref Point value, out bool result)
+		{
+			result = ((value.ToVector2() - Center).Length() <= Radius);
+		}
+
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Vector2"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The coordinates to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <returns><c>true</c> if the provided <see cref="Vector2"/> lies inside this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Contains(Vector2 value)
+		{
+			return ((value - Center).Length() <= Radius);
+		}
+
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Vector2"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The coordinates to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <param name="result"><c>true</c> if the provided <see cref="Vector2"/> lies inside this <see cref="Circle"/>; <c>false</c> otherwise. As an output parameter.</param>
+		public void Contains(ref Vector2 value, out bool result)
+		{
+			result = ((value - Center).Length() <= Radius);
+		}
+
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Circle"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The <see cref="Circle"/> to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <returns><c>true</c> if the provided <see cref="Circle"/>'s center lie entirely inside this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Contains(Circle value)
+		{
+			Vector2 distanceOfCenter = value.Center - Center;
+			float radii = Radius - value.Radius;
+
+			return ((distanceOfCenter.X * distanceOfCenter.X) + (distanceOfCenter.Y * distanceOfCenter.Y) <= Math.Abs(radii * radii));
+		}
+
+		/// <summary>
+		/// Gets whether or not the provided <see cref="Circle"/> lies within the bounds of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">The <see cref="Circle"/> to check for inclusion in this <see cref="Circle"/>.</param>
+		/// <param name="result"><c>true</c> if the provided <see cref="Circle"/>'s center lie entirely inside this <see cref="Circle"/>; <c>false</c> otherwise. As an output parameter.</param>
+		public void Contains(ref Circle value, out bool result)
+		{
+			Vector2 distanceOfCenter = value.Center - Center;
+			float radii = Radius - value.Radius;
+
+			result = ((distanceOfCenter.X * distanceOfCenter.X) + (distanceOfCenter.Y * distanceOfCenter.Y) <= Math.Abs(radii * radii));
+		}
+
+		/// <summary>
+		/// Compares whether current instance is equal to specified <see cref="Object"/>.
+		/// </summary>
+		/// <param name="obj">The <see cref="Object"/> to compare.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public override bool Equals(object obj)
+		{
+			return (obj is Circle) && this == ((Circle)obj);
+		}
+
+		/// <summary>
+		/// Compares whether current instance is equal to specified <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="other">The <see cref="Circle"/> to compare.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public bool Equals(Circle other)
+		{
+			return this == other;
+		}
+
+		/// <summary>
+		/// Gets the hash code of this <see cref="Circle"/>.
+		/// </summary>
+		/// <returns>Hash code of this <see cref="Circle"/>.</returns>
+		public override int GetHashCode()
+		{
+			return (Center.GetHashCode() ^ Radius.GetHashCode());
+		}
+
+		/// <summary>
+		/// Adjusts the size of this <see cref="Circle"/> by specified radius amount. 
+		/// </summary>
+		/// <param name="radiusAmount">Value to adjust the radius by.</param>
+		public void Inflate(float radiusAmount)
+		{
+			Center -= new Vector2(radiusAmount);
+			Radius += radiusAmount * 2;
+		}
+
+		/// <summary>
+		/// Gets whether or not a specified <see cref="Circle"/> intersects with this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">Other <see cref="Circle"/>.</param>
+		/// <returns><c>true</c> if other <see cref="Circle"/> intersects with this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Intersects(Circle value)
+		{
+			Vector2 distanceOfCenter = value.Center - Center;
+			float radii = Radius + value.Radius;
+
+			return ((distanceOfCenter.X * distanceOfCenter.X) + (distanceOfCenter.Y * distanceOfCenter.Y) < (radii * radii));
+		}
+
+		/// <summary>
+		/// Gets whether or not a specified <see cref="Circle"/> intersects with this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">Other <see cref="Circle"/>.</param>
+		/// <param name="result"><c>true</c> if other <see cref="Circle"/> intersects with this <see cref="Circle"/>; <c>false</c> otherwise. As an output parameter.</param>
+		public void Intersects(ref Circle value, out bool result)
+		{
+			Vector2 distanceOfCenter = value.Center - Center;
+			float radii = Radius + value.Radius;
+
+			result = ((distanceOfCenter.X * distanceOfCenter.X) + (distanceOfCenter.Y * distanceOfCenter.Y) < (radii * radii));
+		}
+
+		/// <summary>
+		/// Gets whether or not a specified <see cref="Rectangle"/> intersects with this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="value">Other <see cref="Rectangle"/>.</param>
+		/// <returns><c>true</c> if other <see cref="Rectangle"/> intersects with this <see cref="Circle"/>; <c>false</c> otherwise.</returns>
+		public bool Intersects(Rectangle value)
+		{
+			Vector2 distance = new Vector2(Math.Abs(Center.X - value.X), Math.Abs(Center.Y - value.Y));
+
+			if (distance.X > (value.Width / 2.0f + Radius))  
+				return false;
+			if (distance.Y > (value.Height / 2.0f + Radius)) 
+				return false;
+
+			if (distance.X <= (value.Width / 2.0f))
+				return true;
+			if (distance.Y <= (value.Height / 2.0f))
+				return true;
+
+
+			float distanceOfCorners = ((distance.X - value.Width / 2.0f) * (distance.X - value.Width / 2.0f) +
+									   (distance.Y - value.Height / 2.0f) * (distance.Y - value.Height / 2.0f));
+
+			return (distanceOfCorners <= (Radius * Radius));
+		}
+
+		/*
+		/// <summary>
+		/// Creates a new <see cref="Circle"/> that contains overlapping region of two other circles.
+		/// </summary>
+		/// <param name="value1">The first <see cref="Circle"/>.</param>
+		/// <param name="value2">The second <see cref="Circle"/>.</param>
+		/// <returns>Overlapping region of the two circles.</returns>
+		public static Circle Intersect(Circle value1, Circle value2)
+		{
+			Circle circle;
+			Intersect(ref value1, ref value2, out circle);
+			return circle;
+		}
+		*/
+
+		/*
+		/// <summary>
+		/// Creates a new <see cref="Circle"/> that contains overlapping region of two other circles.
+		/// </summary>
+		/// <param name="value1">The first <see cref="Circle"/>.</param>
+		/// <param name="value2">The second <see cref="Circle"/>.</param>
+		/// <param name="result">Overlapping region of the two circles as an output parameter.</param>
+		public static void Intersect(ref Circle value1, ref Circle value2, out Circle result)
+		{
+			if (value1.Intersects(value2))
+			{
+				throw new NotImplementedException();
+			}
+			else
+			{
+				result = new Circle(Vector2.Zero, 0.0f);
+			}
+		}
+		*/
+
+		/// <summary>
+		/// Changes the <see cref="Location"/> of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="offsetX">The x coordinate to add to this <see cref="Circle"/>.</param>
+		/// <param name="offsetY">The y coordinate to add to this <see cref="Circle"/>.</param>
+		public void Offset(float offsetX, float offsetY)
+		{
+			Center.X += (int)offsetX;
+			Center.Y += (int)offsetY;
+		}
+
+		/// <summary>
+		/// Changes the <see cref="Location"/> of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="amount">The x and y components to add to this <see cref="Circle"/>.</param>
+		public void Offset(Point amount)
+		{
+			Center.X += amount.X;
+			Center.Y += amount.Y;
+		}
+
+		/// <summary>
+		/// Changes the <see cref="Location"/> of this <see cref="Circle"/>.
+		/// </summary>
+		/// <param name="amount">The x and y components to add to this <see cref="Circle"/>.</param>
+		public void Offset(Vector2 amount)
+		{
+			Center.X += amount.X;
+			Center.Y += amount.Y;
+		}
+
+		/// <summary>
+		/// Returns a <see cref="String"/> representation of this <see cref="Circle"/> in the format:
+		/// {Center:[<see cref="Center"/>] Radius:[<see cref="Radius"/>]}
+		/// </summary>
+		/// <returns><see cref="String"/> representation of this <see cref="Circle"/>.</returns>
+		public override string ToString()
+		{
+			return "{Center:" + Center.ToString() + " Radius:" + Radius + "}";
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Rectangle"/> large enough to fit this <see cref="Circle"/>  
+		/// </summary>
+		/// <returns><see cref="Rectangle"/> which contains this <see cref="Circle"/></returns>
+		public Rectangle ToRectangle()
+		{
+			return new Rectangle((int)(Center.X - (Radius / 2.0f)),
+								 (int)(Center.Y - (Radius / 2.0f)),
+								 (int)Radius,
+								 (int)Radius);
+		}
+
+		/*
+		/// <summary>
+		/// Creates a new <see cref="Circle"/> that completely contains two other circles.
+		/// </summary>
+		/// <param name="value1">The first <see cref="Circle"/>.</param>
+		/// <param name="value2">The second <see cref="Circle"/>.</param>
+		/// <returns>The union of the two circles.</returns>
+		public static Circle Union(Circle value1, Circle value2)
+		{
+			throw new NotImplementedException();
+		}
+		*/
+
+		/*
+		/// <summary>
+		/// Creates a new <see cref="Circle"/> that completely contains two other circles.
+		/// </summary>
+		/// <param name="value1">The first <see cref="Circle"/>.</param>
+		/// <param name="value2">The second <see cref="Circle"/>.</param>
+		/// <param name="result">The union of the two circles as an output parameter.</param>
+		public static void Union(ref Circle value1, ref Circle value2, out Circle result)
+		{
+			throw new NotImplementedException();
+		}
+		*/
+				
+		#endregion
+	}
+}
---
+++ Test/Framework/CircleTest.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Framework
+{
+    class CircleTest
+    {
+        [Test]
+        public void ConstructorsAndProperties()
+        {
+#if !XNA
+            var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+
+            // Constructor
+
+            Assert.AreEqual(new Circle() { Center = new Vector2(200.0f, 300.0f), Radius = 100.0f }, circle);
+
+            // Left property
+
+            Assert.AreEqual(200.0f - 100.0f, circle.Left);
+
+            // Right property
+
+            Assert.AreEqual(200.0f + 100.0f, circle.Right);
+
+            // Top property
+
+            Assert.AreEqual(300.0f - 100.0f, circle.Top);
+
+            // Bottom property
+
+            Assert.AreEqual(300.0f + 100.0f, circle.Bottom);
+
+            // Location property
+
+            Assert.AreEqual(new Point(200, 300), circle.Location);
+
+            // Center property
+
+            Assert.AreEqual(new Vector2(200.0f, 300.0f), circle.Center);
+
+            // Radius property
+
+            Assert.AreEqual(100.0f, circle.Radius);
+
+            // IsEmpty property
+
+            Assert.AreEqual(false, circle.IsEmpty);
+            Assert.AreEqual(true, new Circle().IsEmpty);
+
+            // Empty - static property 
+
+            Assert.AreEqual(new Circle(),Circle.Empty);
+#endif
+        }
+
+#if !XNA
+
+
+        [Test]
+        public void ContainsPoint()
+        {
+            var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+
+            var p1 = new Point(-1, -1);
+            var p2 = new Point(110, 300);
+            var p3 = new Point(200, 300);
+            var p4 = new Point(290, 300);
+            var p5 = new Point(400, 400);
+
+            bool result;
+
+            circle.Contains(ref p1, out result);
+            Assert.AreEqual(false, result);
+            circle.Contains(ref p2, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p3, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p4, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p5, out result);
+            Assert.AreEqual(false, result);
+
+            Assert.AreEqual(false, circle.Contains(p1));
+            Assert.AreEqual(true, circle.Contains(p2));
+            Assert.AreEqual(true, circle.Contains(p3));
+            Assert.AreEqual(true, circle.Contains(p4));
+            Assert.AreEqual(false, circle.Contains(p5));
+        }
+
+        [Test]
+        public void ContainsVector2()
+        {
+             var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+
+            var p1 = new Vector2(-1, -1);
+            var p2 = new Vector2(110, 300);
+            var p3 = new Vector2(200, 300);
+            var p4 = new Vector2(290, 300);
+            var p5 = new Vector2(400, 400);
+
+            bool result;
+
+            circle.Contains(ref p1, out result);
+            Assert.AreEqual(false, result);
+            circle.Contains(ref p2, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p3, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p4, out result);
+            Assert.AreEqual(true, result);
+            circle.Contains(ref p5, out result);
+            Assert.AreEqual(false, result);
+
+            Assert.AreEqual(false, circle.Contains(p1));
+            Assert.AreEqual(true, circle.Contains(p2));
+            Assert.AreEqual(true, circle.Contains(p3));
+            Assert.AreEqual(true, circle.Contains(p4));
+            Assert.AreEqual(false, circle.Contains(p5));
+        }
+
+        [Test]
+        public void ContainsFloats()
+        {
+            var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+
+            float x1 = -1;   float y1 = -1;
+            float x2 = 110; float y2 = 300;
+            float x3 = 200; float y3 = 300;
+            float x4 = 290; float y4 = 300;
+            float x5 = 400; float y5 = 400;
+
+            Assert.AreEqual(false, circle.Contains(x1, y1));
+            Assert.AreEqual(true, circle.Contains(x2, y2));
+            Assert.AreEqual(true, circle.Contains(x3, y3));
+            Assert.AreEqual(true, circle.Contains(x4, y4));
+            Assert.AreEqual(false, circle.Contains(x5, y5));
+        }
+
+        [Test]
+        public void ContainsCircle()
+        {
+            var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+
+            var circ1 = new Circle(new Vector2(199.0f, 299.0f), 100.0f);
+            var circ2 = new Circle(new Vector2(200.0f, 300.0f), 25.0f);
+            var circ3 = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+            var circ4 = new Circle(new Vector2(201.0f, 301.0f), 100.0f);
+
+            bool result;
+
+            circle.Contains(ref circ1, out result);
+
+            Assert.AreEqual(false, result);
+
+            circle.Contains(ref circ2, out result);
+
+            Assert.AreEqual(true, result);
+
+            circle.Contains(ref circ3, out result);
+
+            Assert.AreEqual(true, result);
+
+            circle.Contains(ref circ4, out result);
+
+            Assert.AreEqual(false, result);
+
+            Assert.AreEqual(false, circle.Contains(circ1));
+            Assert.AreEqual(true, circle.Contains(circ2));
+            Assert.AreEqual(true, circle.Contains(circ3));
+            Assert.AreEqual(false, circle.Contains(circ4));
+        }
+
+        [Test]
+        public void Inflate()
+        {
+            var circle = new Circle(new Vector2(200.0f, 300.0f), 100.0f);
+            circle.Inflate(100.0f);
+            Assert.AreEqual(new Circle(new Vector2(100.0f, 200.0f), 300.0f), circle);
+        }
+        
+        [Test]
+        public void ToStringTest()
+        {
+            Assert.AreEqual("{Center:{{X:200 Y:300}} Radius:100}", new Circle(new Vector2(200.0f, 300.0f), 100.0f).ToString());
+        }
+
+        [Test]
+        public void ToRectangleTest()
+        {
+            Assert.AreEqual(new Rectangle(150, 250, 100, 100), new Circle(new Vector2(200.0f, 300.0f), 100.0f).ToRectangle());
+        }
+
+#endif
+    }
+}
