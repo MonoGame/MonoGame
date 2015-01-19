dbf37741e14b985e9947a02032f97bced7b1b461
Simon (darkside) Jackson <darkside@zenithmoon.com>
2015-01-19 11:04:32 +0000

    Updated NuSpec's based on feedback

--- NuGetPackages/tools/MonoGame.Framework/Linux/install.ps1
+++ NuGetPackages/tools/MonoGame.Framework/Linux/install.ps1
@@ -1,6 +1,8 @@
 param($installPath, $toolsPath, $package, $project)
 $file1 = $project.ProjectItems.Item("SDL_mixer.dll")
 $file2 = $project.ProjectItems.Item("SDL.dll")
+$file3 = $project.ProjectItems.Item("OpenTK.dll.config")
+$file4 = $project.ProjectItems.Item("Tao.Sdl.dll.config")
 
 // set 'Copy To Output Directory' to 'Copy if newer'
 $copyToOutput1 = $file1.Properties.Item("CopyToOutputDirectory")
@@ -8,3 +10,9 @@ $copyToOutput1.Value = 2
 
 $copyToOutput2 = $file2.Properties.Item("CopyToOutputDirectory")
 $copyToOutput2.Value = 2
+
+$copyToOutput3 = $file3.Properties.Item("CopyToOutputDirectory")
+$copyToOutput3.Value = 2
+
+$copyToOutput4 = $file4.Properties.Item("CopyToOutputDirectory")
+$copyToOutput4.Value = 2
