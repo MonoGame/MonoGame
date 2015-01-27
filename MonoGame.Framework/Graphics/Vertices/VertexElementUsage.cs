cefff44db228a1d696ac3b79a89a57aa53bffc40
Shqrdx <chaosdev@mail.ru>
2015-01-27 19:54:54 +0300

    Docs added.

--- MonoGame.Framework/Graphics/Vertices/VertexElementUsage.cs
+++ MonoGame.Framework/Graphics/Vertices/VertexElementUsage.cs
@@ -1,19 +1,65 @@
-﻿namespace Microsoft.Xna.Framework.Graphics
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework.Graphics
 {
+    /// <summary>
+    /// Defines usage for vertex elements.
+    /// </summary>
     public enum VertexElementUsage
     {
+        /// <summary>
+        /// Position data.
+        /// </summary>
         Position,
-        Color,
-        TextureCoordinate,
+        /// <summary>
+        /// Color data.
+        /// </summary>
+        Color,   
+        /// <summary>
+        /// Texture coordinate data or can be used for user-defined data.
+        /// </summary>
+        TextureCoordinate, 
+        /// <summary>
+        /// Normal data.
+        /// </summary>
         Normal,
+        /// <summary>
+        /// Binormal data.
+        /// </summary>
         Binormal,
+        /// <summary>
+        /// Tangent data.
+        /// </summary>
         Tangent,
+        /// <summary>
+        /// Blending indices data.
+        /// </summary>
         BlendIndices,
-        BlendWeight,
+        /// <summary>
+        /// Blending weight data.
+        /// </summary>
+        BlendWeight,     
+        /// <summary>
+        /// Depth data.
+        /// </summary>
         Depth,
-        Fog,
+        /// <summary>
+        /// Fog data.
+        /// </summary>
+        Fog,      
+        /// <summary>
+        /// Point size data. Usable for drawing point sprites.
+        /// </summary>
         PointSize,
-        Sample,
+        /// <summary>
+        /// Sampler data for specifies the displacement value to look up.
+        /// </summary>
+        Sample,     
+        /// <summary>
+        /// Single, positive float value, specifies a tessellation factor used in the tessellation unit to control the rate of tessellation.
+        /// </summary>
         TessellateFactor
     }
 }
