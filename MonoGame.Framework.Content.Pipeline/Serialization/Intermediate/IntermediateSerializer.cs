MonoGame/MonoGame

commit 54cdb05f1dfaaec05c13582b26d1d5c5c04b09ba

Add support for polymorphic generic types in IntermediateSerializer

MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/IntermediateSerializer.cs
@@ -242,6 +242,21 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
                 typeName = typeName.Replace(pair.Key, pair.Value);
             var expandedName = typeName;
 
+            // If this a generic type, handle it separately.
+            if (typeName.EndsWith("]"))
+            {
+                var openBracketIndex = typeName.IndexOf("[");
+
+                var typeNameWithoutArguments = typeName.Substring(0, openBracketIndex);
+
+                var genericArgumentsString = typeName.Substring(openBracketIndex + 1, typeName.Length - openBracketIndex - 2);
+                var genericArgumentsArray = genericArgumentsString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var genericArguments = genericArgumentsArray.Select(FindType).ToArray();
+
+                foundType = FindType(typeNameWithoutArguments + "`" + genericArguments.Length);
+                return (foundType == null) ? null : foundType.MakeGenericType(genericArguments);
+            }
+
             foundType = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
                          from type in assembly.GetTypes()
                          where type.FullName == typeName || type.Name == typeName
@@ -256,7 +271,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
         /// <summary>
         /// Gets the (potentially) aliased name for any type.
         /// </summary>
-        internal string GetTypeName(Type type)
+        internal string GetFullTypeName(Type type)
         {
             string typeName;
 
@@ -269,7 +284,35 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
                 return typeName;
 
             // Fallback to full type name.
-            return type.FullName;
+            var typeNamespace = type.Namespace;
+            if (!string.IsNullOrEmpty(typeNamespace))
+                typeName = typeNamespace + ".";
+            typeName += GetTypeName(type);
+
+            return typeName;
+        }
+
+        /// <summary>
+        /// Returns the name of the type, without the namespace.
+        /// For generic types, we add the type parameters in square brackets.
+        /// i.e. List&lt;int&gt; becomes List[int]
+        /// </summary>
+        internal string GetTypeName(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var typeName = type.Name;
+                int genericBacktickIndex = typeName.IndexOf("`");
+                if (genericBacktickIndex >= 0)
+                    typeName = typeName.Substring(0, genericBacktickIndex);
+
+                var result = typeName + "[";
+                result += string.Join(",", type.GetGenericArguments().Select(GetFullTypeName));
+                result += "]";
+                return result;
+            }
+
+            return type.Name;
         }
 
         internal bool AlreadyScanned(object value)

MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/IntermediateWriter.cs
@@ -213,7 +213,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
 
         public void WriteTypeName(Type type)
         {
-            Xml.WriteString(Serializer.GetTypeName(type));
+            Xml.WriteString(Serializer.GetFullTypeName(type));
         }
     }        
 }

MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/NamespaceAliasHelper.cs
@@ -148,7 +148,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
                 AliasedNamespace namespaceAlias;
                 if (_namespaceLookupReverse.TryGetValue(type.Namespace, out namespaceAlias))
                 {
-                    typeName = namespaceAlias.Alias + ":" + namespaceAlias.TypePrefix + type.Name;
+                    typeName = namespaceAlias.Alias + ":" + namespaceAlias.TypePrefix + _serializer.GetTypeName(type);
                     return true;
                 }
             }

Test/Assets/Xml/13_PolymorphicTypes.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<XnaContent>
+<XnaContent xmlns:Generic="System.Collections.Generic">
   <Asset Type="PolymorphicTypes">
     <Hello Type="string">World</Hello>
     <Elf Type="int">23</Elf>
@@ -25,5 +25,20 @@
         <Value>true</Value>
       </Item>
     </UntypedArray>
+    <IntCollection Type="Generic:List[int]">1 4 6</IntCollection>
+    <UntypedDictionary Type="Generic:Dictionary[int,PolymorphicA]">
+      <Item>
+        <Key>1</Key>
+        <Value>
+          <Value>true</Value>
+        </Value>
+      </Item>
+      <Item>
+        <Key>5</Key>
+        <Value>
+          <Value>false</Value>
+        </Value>
+      </Item>
+    </UntypedDictionary>
   </Asset>
 </XnaContent>

Test/ContentPipeline/AssetTestClasses.cs
@@ -232,6 +232,8 @@ public class PolymorphicTypes
     public object Elf;
     public PolymorphicA[] TypedArray;
     public Array UntypedArray;
+    public ICollection<int> IntCollection;
+    public object UntypedDictionary;
 }
 #endregion
 

Test/ContentPipeline/IntermediateDeserializerTest.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web;
 using System.Xml;
 using Microsoft.Xna.Framework;
@@ -308,6 +309,19 @@ namespace MonoGame.Tests.ContentPipeline
                 Assert.AreEqual(true, ((PolymorphicB)polymorphicTypes.UntypedArray.GetValue(1)).Value);
                 Assert.IsAssignableFrom<PolymorphicC>(polymorphicTypes.UntypedArray.GetValue(2));
                 Assert.AreEqual(true, ((PolymorphicC)polymorphicTypes.UntypedArray.GetValue(2)).Value);
+
+                Assert.NotNull(polymorphicTypes.IntCollection);
+                Assert.IsInstanceOf<List<int>>(polymorphicTypes.IntCollection);
+                Assert.AreEqual(3, polymorphicTypes.IntCollection.Count);
+                Assert.AreEqual(1, polymorphicTypes.IntCollection.ElementAt(0));
+                Assert.AreEqual(4, polymorphicTypes.IntCollection.ElementAt(1));
+                Assert.AreEqual(6, polymorphicTypes.IntCollection.ElementAt(2));
+
+                Assert.NotNull(polymorphicTypes.UntypedDictionary);
+                Assert.IsInstanceOf<Dictionary<int, PolymorphicA>>(polymorphicTypes.UntypedDictionary);
+                Assert.AreEqual(2, ((Dictionary<int, PolymorphicA>) polymorphicTypes.UntypedDictionary).Count);
+                Assert.AreEqual(true, ((Dictionary<int, PolymorphicA>) polymorphicTypes.UntypedDictionary)[1].Value);
+                Assert.AreEqual(false, ((Dictionary<int, PolymorphicA>) polymorphicTypes.UntypedDictionary)[5].Value);
             });
         }
 

Test/ContentPipeline/IntermediateSerializerTest.cs
@@ -266,6 +266,14 @@ namespace MonoGame.Tests.ContentPipeline
                     new PolymorphicA { Value = true },
                     new PolymorphicB { Value = true },
                     new PolymorphicC { Value = true }
+                },
+
+                IntCollection = new List<int> { 1, 4, 6 },
+
+                UntypedDictionary = new Dictionary<int, PolymorphicA>
+                {
+                    { 1, new PolymorphicA { Value = true } },
+                    { 5, new PolymorphicA { Value = false } }
                 }
             });
         }

commit 3e4024cd914a5e6bb4f3ec71ad34c7d601d764dd

Restore support for serializing nested types
(broken in 54cdb05f1dfaaec05c13582b26d1d5c5c04b09ba)

MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/IntermediateSerializer.cs
@@ -319,6 +319,12 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
                 return result;
             }
 
+            if (type.IsArray)
+                return GetTypeName(type.GetElementType()) + "[]";
+
+            if (type.IsNested)
+                return type.DeclaringType.Name + "+" + type.Name;
+
             return type.Name;
         }
 