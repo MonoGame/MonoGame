f166c3a1dc6483e2846ee9609ddbade2bc4faf11
Shqrdx <chaosdev@mail.ru>
2015-02-12 12:21:31 +0300

    Fix

--- Test/Framework/EnumConformingTest.cs
+++ Test/Framework/EnumConformingTest.cs
@@ -56,14 +56,15 @@ namespace MonoGame.Tests.Framework
         [Test]
         public void StencilOperationEnum()
         {
-            Assert.AreEqual(0, (int)(StencilOperation.Keep));
-            Assert.AreEqual(1, (int)(StencilOperation.Zero));
-            Assert.AreEqual(2, (int)(StencilOperation.Replace));
-            Assert.AreEqual(3, (int)(StencilOperation.Increment));
-            Assert.AreEqual(4, (int)(StencilOperation.Decrement));
-            Assert.AreEqual(5, (int)(StencilOperation.IncrementSaturation));
-            Assert.AreEqual(6, (int)(StencilOperation.DecrementSaturation));
-            Assert.AreEqual(7, (int)(StencilOperation.Invert));
+            Assert.AreEqual(0, (int) (StencilOperation.Keep));
+            Assert.AreEqual(1, (int) (StencilOperation.Zero));
+            Assert.AreEqual(2, (int) (StencilOperation.Replace));
+            Assert.AreEqual(3, (int) (StencilOperation.Increment));
+            Assert.AreEqual(4, (int) (StencilOperation.Decrement));
+            Assert.AreEqual(5, (int) (StencilOperation.IncrementSaturation));
+            Assert.AreEqual(6, (int) (StencilOperation.DecrementSaturation));
+            Assert.AreEqual(7, (int) (StencilOperation.Invert));
+        }
 
         [Test]
         public void SurfaceFormateEnum()
