2a8af0262095e645dc8b40dda045c64b57e33a3f
Tim Jones <tim@timjones.tw>
2015-02-13 18:51:16 +0800

    Move GraphicsResource list to GraphicsDevice

--- Tools/2MGFX/MonoGame.Framework/GraphicsDevice.cs
+++ Tools/2MGFX/MonoGame.Framework/GraphicsDevice.cs
@@ -1,7 +1,18 @@
-﻿namespace Microsoft.Xna.Framework.Graphics
+﻿using System;
+
+namespace Microsoft.Xna.Framework.Graphics
 {
     // Dummy class for 2MGFX to compile.
     public class GraphicsDevice
     {
+        internal void AddResourceReference(WeakReference resourceReference)
+        {
+            
+        }
+
+        internal void RemoveResourceReference(WeakReference resourceReference)
+        {
+            
+        }
     }
 }
