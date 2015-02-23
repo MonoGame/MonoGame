95dff9d9e068f703cbb4b99968d63dbb4513ecc8
Ernst Uys <ernst@disportium.com>
2015-02-23 20:40:26 +0200

    Fix for triangle strip case of GetElementCountArray.

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -952,7 +952,7 @@ namespace Microsoft.Xna.Framework.Graphics
                 case PrimitiveType.TriangleList:
                     return primitiveCount * 3;
                 case PrimitiveType.TriangleStrip:
-                    return 3 + (primitiveCount - 1); // ???
+                    return primitiveCount + 2;
             }
 
             throw new NotSupportedException();
