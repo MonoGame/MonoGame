aef708b39ca65830c8595a0c45a2147048679d20
John Beshir <john@beshir.org>
2015-03-09 13:06:37 +0000

    Improve avoidance of redundant vertex buffer calls, make actually work.

--- MonoGame.Framework/Graphics/GraphicsDevice.OpenGL.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.OpenGL.cs
@@ -826,6 +826,7 @@ namespace Microsoft.Xna.Framework.Graphics
                     GraphicsExtensions.CheckGLError();
                     _vertexBufferApplied = false;
                 }
+                _vertexBufferDirty = false;
             }
 
             if (_vertexShader == null)
@@ -860,9 +861,10 @@ namespace Microsoft.Xna.Framework.Graphics
 			var target = PrimitiveTypeGL(primitiveType);
 			var vertexOffset = (IntPtr)(_vertexBuffer.VertexDeclaration.VertexStride * baseVertex);
 
-            if (!_vertexBufferApplied || !_vertexShaderApplied || _lastVertexOffset != vertexOffset)
+            bool vertexBufferChanged = !_vertexBufferApplied || _lastVertexOffset != vertexOffset;
+            if (vertexBufferChanged || !_vertexShaderApplied)
             {
-                _vertexBuffer.VertexDeclaration.Apply(_vertexShader, vertexOffset);
+                _vertexBuffer.VertexDeclaration.Apply(_vertexShader, vertexOffset, vertexBufferChanged);
                 _vertexBufferApplied = true;
                 _vertexShaderApplied = true;
                 _lastVertexOffset = vertexOffset;
@@ -891,7 +893,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
             // Setup the vertex declaration to point at the VB data.
             vertexDeclaration.GraphicsDevice = this;
-            vertexDeclaration.Apply(_vertexShader, vbHandle.AddrOfPinnedObject());
+            vertexDeclaration.Apply(_vertexShader, vbHandle.AddrOfPinnedObject(), true);
             _vertexBufferApplied = false;
 
             //Draw
@@ -908,9 +910,10 @@ namespace Microsoft.Xna.Framework.Graphics
         {
             ApplyState(true);
 
-            if (!_vertexBufferApplied || !_vertexShaderApplied || _lastVertexOffset != IntPtr.Zero)
+            bool vertexBufferChanged = !_vertexBufferApplied || _lastVertexOffset != IntPtr.Zero;
+            if (vertexBufferChanged || !_vertexShaderApplied)
             {
-                _vertexBuffer.VertexDeclaration.Apply(_vertexShader, IntPtr.Zero);
+                _vertexBuffer.VertexDeclaration.Apply(_vertexShader, IntPtr.Zero, vertexBufferChanged);
                 _vertexBufferApplied = true;
                 _vertexShaderApplied = true;
                 _lastVertexOffset = IntPtr.Zero;
@@ -941,7 +944,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
             // Setup the vertex declaration to point at the VB data.
             vertexDeclaration.GraphicsDevice = this;
-            vertexDeclaration.Apply(_vertexShader, vertexAddr);
+            vertexDeclaration.Apply(_vertexShader, vertexAddr, true);
             _vertexBufferApplied = false;
 
             //Draw
@@ -975,7 +978,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
             // Setup the vertex declaration to point at the VB data.
             vertexDeclaration.GraphicsDevice = this;
-            vertexDeclaration.Apply(_vertexShader, vertexAddr);
+            vertexDeclaration.Apply(_vertexShader, vertexAddr, true);
             _vertexBufferApplied = false;
 
             //Draw
