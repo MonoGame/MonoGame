0632ec611217985c2c4738b2f8f1777a056f0583
Steve Williams <slygamer@gmail.com>
2015-03-20 02:51:15 +1000

    Loop length in XNB now matches XNA The XNB File Format document specifies the loop start and loop length are in bytes, but they are in samples. Loop start and length are now populated in AudioContent when loading the sample data. The loop start and loop length are no longer treated as bytes in XAudio playback. XNA and MonoGame generated XNB files are now only different by one bit (1ms in duration).
    Fixes #3507

--- MonoGame.Framework.Content.Pipeline/Audio/AudioContent.cs
+++ MonoGame.Framework.Content.Pipeline/Audio/AudioContent.cs
@@ -281,6 +281,10 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Audio
                     channelCount,
                     format,
                     sampleRate);
+
+                // Loop start and length in number of samples. Defaults to entire sound
+                loopStart = 0;
+                loopLength = data.Count / ((bitsPerSample / 8) * channelCount);
             }
             finally
             {
