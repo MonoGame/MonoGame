7b7cf979fb4cd43cc268e9cb991889b687b87f64
Shqrdx <chaosdev@mail.ru>
2015-03-19 08:40:06 +0300

    New docs

--- MonoGame.Framework/Graphics/Vertices/IVertexType.cs
+++ MonoGame.Framework/Graphics/Vertices/IVertexType.cs
@@ -1,7 +1,17 @@
-﻿namespace Microsoft.Xna.Framework.Graphics
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework.Graphics
 {
+    /// <summary>
+    /// Interface which must be implemented for vertex types.
+    /// </summary>
     public interface IVertexType
     {
+        /// <summary>
+        /// Returns a vertex declaration for per-vertex data.
+        /// </summary>
         VertexDeclaration VertexDeclaration
         {
             get;
