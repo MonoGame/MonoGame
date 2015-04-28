b59a580445e9a5fe59c3eb8766711ea880099dbf
Shqrdx <chaosdev@mail.ru>
2015-04-28 08:26:02 +0300

    Added set accessor to GraphicsMetrics

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -164,7 +164,7 @@ namespace Microsoft.Xna.Framework.Graphics
         /// <summary>
         /// The rendering data for debugging and profiling. 
         /// </summary>
-        public GraphicsMetrics GraphicsMetrics { get { return _graphicsMetrics; } }
+        public GraphicsMetrics GraphicsMetrics { get { return _graphicsMetrics; } set { _graphicsMetrics = value; } }
 
         internal GraphicsDevice(GraphicsDeviceInformation gdi)
         {
