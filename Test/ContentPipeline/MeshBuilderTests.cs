14e2c895b0555f00e2d0ed49053406601041bab4
Mattias Cibien <mattias@mattiascibien.net>
2015-08-15 01:22:47 +0200

    Added unit test class and added files to protobuild definitions.

---
+++ Test/ContentPipeline/MeshBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.ContentPipeline
+{
+    class MeshBuilderTests
+    {
+        [Test]
+        public void BasicMeshBuilderTest()
+        {
+            
+        }
+    }
+}
