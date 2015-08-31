763e51fd6315b3345fd0fa26ee1d936272f596fd
Thomas Altenburger <mister.helmut@gmail.com>
2015-08-31 15:49:57 +0200

    Fix for NullReferenceException if trying to switch song rapidly

--- MonoGame.Framework/Media/MediaPlayer.WMS.cs
+++ MonoGame.Framework/Media/MediaPlayer.WMS.cs
@@ -152,7 +152,11 @@ namespace Microsoft.Xna.Framework.Media
                 _session.Stop();
                 _session.ClearTopologies();
                 _session.Close();
-                _volumeController.Dispose();
+                if (_volumeController != null)
+                {
+                    _volumeController.Dispose();
+                    _volumeController = null;
+                }
                 _clock.Dispose();
             }
 
@@ -198,8 +202,11 @@ namespace Microsoft.Xna.Framework.Media
             _session.ClearTopologies();
             _session.Stop();
             _session.Close();
-            _volumeController.Dispose();
-            _volumeController = null;
+            if (_volumeController != null)
+            {
+                _volumeController.Dispose();
+                _volumeController = null;
+            }
             _clock.Dispose();
             _clock = null;
         }
