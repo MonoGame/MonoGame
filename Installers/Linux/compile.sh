04c8c7323fb00254b2eb8c9719ccb9479a95177e
cra0zy <cra0zy@gmail.com>
2016-01-14 09:48:47 +0100

    Set correct permission to files before adding them to the deb package

--- Installers/Linux/compile.sh
+++ Installers/Linux/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+find tmp_deb/ -exec chown 1000:1000 {} \;
 dpkg --build tmp_deb monogame-sdk.deb
 ./../../ThirdParty/Dependencies/makeself/makeself.sh tmp_run/ monogame-sdk.run "Monogame Pipeline Installer" ./postinstall.sh
