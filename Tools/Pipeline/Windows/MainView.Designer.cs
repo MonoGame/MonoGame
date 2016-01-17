d4d9b7cd1b3ec7406a2f82414d28694024612060
cra0zy <cra0zy@gmail.com>
2016-01-17 15:16:21 +0100

    [Win Pipeline] Fix treeview events

--- Tools/Pipeline/Windows/MainView.Designer.cs
+++ Tools/Pipeline/Windows/MainView.Designer.cs
@@ -761,6 +761,11 @@ namespace MonoGame.Tools.Pipeline
             this._treeView.Name = "_treeView";
             this._treeView.Size = new System.Drawing.Size(249, 200);
             this._treeView.TabIndex = 0;
+            this._treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeViewAfterSelect);
+            this._treeView.DragDrop += new System.Windows.Forms.DragEventHandler(this._treeView_DragDrop);
+            this._treeView.DragOver += new System.Windows.Forms.DragEventHandler(this._treeView_DragOver);
+            this._treeView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TreeViewOnKeyDown);
+            this._treeView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TreeViewMouseUp);
             // 
             // _outputTabs
             // 
