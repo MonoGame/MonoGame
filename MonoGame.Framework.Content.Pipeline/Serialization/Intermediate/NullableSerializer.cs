0d7a383bf262fb22bfaac5e734d6158e9887e834
Tim Jones <tim@timjones.tw>
2016-01-28 01:05:09 +0800

    Fix nullable enum serialization bug
    Prior to this fix, serializing or deserializing a nullable enum using
    IntermediateSerializer would crash in NullableSerializer.Initialize
    because EnumSerializer couldn't be cast to ElementSerializer<T>.

--- MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/NullableSerializer.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/NullableSerializer.cs
@@ -9,25 +9,26 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate
     [ContentTypeSerializer]
     class NullableSerializer<T> : ContentTypeSerializer<T?> where T : struct
     {
-        private ElementSerializer<T> _serializer;
+        private ContentTypeSerializer _serializer;
+        private ContentSerializerAttribute _format;
 
         protected internal override void Initialize(IntermediateSerializer serializer)
         {
-            _serializer = (ElementSerializer<T>)serializer.GetTypeSerializer(typeof(T));
+            _serializer = serializer.GetTypeSerializer(typeof(T));
+            _format = new ContentSerializerAttribute
+            {
+                FlattenContent = true
+            };
         }
 
         protected internal override T? Deserialize(IntermediateReader input, ContentSerializerAttribute format, T? existingInstance)
         {
-            var list = new List<T>();
-            _serializer.Deserialize(input, list);
-            return list.First();            
+            return input.ReadRawObject<T>(_format, _serializer);
         }
 
         protected internal override void Serialize(IntermediateWriter output, T? value, ContentSerializerAttribute format)
         {
-            var results = new List<string>();
-            _serializer.Serialize(value.Value, results);
-            output.Xml.WriteRaw(results.First());
+            output.WriteRawObject<T>(value.Value, _format, _serializer);
         }
     }
 }
