6738f331995162cf3fea1752b84d17edc0aa1b31
Tim Jones <tim@timjones.tw>
2016-02-01 22:15:15 +0800

    Split platform-specific parts of RenderTargetCube into separate files

---
+++ MonoGame.Framework/Graphics/RenderTargetCube.OpenGL.cs
@@ -0,0 +1,16 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    public partial class RenderTargetCube
+    {
+        private void PlatformConstruct(GraphicsDevice graphicsDevice, SurfaceFormat preferredFormat, DepthFormat preferredDepthFormat, int preferredMultiSampleCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
