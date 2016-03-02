aa8e90eeb872832a9512276b926942577dd7d805
Dean Ellis <dellis1972@googlemail.com>
2016-03-02 15:07:11 +0000

    Split out Title Container into partial classes This should make porting to other platforms easier. Also implemented the GetAssets/GetFiles functionality from #4542

---
+++ MonoGame.Framework/TitleContainer.Android.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework.Utilities;
+using System.Collections.Generic;
+
+namespace Microsoft.Xna.Framework
+{
+    public static partial class TitleContainer
+    {
+        static TitleContainer() 
+        {
+            Location = String.Empty;
+        }
+
+        internal static Stream PlatformOpenStream (string safeName)
+        {
+            return Android.App.Application.Context.Assets.Open(safeName);
+        }
+
+        internal static List<string> PlatformGetFiles (string directory, string filter)
+        {
+            var results = new List<string>();
+            var files = Game.Activity.Assets.List (directory);
+            for (int i=files.Length; i > 0; i--)
+            {
+                var ext = Path.GetExtension (files[i]);
+                if (filter.EndsWith ( filter.EndsWith ("*") ? filter : ext))
+                {
+                    results.Add(files[i]);
+                }
+            }
+            return results;
+        }
+    }
+}
+
---
+++ MonoGame.Framework/TitleContainer.iOS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+#if IOS
+using Foundation;
+using UIKit;
+#endif
+
+namespace Microsoft.Xna.Framework
+{
+    public static partial class TitleContainer
+    {
+        static internal bool SupportRetina { get; private set; }
+        static internal int RetinaScale { get; private set; }
+
+         static TitleContainer() 
+        {
+            Location = NSBundle.MainBundle.ResourcePath;
+            SupportRetina = UIScreen.MainScreen.Scale >= 2.0f;
+            RetinaScale = (int)Math.Round(UIScreen.MainScreen.Scale);
+        }
+
+        internal static Stream PlatformOpenStream (string safeName)
+        {
+            var absolutePath = Path.Combine(Location, safeName);
+            if (SupportRetina)
+            {
+                for (var scale = RetinaScale; scale >= 2; scale--)
+                {
+                    // Insert the @#x immediately prior to the extension. If this file exists
+                    // and we are on a Retina device, return this file instead.
+                    var absolutePathX = Path.Combine(Path.GetDirectoryName(absolutePath),
+                        Path.GetFileNameWithoutExtension(absolutePath)
+                        + "@" + scale + "x" + Path.GetExtension(absolutePath));
+                    if (File.Exists(absolutePathX))
+                        return File.OpenRead(absolutePathX);
+                }
+            }
+            return File.OpenRead(absolutePath);
+        }
+
+        internal static List<string> PlatformGetFiles (string directory, string filter)
+        {
+            return new List<string> (Directory.GetFiles(directory, filter));
+        }
+    }
+}
+
