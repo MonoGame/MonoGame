40d707937bd0e5608897b7322241ddeb2f1190e9
cra0zy <cra0zy@gmail.com>
2016-03-13 15:56:52 +0100

    [DesktopGL] Fixed getting mouse position

--- MonoGame.Framework/Input/Mouse.cs
+++ MonoGame.Framework/Input/Mouse.cs
@@ -140,9 +140,10 @@ namespace Microsoft.Xna.Framework.Input
 #elif DESKTOPGL || ANGLE
 
             var state = OpenTK.Input.Mouse.GetCursorState();
+            var clientBounds = window.ClientBounds;
             
-            window.MouseState.X = state.X;
-            window.MouseState.Y = state.Y;
+            window.MouseState.X = state.X - clientBounds.X;
+            window.MouseState.Y = state.Y - clientBounds.Y;
 
             window.MouseState.LeftButton = (ButtonState)state.LeftButton;
             window.MouseState.RightButton = (ButtonState)state.RightButton;
