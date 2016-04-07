ea7724f88c5f7bc10fc9958b06437966107c5242
Tom Spilman <tom@sickheadgames.com>
2016-04-07 00:18:03 -0500

    Added partial test for back buffer MSAA.

--- Test/Framework/Visual/GraphicsDeviceManagerTest.cs
+++ Test/Framework/Visual/GraphicsDeviceManagerTest.cs
@@ -69,5 +69,102 @@ namespace MonoGame.Tests.Visual
             game.Run();
             game.Dispose();
         }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void MSAAEnabled(bool enabled)
+        {
+            var game = new TestGameBase();
+            var gdm = new GraphicsDeviceManager(game);
+            gdm.PreferMultiSampling = enabled;
+            gdm.GraphicsProfile = GraphicsProfile.HiDef;
+
+            gdm.PreparingDeviceSettings += (sender, args) =>
+            {
+                var pp = args.GraphicsDeviceInformation.PresentationParameters;
+                if (!enabled)
+                    Assert.AreEqual(0, pp.MultiSampleCount);
+                else
+                {
+                    Assert.Less(0, pp.MultiSampleCount);
+                    pp.MultiSampleCount = 1024;
+                }
+            };
+
+            Texture2D tex = null;
+            SpriteBatch spriteBatch = null;
+
+            game.InitializeWith += (sender, args) =>
+            {
+                tex = new Texture2D(game.GraphicsDevice, 1, 1);
+                tex.SetData(new[] { Color.White.PackedValue });
+                spriteBatch = new SpriteBatch(game.GraphicsDevice);
+            };
+
+            game.PreDrawWith += (sender, args) =>
+            {
+                if (enabled)
+                {
+                    var pp = game.GraphicsDevice.PresentationParameters;
+                    Assert.Less(0, pp.MultiSampleCount);
+                    Assert.AreNotEqual(1024, pp.MultiSampleCount);
+                }
+
+                game.GraphicsDevice.Clear(Color.Black);
+
+                spriteBatch.Begin();
+                spriteBatch.Draw(tex, new Vector2(800 / 2, 480 / 2), null, Color.White, MathHelper.ToRadians(45), new Vector2(0.5f), 200, SpriteEffects.None, 0);
+                spriteBatch.End();
+            };
+
+#if XNA
+            var data = new Color[800 * 480];
+            game.DrawWith += (sender, args) =>
+            {
+                game.GraphicsDevice.GetBackBufferData(data);
+            };
+#endif
+
+            game.ExitCondition = x => x.DrawNumber > 1;
+            game.Run();
+
+#if XNA
+            float black = 0;
+            float white = 0;
+            float grey = 0;
+            foreach (var c in data)
+            {
+                if (c == Color.Black)
+                    ++black;
+                else if (c == Color.White)
+                    ++white;
+                else if (c.R == c.G && c.G == c.B && c.R > 0 && c.R < 255)
+                    ++grey;
+            }
+
+            // General percentage of black and white pixels we should be getting.
+            black /= data.Length;
+            white /= data.Length;
+            Assert.Less(black, 0.9f);
+            Assert.Greater(black, 0.8f);
+            Assert.Less(white, 0.2f);
+            Assert.Greater(white, 0.1f);
+
+            // If enabled we should have at least a few grey pixels
+            // else we should have zero grey pixels.
+            grey /= data.Length;
+            if (!enabled)
+                Assert.AreEqual(0, grey);
+            else
+            {
+                Assert.Less(grey, 0.01f);
+                Assert.Greater(grey, 0.001f);
+            }
+#endif
+
+            tex.Dispose();
+            spriteBatch.Dispose();
+            game.Dispose();
+        }
     }
 }
