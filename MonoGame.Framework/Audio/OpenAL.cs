5b16b8f3ac5746a1f6891e37df95a3af36b13e33
cra0zy <cra0zy@gmail.com>
2016-04-26 15:43:28 +0200

    Fix wrong enum value in OpenAL

--- MonoGame.Framework/Audio/OpenAL.cs
+++ MonoGame.Framework/Audio/OpenAL.cs
@@ -50,7 +50,7 @@ namespace OpenAL
         Initial = 0x1011,
         Playing = 0x1012,
         Paused = 0x1013,
-        Stopped = 0x1012,
+        Stopped = 0x1014,
     }
 
     public enum ALListener3f
