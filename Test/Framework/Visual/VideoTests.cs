69a9547847757faad0ab8266e77ab6e7fe618bb3
Steve Williams <dev@konaju.com>
2016-05-09 13:17:23 +1000

    VideoPlayer - Added missing call to Dispose(false) in the finalizer. VideoPlayer.WMS - All MediaFoundation calls are asynchronous.  Need to use the event callback to know when to call the next MF function.  Added internal state to cater for the event system and still behave synchronous to the caller.  It gets interesting when some MF calls do not trigger an event due to the current state of the MF session. - The session clock was not a good indicator of state as it continues after the video has finished normally. - Looping and non-looping videos now work correctly. - Playing a video immediately after another video no longer throws an InvalidOperationException because the video did not appear to start the session correctly (revealed in unit tests).  Solved by handling events correctly. Tests - Added VideoImporterTests, VideoTests and related assets. - Corrected pathParths -> pathPaths.

---
+++ Test/Framework/Visual/VideoTests.cs
@@ -0,0 +1,101 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using NUnit.Framework;
+using System;
+
+namespace MonoGame.Tests.Visual
+{
+    [TestFixture]
+    class VideoTests : VisualTestFixtureBase
+    {
+        SpriteBatch _spriteBatch;
+        Video _video;
+        VideoPlayer _player;
+        TimeSpan _start;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            Game.LoadContentWith += (sender, e) =>
+            {
+                _spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+                _video = Game.Content.Load<Video>(Paths.Video("SampleVideo_360x240_5s"));
+                _player.Play(_video);
+                _start = e.FrameInfo.TotalGameTime;
+            };
+
+            Game.UnloadContentWith += (sender, e) =>
+            {
+                _spriteBatch.Dispose();
+                _spriteBatch = null;
+                _player.Dispose();
+                _player = null;
+                _video = null;
+            };
+
+            Game.PreDrawWith += (sender, e) =>
+            {
+                Game.GraphicsDevice.Clear(Color.CornflowerBlue);
+            };
+        }
+
+        [Test]
+        public void Plain()
+        {
+            Game.ExitCondition = x => _player.State == MediaState.Stopped;
+
+            Game.InitializeWith += (sender, e) =>
+            {
+                Game.Window.Title = "Not Looped";
+                _player = new VideoPlayer();
+            };
+
+            Game.UpdateWith += (sender, e) =>
+            {
+            };
+
+            Game.DrawWith += (sender, e) =>
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(_player.GetTexture(), Vector2.Zero, Color.White);
+                _spriteBatch.End();
+            };
+
+            Game.Run();
+        }
+
+        [Test]
+        public void Looped()
+        {
+            Game.ExitCondition = x => (x.TotalGameTime - _start).TotalSeconds > 8.0;
+
+            Game.InitializeWith += (sender, e) =>
+            {
+                Game.Window.Title = "Looped";
+                _player = new VideoPlayer();
+                _player.IsLooped = true;
+            };
+
+            Game.UpdateWith += (sender, e) =>
+            {
+                Assert.AreNotEqual(_player.State, MediaState.Stopped);
+            };
+
+            Game.DrawWith += (sender, e) =>
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(_player.GetTexture(), Vector2.Zero, Color.White);
+                _spriteBatch.End();
+            };
+
+            Game.Run();
+        }
+    }
+}
