74eaa44cd1812fef39d089fabab0105f294279af
alynch <alexlynch1@hotmail.co.uk>
2016-05-20 18:29:53 +0100

    Fix for NullReferenceException - Switched to use ICollection interface, so Value can be a Collection as well as a List

--- Tools/Pipeline/Controls/PropertyCells/CellRefs.cs
+++ Tools/Pipeline/Controls/PropertyCells/CellRefs.cs
@@ -3,6 +3,7 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Eto.Forms;
 
@@ -10,20 +11,23 @@ namespace MonoGame.Tools.Pipeline
 {
     public class CellRefs : CellBase
     {
-        public CellRefs(string category, string name, object value, EventHandler eventHandler) : base(category, name, value, eventHandler)
+        public CellRefs(string category, string name, object value, EventHandler eventHandler)
+            : base(category, name, value, eventHandler)
         {
             if (Value == null)
                 Value = new List<string>();
 
-            DisplayValue = (Value as List<string>).Count > 0 ? "Collection" : "None";
+            DisplayValue = (Value as ICollection).Count > 0 ? "Collection" : "None";
         }
 
         public override void Edit(PixelLayout control)
         {
-            var dialog = new ReferenceDialog(PipelineController.Instance, (Value as List<string>).ToArray());
+            string[] refs = new string[(Value as ICollection).Count];
+            (Value as ICollection).CopyTo(refs, 0);
+
+            var dialog = new ReferenceDialog(PipelineController.Instance, refs);
             if (dialog.Run(control) == DialogResult.Ok && _eventHandler != null)
                 _eventHandler(dialog.References, EventArgs.Empty);
         }
     }
 }
-
