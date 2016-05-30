e2eeab94d624f0968d233c1789ea33a30b74de07
Jesse Gielen <jessegielen@hotmail.com>
2016-05-30 19:50:43 +0200

    Fix license and Protobuild definition

--- MonoGame.Framework.Content.Pipeline/Serialization/Compiler/EffectMaterialWriter.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Compiler/EffectMaterialWriter.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
