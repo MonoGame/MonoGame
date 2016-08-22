82fff358afbece081cd306776729e1cb0e2ec568
MichaelDePiazzi <MichaelDePiazzi@users.noreply.github.com>
2016-08-22 13:33:09 +0800

    WinDX: Implemented a more reliable keyboard input method

--- MonoGame.Framework/Input/Keyboard.Windows.cs
+++ MonoGame.Framework/Input/Keyboard.Windows.cs
@@ -4,21 +4,57 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.Xna.Framework.Input
 {
     public static partial class Keyboard
     {
-        static List<Keys> _keys;
+        private static readonly Keys[] DefinedKeys;
+
+        private static readonly byte[] _keyState = new byte[256];
+        private static readonly List<Keys> _keys = new List<Keys>(10);
+
+        private static bool _isActive;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        private static readonly Predicate<Keys> IsKeyReleasedPredicate = key => !IsKeyPressed(key);
+
+        static Keyboard()
+        {
+            DefinedKeys = Enum.GetValues(typeof(Keys)).Cast<Keys>()
+                .Where(k => ((int)k >= 1) && ((int)k <= 255))
+                .ToArray();
+        }
 
         private static KeyboardState PlatformGetState()
         {
+            if (_isActive && GetKeyboardState(_keyState))
+            {
+                _keys.RemoveAll(IsKeyReleasedPredicate);
+
+                foreach (var key in DefinedKeys)
+                    if (IsKeyPressed(key) && !_keys.Contains(key))
+                        _keys.Add(key);
+            }
+
             return new KeyboardState(_keys, Console.CapsLock, Console.NumberLock);
         }
 
-        internal static void SetKeys(List<Keys> keys)
+        private static bool IsKeyPressed(Keys key)
         {
-            _keys = keys;
+            return ((_keyState[(int)key] & 0x80) != 0);
+        }
+
+        internal static void SetActive(bool isActive)
+        {
+            _isActive = isActive;
+            if (!_isActive)
+                _keys.Clear();
         }
     }
 }
