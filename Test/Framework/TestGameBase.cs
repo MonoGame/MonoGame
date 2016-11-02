08c4bda9eaa778092957b98c779b3fac52fe3ff6
James Neal <jneal0110@gmail.com>
2016-11-02 15:52:01 -0700

    Set the entry assembly when under test

--- Test/Framework/TestGameBase.cs
+++ Test/Framework/TestGameBase.cs
@@ -95,6 +95,23 @@ namespace MonoGame.Tests {
 #endif
 			Services.AddService<IFrameInfoSource> (this);
 			SuppressExtraUpdatesAndDraws = true;
+			SetEntryAssembly(Assembly.GetCallingAssembly());
+		}
+
+		/// <summary>
+		/// Allows setting the Entry Assembly when needed. 
+		/// Use AssemblyUtilities.SetEntryAssembly() as first line in XNA ad hoc tests
+		/// </summary>
+		/// <param name="assembly">Assembly to set as entry assembly</param>
+		public static void SetEntryAssembly(Assembly assembly)
+		{
+			AppDomainManager manager = new AppDomainManager();
+			FieldInfo entryAssemblyfield = manager.GetType().GetField("m_entryAssembly", BindingFlags.Instance | BindingFlags.NonPublic);
+			entryAssemblyfield.SetValue(manager, assembly);
+
+			AppDomain domain = AppDomain.CurrentDomain;
+			FieldInfo domainManagerField = domain.GetType().GetField("_domainManager", BindingFlags.Instance | BindingFlags.NonPublic);
+			domainManagerField.SetValue(domain, manager);
 		}
 
 		#region IFrameInfoSource Implementation
