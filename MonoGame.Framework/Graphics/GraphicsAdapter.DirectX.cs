25218e60ad3521f54f42499cce1110ab7346e628
Steve Williams <dev@konaju.com>
2016-12-21 00:38:20 +1000

    Remove unnecessary using clause.

--- MonoGame.Framework/Graphics/GraphicsAdapter.DirectX.cs
+++ MonoGame.Framework/Graphics/GraphicsAdapter.DirectX.cs
@@ -2,7 +2,6 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using SharpDX.Direct3D11;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
