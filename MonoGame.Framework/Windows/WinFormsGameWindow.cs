406da833b29dfe816e10b632ac3c5de7ec9238d4
Jesse Gielen <jessegielen@hotmail.com>
2017-01-16 13:32:11 +0100

    Filter ONLY WM_QUIT messages in WinFormsGameWindow

--- MonoGame.Framework/Windows/WinFormsGameWindow.cs
+++ MonoGame.Framework/Windows/WinFormsGameWindow.cs
@@ -354,17 +354,10 @@ namespace MonoGame.Framework
             // This is critical for some NUnit runners which
             // typically will run all the tests on the same
             // process/thread.
-#if DEBUG
-            var msg = new NativeMessage();
-            do
+            NativeMessage msg;
+            while (PeekMessage(out msg, IntPtr.Zero, WM_QUIT, WM_QUIT, 1))
             {
-                if (msg.msg == WM_QUIT)
-                    break;
-
-                Thread.Sleep(100);
-            } 
-            while (PeekMessage(out msg, IntPtr.Zero, 0, 1 << 5, 1));
-#endif
+            }
         }
 
         public void CenterForm()
