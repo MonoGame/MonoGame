707af66b1dd203bd3b473ed67439197e65511a89
Nikos Kastellanos <nkastellanos@gmail.com>
2017-01-29 11:29:57 +0200

    Add GraphicsContext [Web]

---
+++ MonoGame.Framework/Graphics/GraphicsContext.Web.cs
@@ -0,0 +1,31 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    internal partial class GraphicsContext : IDisposable
+    {
+        public GraphicsContext(GraphicsDevice device)
+        {
+            Initialize(device);
+        }
+        
+        #region Implement IDisposable
+        private void PlatformDispose(bool disposing)
+        {            
+            if (disposing)
+            {
+                // Release managed objects
+                // ...
+            }
+
+            // Release native objects
+            // ...          
+        }
+        #endregion
+    }
+    
+}
