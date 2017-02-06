9f8dc3b1f590952334b147f199395536dd87d416
siudeks <siudeks@gmail.com>
2017-02-06 23:16:07 +0100

    Added license header to tests ModelTest.cs.

--- Test/Framework/Graphics/ModelTest.cs
+++ Test/Framework/Graphics/ModelTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using NUnit.Framework;
 using System;
