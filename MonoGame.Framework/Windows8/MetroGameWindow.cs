fccfbb0e6616d46de08343fa57af01184ced5c02
Nikos Kastellanos <nkastellanos@gmail.com>
2017-02-26 18:56:59 +0200

    [W8.1/WP8.1] Update Focus before Tick()

--- MonoGame.Framework/Windows8/MetroGameWindow.cs
+++ MonoGame.Framework/Windows8/MetroGameWindow.cs
@@ -37,6 +37,8 @@ namespace Microsoft.Xna.Framework
         private Rectangle _newClientBounds;
         private bool _isOrientationChanged = false;
         private DisplayOrientation _newOrientation;
+        private bool _isFocusChanged = false;
+        private CoreWindowActivationState _newActivationState;
 
 
         private Vector2 _backBufferScale;
@@ -130,10 +132,24 @@ namespace Microsoft.Xna.Framework
 
         private void Window_FocusChanged(CoreWindow sender, WindowActivatedEventArgs args)
         {
-            if (args.WindowActivationState == CoreWindowActivationState.Deactivated)
-                Platform.IsActive = false;
-            else
-                Platform.IsActive = true;
+            lock (_eventLocker)
+            {
+                _isFocusChanged = true;
+                _newActivationState = args.WindowActivationState;
+            }
+        }
+
+        private void UpdateFocus()
+        {
+            lock (_eventLocker)
+            {
+                _isFocusChanged = false;
+
+                if (_newActivationState == CoreWindowActivationState.Deactivated)
+                    Platform.IsActive = false;
+                else
+                    Platform.IsActive = true;
+            }
         }
 
         private void Window_Closed(CoreWindow sender, CoreWindowEventArgs args)
@@ -331,6 +347,9 @@ namespace Microsoft.Xna.Framework
             // Update orientation
             if (_isOrientationChanged)
                 UpdateOrientation();
+
+            if (_isFocusChanged)
+                UpdateFocus();
         }
 
         internal void Tick()
