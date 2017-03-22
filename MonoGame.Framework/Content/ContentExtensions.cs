6e44ceb448b7e717606d1317c88bc623e8519663
cra0zy <cra0zy@gmail.com>
2017-03-22 13:30:54 +0100

    Use GetTypeInfo code on all platforms

--- MonoGame.Framework/Content/ContentExtensions.cs
+++ MonoGame.Framework/Content/ContentExtensions.cs
@@ -2,24 +2,15 @@
 using System.Reflection;
 using System.Linq;
 
-#if WINRT
-using System.Reflection.Emit;
-#endif
-
 namespace Microsoft.Xna.Framework.Content
 {
     internal static class ContentExtensions
     {
         public static ConstructorInfo GetDefaultConstructor(this Type type)
         {
-#if WINRT
             var typeInfo = type.GetTypeInfo();
             var ctor = typeInfo.DeclaredConstructors.FirstOrDefault(c => !c.IsStatic && c.GetParameters().Length == 0);
             return ctor;
-#else
-            var attrs = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
-            return type.GetConstructor(attrs, null, new Type[0], null);
-#endif
         }
 
         public static PropertyInfo[] GetAllProperties(this Type type)
@@ -30,43 +21,27 @@ namespace Microsoft.Xna.Framework.Content
             // all properties in this list are defined in this class by comparing
             // its get method with that of it's base class. If they're the same
             // Then it's an overridden property.
-#if WINRT
             PropertyInfo[] infos= type.GetTypeInfo().DeclaredProperties.ToArray();
             var nonStaticPropertyInfos = from p in infos
                                          where (p.GetMethod != null) && (!p.GetMethod.IsStatic) &&
                                          (p.GetMethod == p.GetMethod.GetRuntimeBaseDefinition())
                                          select p;
             return nonStaticPropertyInfos.ToArray();
-#else
-            const BindingFlags attrs = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
-            var allProps = type.GetProperties(attrs).ToList();
-            var props = allProps.FindAll(p => p.GetGetMethod(true) != null && p.GetGetMethod(true) == p.GetGetMethod(true).GetBaseDefinition()).ToArray();
-            return props;
-#endif
         }
 
 
         public static FieldInfo[] GetAllFields(this Type type)
         {
-#if WINRT
             FieldInfo[] fields= type.GetTypeInfo().DeclaredFields.ToArray();
             var nonStaticFields = from field in fields
                     where !field.IsStatic
                     select field;
             return nonStaticFields.ToArray();
-#else
-            var attrs = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
-            return type.GetFields(attrs);
-#endif
         }
 
         public static bool IsClass(this Type type)
         {
-#if WINRT
             return type.GetTypeInfo().IsClass;
-#else
-            return type.IsClass;
-#endif
         }
     }
 }
