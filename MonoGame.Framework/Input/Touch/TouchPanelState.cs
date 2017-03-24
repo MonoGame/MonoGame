9c69ff9c9326b9850fc245db6a5c8016d54f1962
Unknown <zigzag312@gmail.com>
2017-03-24 15:01:18 +0100

    Remove forgotten touches

--- MonoGame.Framework/Input/Touch/TouchPanelState.cs
+++ MonoGame.Framework/Input/Touch/TouchPanelState.cs
@@ -43,7 +43,17 @@ namespace Microsoft.Xna.Framework.Input.Touch
         /// <summary>
         /// The current timestamp that we use for setting the timestamp of new TouchLocations
         /// </summary>
-        internal static TimeSpan CurrentTimestamp { get; set; }
+        internal static TimeSpan CurrentTimestamp
+        {
+            get { return _currentTimestamp; }
+            set
+            {
+                _previousTimestamp = _currentTimestamp;
+                _currentTimestamp = value;
+            }
+        }
+        private static TimeSpan _currentTimestamp;
+        private static TimeSpan _previousTimestamp;
 
         /// <summary>
         /// The mapping between platform specific touch ids
@@ -144,6 +154,11 @@ namespace Microsoft.Xna.Framework.Input.Touch
                 {
                     _touchState.RemoveAt(i);
                 }
+                // Remove forgotten-old touches
+                else if (touch.Timestamp < _previousTimestamp 
+                    && touch.State != TouchLocationState.Released
+                    && touch.State != TouchLocationState.Invalid)
+                    _touchState.RemoveAt(i);
             }
 
             var result = (_touchState.Count > 0) ? new TouchCollection(_touchState.ToArray()) : TouchCollection.Empty;
