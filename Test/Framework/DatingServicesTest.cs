02d2e846dd2a6e1fd23b2233da3c2f6c3e1bad38
cra0zy <cra0zy@gmail.com>
2017-04-01 23:07:15 +0200

    Add Unit tests

---
+++ Test/Framework/DatingServicesTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Framework
+{
+    public class DatingServicesTest
+    {
+        [Test]
+        public void TestConnection()
+        {
+            Contract.Requires(DatingService.Response != "Shutdown");
+        }
+    }
+}
