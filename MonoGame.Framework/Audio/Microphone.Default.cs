63aa0d6e8f062a3f4e84fcd8028fc6c530eff12b
Thomas Altenburger <mister.helmut@gmail.com>
2017-04-09 16:11:55 +0200

    Removed wrong default using statement

--- MonoGame.Framework/Audio/Microphone.Default.cs
+++ MonoGame.Framework/Audio/Microphone.Default.cs
@@ -2,7 +2,6 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using OpenAL;
 using System;
 using System.Collections.Generic;
 
