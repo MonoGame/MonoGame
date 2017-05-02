282a6b858857c63583f911d1ac63e55fdcb82b42
Jesse Gielen <jessegielen@hotmail.com>
2017-05-02 18:42:08 +0200

    Remove GetHighestSupportedGraphicsProfile (wrong merge)

--- MonoGame.Framework/Graphics/GraphicsDevice.OpenGL.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.OpenGL.cs
@@ -1198,11 +1198,6 @@ namespace Microsoft.Xna.Framework.Graphics
 #endif
         }
 
-        private static GraphicsProfile PlatformGetHighestSupportedGraphicsProfile(GraphicsDevice graphicsDevice)
-        {
-           return GraphicsProfile.HiDef;
-        }
-
         private void PlatformGetBackBufferData<T>(Rectangle rect, T[] data, int startIndex, int count) where T : struct
         {
             var tSize = Marshal.SizeOf(typeof(T));
--- MonoGame.Framework/Graphics/GraphicsDevice.Web.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.Web.cs
@@ -104,11 +104,6 @@ namespace Microsoft.Xna.Framework.Graphics
             throw new NotImplementedException();
         }
 
-        private static GraphicsProfile PlatformGetHighestSupportedGraphicsProfile(GraphicsDevice graphicsDevice)
-        {
-            return GraphicsProfile.HiDef;
-        }
-        
         private static Rectangle PlatformGetTitleSafeArea(int x, int y, int width, int height)
         {
             return new Rectangle(x, y, width, height);
