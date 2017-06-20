62e9f5c22b80efc02149162c2e21006af0797fce
matevž ropret <ropret.m@gmail.com>
2017-06-20 14:34:30 +0200

    Fix for android 7 nougat for black screen when split-screen is activated

--- IDE/MonoDevelop/MonoDevelop.MonoGame.Android/templates/Android/Activity1.cs
+++ IDE/MonoDevelop/MonoDevelop.MonoGame.Android/templates/Android/Activity1.cs
@@ -22,7 +22,8 @@ namespace ${Namespace}
 	           ConfigurationChanges = ConfigChanges.Orientation | 
 	                                  ConfigChanges.KeyboardHidden | 
 	                                  ConfigChanges.Keyboard |
-	                                  ConfigChanges.ScreenSize)]
+	                                  ConfigChanges.ScreenSize |
+                                      ConfigChanges.ScreenLayout)]
 	public class Activity1 : AndroidGameActivity
 	{
 		protected override void OnCreate (Bundle bundle)
