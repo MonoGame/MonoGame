58b02ccc61a3af55070e0bf9dd711184a18e31c9
cra0zy <cra0zy@gmail.com>
2017-07-26 15:21:37 +0200

    [DesktopGL] Fix Joystick enum (fixes #5729)

--- MonoGame.Framework/SDL/SDL2.cs
+++ MonoGame.Framework/SDL/SDL2.cs
@@ -649,11 +649,11 @@ internal static class Sdl
         [Flags]
         public enum Hat : byte
         {
-            Centered,
-            Up,
-            Right,
-            Down,
-            Left,
+            Centered = 0,
+            Up = 1 << 0,
+            Right = 1 << 1,
+            Down = 1 << 2,
+            Left = 1 << 3
         }
 
         [StructLayout(LayoutKind.Sequential)]
