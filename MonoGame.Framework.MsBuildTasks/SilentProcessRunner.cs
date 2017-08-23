fd4d8d1ed5f952012aa3e69fdc9b9e92918809c7
Darrell Tunnell <darrell.tunnell@googlemail.com>
2017-08-23 01:48:58 +0100

    Fixes #5892 - Fix xbuild. I have moved the complicated logic from the targets file, into a seperate msbuild task, because: 1. xbuild can't handle it. 2. This opens a path to testing the msbuild task with nunit / xunit etc which will improve our test coverage.

---
+++ MonoGame.Framework.MsBuildTasks/SilentProcessRunner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MonoGame.Framework.MsBuildTasks
+{
+
+    public static class SilentProcessRunner
+    {
+
+        public static int ExecuteCommand(string executable, string arguments, string workingDirectory, Action<string> output, Action<string> error)
+        {
+
+            try
+            {
+
+                using (var process = new Process())
+                {
+
+                    process.StartInfo.FileName = executable;
+                    process.StartInfo.Arguments = arguments;
+                    process.StartInfo.WorkingDirectory = workingDirectory;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+
+
+                    using (var outputWaitHandle = new AutoResetEvent(false))
+                    using (var errorWaitHandle = new AutoResetEvent(false))
+                    {
+                        process.OutputDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+                                output(e.Data);
+                            }
+                        };
+
+                        process.ErrorDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                errorWaitHandle.Set();
+                            }
+                            else
+                            {
+                                error(e.Data);
+                            }
+                        };
+
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        process.WaitForExit();
+
+                        outputWaitHandle.WaitOne();
+                        errorWaitHandle.WaitOne();
+
+                        return process.ExitCode;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Error when attempting to execute {0}: {1}", executable, ex.Message), ex);
+            }
+        }
+    }
+}
