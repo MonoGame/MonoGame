dad70931cba24850a9e0df70e27bf71326aeae70
Dean Ellis <dellis1972@googlemail.com>
2017-08-27 11:52:19 +0100

    PR #4997 Introduced a crash in xbuild. This is because of a bug in xbuild where `<ItemGroup>` with Metadata are not supported. Instead you need to use `CreateItem`.
    This commit fixes that issue.
    
    There are also other issues which need to be resolved with #4997.
    
    That said this commit also adds a new unit test for the content system.
    There is a sample project which we build as part of the unit test.
    We check the output to make sure it is in the right place. This test
    should be expaned on to include tests for Shared Projects and other
    senarios that users use. It should allow use to catch issues such as
    what happened in #4997 at the point the PR is opened.

---
+++ Test/ContentPipeline/BuilderTargetsTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using NUnit.Framework;
+namespace MonoGame.Tests.ContentPipeline
+{
+    [TestFixture]
+    public class BuilderTargetsTest
+    {
+        bool RunBuild (string buildTool, string projectFile, params string[] parameters)
+        {
+            var root = Path.GetDirectoryName(typeof(BuilderTargetsTest).Assembly.Location);
+            var psi = new ProcessStartInfo(buildTool)
+            {
+                Arguments = projectFile + " /t:BuildContent " + string.Join(" ", parameters),
+                WorkingDirectory = root,
+            };
+            using (var process = Process.Start(psi))
+            {
+                process.WaitForExit();
+                return process.ExitCode == 0;
+            }
+        }
+
+        static object[] BuilderTargetsBuildTools = new object[] {
+            "msbuild",
+            "xbuild",
+        };
+
+        [Test]
+        [TestCaseSource("BuilderTargetsBuildTools")]
+        public void BuildSimpleProject (string buildTool)
+        {
+            if (buildTool == "xbuild" && Environment.OSVersion.Platform == PlatformID.Win32NT)
+                Assert.Ignore("Skipping xbuild tests on windows");
+
+            var root = Path.GetDirectoryName(typeof(BuilderTargetsTest).Assembly.Location);
+            var outputPath = Path.Combine(root, "Assets", "Projects", "Content", "bin");
+            if (Directory.Exists(outputPath))
+                Directory.Delete(outputPath, recursive: true);
+
+            var result = RunBuild(buildTool, Path.Combine("Assets", "Projects", "BuildSimpleProject.csproj"), new string[] {
+                "/p:MonoGameContentBuilderExe=" + Path.Combine(root, "MGCB.exe")
+            });
+            Assert.AreEqual(true, result, "Content Build should have succeeded.");
+            var contentFont = Path.Combine(outputPath, "DesktopGL", "Content", "ContentFont.xnb");
+            Assert.IsTrue(File.Exists(contentFont), "'" + contentFont + "' should exist.");
+ 
+        }
+    }
+}
