d0390031bf5336fdb87f834f24674c2a51d9f3f0
Dean Ellis <dellis1972@googlemail.com>
2017-08-31 22:59:13 +0100

    Initial Commit

---
+++ Test/ContentPipeline/FontDescriptionProcessorTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.ContentPipeline
+{
+    class FontDescriptionTests
+    {
+    }
+}
