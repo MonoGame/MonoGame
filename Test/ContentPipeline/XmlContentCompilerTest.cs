ff69d64d95c992b97621bd26d89402c3bca2f3b2
joncloud <jdberube@gmail.com>
2017-10-08 08:51:43 -0700

    Fixes property content serialization detection when using a property named `Item`.
    * Adjusts indexer property detection to check for Index Parameters instead of get/set method's parameters.
    * Moves test compliation in order to share between tests.
    * Creates new tests to verify changes.

---
+++ Test/ContentPipeline/XmlContentCompilerTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MonoGame.Tests.ContentPipeline
+{
+    class XmlContentCompilerTest
+    {
+        [Test]
+        public void ShouldSerializePropertyGivenNameOfItem()
+        {
+            var expected = new HasNoIndexer { Item = "lorem-ipsum" };
+
+            TestCompiler.CompileAndLoadAssets(expected, result =>
+            {
+                Assert.AreEqual(expected.Item, result.Item);
+            });
+        }
+
+        class HasNoIndexer
+        {
+            public string Item { get; set; }
+        }
+
+        [Test]
+        public void ShouldNotSerializePropertyGivenIndexer()
+        {
+            var expected = new HasIndexer();
+            expected["anything"] = "value";
+
+            TestCompiler.CompileAndLoadAssets(expected, result =>
+            {
+                Assert.AreNotEqual(expected["anything"], result["anything"]);
+            });
+        }
+
+        class HasIndexer
+        {
+            readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+            public string this[string key]
+            {
+                get => _dictionary.TryGetValue(key, out string value) ? value : null;
+                set => _dictionary[key] = value;
+            }
+        }
+    }
+}
