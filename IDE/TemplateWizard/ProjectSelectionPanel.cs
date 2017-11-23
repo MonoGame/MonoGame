b491287fd5eb8fc6a3c95a43e2c84f884e68d2fc
Dean Ellis <dellis1972@googlemail.com>
2017-11-23 11:03:58 +0000

    Reworked directory structure. Added Empty Addin and TemplateWizard Library

---
+++ IDE/TemplateWizard/ProjectSelectionPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace TemplateWizard
+{	
+
+	public partial class ProjectSelectionPanel
+	{	
+		public ProjectSelectionPanel()
+		{
+			InitializeComponent();
+		}
+	}
+}
