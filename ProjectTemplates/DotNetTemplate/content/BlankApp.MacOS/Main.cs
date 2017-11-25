ae9852067134081990d8bb44c8223c883a626717
Dean Ellis <dellis1972@googlemail.com>
2017-11-25 21:13:56 +0000

    Added MacOS, tvOS and Pipeline Extension

---
+++ ProjectTemplates/DotNetTemplate/content/BlankApp.MacOS/Main.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AppKit;
+using Foundation;
+#endregion
+
+namespace BlankApp.MacOS
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var game = new Game1 ()) {
+			game.Run ();
+			}
+		}
+	}
+}
