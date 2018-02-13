456b6213bd98ae9411d0a72b4808171c86a1c229
Dean Ellis <dellis1972@googlemail.com>
2018-02-13 18:58:15 +0000

    Fixed tvOS Game1 class

--- ProjectTemplates/DotNetTemplate/content/BlankApp.tvOS/Game1.cs
+++ ProjectTemplates/DotNetTemplate/content/BlankApp.tvOS/Game1.cs
@@ -52,13 +52,6 @@ namespace BlankApp.tvOS
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // For Mobile devices, this logic will close the Game when the Back button is pressed
-            // Exit() is obsolete on iOS
-            #if !__IOS__ &&  !__TVOS__
-            if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) 
-                Exit ();
-            #endif
-            
             // TODO: Add your update logic here
             
             base.Update(gameTime);
