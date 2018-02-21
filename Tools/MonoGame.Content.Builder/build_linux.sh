c418b0f6b18c7c59f3d02931e436514b44ce1fa0
cra0zy <cra0zy@gmail.com>
2018-02-21 13:34:26 +0100

    Fix slashes

--- Tools/MonoGame.Content.Builder/build_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dotnet restore
-
-# I had to improvise
-dotnet msbuild /property:ONLYCORE=True /property:Configuration=Release
-msbuild /property:ONLYNET=True /property:Configuration=Release
-
-nuget pack MonoGame.Content.Builder.nuspec
