6342afcbe95ea5d02e6b3ea6628ee05133e599ac
Dean Ellis <dellis1972@googlemail.com>
2018-03-19 13:19:48 +0000

    Initial Commit of vsix stuff

---
+++ Installers/vsix/MonoGamePackage.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace MonoGame.Sdk
+{
+	[Guid("692e2fb1-e079-47ce-a52b-e804726bc407")]
+	public class MonoGamePackage : Package {
+	}
+}
