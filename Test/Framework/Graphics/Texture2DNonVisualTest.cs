6d6299adb04071d77491ed7e08cf4a84643381ac
Roman Shapiro <eleroman@mail.ru>
2018-11-02 00:47:35 +0700

    Added rewind to the start of the stream in the Texture2D.FromStream

--- Test/Framework/Graphics/Texture2DNonVisualTest.cs
+++ Test/Framework/Graphics/Texture2DNonVisualTest.cs
@@ -103,6 +103,35 @@ namespace MonoGame.Tests.Graphics
         }
 
         [TestCase]
+        public void FromStreamAtTheEnd()
+        {
+            // Check whether texture can be loaded if a stream being at its end
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var fileStream = File.OpenRead("Assets/Textures/red_128.png"))
+                {
+                    fileStream.CopyTo(memoryStream);
+                }
+                using (var texture = Texture2D.FromStream(gd, memoryStream))
+                {
+                    Assert.AreEqual(8, texture.Width);
+                    Assert.AreEqual(8, texture.Height);
+                    Assert.AreEqual(1, texture.LevelCount);
+                    var pngData = new Color[8 * 8];
+                    texture.GetData(pngData);
+
+                    for (var i = 0; i < pngData.Length; i++)
+                    {
+                        Assert.AreEqual(255, pngData[i].R);
+                        Assert.AreEqual(0, pngData[i].G);
+                        Assert.AreEqual(0, pngData[i].B);
+                        Assert.AreEqual(128, pngData[i].A);
+                    }
+                }
+            }
+        }
+
+        [TestCase]
         public void FromStreamBlackAlpha()
         {
             // XNA will make any pixel with an alpha value
