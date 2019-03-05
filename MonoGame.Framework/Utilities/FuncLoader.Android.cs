09317ba0cdb1ad5920dab8d5eae3be1d8a76677e
Tom Spilman <tom@sickheadgames.com>
2019-03-05 10:04:26 -0600

    Fix for Amazon Fire HD 8 tablet not loading the OpenAL library.

--- MonoGame.Framework/Utilities/FuncLoader.Android.cs
+++ MonoGame.Framework/Utilities/FuncLoader.Android.cs
@@ -1,4 +1,6 @@
+using Android.App;
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace MonoGame.Utilities
@@ -15,7 +17,27 @@ namespace MonoGame.Utilities
 
         public static IntPtr LoadLibrary(string libname)
         {
-            return dlopen(libname, RTLD_LAZY);
+            // Let the OS search for the library by default.
+            IntPtr lib = dlopen(libname, RTLD_LAZY);
+            if (lib != IntPtr.Zero)
+            {
+                Console.WriteLine("FuncLoader.LoadLibrary {0}", libname);
+                return lib;
+            }
+
+            // Some Android devices won't search the native library path
+            // for the library, so we have to do it manually here.
+            var nlibpath = Application.Context.ApplicationInfo.NativeLibraryDir;
+            var libpath = Path.Combine(nlibpath, libname);
+            lib = dlopen(libpath, RTLD_LAZY);
+            if (lib != IntPtr.Zero)
+            {	
+                Console.WriteLine("FuncLoader.LoadLibrary {0}", libpath);
+                return lib;
+            }
+
+            Console.WriteLine("FuncLoader.LoadLibrary {0} Not Found!", libname);
+            return IntPtr.Zero;
         }
 
         public static T LoadFunction<T>(IntPtr library, string function, bool throwIfNotFound = false)
