4df22bfcc336e24fda3a73c25b89322633abb786
Nikos Kastellanos <nkastellanos@gmail.com>
2019-07-17 03:26:47 +0300

    Remove Obsolete SpriteBatch.Draw(...)
    This removes the Draw(...) method previously marked as Obsolete in #5517

--- MonoGame.Framework/Graphics/SpriteBatch.cs
+++ MonoGame.Framework/Graphics/SpriteBatch.cs
@@ -168,59 +168,6 @@ namespace Microsoft.Xna.Framework.Graphics
         /// Submit a sprite for drawing in the current batch.
         /// </summary>
         /// <param name="texture">A texture.</param>
-        /// <param name="position">The drawing location on screen or null if <paramref name="destinationRectangle"> is used.</paramref></param>
-        /// <param name="destinationRectangle">The drawing bounds on screen or null if <paramref name="position"> is used.</paramref></param>
-        /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
-        /// <param name="origin">An optional center of rotation. Uses <see cref="Vector2.Zero"/> if null.</param>
-        /// <param name="rotation">An optional rotation of this sprite. 0 by default.</param>
-        /// <param name="scale">An optional scale vector. Uses <see cref="Vector2.One"/> if null.</param>
-        /// <param name="color">An optional color mask. Uses <see cref="Color.White"/> if null.</param>
-        /// <param name="effects">The optional drawing modificators. <see cref="SpriteEffects.None"/> by default.</param>
-        /// <param name="layerDepth">An optional depth of the layer of this sprite. 0 by default.</param>
-        /// <exception cref="InvalidOperationException">Throwns if both <paramref name="position"/> and <paramref name="destinationRectangle"/> been used.</exception>
-        /// <remarks>This overload uses optional parameters. This overload requires only one of <paramref name="position"/> and <paramref name="destinationRectangle"/> been used.</remarks>
-        [Obsolete("In future versions this method can be removed.")]
-        public void Draw (Texture2D texture,
-                Vector2? position = null,
-                Rectangle? destinationRectangle = null,
-                Rectangle? sourceRectangle = null,
-                Vector2? origin = null,
-                float rotation = 0f,
-                Vector2? scale = null,
-                Color? color = null,
-                SpriteEffects effects = SpriteEffects.None,
-                float layerDepth = 0f)
-        {
-
-            // Assign default values to null parameters here, as they are not compile-time constants
-            if(!color.HasValue)
-                color = Color.White;
-            if(!origin.HasValue)
-                origin = Vector2.Zero;
-            if(!scale.HasValue)
-                scale = Vector2.One;
-
-            // If both drawRectangle and position are null, or if both have been assigned a value, raise an error
-            if((destinationRectangle.HasValue) == (position.HasValue))
-            {
-                throw new InvalidOperationException("Expected drawRectangle or position, but received neither or both.");
-            }
-            else if(position != null)
-            {
-                // Call Draw() using position
-                Draw(texture, (Vector2)position, sourceRectangle, (Color)color, rotation, (Vector2)origin, (Vector2)scale, effects, layerDepth);
-            }
-            else
-            {
-                // Call Draw() using drawRectangle
-                Draw(texture, (Rectangle)destinationRectangle, sourceRectangle, (Color)color, rotation, (Vector2)origin, effects, layerDepth);
-            }
-        }
-
-        /// <summary>
-        /// Submit a sprite for drawing in the current batch.
-        /// </summary>
-        /// <param name="texture">A texture.</param>
         /// <param name="position">The drawing location on screen.</param>
         /// <param name="sourceRectangle">An optional region on the texture which will be rendered. If null - draws full texture.</param>
         /// <param name="color">A color mask.</param>
