254b6ce6b2a728a59992f6a38a6d3bd3337af0c3
Roman Shapiro <eleroman@mail.ru>
2019-08-10 19:46:40 +0700

    Removed extra comments

--- MonoGame.Framework/Platform/MonogamePlatform.tvOS.cs
+++ MonoGame.Framework/Platform/MonogamePlatform.tvOS.cs
@@ -1,8 +1,5 @@
 ﻿namespace Microsoft.Xna.Framework
 {
-    /// <summary>
-    /// Utility class that returns information about the underlying game platform((this API is unique to MonoGame)
-    /// </summary>
     partial class MonogamePlatform
     {
         private static MonogamePlatformType PlatformGetCurrent()
