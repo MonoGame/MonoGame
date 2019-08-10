e86c868a4db6af5eeff1b399dccfbb85dfed9ea8
Roman Shapiro <eleroman@mail.ru>
2019-08-10 22:54:03 +0700

    Yet another refactoring

--- MonoGame.Framework/PlatformInfo.Platform.cs
+++ MonoGame.Framework/PlatformInfo.Platform.cs
@@ -1,72 +1,54 @@
 ﻿namespace Microsoft.Xna.Framework
 {
-#if ANDROID
     partial class PlatformInfo
     {
+#if ANDROID
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.Android;
         }
-    }
 #endif
 
 #if DESKTOPGL
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.DesktopGL;
         }
-    }
 #endif
 
 #if IOS && !TVOS
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.iOS;
         }
-    }
 #endif
 
 #if TVOS
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.tvOS;
         }
-    }
 #endif
 
 #if WEB
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.Web;
         }
-    }
 #endif
 
 #if WINDOWS && DIRECTX
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.Windows;
         }
-    }
 #endif
 
 #if WINDOWS_UAP
-    partial class PlatformInfo
-    {
         private static MonogamePlatform PlatformGetCurrent()
         {
             return MonogamePlatform.WindowsUniversal;
         }
-    }
 #endif
+    }
 }
