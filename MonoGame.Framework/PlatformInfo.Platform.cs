0a8c4aa603c61d421f1b024b4890734ceeabfe9c
Roman Shapiro <eleroman@mail.ru>
2019-08-22 03:36:24 +0700

    Add API to query game platform at runtime (#6846)

---
+++ MonoGame.Framework/PlatformInfo.Platform.cs
@@ -0,0 +1,54 @@
+﻿namespace Microsoft.Xna.Framework
+{
+    partial class PlatformInfo
+    {
+#if ANDROID
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.Android;
+        }
+#endif
+
+#if DESKTOPGL
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.DesktopGL;
+        }
+#endif
+
+#if IOS && !TVOS
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.iOS;
+        }
+#endif
+
+#if TVOS
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.tvOS;
+        }
+#endif
+
+#if WEB
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.Web;
+        }
+#endif
+
+#if WINDOWS && DIRECTX
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.Windows;
+        }
+#endif
+
+#if WINDOWS_UAP
+        private static MonogamePlatform PlatformGetCurrent()
+        {
+            return MonogamePlatform.WindowsUniversal;
+        }
+#endif
+    }
+}
---
+++ MonoGame.Framework/PlatformInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.Xna.Framework
+{
+    /// <summary>
+    /// Type of the underlying game platform
+    /// </summary>
+    public enum MonogamePlatform
+    {
+        Android,
+        iOS,
+        tvOS,
+        DesktopGL,
+        Windows,
+        WindowsUniversal,
+        Web,
+        PSVita,
+        XBoxOne,
+        PlayStation4,
+        Switch
+    }
+
+    /// <summary>
+    /// Utility class that returns information about the underlying platform
+    /// </summary>
+    public static partial class PlatformInfo
+    {
+        /// <summary>
+        /// Underlying game platform type
+        /// </summary>
+        public static MonogamePlatform MonogamePlatform
+        {
+            get
+            {
+                return PlatformGetCurrent();
+            }
+        }
+    }
+}
