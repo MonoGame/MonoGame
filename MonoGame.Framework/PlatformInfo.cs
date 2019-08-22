851c1b21eb3f7da5802ee14329eed3f22dc6669f
Roman Shapiro <eleroman@mail.ru>
2019-08-23 01:14:56 +0700

    Add API to query graphical system at runtime and fixup platform API (#6872)
    * Add API to query graphical system at runtime
    
    * Applied comments from #6846

--- MonoGame.Framework/PlatformInfo.cs
+++ MonoGame.Framework/PlatformInfo.cs
@@ -1,9 +1,13 @@
-﻿namespace Microsoft.Xna.Framework
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace MonoGame.Framework.Utilities
 {
     /// <summary>
     /// Type of the underlying game platform
     /// </summary>
-    public enum MonogamePlatform
+    public enum MonoGamePlatform
     {
         Android,
         iOS,
@@ -19,18 +23,56 @@
     }
 
     /// <summary>
+    /// Graphics backend
+    /// </summary>
+    public enum GraphicsBackend
+    {
+        DirectX,
+        OpenGL
+    }
+
+    /// <summary>
     /// Utility class that returns information about the underlying platform
     /// </summary>
-    public static partial class PlatformInfo
+    public static class PlatformInfo
     {
         /// <summary>
         /// Underlying game platform type
         /// </summary>
-        public static MonogamePlatform MonogamePlatform
+        public static MonoGamePlatform MonoGamePlatform
         {
             get
             {
-                return PlatformGetCurrent();
+#if ANDROID
+                return MonoGamePlatform.Android;
+#elif DESKTOPGL
+                return MonoGamePlatform.DesktopGL;
+#elif IOS && !TVOS
+                return MonoGamePlatform.iOS;
+#elif TVOS
+                return MonoGamePlatform.tvOS;
+#elif WEB
+                return MonoGamePlatform.Web;
+#elif WINDOWS && DIRECTX
+                return MonoGamePlatform.Windows;
+#elif WINDOWS_UAP
+                return MonoGamePlatform.WindowsUniversal;
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Graphics backend
+        /// </summary>
+        public static GraphicsBackend GraphicsBackend
+        {
+            get
+            {
+#if DIRECTX
+                return GraphicsBackend.DirectX;
+#else
+                return GraphicsBackend.OpenGL;
+#endif
             }
         }
     }
