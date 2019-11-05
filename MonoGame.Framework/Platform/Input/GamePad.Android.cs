4fb7614fe87286aea043e30c5380683b9ea8a526
Thomas Altenburger <mister.helmut@gmail.com>
2019-11-05 17:07:59 +0100

    Implement trigger vibrations on UWP and add overloads

--- MonoGame.Framework/Platform/Input/GamePad.Android.cs
+++ MonoGame.Framework/Platform/Input/GamePad.Android.cs
@@ -162,7 +162,7 @@ namespace Microsoft.Xna.Framework.Input
             return state;
         }
 
-        private static bool PlatformSetVibration(int index, float leftMotor, float rightMotor)
+        private static bool PlatformSetVibration(int index, float leftMotor, float rightMotor, float leftTrigger, float rightTrigger)
         {
             var gamePad = GamePads[index];
             if (gamePad == null)
