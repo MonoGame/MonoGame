fb611bf5b56bdcbf581cd30116ed8be4ba5bea68
Thomas Altenburger <mister.helmut@gmail.com>
2019-11-07 09:21:40 +0100

    Removed duplicate templates

--- ProjectTemplates/DotNetTemplate/MonoGame.Templates.CSharp/content/MonoGame.Application.WindowsDX.CSharp/Program.cs
+++ ProjectTemplates/DotNetTemplate/MonoGame.Templates.CSharp/content/MonoGame.Application.WindowsDX.CSharp/Program.cs
@@ -2,14 +2,8 @@
 
 namespace MGNamespace
 {
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
