5ce9299a5011ca6dc05a1e8258be45f7af302842
Lautenschlager-id <tai.romani@hotmail.com>
2019-11-08 16:56:03 -0300

    Add Tau constant

--- MonoGame.Framework/MathHelper.cs
+++ MonoGame.Framework/MathHelper.cs
@@ -46,6 +46,12 @@ namespace Microsoft.Xna.Framework
         /// </summary>
         public const float TwoPi = (float)(Math.PI * 2.0);
         
+		/// <summary>
+        /// Represents the value of pi times two(6.28318548).
+        /// This is an alias of TwoPi.
+        /// </summary>
+        public const float Tau = TwoPi;
+        
         /// <summary>
         /// Returns the Cartesian coordinate for one axis of a point that is defined by a given triangle and two normalized barycentric (areal) coordinates.
         /// </summary>
