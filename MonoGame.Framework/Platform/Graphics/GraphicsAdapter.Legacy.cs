af2f2bfc6627850cf82d047b8ee276b729452bed
Sebastian Kroskiewicz <sebastian.kroskiewicz@gmail.com>
2019-12-07 23:55:14 +0100

    Fix: non-OpenGL platforms failing to build

--- MonoGame.Framework/Platform/Graphics/GraphicsAdapter.Legacy.cs
+++ MonoGame.Framework/Platform/Graphics/GraphicsAdapter.Legacy.cs
@@ -2,7 +2,9 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
+#if DESKTOPGL
 using MonoGame.OpenGL;
+#endif
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
