25537e7d4a0640bee10d2d4f12abc57bd2d89dd3
Francesco Bonizzi <fbonizzi.90@gmail.com>
2020-01-20 11:51:55 +0100

    WindowsUniversal: _coreIndependentInputSource assignation fix

--- MonoGame.Framework/Platform/WindowsUniversal/InputEvents.cs
+++ MonoGame.Framework/Platform/WindowsUniversal/InputEvents.cs
@@ -74,18 +74,17 @@ namespace Microsoft.Xna.Framework
                 {
                     var inputDevices = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Touch;
 
-                    CoreIndependentInputSource coreIndependentInputSource;
                     if (inputElement is SwapChainBackgroundPanel)
-                        coreIndependentInputSource = ((SwapChainBackgroundPanel)inputElement).CreateCoreIndependentInputSource(inputDevices);
+                        _coreIndependentInputSource = ((SwapChainBackgroundPanel)inputElement).CreateCoreIndependentInputSource(inputDevices);
                     else
-                        coreIndependentInputSource = ((SwapChainPanel)inputElement).CreateCoreIndependentInputSource(inputDevices);
+                        _coreIndependentInputSource = ((SwapChainPanel)inputElement).CreateCoreIndependentInputSource(inputDevices);
 
-                    coreIndependentInputSource.PointerPressed += CoreWindow_PointerPressed;
-                    coreIndependentInputSource.PointerMoved += CoreWindow_PointerMoved;
-                    coreIndependentInputSource.PointerReleased += CoreWindow_PointerReleased;
-                    coreIndependentInputSource.PointerWheelChanged += CoreWindow_PointerWheelChanged;
+                    _coreIndependentInputSource.PointerPressed += CoreWindow_PointerPressed;
+                    _coreIndependentInputSource.PointerMoved += CoreWindow_PointerMoved;
+                    _coreIndependentInputSource.PointerReleased += CoreWindow_PointerReleased;
+                    _coreIndependentInputSource.PointerWheelChanged += CoreWindow_PointerWheelChanged;
 
-                    coreIndependentInputSource.Dispatcher.ProcessEvents(CoreProcessEventsOption.ProcessUntilQuit);
+                    _coreIndependentInputSource.Dispatcher.ProcessEvents(CoreProcessEventsOption.ProcessUntilQuit);
                 });
                 var inputWorker = ThreadPool.RunAsync(workItemHandler, WorkItemPriority.High, WorkItemOptions.TimeSliced);
             }
