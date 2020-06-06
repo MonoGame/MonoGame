27d5603e333ed62078fada094b90a33bddfb3c2d
harry-cpp <cra0zy@gmail.com>
2020-06-07 01:05:22 +0200

    [MGCB Editor] macOS fixes
    - Fixed register/unregister on macOS
    - Fixed MG Addin for VS for Mac not calling MGCB Editor properly
    - Fixed MGCB Editor versioning on macOS

--- Tools/MonoGame.Content.Builder.Editor/Platform/Mac/FileAssociation.Mac.cs
+++ Tools/MonoGame.Content.Builder.Editor/Platform/Mac/FileAssociation.Mac.cs
@@ -1,52 +1,62 @@
-﻿using MonoGame.Tools.Pipeline.Utilities;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 
 namespace MonoGame.Tools.Pipeline
 {
     public static class FileAssociation
     {
-        private const string lsregisterPath = "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister";
+        private readonly static string appPath = Path.Combine(Environment.GetEnvironmentVariable("HOME"), "Applications/MGCB Editor.app");
 
         public static void Associate()
         {
-            RunLsregister("-v -f");
+            InstallApplication();
         }
 
         public static void Unassociate()
         {
-            RunLsregister("-v -f -u");
+            UninstallApplication();
         }
 
-        private static void RunLsregister(string arguments)
+        private static void InstallApplication()
         {
-            // Assuming we're running in .app/Contents/MacOS, go back up to the .app
-            var appPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "../.."));
-            if (Path.GetExtension(appPath) != ".app")
+            Console.WriteLine("Installing application...");
+
+            var baseAppPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "../.."));
+            if (Path.GetExtension(baseAppPath) != ".app")
             {
                 throw new FileNotFoundException("Not running from within the app package");
             }
 
-            var process = new Process
+            CopyDirectory(baseAppPath, appPath);
+
+            Console.WriteLine("Installation complete!");
+        }
+
+        private static void UninstallApplication()
+        {
+            Console.WriteLine("Uninstalling aplication...");
+
+            if (Directory.Exists(appPath))
             {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = lsregisterPath,
-                    Arguments = $"{arguments} \"{appPath}\"",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                }
-            };
+                Directory.Delete(appPath, true);
+            }
 
-            process.OutputDataReceived += (sender, eventArgs) => Console.WriteLine(eventArgs.Data);
-            process.ErrorDataReceived += (sender, eventArgs) => Console.Error.WriteLine(eventArgs.Data);
+            Console.WriteLine("Uninstallation complete!");
+        }
 
-            process.Start();
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.WaitForExit();
+        private static void CopyDirectory(string sourceDirName, string destDirName)
+        {
+            var dir = new DirectoryInfo(sourceDirName);
+
+            if (!Directory.Exists(destDirName))
+                Directory.CreateDirectory(destDirName);
+
+            foreach (var file in dir.GetFiles())
+                file.CopyTo(Path.Combine(destDirName, file.Name), false);
+
+            foreach (var subdir in dir.GetDirectories())
+                CopyDirectory(subdir.FullName, Path.Combine(destDirName, subdir.Name));
         }
     }
 }
