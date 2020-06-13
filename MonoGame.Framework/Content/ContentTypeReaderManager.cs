e435491dd32dff8fbfa4f02ba31abc8ef117145c
cra0zy <cra0zy@gmail.com>
2020-06-13 14:19:32 +0200

    Fix content failing to load due to mscorlib and System.Private.CoreLib (fixes #7174)

--- MonoGame.Framework/Content/ContentTypeReaderManager.cs
+++ MonoGame.Framework/Content/ContentTypeReaderManager.cs
@@ -21,6 +21,7 @@ namespace Microsoft.Xna.Framework.Content
 
         private static readonly string _assemblyName;
 
+        private static readonly bool _isRunningOnNetCore = Type.GetType("System.Private.CoreLib") != null;
 
         static ContentTypeReaderManager()
         {
@@ -224,6 +225,11 @@ namespace Microsoft.Xna.Framework.Content
             preparedType = preparedType.Replace(", Microsoft.Xna.Framework.Graphics", string.Format(", {0}", _assemblyName));
             preparedType = preparedType.Replace(", Microsoft.Xna.Framework.Video", string.Format(", {0}", _assemblyName));
             preparedType = preparedType.Replace(", Microsoft.Xna.Framework", string.Format(", {0}", _assemblyName));
+
+            if (_isRunningOnNetCore)
+                preparedType = preparedType.Replace("mscorlib", "System.Private.CoreLib");
+            else
+                preparedType = preparedType.Replace("System.Private.CoreLib", "mscorlib");
 
             return preparedType;
         }
