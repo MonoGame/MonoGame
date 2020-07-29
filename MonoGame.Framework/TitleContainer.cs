0680a554c49b29cabc1f1639bfb5a5ef696a72d0
Jure-BB <>
2020-07-29 22:09:04 +0200

    line endings

--- MonoGame.Framework/TitleContainer.cs
+++ MonoGame.Framework/TitleContainer.cs
@@ -4,7 +4,7 @@
 
 using System;
 using System.IO;
-using MonoGame.Framework.Utilities;
+using  MonoGame.Framework.Utilities;
 
 namespace Microsoft.Xna.Framework
 {
--- MonoGame.Framework/Utilities/FileHelpers.cs
+++ MonoGame.Framework/Utilities/FileHelpers.cs
@@ -5,7 +5,7 @@
 using System;
 using System.IO;
 using System.Text;
-using System.Text.RegularExpressions;
+using  System.Text.RegularExpressions;
 
 namespace MonoGame.Framework.Utilities
 {
