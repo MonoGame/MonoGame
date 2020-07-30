f31a98e7df484b62d3380e00dc58388b44e215ba
Harry <harry.cpp@outlook.com>
2020-07-30 21:25:47 +0200

    Add License header to VS for Mac files

--- Templates/VisualStudioForMac/MGPad.cs
+++ Templates/VisualStudioForMac/MGPad.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
 using Eto;
 using Eto.Forms;
 using MonoDevelop.Ide.Gui;
