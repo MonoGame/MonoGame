0c83e8202f6e8ead1603132deb5c28b2cdf73bc0
markus <protonen_hoetz@gmx.at>
2020-09-01 09:51:32 +0200

    Recompile all shaders. Add ESSL shaders for Android and iOS.

--- MonoGame.Framework/Platform/Graphics/Effect/EffectResource.OpenGL.cs
+++ MonoGame.Framework/Platform/Graphics/Effect/EffectResource.OpenGL.cs
@@ -6,11 +6,20 @@ namespace Microsoft.Xna.Framework.Graphics
 {
     internal partial class EffectResource
     {
+#if IOS || ANDROID
+        const string AlphaTestEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.AlphaTestEffect.ogles.mgfxo";
+        const string BasicEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.BasicEffect.ogles.mgfxo";
+        const string DualTextureEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.DualTextureEffect.ogles.mgfxo";
+        const string EnvironmentMapEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.EnvironmentMapEffect.ogles.mgfxo";
+        const string SkinnedEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.SkinnedEffect.ogles.mgfxo";
+        const string SpriteEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.SpriteEffect.ogles.mgfxo";
+#else
         const string AlphaTestEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.AlphaTestEffect.ogl.mgfxo";
         const string BasicEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.BasicEffect.ogl.mgfxo";
         const string DualTextureEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.DualTextureEffect.ogl.mgfxo";
         const string EnvironmentMapEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.EnvironmentMapEffect.ogl.mgfxo";
         const string SkinnedEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.SkinnedEffect.ogl.mgfxo";
         const string SpriteEffectName = "Microsoft.Xna.Framework.Platform.Graphics.Effect.Resources.SpriteEffect.ogl.mgfxo";
+#endif
     }
 }
