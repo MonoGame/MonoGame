50d8c49f20584c315a48ab4dad197e4b3d7933e9
Thomas Altenburger <mister.helmut@gmail.com>
2021-02-16 10:52:17 +0100

    Default implementation for KeyboardInput and MessageBox

---
+++ MonoGame.Framework/Platform/Input/KeyboardInput.Default.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.Xna.Framework.Input
+{
+    public static partial class KeyboardInput
+    {
+        private static Task<string> PlatformShow(string title, string description, string defaultText, bool usePasswordMode)
+        {
+            throw new NotImplementedException("KeyboardInput is not implemented on this platform.");
+        }
+
+        private static void PlatformCancel(string result)
+        {
+            throw new NotImplementedException("KeyboardInput is not implemented on this platform.");
+        }
+    }
+}
