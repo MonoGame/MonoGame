b346e6ba5105e71178ddcec1476e8d9f9f468ac6
kchamara <kadupitige.a.chamar@wsu.edu>
2021-09-29 14:58:26 -0700

    Done adding Documentation for SongCollection file.

--- MonoGame.Framework/Media/SongCollection.cs
+++ MonoGame.Framework/Media/SongCollection.cs
@@ -8,16 +8,28 @@ using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Media
 {
+    /// <summary>
+    /// A collection of songs in the song library.The SongCollection does not immediately instantiate instances of all songs in
+    /// the collection. Instead, individual Song objects are created each time a user accesses a song through the collection's Item. 
+    /// </summary>
 	public class SongCollection : ICollection<Song>, IEnumerable<Song>, IEnumerable, IDisposable
 	{
         private bool isReadOnly = false;
+        //The list that holds the collection of songs 
         private readonly List<Song> innerlist;
 
+        /// <summary>
+        /// The public constructor that initalizes the song collection list  
+        /// </summary>
         public SongCollection()
         {
             innerlist = new List<Song>();
         }
 
+        /// <summary>
+        /// Internal constructor that sets the inner list based on the song list that is passed into the constructor
+        /// </summary>
+        /// <param name="songs"> A list of songs</param>
         internal SongCollection(List<Song> songs)
         {
             this.innerlist = songs;
@@ -26,7 +38,11 @@ namespace Microsoft.Xna.Framework.Media
         public void Dispose()
         {
         }
-		
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the SongCollection
+        /// </summary>
+        /// <returns> the enumerator</returns>
 		public IEnumerator<Song> GetEnumerator()
         {
             return innerlist.GetEnumerator();
@@ -37,6 +53,9 @@ namespace Microsoft.Xna.Framework.Media
             return innerlist.GetEnumerator();
         }
 
+        /// <summary>
+        /// Count the song items in the Song Collection and returns the number
+        /// </summary>
         public int Count
         {
             get
@@ -53,6 +72,11 @@ namespace Microsoft.Xna.Framework.Media
 		    }
         }
 
+        /// <summary>
+        /// Checks the song on a given index and return it. 
+        /// </summary>
+        /// <param name="index"> Index that is specified to search </param>
+        /// <returns>Returns the song name at the index </returns>
         public Song this[int index]
         {
             get
@@ -60,7 +84,11 @@ namespace Microsoft.Xna.Framework.Media
 				return this.innerlist[index];
             }
         }
-		
+
+        /// <summary>
+        /// Allows to add a song item into the song collection list. 
+        /// </summary>
+        /// <param name="item"> The song that needs be added </param>
 		public void Add(Song item)
         {
 
@@ -84,12 +112,19 @@ namespace Microsoft.Xna.Framework.Media
 
             this.innerlist.Add(item);
         }
-		
+
+        /// <summary>
+        /// Clears the song collection list 
+        /// </summary>
 		public void Clear()
         {
             innerlist.Clear();
         }
-        
+
+        /// <summary>
+        /// Songs will be cloned into the list 
+        /// </summary>
+        /// <returns> Returns the list with the cloned songs </returns>
         public SongCollection Clone()
         {
             SongCollection sc = new SongCollection();
@@ -97,22 +132,42 @@ namespace Microsoft.Xna.Framework.Media
                 sc.Add(song);
             return sc;
         }
-        
+
+        /// <summary>
+        /// Checks whether a song will be in the Song Collection. If it does contain return true otherwise false. 
+        /// </summary>
+        /// <param name="item"> The song item that will be searched through the list </param>
+        /// <returns> Returns true or false</returns>
         public bool Contains(Song item)
         {
             return innerlist.Contains(item);
         }
-        
+
+        /// <summary>
+        /// Copies the list into an array
+        /// </summary>
+        /// <param name="array"> The array that will be get copied intp </param>
+        /// <param name="arrayIndex">The index that will be get copied </param>
         public void CopyTo(Song[] array, int arrayIndex)
         {
             innerlist.CopyTo(array, arrayIndex);
         }
-		
+
+        /// <summary>
+        /// Find the index of a song from the song list
+        /// </summary>
+        /// <param name="item"> Song item that needs to be searched </param>
+        /// <returns> The index of the given song </returns>
 		public int IndexOf(Song item)
         {
             return innerlist.IndexOf(item);
         }
-        
+
+        /// <summary>
+        /// Remove method removes the given song from the Song Collection
+        /// </summary>
+        /// <param name="item">The song item that needs to be removed</param>
+        /// <returns> The list after removing the song. </returns>
         public bool Remove(Song item)
         {
             return innerlist.Remove(item);
