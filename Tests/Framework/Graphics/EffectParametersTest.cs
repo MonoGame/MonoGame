80fc1aa02cdc8dcfd204b1c518228a3809548c92
kchamara <kadupitige.a.chamar@wsu.edu>
2021-09-30 17:45:42 -0700

    Added a test class and a unit test for EffectParameters

---
+++ Tests/Framework/Graphics/EffectParametersTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Moq;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Graphics
+{
+    internal class EffectParametersTest : GraphicsDeviceTestFixtureBase
+    {
+        [Test]
+        public void TestSetValue()
+        {
+            var effect = new BasicEffect(game.GraphicsDevice);
+            effect.TextureEnabled = true;
+            effect.Texture = null;
+            effect.Parameters["SpecularPower"].SetValue(50);
+            var val= effect.Parameters["SpecularPower"].GetValueSingle();
+            Assert.AreEqual(50f, val);
+        }
+
+    }
+}
