b3a911a4b6c0249a2f166af1ef27ae60b3acd2f4
Cory Kroll <corykroll@gmail.com>
2023-04-18 05:56:44 -0500

    Added null check for ExtendedGamepad.ButtonOptions for iOS compatibility (#7966)

--- MonoGame.Framework/Platform/Input/GamePad.iOS.cs
+++ MonoGame.Framework/Platform/Input/GamePad.iOS.cs
@@ -153,7 +153,7 @@ namespace Microsoft.Xna.Framework.Input
 
                     if (controller.ExtendedGamepad.ButtonMenu.IsPressed)
                         buttons |= Buttons.Start;
-                    if (controller.ExtendedGamepad.ButtonOptions.IsPressed)
+                    if (controller.ExtendedGamepad.ButtonOptions?.IsPressed == true)
                         buttons |= Buttons.Back;
 
                     if (controller.ExtendedGamepad.DPad.Up.IsPressed)
