6c5dcc4201fe288e65d41809cc1ca4e821cba51d
Brett208 <15183337+Brett208@users.noreply.github.com>
2023-08-20 20:18:57 -0400

    Correct more typos in source code comments related to input

--- MonoGame.Framework/Input/GamePadState.cs
+++ MonoGame.Framework/Input/GamePadState.cs
@@ -59,7 +59,7 @@ namespace Microsoft.Xna.Framework.Input
         /// using the specified GamePadThumbSticks, GamePadTriggers, GamePadButtons, and GamePadDPad.
         /// </summary>
         /// <param name="thumbSticks">Initial thumbstick state.</param>
-        /// <param name="triggers">Initial trigger state..</param>
+        /// <param name="triggers">Initial trigger state.</param>
         /// <param name="buttons">Initial button state.</param>
         /// <param name="dPad">Initial directional pad state.</param>
         public GamePadState(GamePadThumbSticks thumbSticks, GamePadTriggers triggers, GamePadButtons buttons, GamePadDPad dPad) : this()
--- MonoGame.Framework/Platform/Android/MonoGameAndroidGameView.cs
+++ MonoGame.Framework/Platform/Android/MonoGameAndroidGameView.cs
@@ -104,7 +104,7 @@ namespace Microsoft.Xna.Framework
 
         public void SurfaceChanged(ISurfaceHolder holder, global::Android.Graphics.Format format, int width, int height)
         {
-            // Set flag to recreate gl surface or rendering can be bad on orienation change or if app 
+            // Set flag to recreate gl surface or rendering can be bad on orientation change or if app 
             // is closed in one orientation and re-opened in another.
             lock (_lockObject)
             {
@@ -391,7 +391,7 @@ namespace Microsoft.Xna.Framework
 
         void processStateRunning(CancellationToken token)
         {
-            // do not run game if surface is not avalible
+            // do not run game if surface is not available
             lock (_lockObject)
             {
                 if (!androidSurfaceAvailable)
@@ -482,7 +482,7 @@ namespace Microsoft.Xna.Framework
                 if (!androidSurfaceAvailable)
                     return;
 
-                // create surface if context is avalible
+                // create surface if context is available
                 if (glContextAvailable && !lostglContext)
                 {
                     try
@@ -496,7 +496,7 @@ namespace Microsoft.Xna.Framework
                     }
                 }
 
-                // create context if not avalible
+                // create context if not available
                 if ((!glContextAvailable || lostglContext))
                 {
                     // Start or Restart due to context loss
@@ -1157,7 +1157,7 @@ namespace Microsoft.Xna.Framework
 
             handled = Keyboard.KeyDown(keyCode);
 
-            // we need to handle the Back key here because it doesnt work any other way
+            // we need to handle the Back key here because it doesn't work any other way
             if (keyCode == Keycode.Back)
             {
                 GamePad.Back = true;
