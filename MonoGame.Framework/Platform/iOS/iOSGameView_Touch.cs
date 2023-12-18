19a948d29f426e04e94642986f1f9da8bea5443d
Peru S <perumaal@mindfulplays.com>
2023-12-18 06:51:34 -0800

    Add high-frequency touch events to iOS GameView (#8092)
    * Fix iOS GamePlatform to run pending background tasks #7520
    
    In other `Platform` classes (such as `SDLGamePlatform` and `AndroidGamePlatform`, `Threading.Run()` is called during `Tick()` or `RunLoop()` (inconsistently, it may be called before or after `Game.Tick()` unfortunately).
    
    Fixing this for `iOSGamePlatform`. `WinForms` and perhaps other platforms (Switch?) may suffer from the same issue.
    
    Note that texture loading from background thread is discouraged however a lot of computationally intensive operations (reading/decoding the image file streams, allocating CPU buffers) could be done in the background thread and hence is a good practice to move such work alone into the bgthread until we request the actual texture from the main thread.
    
    * Update docs for sync/test code
    
    * Fix typo in comment
    
    * Add coalesced touch events to iOS GameView
    
    * Add coalesced touch API to TouchPanel*
    
    * Fix UTF-8 copyright mistake
    
    * Ensure we process coalesced touches only if the client asked for it
    
    * Avoid using C#5.0+ features (expression-bodied properties)
    
    * Suggested changes
    
    * Remove Coalesced API (new) and update existing touch event with a flag for `isHighFrequency`.
     - Also consistently use `high-frequency` instead of `coalesced` when referring to this new behavior.
    
    * Remove `out int` C# 7.0 / use C# 5.0 feature instead
    
    * Update based on @harry-cpp comments
    
    * Rename to `origin` instead of `upstream`
    
    * Remove git stuff
    
    * Address PR comments
    
    * Change (c) UTF-8 due to codepage issues, my bad.
    
    ---------
    
    Co-authored-by: Tom Spilman <tom@sickheadgames.com>
    Co-authored-by: Thao D <thao.mindfulplay@gmail.com>
    Co-authored-by: Simon (Darkside) Jackson <darkside@zenithmoon.com>
    Co-authored-by: mindfulplay.build <mindfulplay@appu.app>

--- MonoGame.Framework/Platform/iOS/iOSGameView_Touch.cs
+++ MonoGame.Framework/Platform/iOS/iOSGameView_Touch.cs
@@ -77,84 +77,122 @@ using UIKit;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 
-namespace Microsoft.Xna.Framework {
-	partial class iOSGameView {
-		
-		static GestureType EnabledGestures
-		{
-			get { return TouchPanel.EnabledGestures; }
-		}
-		
-		#region Touches
+namespace Microsoft.Xna.Framework
+{
+    partial class iOSGameView
+    {
 
-		public override void TouchesBegan (NSSet touches, UIEvent evt)
-		{
-			base.TouchesBegan (touches, evt);
-			FillTouchCollection (touches);
-		}
+        static GestureType EnabledGestures
+        {
+            get { return TouchPanel.EnabledGestures; }
+        }
 
-		public override void TouchesEnded (NSSet touches, UIEvent evt)
-		{
-			base.TouchesEnded (touches, evt);
-			FillTouchCollection (touches);
-		}
+        #region Touches
 
-		public override void TouchesMoved (NSSet touches, UIEvent evt)
-		{
-			base.TouchesMoved (touches, evt);
-			FillTouchCollection (touches);
-		}
+        public override void TouchesBegan(NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+            FillTouchCollection(touches, evt);
+        }
 
-		public override void TouchesCancelled (NSSet touches, UIEvent evt)
-		{
-			base.TouchesCancelled (touches, evt);
-			FillTouchCollection (touches);
-		}
-		
-		// TODO: Review FillTouchCollection
-		private void FillTouchCollection (NSSet touches)
-		{
-			if (touches.Count == 0)
-				return;
+        public override void TouchesEnded(NSSet touches, UIEvent evt)
+        {
+            base.TouchesEnded(touches, evt);
+            FillTouchCollection(touches, evt);
+        }
 
-			var touchesArray = touches.ToArray<UITouch> ();
-			for (int i = 0; i < touchesArray.Length; ++i) {
-				var touch = touchesArray [i];
+        public override void TouchesMoved(NSSet touches, UIEvent evt)
+        {
+            base.TouchesMoved(touches, evt);
+            FillTouchCollection(touches, evt);
+        }
 
-				//Get position touch
-				var location = touch.LocationInView (touch.View);
-				var position = GetOffsetPosition (new Vector2 ((float)location.X, (float)location.Y), true);
-				var id = touch.Handle.GetHashCode();
+        public override void TouchesCancelled(NSSet touches, UIEvent evt)
+        {
+            base.TouchesCancelled(touches, evt);
+            FillTouchCollection(touches, evt);
+        }
 
-				switch (touch.Phase) 
+        // Process and fill touch events: process high-frequency events if they are available, otherwise,
+        // use the last-frame touch events.
+        private void FillTouchCollection(NSSet touches, UIEvent evt)
+        {
+            if (touches.Count == 0)
+                return;
+
+            var touchesArray = touches.ToArray<UITouch>();
+            for (int touchIndex = 0; touchIndex < touchesArray.Length; ++touchIndex)
+            {
+                var touch = touchesArray[touchIndex];
+                var id = touch.Handle.GetHashCode();
+                FillTouch(touch, id, false);
+
+                if (TouchPanel.EnableHighFrequencyTouch)
                 {
-				//case UITouchPhase.Stationary:
-				case UITouchPhase.Moved:
-					TouchPanel.AddEvent(id, TouchLocationState.Moved, position);					
-					break;
-				case UITouchPhase.Began:
-                    TouchPanel.AddEvent(id, TouchLocationState.Pressed, position);
-					break;
-				case UITouchPhase.Ended	:
-                    TouchPanel.AddEvent(id, TouchLocationState.Released, position);
-					break;
-				case UITouchPhase.Cancelled:
-                    TouchPanel.AddEvent(id, TouchLocationState.Released, position);
-					break;
-				default:
-					break;
-				}
-			}
-		}
-		
-		// TODO: Review GetOffsetPosition, hopefully it can be removed now.
-		public Vector2 GetOffsetPosition (Vector2 position, bool useScale)
-		{
-			if (useScale)
-				return position * (float)Layer.ContentsScale;
-			return position;
-		}
+                    var coalescedTouches = evt.GetCoalescedTouches(touch);
+                    if (coalescedTouches != null)
+                    {
+                        // Per the document https://developer.apple.com/documentation/uikit/uievent/1613808-coalescedtouches,
+                        // there may be a few coalesced touch events between two subsequence frames. The frequence of these
+                        // events is perhaps more than the display frequency, and perhaps max out at 240Hz for Apple Pencil
+                        // and so on.
+                        for (int coalescedIndex = 0; coalescedIndex < coalescedTouches.Length; ++coalescedIndex)
+                        {
+                            FillTouch(coalescedTouches[coalescedIndex], id, true);
+                        }
+                    }
+                }
+            }
+        }
 
-		#endregion Touches
-	}
+        private void FillTouch(UITouch touch, int id, bool coalesced)
+        {
+            //Get position touch
+            var location = touch.LocationInView(touch.View);
+            var position = GetOffsetPosition(new Vector2((float)location.X, (float)location.Y), true);
+            switch (touch.Phase)
+            {
+            case UITouchPhase.Stationary:
+                if (coalesced)
+                    TouchPanel.AddHighResolutionTouchEvent(id, TouchLocationState.Moved, position);
+                break;
+            case UITouchPhase.Moved:
+                if (coalesced)
+                    TouchPanel.AddHighResolutionTouchEvent(id, TouchLocationState.Moved, position);
+                else
+                    TouchPanel.AddEvent(id, TouchLocationState.Moved, position);
+                break;
+            case UITouchPhase.Began:
+                if (coalesced)
+                    TouchPanel.AddHighResolutionTouchEvent(id, TouchLocationState.Pressed, position);
+                else
+                    TouchPanel.AddEvent(id, TouchLocationState.Pressed, position);
+                break;
+            case UITouchPhase.Ended:
+                if (coalesced)
+                    TouchPanel.AddHighResolutionTouchEvent(id, TouchLocationState.Released, position);
+                else
+                    TouchPanel.AddEvent(id, TouchLocationState.Released, position);
+                break;
+            case UITouchPhase.Cancelled:
+                if (coalesced)
+                    TouchPanel.AddHighResolutionTouchEvent(id, TouchLocationState.Released, position);
+                else
+                    TouchPanel.AddEvent(id, TouchLocationState.Released, position);
+                break;
+            default:
+                break;
+            }
+        }
+
+        // UI touch events are returned in content space while MonoGame uses screen-space pixel coordinates.
+        public Vector2 GetOffsetPosition(Vector2 position, bool useScale)
+        {
+            if (useScale)
+                return position * (float)Layer.ContentsScale;
+            return position;
+        }
+
+        #endregion Touches
+    }
 }
