830caca2fb924d3e163d4397cd9d0affb89a5e09
Peru S <perumaal@mindfulplays.com>
2023-12-20 17:22:06 -0800

    Add end-to-end test framework inspired from iOS interactive tests
    This brings interactive tests from iOS and allows them to be run in any environment.
    Specifically, given a set of `Game` classes under `Tests/Interactive/Tests`, we can run that `Game` instance in a test harness under DesktopGL (Mac/Windows)/iOS (Android coming next).
    
    - Lots of reshuffling from Tests/Interactive/iOS -> Common/Tests
      - Also added doc to run the tests in simulator/on-device.
    - New TestRunners (for now DesktopGL on Mac/Windows and iOS)- will add Android next
      - DesktopGL only allows running one test per run, due to STA/SDL Window event loop.
    - Added doc in Tests/Interactive/README.md on how to run the test runners.

---
+++ Tests/Interactive/TestRunners/DesktopGL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MonoGame.InteractiveTests
+{
+    class TestRunner
+    {
+        /// <summary>
+        /// Runs a specific <see cref="InteractiveTest"/> instance specified from the
+        /// command line. If multiple tests match, the first one is run.
+        /// Invoking without any command-line args shows all the discovered tests.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var tests = new InteractiveTests();
+            Console.WriteLine($"Provided options : {string.Join(' ', args)}; {tests.HelpStr()}");
+            var filteredTests = tests.Parse(args);
+            if (filteredTests.Count > 0)
+            {
+                var test = filteredTests[0];
+                var testGame = test.Create() as TestGame;
+                if (testGame != null)
+                {
+                    Console.WriteLine($"--Running {test.Name}");
+                    testGame.Exiting += (o, e) => { testGame.Exit(); };
+                    testGame.Run();
+                    Console.WriteLine($"--Finished {test.Name}");
+                }
+
+                // We currently support running exactly one test per run.
+                // This is due to MonoGame limitation of creating a single SDL thread/Window.
+            }
+        }
+    }
+}
