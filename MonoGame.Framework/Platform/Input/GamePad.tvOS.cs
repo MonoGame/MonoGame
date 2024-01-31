55818bd0c0b0ce7c83207a34761712b62664b539
Dean Ellis <dellis1972@googlemail.com>
2024-01-31 09:42:56 +0000

    Update Copyright to MonoGame Foundation, Inc (#8126)
    * Update Copyright to MonoGame Foundation, Inc
    
    * Update all code to use LICENSE.txt
    
    * Fix up weird character encoding

--- MonoGame.Framework/Platform/Input/GamePad.tvOS.cs
+++ MonoGame.Framework/Platform/Input/GamePad.tvOS.cs
@@ -1,4 +1,4 @@
-﻿// MonoGame - Copyright (C) The MonoGame Team
+﻿// MonoGame - Copyright (C) MonoGame Foundation, Inc
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 using GameController;
