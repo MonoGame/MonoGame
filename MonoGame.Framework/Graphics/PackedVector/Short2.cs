750a493b11ae568bc4718b72fd12a1ff8b1500d5
Christopher Whitley <aristurtledev@gmail.com>
2024-02-13 15:18:06 -0500

    Add missing XML documentation for Short2

--- MonoGame.Framework/Graphics/PackedVector/Short2.cs
+++ MonoGame.Framework/Graphics/PackedVector/Short2.cs
@@ -7,30 +7,71 @@ using System;
 
 namespace Microsoft.Xna.Framework.Graphics.PackedVector
 {
+    /// <summary>
+    /// Packed vector type containing two 16-bit signed integer values.
+    /// </summary>
 	public struct Short2 : IPackedVector<uint>, IEquatable<Short2>
 	{
 		private uint _short2Packed;
 
+        /// <summary>
+        /// Creates a new <see cref="Short2"/> from the specified <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="vector">
+        /// The <see cref="Vector2"/> whos components contain the initial values
+        /// for this <see cref="Short2"/>.
+        /// </param>
 		public Short2 (Vector2 vector)
 		{
 			_short2Packed = PackInTwo (vector.X, vector.Y);
 		}
 
+        /// <summary>
+        /// Creates a new <see cref="Short2"/> from the specified component values.
+        /// </summary>
+        /// <param name="x">The initial x-component value.</param>
+        /// <param name="y">The initial y-component value.</param>
 		public Short2 (Single x,Single y)
 		{
 			_short2Packed = PackInTwo (x, y);
 		}
 
+        /// <summary>
+        /// Returns a value that indicates whether two <see cref="Short2"/>
+        /// values are not equal.
+        /// </summary>
+        /// <param name="a">The <see cref="Short2"/> on the left of the inequality operator.</param>
+        /// <param name="b">The <see cref="Short2"/> on the right of the inequality operator.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="a"/> and <paramref name="b"/>
+        /// are different; otherwise, <see langword="false"/>.
+        /// </returns>
 		public static bool operator !=(Short2 a, Short2 b)
 		{
 			return a.PackedValue != b.PackedValue;
 		}
 
+        /// <summary>
+        /// Returns a value that indicates whether two <see cref="Short2"/>
+        /// values are equal.
+        /// </summary>
+        /// <param name="a">The <see cref="Short2"/> on the left of the equality operator.</param>
+        /// <param name="b">The <see cref="Short2"/> on the right of the equality operator.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="a"/> and <paramref name="b"/>
+        /// are equal; otherwise, <see langword="false"/>.
+        /// </returns>
 		public static bool operator ==(Short2 a, Short2 b)
 		{
 			return a.PackedValue == b.PackedValue;
 		}
 
+        /// <summary>
+        /// Gets or Sets the packed representation of this <see cref="Short2"/>.
+        /// </summary>
+        /// <value>
+        /// The packed representation of this <see cref="Short2"/>
+        /// </value>
 		public uint PackedValue
         {
 			get
@@ -43,6 +84,15 @@ namespace Microsoft.Xna.Framework.Graphics.PackedVector
 			}
 		}
 
+        /// <summary>
+        /// Returns a value that indicates whether this <see cref="Short2"/>
+        /// and a specified object are equal.
+        /// </summary>
+        /// <param name="obj">The object to compare with this <see cref="Short2"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if this <see cref="Short2"/> and
+        /// <paramref name="obj"/> are equal; otherwise, <see langword="false"/>.
+        /// </returns>
 		public override bool Equals (object obj)
 		{
             if (obj is Short2)
@@ -50,21 +100,48 @@ namespace Microsoft.Xna.Framework.Graphics.PackedVector
             return false;
 		}
 
+        /// <summary>
+        /// Returns a value tha indicates whether this <see cref="Short2"/>
+        /// and a specified <see cref="Short2"/> are equal.
+        /// </summary>
+        /// <param name="other">The other <see cref="Short2"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the two <see cref="Short2"/> values
+        /// are equal; otherwise, <see langword="false"/>.
+        /// </returns>
 		public bool Equals (Short2 other)
 		{
             return this == other;
 		}
 
+        /// <summary>
+        /// Returns the hash code for this <see cref="Short2"/> value.
+        /// </summary>
+        /// <returns>
+        /// The 32-bit signed integer hash code for this <see cref="Short2"/> value.
+        /// </returns>
 		public override int GetHashCode ()
 		{
 			return _short2Packed.GetHashCode();
 		}
 
+        /// <summary>
+        /// Returns the string representation of this <see cref="Short2"/> value.
+        /// </summary>
+        /// <returns>
+        /// The string representation of this <see cref="Short2"/> value.
+        /// </returns>
 		public override string ToString ()
 		{
             return _short2Packed.ToString("x8");
 		}
 
+        /// <summary>
+        /// Expands this <see cref="Short2"/> to a <see cref="Vector2"/>
+        /// </summary>
+        /// <returns>
+        /// The expanded <see cref="Short2"/> as a <see cref="Vector2"/>.
+        /// </returns>
 		public Vector2 ToVector2 ()
 		{
 			var v2 = new Vector2 ();
