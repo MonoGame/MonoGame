1bc36c3ebd763335f3bcfa9594d338bb9c45d615
Rix <157877453+Maniekko@users.noreply.github.com>
2024-02-13 14:16:39 +0300

    Add missing XML documentation for WaveBank

--- MonoGame.Framework/Audio/Xact/WaveBank.cs
+++ MonoGame.Framework/Audio/Xact/WaveBank.cs
@@ -393,6 +393,10 @@ namespace Microsoft.Xna.Framework.Audio
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Allows this object to attempt to free resources and perform other
+        /// cleanup operations before garbage collection reclaims the object.
+        /// </summary>
         ~WaveBank()
         {
             Dispose(false);
