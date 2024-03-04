111a88f9d68540797aae95ca880d948d6ecb83d6
Christopher Whitley <aristurtledev@gmail.com>
2024-03-04 13:49:47 -0500

    Empty summary tag for protected OpenStream

--- MonoGame.Framework/Content/ContentManager.cs
+++ MonoGame.Framework/Content/ContentManager.cs
@@ -371,6 +371,7 @@ namespace Microsoft.Xna.Framework.Content
             return result;
 		}
 		
+        /// <summary />
 		protected virtual Stream OpenStream(string assetName)
 		{
 			Stream stream;
