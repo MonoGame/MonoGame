0d63701a28adb1d6d8661880045ad0698cb2284d
Christopher Whitley <aristurtledev@gmail.com>
2024-03-05 12:45:07 -0500

    Updates DOTNET_URL to current .net8

--- Tools/MonoGame.Effect.Compiler/mgfxc_wine_setup.sh
+++ Tools/MonoGame.Effect.Compiler/mgfxc_wine_setup.sh
@@ -36,7 +36,7 @@ wine64 regedit crashdialog.reg
 popd
 
 # get dotnet
-DOTNET_URL="https://download.visualstudio.microsoft.com/download/pr/44d08222-aaa9-4d35-b24b-d0db03432ab7/52a4eb5922afd19e8e0d03e0dbbb41a0/dotnet-sdk-6.0.302-win-x64.zip"
+DOTNET_URL="https://dotnetcli.azureedge.net/dotnet/Sdk/8.0.201/dotnet-sdk-8.0.201-win-x64.zip"
 curl $DOTNET_URL --output "$SCRIPT_DIR/dotnet-sdk.zip"
 7z x "$SCRIPT_DIR/dotnet-sdk.zip" -o"$WINEPREFIX/drive_c/windows/system32/"
 
