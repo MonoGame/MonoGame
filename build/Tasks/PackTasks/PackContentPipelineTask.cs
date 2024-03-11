f3b7c0d05f9e217b9a0d7d6ba2164523cf3f3114
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:17:00 -0400

    Added PackContentPipeline Task

---
+++ build/Tasks/PackTasks/PackContentPipelineTask.cs
@@ -0,0 +1,16 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+using MonoGame.Framework.Build.Tasks.BuildTasks;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackContentPipeline")]
+[IsDependentOn(typeof(BuildContentPipelineTask))]
+public sealed class PackContentPipelineTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameEffectCompiler, context.DotNetPackSettings);
+        context.DotNetPack(ProjectPaths.MonoGameFrameworkContentPipeline, context.DotNetPackSettings);
+    }
+}
