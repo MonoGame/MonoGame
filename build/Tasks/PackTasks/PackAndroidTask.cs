30992846f78c35d2956019dbe78f631476418352
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:16:51 -0400

    Added PackAndroid Task

---
+++ build/Tasks/PackTasks/PackAndroidTask.cs
@@ -0,0 +1,26 @@
+﻿using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+using MonoGame.Framework.Build.Tasks.BuildTasks;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackAndroid")]
+[IsDependentOn(typeof(BuildAndroidTask))]
+public sealed class PackAndroidTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        if (context.GetMSBuildWith("Component.Xamarin"))
+        {
+            return true;
+        }
+
+        return context.DirectoryExists("/Library/Frameworks/Xamarin.Android.framework");
+    }
+
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameFrameworkAndroid, context.DotNetPackSettings);
+    }
+}
