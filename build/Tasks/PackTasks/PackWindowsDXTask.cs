036ddb875554e3cd8b1a20940672759714b2b497
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:18:08 -0400

    Added PackWindowsDX Task

---
+++ build/Tasks/PackTasks/PackWindowsDXTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+using MonoGame.Framework.Build.Tasks.BuildTasks;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackWindowsDX")]
+[IsDependentOn(typeof(BuildWindowsDXTask))]
+public sealed class PackWindowsDXTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        return context.IsRunningOnWindows();
+    }
+
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameFrameworkWindowsDX, context.DotNetPackSettings);
+    }
+}
