7339ea6ab1bfc6980963bd8e57291231ff7ef3ef
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:05:14 -0400

    Added BuildConsoleCheck Task

---
+++ build/Tasks/BuildTasks/BuildConsoleCheckTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.BuildTasks;
+
+[TaskName("BuildConsoleCheck")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class BuildConsoleCheckTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        return context.IsRunningOnWindows();
+    }
+
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore(ProjectPaths.MonoGameFrameworkConsoleCheck, context.DotNetRestoreSettings);
+        context.DotNetBuild(ProjectPaths.MonoGameFrameworkConsoleCheck, context.DotNetBuildSettings);
+    }
+}
