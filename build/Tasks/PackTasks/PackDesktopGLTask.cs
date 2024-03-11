44d42abb43702328d21f3df266520ed769a4c6f3
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:17:08 -0400

    Added PackDesktopGL Task

---
+++ build/Tasks/PackTasks/PackDesktopGLTask.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+using MonoGame.Framework.Build.Tasks.BuildTasks;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackDesktopGL")]
+[IsDependentOn(typeof(BuildDesktopGLTask))]
+public sealed class PackDesktopGLTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameFrameworkDesktopGL, context.DotNetPackSettings);
+    }
+}
