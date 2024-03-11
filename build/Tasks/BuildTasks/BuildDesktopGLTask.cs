d817dc8501aaea230a3b1e30dd890667c1737d62
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:05:51 -0400

    Added BuildDesktopGL Task

---
+++ build/Tasks/BuildTasks/BuildDesktopGLTask.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.BuildTasks;
+
+[TaskName("BuildDesktopGL")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class BuildDesktopGLTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore(ProjectPaths.MonoGameFrameworkDesktopGL, context.DotNetRestoreSettings);
+        context.DotNetBuild(ProjectPaths.MonoGameFrameworkDesktopGL, context.DotNetBuildSettings);
+    }
+}
