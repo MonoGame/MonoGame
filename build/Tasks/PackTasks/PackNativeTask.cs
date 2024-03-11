68297b2379afae9dcf10a7461aab22329d55cff5
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:17:38 -0400

    Added PackNative Task

---
+++ build/Tasks/PackTasks/PackNativeTask.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+using MonoGame.Framework.Build.Tasks.BuildTasks;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackNative")]
+[IsDependentOn(typeof(BuildNativeTask))]
+public sealed class PackNativeTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameFrameworkNative, context.DotNetPackSettings);
+    }
+}
