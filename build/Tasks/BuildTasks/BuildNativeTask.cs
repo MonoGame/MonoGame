2206de17f6e4a9487194cbea7062665ba7c04999
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:06:07 -0400

    Added BuildNative Task

---
+++ build/Tasks/BuildTasks/BuildNativeTask.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.BuildTasks;
+
+[TaskName("BuildNative")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class BuildNativeTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore(ProjectPaths.MonoGameFrameworkNative, context.DotNetRestoreSettings);
+        context.DotNetBuild(ProjectPaths.MonoGameFrameworkNative, context.DotNetBuildSettings);
+    }
+}
