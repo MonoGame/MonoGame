ca0e985be41b6254b1c4304ff1bee691654a9406
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 01:57:13 -0400

    Added prep task

---
+++ build/Tasks/PrepTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common;
+using Cake.Common.IO;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks;
+
+[TaskName("Prep")]
+public sealed class PrepTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        if (!context.IsRunningOnWindows())
+        {
+            //  SET MGFXC_WINE_PATH for building shaders on macOS and Linux
+            System.Environment.SetEnvironmentVariable("MGFXC_WINE_PATH", context.EnvironmentVariable("HOME") + "/.winemonogame");
+        }
+
+        context.CleanDirectory(context.ArtifactsDirectory);
+        context.CreateDirectory(context.ArtifactsDirectory);
+    }
+}
