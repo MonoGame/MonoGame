456e36e0491f5473fec4c02e2f3267f3e57266f2
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:17:29 -0400

    Added PackDotNetTemplates Task

---
+++ build/Tasks/PackTasks/PackDotNetTemplatesTask.cs
@@ -0,0 +1,14 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.PackTasks;
+
+[TaskName("PackDotNetTemplates")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class PackDotNetTemplatesTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetPack(ProjectPaths.MonoGameTemplatesCSharp, context.DotNetPackSettings);
+    }
+}
