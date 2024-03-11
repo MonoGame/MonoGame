57e99a11f7dc3481676055e827c5c72a97bc5852
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:15:12 -0400

    Added BuildiOS Task

---
+++ build/Tasks/BuildTasks/BuildiOSTask.cs
@@ -0,0 +1,27 @@
+﻿using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.BuildTasks;
+
+[TaskName("BuildiOS")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class BuildiOSTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        if (context.GetMSBuildWith("Component.Xamarin"))
+        {
+            return true;
+        }
+
+        return context.DirectoryExists("/Library/Frameworks/Xamarin.iOS.framework");
+    }
+
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore(ProjectPaths.MonoGameFrameworkiOS, context.DotNetRestoreSettings);
+        context.DotNetBuild(ProjectPaths.MonoGameFrameworkiOS, context.DotNetBuildSettings);
+    }
+}
+
