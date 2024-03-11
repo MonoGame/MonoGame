9d68d1c7bfaa7a61e0eb159c341ae47e09ac2310
Christopher Whitley <aristurtledev@gmail.com>
2024-03-11 02:07:38 -0400

    Added BuildWindowsDX Task

---
+++ build/Tasks/BuildTasks/BuildWindowsDXTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace MonoGame.Framework.Build.Tasks.BuildTasks;
+
+[TaskName("BuildWindowsDX")]
+[IsDependentOn(typeof(PrepTask))]
+public sealed class BuildWindowsDXTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        return context.IsRunningOnWindows();
+    }
+
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore(ProjectPaths.MonoGameFrameworkWindowsDX, context.DotNetRestoreSettings);
+        context.DotNetBuild(ProjectPaths.MonoGameFrameworkWindowsDX, context.DotNetBuildSettings);
+    }
+}
