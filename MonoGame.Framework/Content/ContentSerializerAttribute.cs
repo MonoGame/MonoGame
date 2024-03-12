10ee59b099741c3561873200d44eb3a5f9ee00f7
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-03-12 04:43:44 -0400

    [Documentation] Adds and Updates ContentSerializerAttribute XML Documentation (#8214)
    * Fixed various spelling errors
    
    * Update wording of constructor
    
    * Add documentation for AllowNull property
    
    * Updated documentation for CollectionItemName property
    
    * Adds documentation for ElementName property
    
    * Add documentation for FlattenContent
    
    * Updates documentation for HasCollectionItemName property
    
    * Add documentation for Optional property
    
    * Add documentation for SharedResource property
    
    * Added documentation for Clone()
    
    * Add documentation for ContentSerializerAttribute

--- MonoGame.Framework/Content/ContentSerializerAttribute.cs
+++ MonoGame.Framework/Content/ContentSerializerAttribute.cs
@@ -5,30 +5,37 @@
 using System;
 
 namespace Microsoft.Xna.Framework.Content
-{	
+{
+    /// <summary>
+    /// Defines a custom <see cref="Attribute"/> that marks a field or property to control how it is serialized or to
+    /// indicate that protected or private data should be included in serialization.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public sealed class ContentSerializerAttribute : Attribute
     {
         private string _collectionItemName;
 
         /// <summary>
-        /// Creates an instance of the attribute.
+        /// Initializes a new instance of the content serializer attribute.
         /// </summary>
         public ContentSerializerAttribute()
         {
             AllowNull = true;
         }
 
+        /// <summary>
+        /// Gets or Sets a value that indicates whether this member can have a null value (default is true).
+        /// </summary>
         public bool AllowNull { get; set; }
 
         /// <summary>
-        /// Returns the overriden XML element name or the default "Item".
+        /// Gets or Sets the XML element name for each item in a collection (default is "Item"). 
         /// </summary>
         public string CollectionItemName
         {
             get
             {
-                // Return the defaul if unset.
+                // Return the default if unset.
                 if (string.IsNullOrEmpty(_collectionItemName))
                     return "Item";
 
@@ -40,12 +47,20 @@ namespace Microsoft.Xna.Framework.Content
             }
         }
 
+        /// <summary>
+        /// Gets or Sets the XML element name (default is the name of the managed type member).
+        /// </summary>
         public string ElementName { get; set; }
 
+        /// <summary>
+        /// Gets or Sets a value indicating whether to write member contents directly into the current XML context
+        /// rather than wrapping the member in a new XML element (default is false).
+        /// </summary>
         public bool FlattenContent { get; set; }
 
         /// <summary>
-        /// Returns true if the default CollectionItemName value was overridden.
+        /// Gets a value that indicates whether an explicit <see cref="CollectionItemName"/> is being used or the
+        /// default value.
         /// </summary>
         public bool HasCollectionItemName
         {
@@ -55,10 +70,22 @@ namespace Microsoft.Xna.Framework.Content
             }
         }
 
+        /// <summary>
+        /// Gets or Sets a value indicating whether to write this element if the member is null and skip past it if not
+        /// found when deserializing XML (default is false).
+        /// </summary>
         public bool Optional { get; set; }
 
+        /// <summary>
+        /// Gets or Sets a value indicating whether this member is reference from multiple parents and should be
+        /// serialized as a unique ID reference (default is false).
+        /// </summary>
         public bool SharedResource { get; set; }
 
+        /// <summary>
+        /// Creates a copy of this content serializer attribute.
+        /// </summary>
+        /// <returns>The copy of this content serializer attribute.</returns>
         public ContentSerializerAttribute Clone()
         {
             var clone = new ContentSerializerAttribute ();
