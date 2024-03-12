1902395d686d9a04dea7c27b20ebc15b4ac72e9c
Christopher Whitley <aristurtledev@gmail.com>
2024-03-12 14:14:13 -0400

    Added documentation for DirectionalLight constructor

--- MonoGame.Framework/Graphics/DirectionalLight.cs
+++ MonoGame.Framework/Graphics/DirectionalLight.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
+
 	public sealed class DirectionalLight
 	{
 		internal EffectParameter diffuseColorParameter;
@@ -16,7 +17,19 @@ namespace Microsoft.Xna.Framework.Graphics
 		Vector3 direction;
 		Vector3 specularColor;
 		bool enabled;
-		
+
+        /// <summary>
+        /// Creates a new instance of the DirectionalLight class with or without a copy of a DirectionalLight instance.
+        /// </summary>
+        /// <param name="directionParameter">The light direction.</param>
+        /// <param name="diffuseColorParameter">The diffuse color.</param>
+        /// <param name="specularColorParameter">The specular color.</param>
+        /// <param name="cloneSource">The cloned source to copy from.</param>
+        /// <remarks>
+        /// The initial parameter values are either copied from the cloned object or set to default values (if the
+        /// coned object is null).  The three Effect Parameters are updated whenever the direction, diffuse color, or
+        /// specular color properties are changed; or you can set these to null if you are using the cloned object.
+        /// </remarks>
 		public DirectionalLight (EffectParameter directionParameter, EffectParameter diffuseColorParameter, EffectParameter specularColorParameter, DirectionalLight cloneSource)
 		{
 			this.diffuseColorParameter = diffuseColorParameter;
