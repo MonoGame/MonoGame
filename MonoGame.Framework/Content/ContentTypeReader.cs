f327ac1f5f247034cdabfe320d46360a39a44a95
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-03-12 04:43:30 -0400

    [Documentation] Adds and Updates ContentTypeReader XML Documentation (#8215)
    * Add documentation for CanDeserializeIntoExistingObject property
    
    * Add documentation for TargetType property
    
    * Add documentation for TypeVersion property
    
    * Add empty summary for protected Constructor
    
    * Add empty summary for protected Initialize
    
    * Add empty summary for protected Read
    
    * Add empty summary for protected Constructor
    
    * Add inheritdoc for Read method override
    
    * Add inheritdoc for Read method override
    
    * Add documentation for ContentTypeReader
    
    * Add documentation for ContentTypeReader<T>
    
    * Should be empty summary, not inheritdoc

--- MonoGame.Framework/Content/ContentTypeReader.cs
+++ MonoGame.Framework/Content/ContentTypeReader.cs
@@ -7,46 +7,71 @@ using System.IO;
 
 namespace Microsoft.Xna.Framework.Content
 {
+    /// <summary>
+    /// Defines the core behavior of content type readers used for reading a specific managed type from an .xnb binary
+    /// format and provides a base for derived classes.
+    /// </summary>
     public abstract class ContentTypeReader
     {
         private Type _targetType;
 
+        /// <summary>
+        /// Gets a value that indicates whether this content type read can deserialize into an object with the same
+        /// type ad defined in the <see cref="TargetType"/> property.
+        /// </summary>
         public virtual bool CanDeserializeIntoExistingObject
         {
             get { return false; }
         }
 
+        /// <summary>
+        /// Gets the type handled by this reader component.
+        /// </summary>
         public Type TargetType
         {
             get { return _targetType; }
         }
 
+        /// <summary>
+        /// Gets the format version number for this type.
+        /// </summary>
         public virtual int TypeVersion
         {
             get { return 0; }   // The default version (unless overridden) is zero
         }
 
+        /// <summary />
         protected ContentTypeReader(Type targetType)
         {
             _targetType = targetType;
         }
 
+        /// <summary />
         protected internal virtual void Initialize(ContentTypeReaderManager manager)
         {
             // Do nothing. Are we supposed to add ourselves to the manager?
         }
 
+        /// <summary />
         protected internal abstract object Read(ContentReader input, object existingInstance);
     }
 
+    /// <summary>
+    /// Defines the core behavior of content type readers used for reading a specific managed type from an .xnb binary
+    /// format and provides a base for derived classes.  Derive from this class to add new data types to the content
+    /// pipeline system.
+    /// </summary>
+    /// <typeparam name="T">The managed type to read.</typeparam>
     public abstract class ContentTypeReader<T> : ContentTypeReader
     {
+        /// <summary />
         protected ContentTypeReader()
             : base(typeof(T))
         {
             // Nothing
         }
 
+        /// <inheritdoc />
         protected internal override object Read(ContentReader input, object existingInstance)
         {
 			// as per the documentation http://msdn.microsoft.com/en-us/library/microsoft.xna.framework.content.contenttypereader.read.aspx
@@ -59,6 +84,7 @@ namespace Microsoft.Xna.Framework.Content
 			return Read(input, (T)existingInstance);
         }
 
+        /// <summary />
         protected internal abstract T Read(ContentReader input, T existingInstance);
     }
 }
