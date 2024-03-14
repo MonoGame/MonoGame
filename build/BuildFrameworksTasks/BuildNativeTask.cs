bb421b2e5ec7039da8a7105985c1df1757d5dd18
Harry <harry.cpp@outlook.com>
2024-03-14 12:34:59 +0100

    [BuildScripts] Switch to CAKE Frosting (#8234)

---
+++ build/BuildFrameworksTasks/BuildNativeTask.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BuildScripts;
+
+[TaskName("Build Native")]
+public sealed class BuildNativeTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+        => context.DotNetPack(context.GetProjectPath(ProjectType.Framework, "Native"), context.DotNetPackSettings);
+}
---
+++ build/BuildFrameworksTasks/BuildUWPTask.cs
@@ -0,0 +1,27 @@
+﻿using Cake.Common.Tools.VSWhere;
+using Cake.Common.Tools.VSWhere.Latest;
+
+namespace BuildScripts;
+
+[TaskName("Build UWP")]
+public sealed class BuildUWPTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context)
+    {
+        if (!context.IsRunningOnWindows())
+            return false;
+
+        var vsLatest = context.VSWhereLatest(new VSWhereLatestSettings()
+        {
+            Requires = "Microsoft.VisualStudio.Component.Windows10SDK.19041" 
+        });
+
+        if (vsLatest == null)
+            return false;
+
+        return context.GetFiles(vsLatest.FullPath + "/**/MSBuild.exe").Count > 0;
+    }
+
+    public override void Run(BuildContext context)
+        => context.MSBuild(context.GetProjectPath(ProjectType.Framework, "WindowsUniversal"), context.MSPackSettings);
+}
