a81306ef62156f2e9048c121a49226870dfe7bc9
harry.cpp <harry.cpp@outlook.com>
2024-03-14 14:29:04 +0100

    [BuildScripts] Don't package ConsoleCheck

--- build/BuildFrameworksTasks/BuildConsoleCheckTask.cs
+++ build/BuildFrameworksTasks/BuildConsoleCheckTask.cs
@@ -7,5 +7,5 @@ public sealed class BuildConsoleCheckTask : FrostingTask<BuildContext>
     public override bool ShouldRun(BuildContext context) => context.IsRunningOnWindows();
 
     public override void Run(BuildContext context)
-        => context.DotNetPack(context.GetProjectPath(ProjectType.Framework, "ConsoleCheck"), context.DotNetPackSettings);
+        => context.DotNetBuild(context.GetProjectPath(ProjectType.Framework, "ConsoleCheck"), context.DotNetBuildSettings);
 }
