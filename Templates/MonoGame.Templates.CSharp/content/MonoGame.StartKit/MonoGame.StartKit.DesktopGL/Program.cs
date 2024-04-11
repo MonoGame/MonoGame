80d1cc86ba53ddc19c37804aeb689517d0b1bb69
CartBlanche <savagesoftware@gmail.com>
2024-04-11 13:59:27 +0100

    Initial StartKit commit.

---
+++ Templates/MonoGame.Templates.CSharp/content/MonoGame.StartKit/MonoGame.StartKit.DesktopGL/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using ___safegamename___.Core;
+
+using var game = new ___safegamename___Game();
+game.Run();
---
+++ Templates/MonoGame.Templates.CSharp/content/MonoGame.StartKit/MonoGame.StartKit.iOS/Program.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------------
+// Program.cs
+//
+// MonoGame Foundation Game Platform
+// Copyright (C) MonoGame Foundation. All rights reserved.
+//-----------------------------------------------------------------------------
+
+#region Using Statements
+using System;
+
+using Foundation;
+using UIKit;
+
+using ___safegamename___.Core;
+#endregion
+
+namespace ___safegamename___.iOS
+{
+    [Register("AppDelegate")]
+    internal class Program : UIApplicationDelegate
+    {
+        private static ___safegamename___Game game;
+
+        internal static void RunGame()
+        {
+            game = new ___safegamename___Game();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
