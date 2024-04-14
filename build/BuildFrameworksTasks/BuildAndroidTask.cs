d493dc72ff5ea63e25985e48a9c16f756fe4d14f
Dean Ellis <dellis1972@googlemail.com>
2024-04-14 21:26:33 +0100

    accpet licences

--- build/BuildFrameworksTasks/BuildAndroidTask.cs
+++ build/BuildFrameworksTasks/BuildAndroidTask.cs
@@ -9,6 +9,7 @@ public sealed class BuildAndroidTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context) {
         var arguments = new DotNetMSBuildSettings();
         arguments.WithProperty("AndroidSdkDirectory", System.Environment.GetEnvironmentVariable ("ANDROID_HOME"));
+        arguments.WithProperty("AcceptAndroidSDKLicenses", "true");
         arguments.WithTarget("InstallAndroidDependencies");
         var installSettings = new DotNetBuildSettings
         {
