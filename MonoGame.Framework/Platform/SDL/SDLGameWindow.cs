662afdfce93e30f079f77b4dcfcc6f928688da8f
samercer2002 <70397624+samercer2002@users.noreply.github.com>
2024-04-18 18:36:54 -0400

    Fixed ScissorRectangle Bug Noted in Issue #8084
    The fix to this problem was testing to see if the ScissorRectangle was enabled and resizing it whenever the viewport was resized. This was not done by default for DesktopGL as far as I could tell.

--- MonoGame.Framework/Platform/SDL/SDLGameWindow.cs
+++ MonoGame.Framework/Platform/SDL/SDLGameWindow.cs
@@ -299,9 +299,16 @@ namespace Microsoft.Xna.Framework
                 _game.GraphicsDevice.PresentationParameters.BackBufferHeight == height) {
                 return;
             }
+
+            Viewport prevViewport = _game.GraphicsDevice.Viewport;
+            Rectangle prevScissorRect = _game.GraphicsDevice.ScissorRectangle;
+
             _game.GraphicsDevice.PresentationParameters.BackBufferWidth = width;
             _game.GraphicsDevice.PresentationParameters.BackBufferHeight = height;
             _game.GraphicsDevice.Viewport = new Viewport(0, 0, width, height);
+
+            if (_game.GraphicsDevice.RasterizerState.ScissorTestEnable && prevScissorRect == prevViewport.Bounds)
+                _game.GraphicsDevice.ScissorRectangle = new Rectangle(0, 0, width, height);
 
             Sdl.Window.GetSize(Handle, out _width, out _height);
 
