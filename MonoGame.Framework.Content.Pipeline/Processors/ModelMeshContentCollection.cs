baf78fea4536e452e5f5ef7bb97b06a35f4c2304
Christopher Whitley <aristurtledev@gmail.com>
2024-06-03 16:09:12 -0400

    Update XML documentation for `ModelMeshContentCollection`

--- MonoGame.Framework.Content.Pipeline/Processors/ModelMeshContentCollection.cs
+++ MonoGame.Framework.Content.Pipeline/Processors/ModelMeshContentCollection.cs
@@ -7,6 +7,9 @@ using System.Collections.ObjectModel;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Processors
 {
+    /// <summary>
+    /// A collection of <see cref="ModelMeshPartContent"/> objects.
+    /// </summary>
     public sealed class ModelMeshContentCollection : ReadOnlyCollection<ModelMeshContent>
     {
         internal ModelMeshContentCollection(IList<ModelMeshContent> list)
