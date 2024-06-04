a21edee093feb0786f0b28c07427197074043174
theLaffa <robert@laffey.au>
2024-06-04 19:55:20 +1000

    Update XML documentation for `Vector2`

--- MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector2.cs
+++ MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector2.cs
@@ -5,9 +5,15 @@ namespace System.Numerics
     /// </summary>
     public struct Vector2
     {
+        /// <summary>The X component of the vector.</summary>
         public Single X;
+
+        /// <summary>The Y component of the vector.</summary>
         public Single Y;
- 
+
+        /// <summary>Creates a vector with the specified values.</summary>
+        /// <param name="x">The value assigned to the <see cref="X"/> field.</param>
+        /// <param name="y">The value assigned to the <see cref="Y"/> field.</param>
         public Vector2(Single x, Single y)
         {
             X = x;
