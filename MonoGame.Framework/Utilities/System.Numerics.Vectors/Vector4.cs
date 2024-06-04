68625ffee4c6394e401b26a312adf674557e1c6f
theLaffa <robert@laffey.au>
2024-06-04 20:59:32 +1000

    Update XML documentation for Vector4

--- MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector4.cs
+++ MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector4.cs
@@ -5,11 +5,20 @@ namespace System.Numerics
     /// </summary>
     public struct Vector4
     {
+        /// <summary>The X component of the vector.</summary>
         public Single X;
+        /// <summary>The Y component of the vector.</summary>
         public Single Y;
+        /// <summary>The Z component of the vector.</summary>
         public Single Z;
+        /// <summary>The W component of the vector.</summary>
         public Single W;
- 
+
+        /// <summary>Creates a vector with the specified values.</summary>
+        /// <param name="x">The value assigned to the <see cref="X"/> field.</param>
+        /// <param name="y">The value assigned to the <see cref="Y"/> field.</param>
+        /// <param name="z">The value assigned to the <see cref="Z"/> field.</param>
+        /// <param name="w">The value assigned to the <see cref="W"/> field.</param>
         public Vector4(Single x, Single y, Single z, Single w)
         {
             X = x;
