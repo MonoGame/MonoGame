023d6465a222e8e96064ed6b5250282d2652a4c8
theLaffa <robert@laffey.au>
2024-06-04 20:55:37 +1000

    Update XML documentation for Vector3

--- MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector3.cs
+++ MonoGame.Framework/Utilities/System.Numerics.Vectors/Vector3.cs
@@ -5,10 +5,17 @@ namespace System.Numerics
     /// </summary>
     public struct Vector3
     {
+        /// <summary>The X component of the vector.</summary>
         public Single X;
+        /// <summary>The Y component of the vector.</summary>
         public Single Y;
+        /// <summary>The Z component of the vector.</summary>
         public Single Z;
- 
+
+        /// <summary>Creates a vector with the specified values.</summary>
+        /// <param name="x">The value assigned to the <see cref="X"/> field.</param>
+        /// <param name="y">The value assigned to the <see cref="Y"/> field.</param>
+        /// <param name="z">The value assigned to the <see cref="Z"/> field.</param>
         public Vector3(Single x, Single y, Single z)
         {
             X = x;
