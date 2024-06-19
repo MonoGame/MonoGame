4540c7cf04985d16a03b658004a0bb8983285a9a
theLaffa <robert@laffey.au>
2024-06-20 08:53:24 +1000

    Update XML documentation for `TextureCubeContent.cs`

--- MonoGame.Framework.Content.Pipeline/Graphics/TextureCubeContent.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/TextureCubeContent.cs
@@ -7,13 +7,23 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 {
+    /// <summary>
+    /// Provides a base class for texture cube content.
+    /// </summary>
     public class TextureCubeContent : TextureContent
     {
+        /// <summary>
+        /// Creates a new TextureCubeContent object.
+        /// </summary>
         public TextureCubeContent() :
             base(new MipmapChainCollection(6, true))
         {
         }
 
+        /// <inheritdoc/>
+        /// <remarks>
+        /// Validate is not implemented for this texture content type.
+        /// </remarks>
         public override void Validate(GraphicsProfile? targetProf)
         {
             throw new NotImplementedException();
