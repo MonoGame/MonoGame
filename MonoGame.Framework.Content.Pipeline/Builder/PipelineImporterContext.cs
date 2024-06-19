ffd163ef193a5da45f94be0289e326faf87b9f2b
theLaffa <robert@laffey.au>
2024-06-20 08:28:46 +1000

    Update XML documentation for `PipelineImporterContext.cs`

--- MonoGame.Framework.Content.Pipeline/Builder/PipelineImporterContext.cs
+++ MonoGame.Framework.Content.Pipeline/Builder/PipelineImporterContext.cs
@@ -6,21 +6,32 @@ using Microsoft.Xna.Framework.Content.Pipeline;
 
 namespace MonoGame.Framework.Content.Pipeline.Builder
 {
+    /// <inheritdoc/>
     public class PipelineImporterContext : ContentImporterContext
     {
         private readonly PipelineManager _manager;
 
+        /// <summary>
+        /// Initializes a new instance of PipelineImporterContext.
+        /// </summary>
+        /// <param name="manager">The pipeline manager to associate with this instance.</param>
         public PipelineImporterContext(PipelineManager manager)
         {
             _manager = manager;
         }
 
+        /// <inheritdoc/>
         public override string IntermediateDirectory { get { return _manager.IntermediateDirectory; } }
+
+        /// <inheritdoc/>
         public override string OutputDirectory { get { return _manager.OutputDirectory; } }
+
+        /// <inheritdoc/>
         public override ContentBuildLogger Logger { get { return _manager.Logger; } }
 
+        /// <inheritdoc/>
         public override void AddDependency(string filename)
-        {            
+        {
         }
     }
 }
