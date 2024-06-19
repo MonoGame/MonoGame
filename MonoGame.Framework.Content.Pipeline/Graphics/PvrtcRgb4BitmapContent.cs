2368a77c7c95912f75c4c8f44744e07d76a09681
theLaffa <robert@laffey.au>
2024-06-19 19:57:11 +1000

    Update XML documentation for `PvrtcRgb4BitmapContent.cs`

--- MonoGame.Framework.Content.Pipeline/Graphics/PvrtcRgb4BitmapContent.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/PvrtcRgb4BitmapContent.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 {
+    /// <summary>
+    /// Provides properties and methods for creating and maintaining a compressed bitmap resource.
+    /// </summary>
     public class PvrtcRgb4BitmapContent : PvrtcBitmapContent
     {
         /// <summary>
