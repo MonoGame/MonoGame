a6cfffcee03fabbff5c5132b489dc01e33d9dbd7
CartBlanche <savagesoftware@gmail.com>
2024-09-19 18:12:36 +1000

    Remove DPI setting from app.manifest and make the call in Program.cs

--- Templates/MonoGame.Templates.CSharp/content/MonoGame.2D.StartKit/___SafeGameName___.WindowsDX/Program.cs
+++ Templates/MonoGame.Templates.CSharp/content/MonoGame.2D.StartKit/___SafeGameName___.WindowsDX/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using ___SafeGameName___.Core;
+using System.Windows.Forms;
 
+Application.SetHighDpiMode(HighDpiMode.SystemAware);  // Adjust the mode as needed
 using var game = new ___SafeGameName___Game();
 game.Run();
