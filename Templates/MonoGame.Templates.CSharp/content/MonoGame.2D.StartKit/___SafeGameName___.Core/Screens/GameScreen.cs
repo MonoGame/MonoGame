149a1958a7c543488b89cba736ca570f02799e07
CartBlanche <savagesoftware@gmail.com>
2024-09-22 21:44:23 +1000

    Move ScreenState to it's own file.

--- Templates/MonoGame.Templates.CSharp/content/MonoGame.2D.StartKit/___SafeGameName___.Core/Screens/GameScreen.cs
+++ Templates/MonoGame.Templates.CSharp/content/MonoGame.2D.StartKit/___SafeGameName___.Core/Screens/GameScreen.cs
@@ -19,18 +19,6 @@ using ___SafeGameName___.ScreenManagers;
 namespace ___SafeGameName___.Screens;
 
 /// <summary>
-/// Enum describes the screen transition state.
-/// </summary>
-public enum ScreenState
-{
-    TransitionOn,
-    Active,
-    TransitionOff,
-    Hidden,
-}
-
-
-/// <summary>
 /// A screen is a single layer that has update and draw logic, and which
 /// can be combined with other layers to build up a complex menu system.
 /// For instance the main menu, the options menu, the "are you sure you
---
+++ Templates/MonoGame.Templates.CSharp/content/MonoGame.2D.StartKit/___SafeGameName___.Core/Screens/ScreenState.cs
@@ -0,0 +1,24 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// GameScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+#endregion
+
+namespace ___SafeGameName___.Screens;
+
+/// <summary>
+/// Enum describes the screen transition state.
+/// </summary>
+public enum ScreenState
+{
+    TransitionOn,
+    Active,
+    TransitionOff,
+    Hidden,
+}
