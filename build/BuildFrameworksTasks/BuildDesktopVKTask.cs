b6774a2dba443b1c8254faea591d25cba58d96f5
Tom Spilman <tom@sickheadgames.com>
2024-09-25 08:20:53 -0500

    Fix DesktopVK build task.

--- build/BuildFrameworksTasks/BuildDesktopVKTask.cs
+++ build/BuildFrameworksTasks/BuildDesktopVKTask.cs
@@ -9,26 +9,34 @@ public sealed class BuildDesktopVKTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        // TODO: This should be moved to its own seperate
+        int exit;
+
+        // TODO: This should be moved to its own separate
         // build process so that it doesn't run on all MG builds.
         {
-            var buildDir = "src/monogame/external/sdl2/sdl/build";
+            var buildDir = "native/monogame/external/sdl2/sdl/build";
             context.CreateDirectory(buildDir);
-            context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A x64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
-            context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release /p:Platform=x64" });
+
+            exit = context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A x64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
+            if (exit != 0)
+                throw new Exception($"SDL2 Cmake failed! {exit}");
+
+            exit = context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release /p:Platform=x64" });
+            if (exit != 0)
+                throw new Exception($"SDL2 build failed! {exit}");
         }
 
         // Generate the native projects.
-        var exit = context.StartProcess("premake5", new ProcessSettings { WorkingDirectory = "src/monogame", Arguments = "clean" });
-        if (exit < 0)
-            throw new Exception($"Setting Premake clean failed! {exit}");
-        exit = context.StartProcess("premake5", new ProcessSettings { WorkingDirectory = "src/monogame", Arguments = "--os=windows --verbose vs2022" });
-        if (exit < 0)
-            throw new Exception($"Setting Premake generation failed! {exit}");
+        exit = context.StartProcess("premake5", new ProcessSettings { WorkingDirectory = "native/monogame", Arguments = "clean" });
+        if (exit != 0)
+            throw new Exception($"Premake clean failed! {exit}");
+        exit = context.StartProcess("premake5", new ProcessSettings { WorkingDirectory = "native/monogame", Arguments = "--os=windows --verbose vs2022" });
+        if (exit != 0)
+            throw new Exception($"Premake generation failed! {exit}");
 
         // Build it.
-        exit = context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = "src/monogame", Arguments = "monogame.sln /p:Configuration=Release /p:Platform=x64" });
-        if (exit < 0)
-            throw new Exception($"Setting build failed! {exit}");
+        exit = context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = "native/monogame", Arguments = "monogame.sln /p:Configuration=Release /p:Platform=x64" });
+        if (exit != 0)
+            throw new Exception($"Native build failed! {exit}");
     }
 }
