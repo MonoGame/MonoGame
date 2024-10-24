9e32a8de9a64151b4ffc9e236c385e0907929bab
CartBlanche <savagesoftware@gmail.com>
2024-10-24 16:08:44 +0100

    Removed unused AssemblyInfo file.

--- Templates/MonoGame.Templates.VSExtension/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
