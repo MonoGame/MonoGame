bd566f530b7c3960f578400a1acbf4d06f95527d
CartBlanche <savagesoftware@gmail.com>
2024-10-24 15:14:42 +0100

    Only upload the vsix, if we're on Windows.

--- build/DeployTasks/UploadArtifactsTask.cs
+++ build/DeployTasks/UploadArtifactsTask.cs
@@ -19,7 +19,9 @@ public sealed class UploadArtifactsTask : AsyncFrostingTask<BuildContext>
         await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "Tests", "Tools", "Release")), $"tests-tools-{os}");
         await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "Tests", "DesktopGL", "Release")), $"tests-desktopgl-{os}");
         if (context.IsRunningOnWindows())
+        {
             await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "Tests", "WindowsDX", "Release")), $"tests-windowsdx-{os}");
-        await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "MonoGame.Templates.VSExtension", "net472")), $"MonoGame.Templates.VSExtension.vsix");
+            await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "MonoGame.Templates.VSExtension", "net472")), $"MonoGame.Templates.VSExtension.vsix");
+        }
     }
 }
