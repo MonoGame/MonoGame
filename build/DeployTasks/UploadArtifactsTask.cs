ae8e8b9288a130c74ce5a10026cafb11ed5a266e
CartBlanche <savagesoftware@gmail.com>
2024-10-29 13:01:20 +0000

    Let's specify the path to the vsix itself.

--- build/DeployTasks/UploadArtifactsTask.cs
+++ build/DeployTasks/UploadArtifactsTask.cs
@@ -21,7 +21,10 @@ public sealed class UploadArtifactsTask : AsyncFrostingTask<BuildContext>
         if (context.IsRunningOnWindows())
         {
             await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "Tests", "WindowsDX", "Release")), $"tests-windowsdx-{os}");
-            await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath(System.IO.Path.Combine(context.BuildOutput, "MonoGame.Templates.VSExtension", "net472")), "MonoGame.Templates.VSExtension.vsix");
+
+            // Assuming that the .vsix file has already been created and is located at this exact path.
+            var vsixFilePath = System.IO.Path.Combine(context.BuildOutput, "MonoGame.Templates.VSExtension", "net472", "MonoGame.Templates.VSExtension.vsix");
+            await context.GitHubActions().Commands.UploadArtifact(new FilePath(vsixFilePath), "MonoGame.Templates.VSExtension.vsix");
         }
     }
 }
