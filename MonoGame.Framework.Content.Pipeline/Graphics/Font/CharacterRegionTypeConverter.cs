617174d6403dd38a818420c035c6894c5bf15a7a
CartBlanche <savagesoftware@gmail.com>
2024-11-01 17:19:48 +0000

    Added more descriptive Exceptions and more checks to make sure we get the right character ranges.

--- MonoGame.Framework.Content.Pipeline/Graphics/Font/CharacterRegionTypeConverter.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/Font/CharacterRegionTypeConverter.cs
@@ -4,68 +4,78 @@ using System.Globalization;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 {
-	public class CharacterRegionTypeConverter : TypeConverter
-	{
-		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
-		{
-			return sourceType == typeof(string);
-		}
+    public class CharacterRegionTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string);
+        }
 
 
-		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-		{
-			// Input must be a string.
-			string source = value as string;
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            // Input must be a string.
+            string source = value as string;
 
-			if (string.IsNullOrEmpty(source))
-			{
-				throw new ArgumentException();
-			}
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentException("Input string cannot be null or empty.");
+            }
 
-			// Supported input formats:
-			//  A
-			//  A-Z
-			//  32-127
-			//  0x20-0x7F
+            // Supported input formats:
+            //  A
+            //  A-Z
+            //  32-127
+            //  0x20-0x7F
 
-			var splitStr = source.Split('-');
-			var split = new char[splitStr.Length];
-			for (int i = 0; i < splitStr.Length; i++)
-			{
-				split[i] = ConvertCharacter(splitStr[i]);
-			}
+            var splitStr = source.Split('-');
+            var split = new char[splitStr.Length];
+            for (int i = 0; i < splitStr.Length; i++)
+            {
+                split[i] = ConvertCharacter(splitStr[i]);
+            }
 
-			switch (split.Length)
-			{
-				case 1:
-				// Only a single character (eg. "a").
-				return new CharacterRegion(split[0], split[0]);
+            switch (split.Length)
+            {
+                case 1:
+                    // Only a single character (eg. "a").
+                    return new CharacterRegion(split[0], split[0]);
 
-				case 2:
-				// Range of characters (eg. "a-z").
-				return new CharacterRegion(split[0], split[1]);
+                case 2:
+                    // Range of characters (eg. "a-z").
+                    return new CharacterRegion(split[0], split[1]);
 
-				default:
-				throw new ArgumentException();
-			}
-		}
-
-
-		static char ConvertCharacter(string value)
-		{
-			if (value.Length == 1)
-			{
-				// Single character directly specifies a codepoint.
-				return value[0];
-			}
-			else
-			{
-				// Otherwise it must be an integer (eg. "32" or "0x20").
-				return (char)(int)intConverter.ConvertFromInvariantString(value);
-			}
-		}
+                default:
+                    throw new ArgumentException("Input format is not supported.");
+            }
+        }
 
 
-		static TypeConverter intConverter = TypeDescriptor.GetConverter(typeof(int));
-	}
+        static char ConvertCharacter(string value)
+        {
+            if (value.Length == 1)
+            {
+                // Single character directly specifies a codepoint.
+                return value[0];
+            }
+            else
+            {
+                if (int.TryParse(value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out int hexResult))
+                {
+                    return (char)hexResult;
+                }
+                else if (int.TryParse(value, out int intResult))
+                {
+                    return (char)intResult;
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid character or codepoint: {value}");
+                    // TODO Do we still need this - return (char)(int)intConverter.ConvertFromInvariantString(value);
+                }
+            }
+        }
+
+        static TypeConverter intConverter = TypeDescriptor.GetConverter(typeof(int));
+    }
 }
