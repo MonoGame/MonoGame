001504f36665decaf3f91b9b37b3412c1fcd726b
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-11-11 22:15:34 -0500

    Added test to ensure rooted path exception thrown

---
+++ Tests/Framework/Content/ContentManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using NUnit.Framework;
+
+namespace MonoGame.Tests.Content
+{
+    public class ContentManagerTest
+    {
+        [TestCase("C:\\image.png")]
+        [TestCase("\\image.png")]
+        [TestCase("//image.png")]
+        public void ThrowExceptionIfAssetNameIsRootedPath(string assetName)
+        {
+            GameServiceContainer services = new GameServiceContainer();
+            ContentManager content = new ContentManager(services, "Content");
+            var exception = Assert.Throws<ContentLoadException>(() => content.Load<Texture2D>(assetName));
+            StringAssert.Contains("rooted (absolute)", exception.Message);
+        }
+    }
+}
