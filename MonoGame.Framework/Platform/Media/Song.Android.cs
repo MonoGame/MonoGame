MonoGame/MonoGame

commit 5e918e91ba6e6db1cfdf0aa0eeeda882ebbc6945

Native AudioDecoder and VideoDecoder (#8510)
This is a follow-up to #8309 and part of the work on #8194.

This implements two new native APIs `AudioDecoder` and `VideoDecoder`.

These native APIs live under the `Video` and `Song` classes.

The design aims to let the native code handle the work of detecting the
media format, selecting the correct decoder, process the media data, and
return frames to C#.

With `AudioDecoder` this means C# keeps a thread and pumps signed 16bit
PCM data to a native `Voice`.

With `VideoDecoder` a C# thread gathers `Texture`s and calls an
`AudioDecoder` to process audio.

There is no public implementation of audio or video decoders yet. The
only implementation currently is for a native audio format of Nintendo
Switch. The video decoder is totally experimental and untested, but i
think it will generally work.

I expect it be a good project for someone or should be made into
bounties for different audio/video codecs.

As always feedback is welcome!

MonoGame.Framework/Platform/Media/Song.Android.cs
@@ -15,8 +15,6 @@ namespace Microsoft.Xna.Framework.Media
         private Album album;
         private Artist artist;
         private Genre genre;
-        private string name;
-        private TimeSpan duration;
         private TimeSpan position;
         private Android.Net.Uri assetUri;
 
@@ -36,9 +34,11 @@ namespace Microsoft.Xna.Framework.Media
             this.album = album;
             this.artist = artist;
             this.genre = genre;
-            this.name = name;
-            this.duration = duration;
             this.assetUri = assetUri;
+            _duration = duration;
+
+            if (this.assetUri != null)
+                _name = name;
         }
 
         private void PlatformInitialize(string fileName)
@@ -168,11 +168,6 @@ namespace Microsoft.Xna.Framework.Media
             return this.genre;
         }
 
-        private TimeSpan PlatformGetDuration()
-        {
-            return this.assetUri != null ? this.duration : _duration;
-        }
-
         private bool PlatformIsProtected()
         {
             return false;
@@ -181,11 +176,6 @@ namespace Microsoft.Xna.Framework.Media
         private bool PlatformIsRated()
         {
             return false;
-        }
-
-        private string PlatformGetName()
-        {
-            return this.assetUri != null ? this.name : Path.GetFileNameWithoutExtension(_name);
         }
 
         private int PlatformGetPlayCount()

MonoGame.Framework/Media/Song.cs
@@ -13,7 +13,8 @@ namespace Microsoft.Xna.Framework.Media
     public sealed partial class Song : IEquatable<Song>, IDisposable
     {
         private string _name;
-		private int _playCount = 0;
+        private string _filePath;
+        private int _playCount = 0;
         private TimeSpan _duration = TimeSpan.Zero;
         bool disposed;
         /// <summary>
@@ -61,8 +62,9 @@ namespace Microsoft.Xna.Framework.Media
         }
 
 		internal Song(string fileName)
-		{			
-			_name = fileName;
+		{
+            _filePath = fileName;
+            _name = Path.GetFileNameWithoutExtension(fileName);
 
             PlatformInitialize(fileName);
         }
@@ -75,7 +77,7 @@ namespace Microsoft.Xna.Framework.Media
 
         internal string FilePath
 		{
-			get { return _name; }
+			get { return _filePath; }
 		}
 
         /// <summary>
@@ -171,7 +173,7 @@ namespace Microsoft.Xna.Framework.Media
         /// </summary>
         public TimeSpan Duration
         {
-            get { return PlatformGetDuration(); }
+            get { return _duration; }
         }
 
         /// <summary>
@@ -195,7 +197,7 @@ namespace Microsoft.Xna.Framework.Media
         /// </summary>
         public string Name
         {
-            get { return PlatformGetName(); }
+            get { return _name; }
         }
 
         /// <summary>

MonoGame.Framework/Platform/Media/Song.Default.cs
@@ -141,11 +141,6 @@ namespace Microsoft.Xna.Framework.Media
             return null;
         }
 
-        private TimeSpan PlatformGetDuration()
-        {
-            return _duration;
-        }
-
         private bool PlatformIsProtected()
         {
             return false;

MonoGame.Framework/Platform/Media/Song.NVorbis.cs
@@ -126,11 +126,6 @@ namespace Microsoft.Xna.Framework.Media
             return null;
         }
 
-        private TimeSpan PlatformGetDuration()
-        {
-            return _duration;
-        }
-
         private bool PlatformIsProtected()
         {
             return false;
@@ -139,11 +134,6 @@ namespace Microsoft.Xna.Framework.Media
         private bool PlatformIsRated()
         {
             return false;
-        }
-
-        private string PlatformGetName()
-        {
-            return Path.GetFileNameWithoutExtension(_name);
         }
 
         private int PlatformGetPlayCount()

MonoGame.Framework/Platform/Media/Song.WMS.cs
@@ -105,11 +105,6 @@ namespace Microsoft.Xna.Framework.Media
             return null;
         }
 
-        private TimeSpan PlatformGetDuration()
-        {
-            return _duration;
-        }
-
         private bool PlatformIsProtected()
         {
             return false;
@@ -118,11 +113,6 @@ namespace Microsoft.Xna.Framework.Media
         private bool PlatformIsRated()
         {
             return false;
-        }
-
-        private string PlatformGetName()
-        {
-            return Path.GetFileNameWithoutExtension(_name);
         }
 
         private int PlatformGetPlayCount()

MonoGame.Framework/Platform/Media/Song.iOS.cs
@@ -16,8 +16,6 @@ namespace Microsoft.Xna.Framework.Media
         private Album album;
         private Artist artist;
         private Genre genre;
-        private string title;
-        private TimeSpan duration;
         #if !TVOS
         private MPMediaItem mediaItem;
         #endif
@@ -40,12 +38,13 @@ namespace Microsoft.Xna.Framework.Media
             this.album = album;
             this.artist = artist;
             this.genre = genre;
-            this.title = title;
-            this.duration = duration;
             #if !TVOS
             this.mediaItem = mediaItem;
             #endif
             this.assetUrl = assetUrl;
+            _duration = duration;
+            if (!string.IsNullOrEmpty(title))
+                _name = title;
         }
 
         private void PlatformInitialize(string fileName)
@@ -193,15 +192,6 @@ namespace Microsoft.Xna.Framework.Media
             return this.genre;
         }
 
-        private TimeSpan PlatformGetDuration()
-        {
-            #if !TVOS
-            if (this.mediaItem != null)
-                return this.duration;
-            #endif
-            return _duration;
-        }
-
         private bool PlatformIsProtected()
         {
             return false;
@@ -210,11 +200,6 @@ namespace Microsoft.Xna.Framework.Media
         private bool PlatformIsRated()
         {
             return false;
-        }
-
-        private string PlatformGetName()
-        {
-            return this.title ?? Path.GetFileNameWithoutExtension(_name);
         }
 
         private int PlatformGetPlayCount()

MonoGame.Framework/Platform/Native/Audio.Interop.cs
@@ -140,7 +140,7 @@ internal static unsafe partial class MGA
     #region Voice
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_Create", StringMarshalling = StringMarshalling.Utf8)]
-    public static partial MGA_Voice* Voice_Create(MGA_System* system);
+    public static partial MGA_Voice* Voice_Create(MGA_System* system, int sampleRate = 0, int channels = 0);
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_Destroy", StringMarshalling = StringMarshalling.Utf8)]
     public static partial void Voice_Destroy(MGA_Voice* voice);
@@ -152,7 +152,7 @@ internal static unsafe partial class MGA
     public static partial void Voice_SetBuffer(MGA_Voice* voice, MGA_Buffer* buffer);
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_AppendBuffer", StringMarshalling = StringMarshalling.Utf8)]
-    public static partial void Voice_AppendBuffer(MGA_Voice* voice, byte[] buffer, int offset, int count, [MarshalAs(UnmanagedType.U1)] bool clear);
+    public static partial void Voice_AppendBuffer(MGA_Voice* voice, byte* buffer, uint size);
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_Play", StringMarshalling = StringMarshalling.Utf8)]
     public static partial void Voice_Play(MGA_Voice* voice, [MarshalAs(UnmanagedType.U1)] bool looped);
@@ -168,6 +168,9 @@ internal static unsafe partial class MGA
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_GetState", StringMarshalling = StringMarshalling.Utf8)]
     public static partial SoundState Voice_GetState(MGA_Voice* voice);
+
+    [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_GetPosition", StringMarshalling = StringMarshalling.Utf8)]
+    public static partial ulong Voice_GetPosition(MGA_Voice* voice);
 
     [LibraryImport(MGP.MonoGameNativeDLL, EntryPoint = "MGA_Voice_SetPan", StringMarshalling = StringMarshalling.Utf8)]
     public static partial void Voice_SetPan(MGA_Voice* voice, float pan);

MonoGame.Framework/Platform/Native/DynamicSoundEffectInstance.Native.cs
@@ -11,7 +11,7 @@ public sealed partial class DynamicSoundEffectInstance : SoundEffectInstance
 {
     private unsafe void PlatformCreate()
     {
-        Voice = MGA.Voice_Create(SoundEffect.System);
+        Voice = MGA.Voice_Create(SoundEffect.System, _sampleRate, (int)_channels);
     }
 
     private unsafe int PlatformGetPendingBufferCount()
@@ -49,7 +49,10 @@ public sealed partial class DynamicSoundEffectInstance : SoundEffectInstance
     private unsafe void PlatformSubmitBuffer(byte[] buffer, int offset, int count)
     {
         if (Voice != null)
-            MGA.Voice_AppendBuffer(Voice, buffer, offset, count, false);
+        {
+            fixed (byte* ptr = buffer)
+                MGA.Voice_AppendBuffer(Voice, ptr + offset, (uint)count);
+        }
     }
 
     private unsafe void PlatformDispose(bool disposing)

commit f405d2f03ea8d9a49976830d5983b7c17725ac16

Fix Android MediaPlayer Song implementation.
Commit 5e918e91 introduced a bug on Android where
song's would not place. This was because it was using
the `name` of the song rather than the `filename`.

Fix up the Android implementation so that it uses the
right field when accessing the audio.

MonoGame.Framework/Platform/Media/Song.Android.cs
@@ -74,27 +74,27 @@ namespace Microsoft.Xna.Framework.Media
         {
             // Prepare the player
             _androidPlayer.Reset();
+            
 
             if (assetUri != null)
             {
                 // Check if we have a direct asset URI.
                 _androidPlayer.SetDataSource(MediaLibrary.Context, this.assetUri);
             }
-            else if (_name.StartsWith("file://"))
+            else if (_filePath.StartsWith("file://"))
             {
                 // Otherwise, check if this is a file URI.
-                _androidPlayer.SetDataSource(_name);
+                _androidPlayer.SetDataSource(_filePath);
             }
             else
             {
                 // Otherwise, assume it's a file path. (This might throw if the file doesn't exist)
-                var afd = Game.Activity?.Assets?.OpenFd(_name);
+                var afd = Game.Activity?.Assets?.OpenFd(_filePath);
                 if (afd != null)
                 {
                 	_androidPlayer.SetDataSource(afd.FileDescriptor, afd.StartOffset, afd.Length);
                 }
             }
-
 
             _androidPlayer.Prepare();
             _androidPlayer.Looping = MediaPlayer.IsRepeating;