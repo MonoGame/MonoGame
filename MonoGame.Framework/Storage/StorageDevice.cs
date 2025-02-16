9d32ccc4b836f59daf0c2ca8f2e3cd264913ade4
Dominique Louis <savagesoftware@gmail.com>
2025-02-16 18:01:42 +0000

    Introduce partial class for platform specific implmentation of StorageDevice properties.

--- MonoGame.Framework/Storage/StorageDevice.cs
+++ MonoGame.Framework/Storage/StorageDevice.cs
@@ -19,55 +19,6 @@ namespace Microsoft.Xna.Framework.Storage
             _directoryCount = directoryCount;
         }
 
-        public long FreeSpace
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).AvailableFreeSpace;
-                }
-                catch (Exception)
-                {
-                    /* TODO StorageDeviceHelper.Path = StorageRoot;
-                    return StorageDeviceHelper.FreeSpace;*/
-                    return -1;
-                }
-            }
-        }
-
-        public bool IsConnected
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).IsReady;
-                }
-                catch (Exception)
-                {
-                    return true;
-                }
-            }
-        }
-
-        public long TotalSpace
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).TotalSize;
-                }
-                catch (Exception)
-                {
-                    /* TODO StorageDeviceHelper.Path = StorageRoot;
-                    return StorageDeviceHelper.TotalSpace; */
-                    return -1;
-                }
-            }
-        }
-
         private string GetDevicePath => _storageContainer?._storagePath ?? StorageRoot;
 
         public Task<StorageContainer> OpenContainerAsync(string displayName, CancellationToken cancellationToken = default)
@@ -79,36 +30,6 @@ namespace Microsoft.Xna.Framework.Storage
         {
             _storageContainer = new StorageContainer(this, displayName, _player);
             return _storageContainer;
-        }
-
-        internal static string StorageRoot
-        {
-            get
-            {
-#if LINUX
-                string osConfigDir = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
-                if (string.IsNullOrEmpty(osConfigDir))
-                {
-                    osConfigDir = Environment.GetEnvironmentVariable("HOME");
-                    if (string.IsNullOrEmpty(osConfigDir))
-                    {
-                        return "."; // Oh well.
-                    }
-                    osConfigDir += "/.local/share";
-                }
-                return osConfigDir;
-#elif MAC
-                string osConfigDir = Environment.GetEnvironmentVariable("HOME");
-                if (string.IsNullOrEmpty(osConfigDir))
-                {
-                    return "."; // Oh well.
-                }
-                osConfigDir += "/Library/Application Support";
-                return osConfigDir;
-#else
-                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-#endif
-            }
         }
     }
 }
