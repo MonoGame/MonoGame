60ea4a5cf2e9b7f13d943f9c1981933369ac9ab6
Dominique Louis <savagesoftware@gmail.com>
2025-02-17 13:32:16 +0000

    Added Storage.Interop.cs as per Tom's suggestions, but need to flesh it out.

---
+++ MonoGame.Framework/Platform/Native/Storage.Interop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Xna.Framework.Storage
+{
+    public sealed partial class StorageDevice
+    {
+       
+    }
+}
