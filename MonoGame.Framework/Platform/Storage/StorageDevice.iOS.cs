0c33459b93a918537e30c9787e20923dd951bb05
Dominique Louis <savagesoftware@gmail.com>
2025-02-17 11:28:21 +0000

    Refactor to centralise the AvailableSpace, IsConnected & TotalSpace Properties and cache DeviceInfo object creation.

--- MonoGame.Framework/Platform/Storage/StorageDevice.iOS.cs
+++ MonoGame.Framework/Platform/Storage/StorageDevice.iOS.cs
@@ -5,60 +5,11 @@ namespace Microsoft.Xna.Framework.Storage
 {
     partial class StorageDevice
     {
-        public long FreeSpace
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).AvailableFreeSpace;
-                }
-                catch (Exception)
-                {
-                    /* TODO StorageDeviceHelper.Path = StorageRoot;
-                    return StorageDeviceHelper.FreeSpace;*/
-                    return -1;
-                }
-            }
-        }
-
-        public bool IsConnected
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).IsReady;
-                }
-                catch (Exception)
-                {
-                    return true;
-                }
-            }
-        }
-
         internal static string StorageRoot
         {
             get
             {
                 return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            }
-        }
-
-        public long TotalSpace
-        {
-            get
-            {
-                try
-                {
-                    return new DriveInfo(GetDevicePath).TotalSize;
-                }
-                catch (Exception)
-                {
-                    /* TODO StorageDeviceHelper.Path = StorageRoot;
-                    return StorageDeviceHelper.TotalSpace; */
-                    return -1;
-                }
             }
         }
     }
