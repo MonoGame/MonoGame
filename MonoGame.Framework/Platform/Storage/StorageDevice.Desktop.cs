70a6fa8774c666299820efacb4526c62163b1ca9
Dominique Louis <savagesoftware@gmail.com>
2025-03-13 11:13:56 +0000

    implement missed *DeleteContainer

--- MonoGame.Framework/Platform/Storage/StorageDevice.Desktop.cs
+++ MonoGame.Framework/Platform/Storage/StorageDevice.Desktop.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Xna.Framework.Storage
 
         private void PlatformDeleteContainer(string containerName)
         {
-            throw new NotImplementedException();
+            _storageContainer.DeleteDirectory(containerName);
         }
 
         private long PlatformFreeSpace()
