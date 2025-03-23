a8d342e73614ca1b12212e8e3c8c1965a5df332f
Future <1609605489@qq.com>
2025-03-23 08:18:23 +0800

    Send editing event with empty string when composition end

--- MonoGame.Framework/Platform/Windows/WinFormsGameForm.cs
+++ MonoGame.Framework/Platform/Windows/WinFormsGameForm.cs
@@ -489,6 +489,7 @@ namespace Microsoft.Xna.Framework.Windows
                 {
                     return;
                 }
+
                 if ((((uint)m.LParam) & GCS_COMPSTR) != 0)
                 {
                     IntPtr himc = ImmGetContext(Handle);
@@ -500,6 +501,11 @@ namespace Microsoft.Xna.Framework.Windows
                         _window.OnTextEditing(new TextInputEventArgs(new string(lpBuf)));
                     }
                     ImmReleaseContext(Handle, himc);
+                }
+
+                if ((((uint)m.LParam) & GCS_RESULTSTR) != 0)
+                {
+                    _window.OnTextEditing(new TextInputEventArgs(string.Empty));
                 }
             }
         }
