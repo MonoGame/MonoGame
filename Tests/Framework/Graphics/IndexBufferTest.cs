86fb922e23bce4712b9496a0a1c40f5eae5b588a
tomspilman <tom@sickheadgames.com>
2025-04-14 10:49:05 -0500

    Added 32bit index buffer tests.

--- Tests/Framework/Graphics/IndexBufferTest.cs
+++ Tests/Framework/Graphics/IndexBufferTest.cs
@@ -15,70 +15,141 @@ namespace MonoGame.Tests.Graphics
         [Test]
         [RunOnUI]
         public void ShouldSetAndGetData()
-        {   
-            var savedData = new short[] { 1, 2, 3, 4 };
-            var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
-            indexBuffer.SetData(savedData);
+        {
+            // Short
+            {
+                var savedData = new short[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
 
-            var readData = new short[4];
-            indexBuffer.GetData(readData, 0, 4);
-            Assert.AreEqual(savedData, readData);
-            
-            indexBuffer.Dispose();
+                var readData = new short[4];
+                indexBuffer.GetData(readData, 0, 4);
+                Assert.AreEqual(savedData, readData);
+
+                indexBuffer.Dispose();
+            }
+
+            // Int
+            {
+                var savedData = new int[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.ThirtyTwoBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
+
+                var readData = new int[4];
+                indexBuffer.GetData(readData, 0, 4);
+                Assert.AreEqual(savedData, readData);
+
+                indexBuffer.Dispose();
+            }
         }
 
         [Test]
         [RunOnUI]
         public void ShouldSetAndGetData_elementCount()
         {
-            var savedData = new short[] { 1, 2, 3, 4 };
-            var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
-            indexBuffer.SetData(savedData);
-            
-            var readData = new short[4];
-            indexBuffer.GetData(readData, 0, 2);
-            Assert.AreEqual(1, readData[0]);
-            Assert.AreEqual(2, readData[1]);
-            Assert.AreEqual(0, readData[2]);
-            Assert.AreEqual(0, readData[3]);
+            // Short
+            {
+                var savedData = new short[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
 
-            indexBuffer.Dispose();
+                var readData = new short[4];
+                indexBuffer.GetData(readData, 0, 2);
+                Assert.AreEqual(1, readData[0]);
+                Assert.AreEqual(2, readData[1]);
+                Assert.AreEqual(0, readData[2]);
+                Assert.AreEqual(0, readData[3]);
+
+                indexBuffer.Dispose();
+            }
+
+            // Int
+            {
+                var savedData = new int[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.ThirtyTwoBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
+
+                var readData = new int[4];
+                indexBuffer.GetData(readData, 0, 2);
+                Assert.AreEqual(1, readData[0]);
+                Assert.AreEqual(2, readData[1]);
+                Assert.AreEqual(0, readData[2]);
+                Assert.AreEqual(0, readData[3]);
+
+                indexBuffer.Dispose();
+            }
         }
 
         [Test]
         [RunOnUI]
         public void ShouldSetAndGetData_startIndex()
         {
-            var savedData = new short[] { 1, 2, 3, 4 };
-            var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
-            indexBuffer.SetData(savedData);
+            // Short
+            {
+                var savedData = new short[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
 
-            var readData = new short[4];
-            indexBuffer.GetData(readData, 2, 2);
-            Assert.AreEqual(0, readData[0]);
-            Assert.AreEqual(0, readData[1]);
-            Assert.AreEqual(1, readData[2]);
-            Assert.AreEqual(2, readData[3]);
+                var readData = new short[4];
+                indexBuffer.GetData(readData, 2, 2);
+                Assert.AreEqual(0, readData[0]);
+                Assert.AreEqual(0, readData[1]);
+                Assert.AreEqual(1, readData[2]);
+                Assert.AreEqual(2, readData[3]);
 
-            indexBuffer.Dispose();
+                indexBuffer.Dispose();
+            }
+
+            // Int
+            {
+                var savedData = new int[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.ThirtyTwoBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
+
+                var readData = new int[4];
+                indexBuffer.GetData(readData, 2, 2);
+                Assert.AreEqual(0, readData[0]);
+                Assert.AreEqual(0, readData[1]);
+                Assert.AreEqual(1, readData[2]);
+                Assert.AreEqual(2, readData[3]);
+
+                indexBuffer.Dispose();
+            }
         }
 
         [Test]
         [RunOnUI]
         public void ShouldSetAndGetData_offsetInBytes()
         {
-            var savedData = new short[] { 1, 2, 3, 4 };
-            var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
-            indexBuffer.SetData(savedData);
+            // Short
+            {
+                var savedData = new short[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
 
-            var readData = new short[2];
-            indexBuffer.GetData(sizeof(short) * 2, readData, 0, 2);
-            Assert.AreEqual(3, readData[0]);
-            Assert.AreEqual(4, readData[1]);
+                var readData = new short[2];
+                indexBuffer.GetData(sizeof(short) * 2, readData, 0, 2);
+                Assert.AreEqual(3, readData[0]);
+                Assert.AreEqual(4, readData[1]);
 
-            indexBuffer.Dispose();
+                indexBuffer.Dispose();
+            }
+
+            // Int
+            {
+                var savedData = new int[] { 1, 2, 3, 4 };
+                var indexBuffer = new IndexBuffer(gd, IndexElementSize.ThirtyTwoBits, savedData.Length, BufferUsage.None);
+                indexBuffer.SetData(savedData);
+
+                var readData = new int[2];
+                indexBuffer.GetData(sizeof(int) * 2, readData, 0, 2);
+                Assert.AreEqual(3, readData[0]);
+                Assert.AreEqual(4, readData[1]);
+
+                indexBuffer.Dispose();
+            }
         }
-        
+
         [Test]
         [RunOnUI]
         public void NullDeviceShouldThrowArgumentNullException()
