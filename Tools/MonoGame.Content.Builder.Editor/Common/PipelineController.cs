90ec6a28ddf088d73c93085b58eda399cf99c7f9
Harry <harry.cpp@outlook.com>
2025-04-23 11:04:59 -0400

    Revert "[MGCB Editor] Added file watcher for the content project file with project reload prompt" (#8728)
    Reverts MonoGame/MonoGame#7797

--- Tools/MonoGame.Content.Builder.Editor/Common/PipelineController.cs
+++ Tools/MonoGame.Content.Builder.Editor/Common/PipelineController.cs
@@ -25,11 +25,6 @@ namespace MonoGame.Tools.Pipeline
         private Task _buildTask;
         private Process _buildProcess;
 
-        private FileSystemWatcher _projectFileWatcher;
-        private bool _reloadProjectPrompted;
-        private bool _projectFileWatcherIgnoreEvent;
-        private DateTime _lastWriteTime;
-
         private readonly List<ContentItemTemplate> _templateItems;
 
         private static readonly string [] _mgcbSearchPaths = new []       
@@ -146,26 +141,6 @@ namespace MonoGame.Tools.Pipeline
             return new PipelineController(view);
         }
 
-        public void SetupProjectFileWatcher(string projectFilePath)
-        {
-            // Setup a file watcher to watch for changes to the project file outside of the editor
-            var dirName = Path.GetDirectoryName(projectFilePath)!;
-            var fileName = Path.GetFileName(projectFilePath);
-            if (_projectFileWatcher == null)
-            {
-                _projectFileWatcher = new FileSystemWatcher(dirName);
-                _projectFileWatcher.Filter = fileName;
-                _projectFileWatcher.EnableRaisingEvents = true;
-                _projectFileWatcher.Changed += ProjectFileWatcherOnChanged;
-            }
-            else
-            {
-                _projectFileWatcher.Path = dirName;
-                _projectFileWatcher.Filter = fileName;
-            }
-            _projectFileWatcherIgnoreEvent = false;
-        }
-
         public void OnProjectModified()
         {            
             Debug.Assert(ProjectOpen, "OnProjectModified called with no project open?");
@@ -219,8 +194,6 @@ namespace MonoGame.Tools.Pipeline
             _project.OriginalPath = projectFilePath;
             ProjectOpen = true;
             ProjectDirty = true;
-
-            SetupProjectFileWatcher(projectFilePath);
 
             UpdateTree();
 
@@ -330,8 +303,6 @@ namespace MonoGame.Tools.Pipeline
                 return;
             }
 
-            SetupProjectFileWatcher(projectFilePath);
-
             UpdateTree();
             View.UpdateTreeItem(_project);
 
@@ -339,46 +310,6 @@ namespace MonoGame.Tools.Pipeline
                 OnProjectLoaded();
 
             UpdateMenu();
-        }
-
-        private void ProjectFileWatcherOnChanged(object sender, FileSystemEventArgs e)
-        {
-            var previousWriteTime = _lastWriteTime;
-            _lastWriteTime = File.GetLastWriteTime(e.FullPath);
-
-            if (_lastWriteTime - previousWriteTime < TimeSpan.FromSeconds(0.1))
-            {
-                // Probably a duplicated event. Ignore
-                return;
-            }
-
-            if (_projectFileWatcherIgnoreEvent)
-            {
-                // Expected trigger from saving the project file. Ignore
-                _projectFileWatcherIgnoreEvent = false;
-                return;
-            }
-
-            if (!_reloadProjectPrompted)
-            {
-                _reloadProjectPrompted = true;
-                View.Invoke(() => PromptReloadProject(e.FullPath));
-            }
-        }
-
-        private void PromptReloadProject(string fullPath)
-        {
-            if (MainWindow.Instance.ShowReloadProjectDialog() == AskResult.Yes)
-            {
-                ProjectDirty = false;
-                OpenProject(fullPath);
-            }
-            else
-            {
-                ProjectDirty = true;
-                UpdateMenu();
-            }
-            _reloadProjectPrompted = false;
         }
 
         public void ClearRecentList()
@@ -397,13 +328,6 @@ namespace MonoGame.Tools.Pipeline
             // save the project if they need too.
             if (!AskSaveProject())
                 return;
-
-            // Uninitialize the File Watcher
-            if (_projectFileWatcher != null)
-            {
-                _projectFileWatcher.Changed -= ProjectFileWatcherOnChanged;
-                _projectFileWatcher = null;
-            }
 
             ProjectOpen = false;
             ProjectDirty = false;
@@ -458,12 +382,7 @@ namespace MonoGame.Tools.Pipeline
 
                 _project.OriginalPath = newFilePath;
 				View.SetTreeRoot(_project);
-
-                SetupProjectFileWatcher(newFilePath); // New file needs a new file watcher
             }
-
-            // Make sure the file watcher doesn't trigger from our file save
-            _projectFileWatcherIgnoreEvent = true;
 
             // Do the save.
             ProjectDirty = false;
