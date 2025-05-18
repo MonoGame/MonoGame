2d0c230c575cef57098bc6cd118eace4f1ee4cc0
Mitchel Disveld <mitchel.disveld@hotmail.com>
2025-05-18 23:43:52 +0200

    use a more modern implementation

--- MonoGame.Framework/Platform/Utilities/CurrentPlatform.cs
+++ MonoGame.Framework/Platform/Utilities/CurrentPlatform.cs
@@ -20,54 +20,23 @@ namespace MonoGame.Framework.Utilities
         private static bool _init = false;
         private static OS _os;
 
-        [DllImport("libc")]
-        static extern int uname(IntPtr buf);
-
         private static void Init()
         {
             if (_init)
                 return;
 
-            var pid = Environment.OSVersion.Platform;
-
-            switch (pid)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                case PlatformID.Win32NT:
-                case PlatformID.Win32S:
-                case PlatformID.Win32Windows:
-                case PlatformID.WinCE:
-                    _os = OS.Windows;
-                    break;
-                case PlatformID.MacOSX:
-                    _os = OS.MacOSX;
-                    break;
-                case PlatformID.Unix:
-                    _os = OS.MacOSX;
-
-                    var buf = IntPtr.Zero;
-                    
-                    try
-                    {
-                        buf = Marshal.AllocHGlobal(8192);
-
-                        if (uname(buf) == 0 && Marshal.PtrToStringAnsi(buf) == "Linux")
-                            _os = OS.Linux;
-                    }
-                    catch
-                    {
-                    }
-                    finally
-                    {
-                        if (buf != IntPtr.Zero)
-                            Marshal.FreeHGlobal(buf);
-                    }
-
-                    break;
-                default:
-                    _os = OS.Unknown;
-                    break;
+                _os = OS.Windows;
             }
-
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                _os = OS.MacOSX;
+            }
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                _os = OS.Linux;
+            }
             _init = true;
         }
 
