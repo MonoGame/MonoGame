4d83b011f29e5203426015e6f3dbea07cb74a6f9
harry.cpp <harry.cpp@outlook.com>
2025-05-25 10:39:48 +0200

    Build tools tests after building mgcb editor

--- build/BuildToolsTasks/BuildToolTestsTask.cs
+++ build/BuildToolsTasks/BuildToolTestsTask.cs
@@ -3,6 +3,7 @@ namespace BuildScripts;
 
 [TaskName("Build Tool Tests")]
 [IsDependentOn(typeof(BuildContentPipelineTask))]
+[IsDependentOn(typeof(BuildMGCBEditorTask))]
 public sealed class BuildToolTestsTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
