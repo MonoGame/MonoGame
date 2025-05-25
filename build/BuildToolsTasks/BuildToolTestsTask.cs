9531828a916c23e14c72091fc7235ad2e6350e67
harry.cpp <harry.cpp@outlook.com>
2025-05-25 20:57:10 +0200

    new solution to the issue

--- build/BuildToolsTasks/BuildToolTestsTask.cs
+++ build/BuildToolsTasks/BuildToolTestsTask.cs
@@ -3,7 +3,6 @@ namespace BuildScripts;
 
 [TaskName("Build Tool Tests")]
 [IsDependentOn(typeof(BuildContentPipelineTask))]
-[IsDependentOn(typeof(BuildMGCBEditorTask))]
 public sealed class BuildToolTestsTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
