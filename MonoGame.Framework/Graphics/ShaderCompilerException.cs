885e3424935d6d986ad116f11c3948050b9b42c4
Tom Spilman <tom@sickheadgames.com>
2025-06-03 11:46:12 -0500

    Made ShaderCompilerException using implict using to test console check language level.

--- MonoGame.Framework/Graphics/ShaderCompilerException.cs
+++ MonoGame.Framework/Graphics/ShaderCompilerException.cs
@@ -6,75 +6,75 @@ using System;
 using System.Runtime.Serialization;
 
 
-namespace Microsoft.Xna.Framework.Graphics
+namespace Microsoft.Xna.Framework.Graphics;
+
+/// <summary>
+/// This exception occurs when a shader compiled at runtime has errors.
+/// It should provide enough information to help diagnose the bug.
+/// </summary>
+[DataContract]
+public sealed class ShaderCompilerException : Exception
 {
     /// <summary>
-    /// This exception occurs when a shader compiled at runtime has errors.
-    /// It should provide enough information to help diagnose the bug.
+    /// The relative shader source file.
     /// </summary>
-    [DataContract]
-    public sealed class ShaderCompilerException : Exception
+    public string SourceFile { get; private set; }
+
+    /// <summary>
+    /// The shader entrypoint function.
+    /// </summary>
+    public string Entrypoint { get; private set; }
+
+    /// <summary>
+    /// The shader stage.
+    /// </summary>
+    public ShaderStage Stage { get; private set; }
+
+    /// <summary>
+    /// The error logging from the shader compiler.
+    /// </summary>
+    public string Errors { get; private set; }
+
+    /// <summary>
+    /// The source code being compiled.
+    /// </summary>
+    public string SourceCode { get; private set; }
+
+    private static string GetMessage(string sourceFile, string entrypoint, ShaderStage stage)
     {
-        /// <summary>
-        /// The relative shader source file.
-        /// </summary>
-        public string SourceFile { get; private set; }
-
-        /// <summary>
-        /// The shader entrypoint function.
-        /// </summary>
-        public string Entrypoint { get; private set; }
-
-        /// <summary>
-        /// The shader stage.
-        /// </summary>
-        public ShaderStage Stage { get; private set; }
-
-        /// <summary>
-        /// The error logging from the shader compiler.
-        /// </summary>
-        public string Errors { get; private set; }
-
-        /// <summary>
-        /// The source code being compiled.
-        /// </summary>
-        public string SourceCode { get; private set; }
-
-        private static string GetMessage(string sourceFile, string entrypoint, ShaderStage stage)
+        switch (stage)
         {
-            switch (stage)
-            {
-                case ShaderStage.Pixel:
-                    return $"Failed to compile pixel shader {entrypoint}. See {sourceFile}.";
+            case ShaderStage.Pixel:
+                return $"Failed to compile pixel shader {entrypoint}. See {sourceFile}.";
 
-                default:
-                case ShaderStage.Vertex:
-                    return $"Failed to compile vertex shader {entrypoint}. See {sourceFile}.";
-            }
+            default:
+            case ShaderStage.Vertex:
+                return $"Failed to compile vertex shader {entrypoint}. See {sourceFile}.";
         }
+    }
 
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="sourceFile">The relative shader source file.</param>
-        /// <param name="entrypoint">The shader entrypoint function.</param>
-        /// <param name="stage">The shader stage.</param>
-        /// <param name="errors">The error logging from the shader compiler.</param>
-        /// <param name="sourceCode">The source code that was being compiled.</param>
-        public ShaderCompilerException(string sourceFile, string entrypoint, ShaderStage stage, string errors, string sourceCode)
-            : base(GetMessage(sourceFile, entrypoint, stage))
-        {
-            SourceFile = sourceFile;
-            Entrypoint = entrypoint;
-            Stage = stage;
-            Errors = errors;
-            SourceCode = sourceCode;
-        }
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="sourceFile">The relative shader source file.</param>
+    /// <param name="entrypoint">The shader entrypoint function.</param>
+    /// <param name="stage">The shader stage.</param>
+    /// <param name="errors">The error logging from the shader compiler.</param>
+    /// <param name="sourceCode">The source code that was being compiled.</param>
+    public ShaderCompilerException(string sourceFile, string entrypoint, ShaderStage stage, string errors, string sourceCode)
+        : base(GetMessage(sourceFile, entrypoint, stage))
+    {
+        SourceFile = sourceFile;
+        Entrypoint = entrypoint;
+        Stage = stage;
+        Errors = errors;
+        SourceCode = sourceCode;
+    }
 
-        public override string ToString()
-        {
-            // Return all the data we got by default.
-            return $"""
+    public override string ToString()
+    {
+        // Return all the data we got by default.
+        return $"""
 {base.ToString()}
 
 ---------------------------------------------------------------------------------------------------
@@ -91,6 +91,5 @@ Source Code:
 
 ---------------------------------------------------------------------------------------------------
 """;                    
-        }
     }
 }
