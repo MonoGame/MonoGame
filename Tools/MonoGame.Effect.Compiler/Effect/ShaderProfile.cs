93c11d2a8643e55f4475ad464c690529d7b90ffd
Dean Ellis <dellis1972@googlemail.com>
2025-06-12 10:36:11 +0000

    Fix build error where the Effect Compiler was referencing the Pipeline (#8839)
    Fix an build error where we were using `ExternalToo.Run` from the Effect
    Compiler.
    Recent changes means we cannot access that method. So lets refactor the
    Vulkan code to use a shared `RunTool` method which can be used from
    DX12.

--- Tools/MonoGame.Effect.Compiler/Effect/ShaderProfile.cs
+++ Tools/MonoGame.Effect.Compiler/Effect/ShaderProfile.cs
@@ -5,9 +5,12 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using MonoGame.Effect.TPGParser;
 
 namespace MonoGame.Effect
@@ -91,6 +94,77 @@ namespace MonoGame.Effect
                 }
 
                 return base.ConvertFrom(context, culture, value);
+            }
+        }
+
+        protected static int RunTool(string exe, string args, out string stdout, out string stderr)
+        {
+            stdout = string.Empty;
+            stderr = string.Empty;
+            var processInfo = new ProcessStartInfo
+            {
+                Arguments = args,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                ErrorDialog = false,
+                FileName = exe,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+            };
+
+            using (var process = new Process { StartInfo = processInfo })
+            {
+                process.Start();
+                string stderrOutput = string.Empty;
+                string stdoutOutput = string.Empty;
+                var stdoutThread = new Thread(new ThreadStart(() =>
+                {
+                    var memory = new MemoryStream();
+                    process.StandardOutput.BaseStream.CopyTo(memory);
+                    var bytes = new byte[memory.Position];
+                    memory.Seek(0, SeekOrigin.Begin);
+                    memory.Read(bytes, 0, bytes.Length);
+                    stdoutOutput = System.Text.Encoding.ASCII.GetString(bytes);
+                }));
+                stdoutThread.Start();
+
+                var stderrThread = new Thread(new ThreadStart(() =>
+                {
+                    var memory = new MemoryStream();
+                    process.StandardError.BaseStream.CopyTo(memory);
+                    var bytes = new byte[memory.Position];
+                    memory.Seek(0, SeekOrigin.Begin);
+                    memory.Read(bytes, 0, bytes.Length);
+                    stderrOutput = System.Text.Encoding.ASCII.GetString(bytes);
+                }));
+                stderrThread.Start();
+
+                process.WaitForExit();
+
+                stdoutThread.Join();
+                stderrThread.Join();
+
+                stderr = stderrOutput;
+                stdout = stdoutOutput;
+
+                return process.ExitCode;
+            }
+        }
+
+        /// <summary>
+        /// Safely deletes the file if it exists.
+        /// </summary>
+        /// <param name="filePath">The path to the file to delete.</param>
+        protected static void DeleteFile(string filePath)
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            catch (Exception)
+            {
             }
         }
     }
