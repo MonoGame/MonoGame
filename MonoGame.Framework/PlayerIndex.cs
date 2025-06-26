dd9f1486e06db388216c32af72aac1ce3c609d79
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-06-26 14:12:35 -0400

    Extend `PlayerIndex` Enum To Support 8 Players On Compatible Platforms (#8809)
    ### Summary
    
    This PR extends the `PlayerIndex` enum to support up to 8 players on
    platforms that can handle more than 4 controllers simultaneously. The
    change uses preprocessor directives to maintain platform-specific
    limitations.
    
    ### Changes
    
    - Added `PlayerIndex.Five` through `PlayerIndex.Eight` (values 4-7).
    - Implemented preprocessor directives `#if !PLAYSTATION4 &&
    !PLAYSTATION5` to exclude additional values on Playstation platforms as
    they only support a maximum of four controllers.

--- MonoGame.Framework/PlayerIndex.cs
+++ MonoGame.Framework/PlayerIndex.cs
@@ -7,6 +7,11 @@ namespace Microsoft.Xna.Framework
     /// <summary>
     /// Defines the index of player for various MonoGame components.
     /// </summary>
+    /// <remarks>
+    /// Use <see cref="GamePad.MaximumGamePadCount" /> to determine the number of supported gamepads on the current 
+    /// platform to ensure that a valid <see cref="PlayerIndex" /> is used when accessing gamepad input.
+    /// Not all platforms support all eight player indices.
+    /// </remarks>
     public enum PlayerIndex
     {
         /// <summary>
@@ -24,6 +29,22 @@ namespace Microsoft.Xna.Framework
         /// <summary>
         /// The fourth player index.
         /// </summary>
-        Four = 3
+        Four = 3,
+        /// <summary>
+        /// The fifth player index.
+        /// </summary>
+        Five = 4,
+        /// <summary>
+        /// The sixth player index.
+        /// </summary>
+        Six = 5,
+        /// <summary>
+        /// The seventh player index.
+        /// </summary>
+        Seven = 6,
+        /// <summary>
+        /// The eighth player index.
+        /// </summary>
+        Eight = 7
     }
 }
