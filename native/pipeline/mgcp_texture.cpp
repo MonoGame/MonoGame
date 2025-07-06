7c9db549b1cd05659c0b4c5a790d7744c04e819c
Sepcnt <30561671+sepcnt@users.noreply.github.com>
2025-07-06 23:59:20 +0800

    Add native pipeline support and texture related functionality (#8871)
    This PR addresses bounty
    https://github.com/MonoGame/MonoGame/issues/8641 by replacing the
    `FreeImage` and `StbImageSharp` dependencies with a new, in-house native
    C++ pipeline library (`mgpipeline`) for texture importing.
    The changes improve modularity and performance by consolidating
    image-related operations into the `mgpipeline` library. Additionally,
    submodule management and project dependencies have been updated to
    reflect these changes.
    
    ### Image Processing Refactor:
    *
    [`MonoGame.Framework.Content.Pipeline/Graphics/GraphicsUtil.cs`](diffhunk://#diff-37a0beedf17141b267965f66061702d42451da617536f0244474d6cf907261b2L20-R94):
    Refactored the `Resize` method to use the `MGCP_Bitmap` structure and
    `MGCP.MP_ResizeBitmap` function from the `mgpipeline` library, replacing
    FreeImage-based resizing logic.
    *
    [`MonoGame.Framework.Content.Pipeline/TextureImporter.cs`](diffhunk://#diff-d274e21bce6bb79d1b085fcf3355b1595041ecee26848bce4b085b9c5551d823L77-L209):
    Replaced FreeImage and Stb-based image importing with
    `MGCP.MP_ImportBitmap` and added support for new texture types. Removed
    redundant methods for channel swapping and BMP loading.
    
    ### Native Library Integration:
    *
    [`MonoGame.Framework.Content.Pipeline/Native/Texture.Interop.cs`](diffhunk://#diff-7b1c87b82a48fd442dacc69b246a854fa6934758fcb75388dfee5e8780e94276R1-R52):
    Introduced a new interop file defining `MGCP_Bitmap` structure and
    native methods (`MP_ImportBitmap`, `MP_ResizeBitmap`, `MP_ExportBitmap`,
    etc.) for interacting with the `mgpipeline` library.
    *
    [`MonoGame.Framework.Content.Pipeline/MonoGame.Framework.Content.Pipeline.csproj`](diffhunk://#diff-393aad48627ab537c1c1e207c5ecc909589fb716844dfaf82fa3ce5739316ef1R93-R104):
    Added `mgpipeline` native library files (`mgpipeline.dll`,
    `libmgpipeline.so`, `libmgpipeline.dylib`) to the project for
    cross-platform support. Removed references to FreeImage and Stb
    libraries.
    
    ### Submodule and Workflow Updates:
    *
    [`.gitmodules`](diffhunk://#diff-fe7afb5c9c916e521401d3fcfb4277d5071798c3baf83baf11d6071742823584R28-R30):
    Added a new submodule for `external/stb`, ensuring proper version
    control for dependencies.
    *
    [`.github/workflows/main.yml`](diffhunk://#diff-7829468e86c1cc5d5133195b5cb48e1ff6c75e3e9203777f6b2e379d9e4882b3R131-R132):
    Updated the repository cloning step to include submodules recursively in
    deploy stage.
    
    ### File Format Export:
    *
    [`MonoGame.Framework.Content.Pipeline/Utilities/FileHelpers.cs`](diffhunk://#diff-1f025d7f4005e47796cc286813c602fa8af27475b50a095469cfe8af7011eee0L48-R82):
    Refactored PNG export logic to use `MGCP.MP_ExportBitmap` instead of
    StbImageWriteSharp, improving error handling and efficiency.
    
    ### Cleanup and Simplification:
    *
    [`MonoGame.Framework.Content.Pipeline/MonoGame.Framework.Content.Pipeline.csproj`](diffhunk://#diff-393aad48627ab537c1c1e207c5ecc909589fb716844dfaf82fa3ce5739316ef1L52-L54):
    Removed unused references to StbImageSharp and StbImageWriteSharp, as
    well as the FreeImage library.
    [[1]](diffhunk://#diff-393aad48627ab537c1c1e207c5ecc909589fb716844dfaf82fa3ce5739316ef1L52-L54)
    [[2]](diffhunk://#diff-393aad48627ab537c1c1e207c5ecc909589fb716844dfaf82fa3ce5739316ef1L78)
    *
    [`MonoGame.Framework.Content.Pipeline/Graphics/GraphicsUtil.cs`](diffhunk://#diff-37a0beedf17141b267965f66061702d42451da617536f0244474d6cf907261b2L52-L61):
    Removed the `BGRAtoRGBA` method, as channel swapping is no longer
    required with the new library.
    
    ---------
    
    Co-authored-by: sepcnt <sepcnt@proton.me>

---
+++ native/pipeline/mgcp_texture.cpp
@@ -0,0 +1,194 @@
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+#include "stb_image_resize2.h"
+
+#include "api_MGCP.h"
+
+void* MP_ImportBitmap(const char* importPath, MGCP_Bitmap& bitmap)
+{
+    FILE* f;
+    int width, height, channels;
+    void* data = nullptr;
+
+    f = stbi__fopen(importPath, "rb");
+    if (!f) goto err;
+
+    if (stbi_is_hdr_from_file(f))
+    {
+        bitmap.type = MGTextureType::RgbaF;
+        data = stbi_loadf_from_file(f, &width, &height, &channels, 4);
+    }
+    else if (stbi_is_16_bit_from_file(f))
+    {
+        bitmap.type = MGTextureType::Rgba16;
+        data = stbi_load_from_file_16(f, &width, &height, &channels, 4);
+    }
+    else
+    {
+        bitmap.type = MGTextureType::Rgba8;
+        data = stbi_load_from_file(f, &width, &height, &channels, 4);
+    }
+
+    fclose(f);
+
+    if (!data)
+        goto err;
+
+    bitmap.width = width;
+    bitmap.height = height;
+
+    bitmap.data = data;
+    return nullptr;
+
+err:
+    return (void*)stbi_failure_reason();
+}
+
+void MP_FreeBitmap(MGCP_Bitmap& bitmap)
+{
+    if (bitmap.data)
+        stbi_image_free(bitmap.data);
+    bitmap.data = nullptr;
+}
+
+inline stbir_filter MP_HeuristicFilter(mgint srcMeasure, mgint dstMeasure)
+{
+    if (dstMeasure != 1 && (srcMeasure == srcMeasure / dstMeasure * dstMeasure || srcMeasure == dstMeasure + 1))
+        return STBIR_FILTER_BOX;
+
+    mgint power = powf(2.0f, floorf(log2f((float)srcMeasure / dstMeasure)));
+    if (power > 1 && srcMeasure / power == dstMeasure)
+        return STBIR_FILTER_CATMULLROM;
+
+    return STBIR_FILTER_DEFAULT;
+}
+
+inline mgint MP_GetBpp(MGTextureType type)
+{
+    switch (type)
+    {
+    case MGTextureType::Rgba8:
+        return 4;
+    case MGTextureType::Rgba16:
+        return 8;
+    case MGTextureType::RgbaF:
+        return 16;
+    default:
+        return 0; // Unsupported type
+    }
+}
+
+void* MP_ResizeBitmap(MGCP_Bitmap& srcBitmap, MGCP_Bitmap& dstBitmap)
+{
+    stbir_datatype data_type;
+    size_t bpp;
+
+    if (!srcBitmap.data || srcBitmap.width <= 0 || srcBitmap.height <= 0 || dstBitmap.width <= 0 || dstBitmap.height <= 0)
+    {
+        return (void*)"Invalid input bitmap or dimensions for resizing.";
+    }
+
+    if (!dstBitmap.data)
+    {
+		return (void*)"Should allocate memory for destination bitmap data before resizing.";
+    }
+
+    bpp = MP_GetBpp(srcBitmap.type);
+
+    switch (srcBitmap.type)
+    {
+    case MGTextureType::Rgba8:
+        data_type = STBIR_TYPE_UINT8;
+        break;
+    case MGTextureType::Rgba16:
+        data_type = STBIR_TYPE_UINT16;
+        break;
+    case MGTextureType::RgbaF:
+        data_type = STBIR_TYPE_FLOAT;
+        break;
+    default:
+        return (void*)"Unsupported source bitmap pixel format for resizing.";
+    }
+
+    dstBitmap.type = srcBitmap.type;
+
+    STBIR_RESIZE resize;
+
+    stbir_resize_init(&resize,
+        srcBitmap.data, srcBitmap.width, srcBitmap.height, 0,
+        dstBitmap.data, dstBitmap.width, dstBitmap.height, 0,
+        STBIR_4CHANNEL, data_type);
+
+    resize.horizontal_edge = STBIR_EDGE_CLAMP;
+    resize.vertical_edge = STBIR_EDGE_CLAMP;
+
+    resize.horizontal_filter = MP_HeuristicFilter(srcBitmap.width, dstBitmap.width);
+    resize.vertical_filter = MP_HeuristicFilter(srcBitmap.height, dstBitmap.height);
+
+    if (!stbir_resize_extended(&resize))
+    {
+        return (void*)stbi_failure_reason();
+    }
+
+    return nullptr;
+
+}
+
+void* MP_ExportBitmap(MGCP_Bitmap& bitmap, const char* exportPath)
+{
+    int bpp;
+    int errno;
+
+    if (!bitmap.data || bitmap.width <= 0 || bitmap.height <= 0)
+    {
+        return (void*)"Invalid bitmap data or dimensions for export.";
+    }
+
+    bpp = MP_GetBpp(bitmap.type);
+    if (bpp == 0)
+    {
+        return (void*)"Unsupported bitmap pixel format for export.";
+    }
+
+    switch (bitmap.format)
+    {
+    case MGTextureFormat::Png:
+        if (bitmap.type == MGTextureType::RgbaF)
+            return (void*)"Exporting float textures to PNG is not supported.";
+        errno = stbi_write_png(exportPath, bitmap.width, bitmap.height, 4, bitmap.data, bitmap.width * bpp);
+        break;
+    case MGTextureFormat::Jpeg:
+        if (bitmap.type != MGTextureType::Rgba8)
+            return (void*)"Exporting non-RGBA8 textures to JPEG is not supported.";
+        errno = stbi_write_jpg(exportPath, bitmap.width, bitmap.height, 4, bitmap.data, 100);
+        break;
+    case MGTextureFormat::Tga:
+        if (bitmap.type != MGTextureType::Rgba8)
+            return (void*)"Exporting non-RGBA8 textures to TGA is not supported.";
+        errno = stbi_write_tga(exportPath, bitmap.width, bitmap.height, 4, bitmap.data);
+        break;
+    case MGTextureFormat::Hdr:
+        if (bitmap.type != MGTextureType::RgbaF)
+            return (void*)"Exporting non-RGBAF textures to HDR is not supported.";
+        errno = stbi_write_hdr(exportPath, bitmap.width, bitmap.height, 4, (float*)bitmap.data);
+        break;
+    case MGTextureFormat::Bmp:
+        if (bitmap.type != MGTextureType::Rgba8)
+            return (void*)"Exporting non-RGBA8 textures to BMP is not supported.";
+        errno = stbi_write_bmp(exportPath, bitmap.width, bitmap.height, 4, bitmap.data);
+        break;
+    default:
+        return (void*)"Unsupported bitmap format for export.";
+    }
+
+    if (errno == 0)
+    {
+        return (void*)stbi_failure_reason();
+    }
+
+    return nullptr;
+}
