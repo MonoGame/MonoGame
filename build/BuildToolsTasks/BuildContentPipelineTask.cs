e5240c54520e3112cd5c305d3c5b8979473de603
sepcnt <sepcnt@proton.me>
2025-07-15 19:06:46 +0800

    Fix StaticLibCheck directory

--- build/BuildToolsTasks/BuildContentPipelineTask.cs
+++ build/BuildToolsTasks/BuildContentPipelineTask.cs
@@ -11,7 +11,14 @@ public sealed class BuildContentPipelineTask : FrostingTask<BuildContext>
         buildPremake.Run(context, "Content Pipeline", "native/pipeline", "pipeline.sln");
 
         StaticLibCheck staticLibCheck = new StaticLibCheck();
-        staticLibCheck.Check(context, "Artifacts/mgpipeline/Release");
+        var platform = context.Environment.Platform.Family switch
+        {
+            PlatformFamily.Windows => "windows",
+            PlatformFamily.OSX => "macosx",
+            PlatformFamily.Linux => "linux",
+            _ => throw new Exception("Unsupported platform family: " + context.Environment.Platform.Family)
+        };
+        staticLibCheck.Check(context, $"Artifacts/native/mgpipeline/{platform}/Release");
 
         var builderPath = context.GetProjectPath(ProjectType.ContentPipeline);
         context.DotNetPackSettings.MSBuildSettings.WithProperty("DisableMonoGameToolAssets", "True");
