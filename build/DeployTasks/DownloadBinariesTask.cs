577e431c8287f5cf360bc06edd61c7d59f6e145c
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2025-08-20 11:45:54 +0100

    Restructure delivery

--- build/DeployTasks/DownloadBinariesTask.cs
+++ build/DeployTasks/DownloadBinariesTask.cs
@@ -24,18 +24,15 @@ public sealed class DownloadBinariesTask : AsyncFrostingTask<BuildContext>
                 PlatformFamily.OSX => "macos",
                 _ => "linux"
             };
-            await DownloadArtifactAsync(context, $"mgcontentbuilder-{platformStr}.{context.Version}", "binaries/MonoGame.Content.Builder/");
-            await DownloadArtifactAsync(context, $"mgeffectcompiler-{platformStr}.{context.Version}", "binaries/MonoGame.Effect.Compiler/");
-            await DownloadArtifactAsync(context, $"mgframework-{platformStr}.{context.Version}", "binaries/MonoGame.Framework/");
+            await DownloadArtifactAsync(context, $"mgcontentbuilder-{platformStr}.{context.Version}", "binaries/MonoGame.Framework.Content.Pipeline/");
+            await DownloadArtifactAsync(context, $"mgeffectcompiler-{platformStr}.{context.Version}", "binaries/MonoGame.Framework.Content.Pipeline/");
             await DownloadArtifactAsync(context, $"mgcontentpipeline-{platformStr}.{context.Version}", "binaries/MonoGame.Framework.Content.Pipeline/");
+            await DownloadArtifactAsync(context, $"mgpipeline-{platformStr}.{context.Version}", "binaries/MonoGame.Framework.Content.Pipeline/");
+            await DownloadArtifactAsync(context, $"mgframework-{platformStr}.{context.Version}", "binaries/MonoGame.Framework/");
         }
 
-        await DownloadArtifactAsync(context, $"mgpipeline-windows.{context.Version}", "binaries/mgpipeline/windows/Release/");
-        await DownloadArtifactAsync(context, $"mgpipeline-macos.{context.Version}", "binaries/mgpipeline/macosx/Release/");
-        await DownloadArtifactAsync(context, $"mgpipeline-linux.{context.Version}", "binaries/mgpipeline/linux/Release/");
-
-        await DownloadArtifactAsync(context, $"mgnative-windows.{context.Version}", "binaries/mgnative/windows/Release/");
-        //await DownloadArtifactAsync(context, $"mgnative-macos.{context.Version}", "binaries/mgnative/macosx/Release/");
-        //await DownloadArtifactAsync(context, $"mgnative-linux.{context.Version}", "binaries/mgnative/linux/Release/");
+        await DownloadArtifactAsync(context, $"mgnative-windows.{context.Version}", "binaries/MonoGame.Framework/");
+        //await DownloadArtifactAsync(context, $"mgnative-macos.{context.Version}", "binaries/MonoGame.Framework/");
+        //await DownloadArtifactAsync(context, $"mgnative-linux.{context.Version}", "binaries/MonoGame.Framework/");
     }
 }
--- build/DeployTasks/UploadArtifactsTask.cs
+++ build/DeployTasks/UploadArtifactsTask.cs
@@ -65,7 +65,7 @@ public sealed class UploadArtifactsTask : AsyncFrostingTask<BuildContext>
         await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath("Artifacts/MonoGame.Content.Builder/Release/"), $"mgcontentbuilder-{os}.{context.Version}");
         await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath("Artifacts/MonoGame.Effect.Compiler/Release/"), $"mgeffectcompiler-{os}.{context.Version}");
         await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath("Artifacts/MonoGame.Framework/"), $"mgframework-{os}.{context.Version}");
-        await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath("Artifacts/MonoGame.Framework.Content.Pipeline/"), $"mgcontentpipeline-{os}.{context.Version}");
+        await context.GitHubActions().Commands.UploadArtifact(new DirectoryPath("Artifacts/MonoGame.Framework.Content.Pipeline/Release"), $"mgcontentpipeline-{os}.{context.Version}");
 
 
         // Upload NuGet packages
