ddc86e0469a047d230b8b9f92fecc36c563ebeed
Dean Ellis <dellis1972@googlemail.com>
2025-08-25 00:27:58 +0100

    Try a fixed timestep

--- Tests/Framework/TestGameBase.cs
+++ Tests/Framework/TestGameBase.cs
@@ -35,6 +35,10 @@ namespace MonoGame.Tests {
             GraphicsAdapter.UseReferenceDevice = true;
             GraphicsAdapter.UseDriverType = GraphicsAdapter.DriverType.FastSoftware;
 
+            /// Enable fixed time step
+            IsFixedTimeStep = true;
+            TargetElapsedTime = TimeSpan.FromSeconds(1.0 / 30.0); // 30 FPS
+
             Services.AddService<IFrameInfoSource>(this);
 			SuppressExtraUpdatesAndDraws = true;
 		}
