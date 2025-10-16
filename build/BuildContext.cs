169b8df6127f72dcb3fe10e93c0100a6b39c905a
Dean Ellis <dellis1972@googlemail.com>
2025-10-16 12:36:10 +0100

    add diagnostic output

--- build/BuildContext.cs
+++ build/BuildContext.cs
@@ -78,7 +78,7 @@ public class BuildContext : FrostingContext
         DotNetPublishSettings = new DotNetPublishSettings
         {
             MSBuildSettings = DotNetMSBuildSettings,
-            Verbosity = DotNetVerbosity.Minimal,
+            Verbosity = DotNetVerbosity.Diagnostic,
             Configuration = buildConfiguration,
             SelfContained = false,
             WorkingDirectory = this.ShellWorkingDir
