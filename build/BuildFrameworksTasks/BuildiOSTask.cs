7b785009ad013c70250789ffe9c95240aa61fe8e
Dean Ellis <dellis1972@googlemail.com>
2025-10-16 14:45:46 +0100

    Install ios workload on windows

--- build/BuildFrameworksTasks/BuildiOSTask.cs
+++ build/BuildFrameworksTasks/BuildiOSTask.cs
@@ -6,7 +6,7 @@ namespace BuildScripts;
 public sealed class BuildiOSTask : FrostingTask<BuildContext>
 {
     private string platformName = "iOS";
-    public override bool ShouldRun(BuildContext context) => context.IsWorkloadInstalled("ios") && !context.IsRunningOnWindows();
+    public override bool ShouldRun(BuildContext context) => context.IsWorkloadInstalled("ios");
 
     public override void Run(BuildContext context) => context.DotNetPack(context.GetProjectPath(ProjectType.Framework, platformName), context.DotNetPackSettings);
 }
