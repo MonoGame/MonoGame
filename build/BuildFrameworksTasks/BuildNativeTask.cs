d73b224049f862db47505a73404ca43abe1bf6de
harry.cpp <harry.cpp@outlook.com>
2025-12-21 18:03:56 +0100

    WIP Native Packaging

--- build/BuildFrameworksTasks/BuildNativeTask.cs
+++ build/BuildFrameworksTasks/BuildNativeTask.cs
@@ -10,6 +10,22 @@ public sealed class BuildNativeTask : FrostingTask<BuildContext>
     {
         context.DotNetPack(context.GetProjectPath(ProjectType.Framework, platformName), context.DotNetPackSettings);
 
+        context.DotNetPack("src/NuGetPackages/MonoGame.Framework/MonoGame.Framework.csproj", context.DotNetPackSettings);
+
+        if (context.Environment.Platform.Family == PlatformFamily.Windows)
+        {
+            context.DotNetPack("src/NuGetPackages/MonoGame.Runtime.Windows.DX12/MonoGame.Runtime.Windows.DX12.csproj", context.DotNetPackSettings);
+            context.DotNetPack("src/NuGetPackages/MonoGame.Runtime.Windows.Vulkan/MonoGame.Runtime.Windows.Vulkan.csproj", context.DotNetPackSettings);
+        }
+        else if (context.Environment.Platform.Family == PlatformFamily.OSX)
+        {
+            context.DotNetPack("src/NuGetPackages/MonoGame.Runtime.Mac.Vulkan/MonoGame.Runtime.Mac.Vulkan.csproj", context.DotNetPackSettings);
+        }
+        else
+        {
+            context.DotNetPack("src/NuGetPackages/MonoGame.Runtime.Linux.Vulkan/MonoGame.Runtime.Linux.Vulkan.csproj", context.DotNetPackSettings);
+        }
+
         context.PublishBinaries(platformName);
     }
 }
