09e17e57e6600a3a8b5e636d4fcd44a9150aa1b1
JakeLegendXIII <jakelegendxiii@gmail.com>
2026-01-01 02:09:49 -0500

    Revert Mono.Xna Team changes because of MIT License

--- MonoGame.Framework/BoundingSphere.cs
+++ MonoGame.Framework/BoundingSphere.cs
@@ -1,4 +1,4 @@
-﻿// MonoGame - Copyright (C) MonoGame Foundation, Inc
+﻿// MIT License - Copyright (C) The Mono.Xna Team
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
