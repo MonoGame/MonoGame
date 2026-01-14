0d8fad5f39710ba00242780ef909fe93022a30ae
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-01-14 09:34:17 -0500

    Remove unused DataAnnotations using causing Native build to break

--- MonoGame.Framework/Line2D.cs
+++ MonoGame.Framework/Line2D.cs
@@ -3,7 +3,6 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 
