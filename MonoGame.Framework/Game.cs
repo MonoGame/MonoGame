d521dd9f8bd9b8e3ed42ed8a30a419385d814464
Dominique Louis <savagesoftware@gmail.com>
2026-03-19 11:25:04 +0000

    Centralise SoundEffect shutdown and resetting the _systemState to SoundSystemState.NotInitialized

--- MonoGame.Framework/Game.cs
+++ MonoGame.Framework/Game.cs
@@ -147,7 +147,9 @@ namespace Microsoft.Xna.Framework
                     ContentTypeReaderManager.ClearTypeCreators();
 
                     if (SoundEffect._systemState == SoundEffect.SoundSystemState.Initialized)
-                        SoundEffect.PlatformShutdown();
+                    {
+                        SoundEffect.Shutdown();
+                    }
                 }
 #if ANDROID
                 Activity = null;
