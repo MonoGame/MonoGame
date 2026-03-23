d544cdf7004561e3ebdcfd503e63b549a356ecf4
Michael Hoffmann <mghoffmann@gmail.com>
2026-03-23 12:07:06 -0600

    Unsubscribed from _components.ComponentAdded and ComponentRemoved in Game.Dispose(bool) method.

--- MonoGame.Framework/Game.cs
+++ MonoGame.Framework/Game.cs
@@ -120,6 +120,8 @@ namespace Microsoft.Xna.Framework
                         if (disposable != null)
                             disposable.Dispose();
                     }
+                    _components.ComponentAdded -= Components_ComponentAdded;
+                    _components.ComponentRemoved -= Components_ComponentRemoved;
                     _components = null;
 
                     if (_content != null)
