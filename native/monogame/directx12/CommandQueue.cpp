27915a510a96dcb9046a389db1e64f75841f39e8
Tom Spilman <tom@sickheadgames.com>
2026-04-19 22:48:55 -0500

    Fix DX12 CommandQueue to support multiple threads submitting work.

--- native/monogame/directx12/CommandQueue.cpp
+++ native/monogame/directx12/CommandQueue.cpp
@@ -22,39 +22,50 @@ void CommandQueue::Create(ID3D12Device* device) {
     ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_GRAPHICS_PPV_ARGS(m_fence.ReleaseAndGetAddressOf())));
     m_fence->Signal(m_lastCompletedFenceValue);
     m_fence->SetName((m_name + L" Fence").c_str());
-
-    m_fenceEvent.Attach(CreateEventEx(nullptr, nullptr, 0, EVENT_MODIFY_STATE | SYNCHRONIZE));
-    if (!m_fenceEvent.IsValid())
-        throw std::system_error(std::error_code(static_cast<int>(GetLastError()), std::system_category()), "CreateEventEx");
 }
 
 uint64_t CommandQueue::ExecuteCommandList(ID3D12CommandList* commandList) {
+
+    std::lock_guard<std::mutex> lock(m_fenceMutex);
+
     // Send the command list off to the GPU for processing.
     ThrowIfFailed(((ID3D12GraphicsCommandList*)commandList)->Close());
     m_queue->ExecuteCommandLists(1, &commandList);
 
-    return SignalFence();
+    // Send the signal.
+    uint64_t fenceValue = m_nextFenceValue.fetch_add(1);
+    m_queue->Signal(m_fence.Get(), fenceValue);
+    return fenceValue;
 }
 
 uint64_t CommandQueue::SignalFence() {
+
     std::lock_guard<std::mutex> lock(m_fenceMutex);
 
-    m_queue->Signal(m_fence.Get(), m_nextFenceValue);
-
-    return m_nextFenceValue++;
+    uint64_t fenceValue = m_nextFenceValue.fetch_add(1);
+    m_queue->Signal(m_fence.Get(), fenceValue);
+    return fenceValue;
 }
 
 
 uint64_t CommandQueue::PollCurrentFenceValue() {
-    m_lastCompletedFenceValue = std::max(m_lastCompletedFenceValue, m_fence->GetCompletedValue());
-    return m_lastCompletedFenceValue;
+
+    uint64_t completed = m_fence->GetCompletedValue();
+    uint64_t current = m_lastCompletedFenceValue.load();
+
+    while ( completed > current &&
+            !m_lastCompletedFenceValue.compare_exchange_weak(current, completed))
+        {}
+
+    return m_lastCompletedFenceValue.load();
 }
 
 bool CommandQueue::IsFenceComplete(uint64_t fenceValue) {
-    if (fenceValue > m_lastCompletedFenceValue)
-        PollCurrentFenceValue();
 
-    return fenceValue <= m_lastCompletedFenceValue;
+    if (fenceValue <= m_lastCompletedFenceValue.load())
+        return true;
+
+    return fenceValue <= PollCurrentFenceValue();
 }
 
 void CommandQueue::InsertWait(uint64_t fenceValue) {
@@ -66,6 +77,9 @@ void CommandQueue::InsertWaitForQueueFence(CommandQueue* otherQueue, uint64_t fe
 }
 
 void CommandQueue::InsertWaitForQueue(CommandQueue* otherQueue) {
+
+    std::lock_guard<std::mutex> lock(m_fenceMutex);
+
     m_queue->Wait(otherQueue->GetFence(), otherQueue->GetNextFenceValue() - 1); // Wait for the last requested fence
 }
 
@@ -73,11 +87,15 @@ void CommandQueue::WaitForFenceCPUBlocking(uint64_t fenceValue) {
     if (IsFenceComplete(fenceValue))
         return;
 
-    std::lock_guard<std::mutex> lock(m_fenceMutex);
+    HANDLE evt = CreateEventEx(nullptr, nullptr, 0, EVENT_MODIFY_STATE | SYNCHRONIZE);
+    m_fence->SetEventOnCompletion(fenceValue, evt);
+    WaitForSingleObjectEx(evt, INFINITE, FALSE);
+    CloseHandle(evt);
 
-    m_fence->SetEventOnCompletion(fenceValue, m_fenceEvent.Get());
-    WaitForSingleObjectEx(m_fenceEvent.Get(), INFINITE, FALSE);
-    m_lastCompletedFenceValue = fenceValue;
+    uint64_t current = m_lastCompletedFenceValue.load();
+    while ( current < fenceValue &&
+            !m_lastCompletedFenceValue.compare_exchange_weak(current, fenceValue))
+        { }
 }
 
 #ifdef _GAMING_XBOX
--- native/monogame/directx12/CommandQueue.h
+++ native/monogame/directx12/CommandQueue.h
@@ -21,7 +21,7 @@ public:
     void InsertWaitForQueue(CommandQueue* otherQueue);
 
     void WaitForFenceCPUBlocking(uint64_t fenceValue);
-    void WaitForIdle() { WaitForFenceCPUBlocking(m_nextFenceValue - 1); }
+    void WaitForIdle() { WaitForFenceCPUBlocking(SignalFence()); }
     uint64_t SignalFence();
 
     ID3D12CommandQueue* Get() { return m_queue.Get(); }
@@ -42,12 +42,10 @@ private:
     std::wstring m_name;
 
     std::mutex m_fenceMutex;
-    std::mutex m_eventMutex;
 
     Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
-    uint64_t m_lastCompletedFenceValue = 0;
-    uint64_t m_nextFenceValue = m_lastCompletedFenceValue + 1;
-    Microsoft::WRL::Wrappers::Event m_fenceEvent;
+    std::atomic<uint64_t> m_lastCompletedFenceValue = 0;
+    std::atomic<uint64_t> m_nextFenceValue = 1;
 };
 
 // Simplification of https://github.com/microsoft/DirectX-Graphics-Samples/blob/master/MiniEngine/Core/CommandAllocatorPool.cpp
