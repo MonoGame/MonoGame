476d51dac90381d2c1df5b07b78791a1a36a0495
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-06-24 12:59:47 -0400

    Fix native SoundEffect duration overflow for large WAV files

--- native/monogame/faudio/MGA_faudio.cpp
+++ native/monogame/faudio/MGA_faudio.cpp
@@ -323,7 +323,7 @@ void MGA_Buffer_InitializeFormat(MGA_Buffer* buffer, mgbyte* waveHeader, mgbyte*
 		assert((length % wformat.nBlockAlign) == 0);
 
 		// Calculate duration
-		buffer->duration = (mgulong)((length * 1000) / buffer->format->nAvgBytesPerSec);
+		buffer->duration = ((mgulong)length * 1000) / buffer->format->nAvgBytesPerSec;
 
 		buffer->length = length;
 		buffer->data = (uint8_t*)malloc(length);
@@ -433,7 +433,7 @@ void MGA_Buffer_InitializePCM(MGA_Buffer* buffer, mgbyte* waveData, mgint offset
 		memcpy(buffer->data, waveData + offset, length);
 
 	// Calculate duration
-	buffer->duration = (mgulong)((length * 1000) / buffer->format->nAvgBytesPerSec);
+	buffer->duration = ((mgulong)length * 1000) / buffer->format->nAvgBytesPerSec;
 
 	// Set the buffer structure passed to SubmitSourceBuffer.
 	memset(&buffer->buffer, 0, sizeof(buffer->buffer));
@@ -469,11 +469,11 @@ void MGA_Buffer_InitializeXact(MGA_Buffer* buffer, mguint codec, mgbyte* waveDat
 	// Calculate duration (approximate for compressed formats)
 	if (codec == 0x166) // WMA
 	{
-		buffer->duration = (mgulong)((length * 1000) / (sampleRate * channels * 2));
+		buffer->duration = ((mgulong)length * 1000) / (sampleRate * channels * 2);
 	}
 	else
 	{
-		buffer->duration = (mgulong)((length * 1000) / buffer->format->nAvgBytesPerSec);
+		buffer->duration = ((mgulong)length * 1000) / buffer->format->nAvgBytesPerSec;
 	}
 }
 
