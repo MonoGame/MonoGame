b536202b04f5abac75ca5713c37988cf43580016
Mustafa Senoglu <mustafa@senoglu.local>
2026-06-25 16:39:31 +0300

    fix: remove deprecated RuntimeHelpers.OffsetToStringData usage
    Replaced `str + RuntimeHelpers.OffsetToStringData / 2` with `str` in
    OpenGL.cs. Modern .NET handles string pinning correctly without manual
    offset calculation.
    
    Also removed unused `using System.Runtime.CompilerServices;` import.
    
    Fixes #9293

--- MonoGame.Framework/Platform/Graphics/OpenGL.cs
+++ MonoGame.Framework/Platform/Graphics/OpenGL.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Diagnostics;
 using MonoGame.Framework.Utilities;
@@ -1591,7 +1590,7 @@ namespace MonoGame.OpenGL
             if (intPtr == IntPtr.Zero) {
                 throw new OutOfMemoryException ();
             }
-            fixed (char* chars = str + RuntimeHelpers.OffsetToStringData / 2) {
+            fixed (char* chars = str) {
                 int bytes = Encoding.ASCII.GetBytes (chars, str.Length, (byte*)((void*)intPtr), num);
                 Marshal.WriteByte (intPtr, bytes, 0);
                 return intPtr;
