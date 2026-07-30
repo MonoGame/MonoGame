e290e32d76c1971cb9d50718636849f83aef0afa
Christopher Whitley <aristurtledev@gmail.com>
2026-07-30 18:11:36 -0400

    shim entry points for parity work

--- native/monogame/opengl/OpenGLContext.h
+++ native/monogame/opengl/OpenGLContext.h
@@ -8,7 +8,10 @@
 #include <SDL_opengl_glext.h>
 
 // SDL declares glGetTexImage here, but this header set does not expose PFNGLGETTEXIMAGEPROC.
+using MGGLCOMPRESSEDTEXIMAGE2DPROC = decltype(&glCompressedTexImage2D);
+using MGGLCOMPRESSEDTEXSUBIMAGE2DPROC = decltype(&glCompressedTexSubImage2D);
 using MGGLGETTEXIMAGEPROC = decltype(&glGetTexImage);
+using MGGLGETCOMPRESSEDTEXIMAGEPROC = decltype(&glGetCompressedTexImage);
 
 struct OpenGLFunctions
 {
@@ -23,43 +26,59 @@ struct OpenGLFunctions
     PFNGLBINDRENDERBUFFERPROC BindRenderbuffer = nullptr;
     PFNGLBINDSAMPLERPROC BindSampler = nullptr;
     PFNGLBINDVERTEXARRAYPROC BindVertexArray = nullptr;
+    PFNGLBLITFRAMEBUFFERPROC BlitFramebuffer = nullptr;
     PFNGLBUFFERDATAPROC BufferData = nullptr;
     PFNGLBUFFERSUBDATAPROC BufferSubData = nullptr;
     PFNGLCHECKFRAMEBUFFERSTATUSPROC CheckFramebufferStatus = nullptr;
     PFNGLCOMPILESHADERPROC CompileShader = nullptr;
+    MGGLCOMPRESSEDTEXIMAGE2DPROC CompressedTexImage2D = nullptr;
+    MGGLCOMPRESSEDTEXSUBIMAGE2DPROC CompressedTexSubImage2D = nullptr;
     PFNGLCREATEPROGRAMPROC CreateProgram = nullptr;
     PFNGLCREATESHADERPROC CreateShader = nullptr;
     PFNGLDELETEBUFFERSPROC DeleteBuffers = nullptr;
     PFNGLDELETEFRAMEBUFFERSPROC DeleteFramebuffers = nullptr;
     PFNGLDELETEPROGRAMPROC DeleteProgram = nullptr;
+    PFNGLDELETEQUERIESPROC DeleteQueries = nullptr;
     PFNGLDELETERENDERBUFFERSPROC DeleteRenderbuffers = nullptr;
     PFNGLDELETESAMPLERSPROC DeleteSamplers = nullptr;
     PFNGLDELETESHADERPROC DeleteShader = nullptr;
     PFNGLDELETEVERTEXARRAYSPROC DeleteVertexArrays = nullptr;
     PFNGLDETACHSHADERPROC DetachShader = nullptr;
     PFNGLDISABLEVERTEXATTRIBARRAYPROC DisableVertexAttribArray = nullptr;
+    PFNGLBEGINQUERYPROC BeginQuery = nullptr;
+    PFNGLDRAWBUFFERSPROC DrawBuffers = nullptr;
+    PFNGLDRAWELEMENTSINSTANCEDPROC DrawElementsInstanced = nullptr;
+    PFNGLENDQUERYPROC EndQuery = nullptr;
     PFNGLGENVERTEXARRAYSPROC GenVertexArrays = nullptr;
     PFNGLDRAWELEMENTSBASEVERTEXPROC DrawElementsBaseVertex = nullptr;
     PFNGLENABLEVERTEXATTRIBARRAYPROC EnableVertexAttribArray = nullptr;
     MGGLGETTEXIMAGEPROC GetTexImage = nullptr;
     PFNGLFRAMEBUFFERRENDERBUFFERPROC FramebufferRenderbuffer = nullptr;
     PFNGLFRAMEBUFFERTEXTURE2DPROC FramebufferTexture2D = nullptr;
+    PFNGLGENERATEMIPMAPPROC GenerateMipmap = nullptr;
     PFNGLGENBUFFERSPROC GenBuffers = nullptr;
     PFNGLGENFRAMEBUFFERSPROC GenFramebuffers = nullptr;
+    PFNGLGENQUERIESPROC GenQueries = nullptr;
     PFNGLGENRENDERBUFFERSPROC GenRenderbuffers = nullptr;
     PFNGLGENSAMPLERSPROC GenSamplers = nullptr;
+    MGGLGETCOMPRESSEDTEXIMAGEPROC GetCompressedTexImage = nullptr;
+    PFNGLGETATTRIBLOCATIONPROC GetAttribLocation = nullptr;
     PFNGLGETPROGRAMINFOLOGPROC GetProgramInfoLog = nullptr;
     PFNGLGETPROGRAMIVPROC GetProgramiv = nullptr;
+    PFNGLGETQUERYOBJECTUIVPROC GetQueryObjectuiv = nullptr;
     PFNGLGETSHADERINFOLOGPROC GetShaderInfoLog = nullptr;
     PFNGLGETSHADERIVPROC GetShaderiv = nullptr;
     PFNGLGETUNIFORMLOCATIONARBPROC GetUniformLocation = nullptr;
     PFNGLLINKPROGRAMPROC LinkProgram = nullptr;
     PFNGLMAPBUFFERPROC MapBuffer = nullptr;
     PFNGLRENDERBUFFERSTORAGEPROC RenderbufferStorage = nullptr;
+    PFNGLRENDERBUFFERSTORAGEMULTISAMPLEPROC RenderbufferStorageMultisample = nullptr;
     PFNGLSAMPLERPARAMETERFPROC SamplerParameterf = nullptr;
     PFNGLSAMPLERPARAMETERFVPROC SamplerParameterfv = nullptr;
     PFNGLSAMPLERPARAMETERIPROC SamplerParameteri = nullptr;
     PFNGLSHADERSOURCEPROC ShaderSource = nullptr;
+    PFNGLTEXIMAGE3DPROC TexImage3D = nullptr;
+    PFNGLTEXSUBIMAGE3DPROC TexSubImage3D = nullptr;
     PFNGLUNIFORM1IPROC Uniform1i = nullptr;
     PFNGLUNIFORM4FVPROC Uniform4fv = nullptr;
     PFNGLUNIFORM4IVPROC Uniform4iv = nullptr;
