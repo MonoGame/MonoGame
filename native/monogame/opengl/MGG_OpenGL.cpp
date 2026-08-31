a873b44c4724c916bdceb243496626f17251acb3
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-08-31 17:13:09 -0400

    Refactor browser specific naming conventions for methods

--- native/monogame/opengl/MGG_OpenGL.cpp
+++ native/monogame/opengl/MGG_OpenGL.cpp
@@ -654,7 +654,22 @@ namespace
         return (1u << drawBufferCount) - 1u;
     }
 
-    void ApplyBrowserAwarePolygonMode(MGFillMode fillMode)
+#if !defined(__EMSCRIPTEN__)
+    GLenum ToPolygonMode(MGFillMode fillMode)
+    {
+        switch (fillMode)
+        {
+            case MGFillMode::Solid:
+                return GL_FILL;
+            case MGFillMode::WireFrame:
+                return GL_LINE;
+            default:
+                MGGL_FAIL("Unsupported fill mode", "unknown OpenGL polygon mode");
+        }
+    }
+#endif
+
+    void ApplyPolygonMode(MGFillMode fillMode)
     {
 #if defined(__EMSCRIPTEN__)
         if (fillMode != MGFillMode::Solid)
@@ -664,7 +679,7 @@ namespace
 #endif
     }
 
-    void ApplyBrowserAwareDrawBuffer(MGG_GraphicsDevice* device, GLenum drawBuffer)
+    void SetDrawBuffer(MGG_GraphicsDevice* device, GLenum drawBuffer)
     {
         assert(device != nullptr);
 
@@ -676,7 +691,7 @@ namespace
 #endif
     }
 
-    void ResetBrowserAwareBackBufferDrawBuffer()
+    void ResetBackBufferDrawBuffer()
     {
 #if !defined(__EMSCRIPTEN__)
         glDrawBuffer(GL_BACK);
@@ -1049,30 +1064,6 @@ namespace
             default:
                 MGGL_FAIL("Unsupported stencil operation", "unknown OpenGL stencil operation");
         }
-    }
-
-    GLenum ToPolygonMode(MGFillMode fillMode)
-    {
-#if defined(__EMSCRIPTEN__)
-        switch (fillMode)
-        {
-            case MGFillMode::Solid:
-            case MGFillMode::WireFrame:
-                return 0;
-            default:
-                MGGL_FAIL("Unsupported fill mode", "unknown OpenGL polygon mode");
-        }
-#else
-        switch (fillMode)
-        {
-            case MGFillMode::Solid:
-                return GL_FILL;
-            case MGFillMode::WireFrame:
-                return GL_LINE;
-            default:
-                MGGL_FAIL("Unsupported fill mode", "unknown OpenGL polygon mode");
-        }
-#endif
     }
 
     void ToColorMask(MGColorWriteChannels channels, GLboolean& red, GLboolean& green, GLboolean& blue, GLboolean& alpha)
@@ -2385,7 +2376,7 @@ void MGG_GraphicsDevice_SetRasterizerState(MGG_GraphicsDevice* device, MGG_Raste
             glFrontFace(offscreen ? GL_CCW : GL_CW);
     }
 
-    ApplyBrowserAwarePolygonMode(info.fillMode);
+    ApplyPolygonMode(info.fillMode);
 
     if (info.scissorTestEnable)
         glEnable(GL_SCISSOR_TEST);
@@ -2485,7 +2476,7 @@ void MGG_GraphicsDevice_SetRenderTargets(MGG_GraphicsDevice* device, MGG_Texture
     if (count == 0)
     {
         device->context.functions.BindFramebuffer(GL_FRAMEBUFFER, 0);
-        ResetBrowserAwareBackBufferDrawBuffer();
+        ResetBackBufferDrawBuffer();
         glReadBuffer(GL_BACK);
         ClearCurrentRenderTargets(device);
         ApplyPosFixup(device);
@@ -2841,7 +2832,7 @@ void MGG_GraphicsDevice_ResolveRenderTargets(MGG_GraphicsDevice* device)
                 renderTarget->handle,
                 0);
             glReadBuffer(GL_COLOR_ATTACHMENT0 + i);
-            ApplyBrowserAwareDrawBuffer(device, GL_COLOR_ATTACHMENT0);
+            SetDrawBuffer(device, GL_COLOR_ATTACHMENT0);
             device->context.functions.BlitFramebuffer(
                 0,
                 0,
@@ -3309,7 +3300,7 @@ MGG_Texture* MGG_RenderTarget_Create(MGG_GraphicsDevice* device, MGTextureType t
             texture->handle,
             0);
     }
-    ApplyBrowserAwareDrawBuffer(device, GL_COLOR_ATTACHMENT0);
+    SetDrawBuffer(device, GL_COLOR_ATTACHMENT0);
     glReadBuffer(GL_COLOR_ATTACHMENT0);
 
     if (depthFormat != MGDepthFormat::None)
@@ -3356,7 +3347,7 @@ MGG_Texture* MGG_RenderTarget_Create(MGG_GraphicsDevice* device, MGTextureType t
             GetTextureImageTarget(texture, 0),
             texture->handle,
             0);
-        ApplyBrowserAwareDrawBuffer(device, GL_COLOR_ATTACHMENT0);
+        SetDrawBuffer(device, GL_COLOR_ATTACHMENT0);
         glReadBuffer(GL_COLOR_ATTACHMENT0);
 
         framebufferStatus = device->context.functions.CheckFramebufferStatus(GL_FRAMEBUFFER);
@@ -3392,7 +3383,7 @@ void MGG_Texture_Destroy(MGG_GraphicsDevice* device, MGG_Texture* texture)
     if (IsRenderTargetBound(device, texture))
     {
         device->context.functions.BindFramebuffer(GL_FRAMEBUFFER, 0);
-        ResetBrowserAwareBackBufferDrawBuffer();
+        ResetBackBufferDrawBuffer();
         glReadBuffer(GL_BACK);
         ClearCurrentRenderTargets(device);
     }
