76d9a2d59f53ada6f4df9230c2e3d6b4961274b9
tomspilman <tom@sickheadgames.com>
2026-09-04 14:01:49 -0500

    WIP storage!

--- MonoGame.Framework/Platform/Storage/StorageDevice.Native.cs
+++ MonoGame.Framework/Platform/Storage/StorageDevice.Native.cs
@@ -1,102 +1,53 @@
-﻿using System;
+﻿// MonoGame - Copyright (C) MonoGame Foundation, Inc
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
 using System.IO;
-using System.Runtime.InteropServices;
+using MonoGame.Interop;
 
 namespace Microsoft.Xna.Framework.Storage
 {
     partial class StorageDevice
     {
-        /// <summary>
-        /// Gets the root directory path for storing application data, adapted to the operating system's conventions.
-        /// </summary>
-        /// <remarks>
-        /// This property determines the appropriate storage location for application data based on the current operating system:
-        /// <list type="bullet">
-        /// <item>
-        /// <term>Linux:</term>
-        /// <description>
-        /// Returns the value of the <c>XDG_DATA_HOME</c> environment variable if set; 
-        /// otherwise, it defaults to <c>$HOME/.local/share</c>. If the home directory cannot be determined, 
-        /// it falls back to the current working directory ("<c>.</c>").
-        /// </description>
-        /// </item>
-        /// <item>
-        /// <term>macOS:</term>
-        /// <description>
-        /// Returns the user's <c>Library/Application Support</c> directory. If the home directory is unavailable,
-        /// it falls back to the current working directory ("<c>.</c>").
-        /// </description>
-        /// </item>
-        /// <item>
-        /// <term>Windows:</term>
-        /// <description>
-        /// Returns the path to the user's <c>LocalApplicationData</c> folder (e.g., <c>C:\Users\Username\AppData\Local</c>).
-        /// </description>
-        /// </item>
-        /// </list>
-        /// This approach ensures that application data is stored in a location consistent with each operating system's guidelines.
-        /// </remarks>
-        /// <returns>
-        /// A string representing the root directory path for application data storage.
-        /// </returns>
-        internal static string StorageRoot
+        internal unsafe MG_StorageDevice* _handle;
+
+        private unsafe void PlatformInitialize()
         {
-            get
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    string osConfigDir = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
-                    if (string.IsNullOrEmpty(osConfigDir))
-                    {
-                        string homeDir = Environment.GetEnvironmentVariable("HOME");
-                        if (string.IsNullOrEmpty(homeDir))
-                        {
-                            return "."; // Fallback to current directory
-                        }
-                        osConfigDir = Path.Combine(homeDir, ".local", "share");
-                    }
-                    return osConfigDir;
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    string osConfigDir = Environment.GetEnvironmentVariable("HOME");
-                    if (string.IsNullOrEmpty(osConfigDir))
-                    {
-                        return "."; // Fallback to current directory
-                    }
-                    osConfigDir = Path.Combine(osConfigDir, "Library", "Application Support");
-                    return osConfigDir;
-                }
-                else // Windows?
-                {
-                    return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                }
-            }
+            _handle = MG_Storage.OpenDevice(_titleName, _player.HasValue ? (int)_player.Value : -1);
         }
 
-        private string PlatformInitialize()
+        private unsafe void PlatformDispose()
         {
-            throw new NotImplementedException();
+            MG_Storage.CloseDevice(_handle);
+            _handle = null;
         }
 
-        private long PlatformTotalSpace()
+        private unsafe long PlatformGetTotalSpace()
         {
-            throw new NotImplementedException();
+            return MG_Storage.GetTotalSpace(_handle);
         }
 
-        private long PlatformFreeSpace()
+        private unsafe long PlatformGetFreeSpace()
         {
-            throw new NotImplementedException();
+            return MG_Storage.GetFreeSpace(_handle);
         }
 
-        private StorageContainer PlatformOpenContainer(string containerName)
+        private unsafe StorageContainer PlatformOpenContainer(string containerName, long requiredFreeBytes)
         {
-            throw new NotImplementedException();
+            MG_StorageContainer* container = MG_Storage.OpenContainer(_handle, containerName, requiredFreeBytes);
+            if (container == null)
+                return null;
+
+            var sc = new StorageContainer(this, containerName, _player);
+            sc._handle = container;
+
+            return sc;
         }
 
-        private void PlatformDeleteContainer(string containerName)
+        private unsafe void PlatformDeleteContainer(string containerName)
         {
-            throw new NotImplementedException();
+            MG_Storage.DeleteContainer(_handle, containerName);
         }
     }
 }
--- MonoGame.Framework/Storage/StorageDevice.cs
+++ MonoGame.Framework/Storage/StorageDevice.cs
@@ -1,14 +1,22 @@
+// MonoGame - Copyright (C) MonoGame Foundation, Inc
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
 using System;
 using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Microsoft.Xna.Framework.Storage
 {
-    public sealed partial class StorageDevice
+    public sealed partial class StorageDevice : IDisposable
     {
+        private string _titleName;
         private readonly PlayerIndex? _player;
-        private StorageContainer _storageContainer;
+
+
+        /// <summary>
+        /// Gets a bool value indicating whether the instance has been disposed.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
 
         /// <summary>
         /// Gets the amount of free space on the device.
@@ -17,14 +25,7 @@ namespace Microsoft.Xna.Framework.Storage
         {
             get
             {
-                try
-                {
-                    return PlatformFreeSpace();
-                }
-                catch (Exception)
-                {
-                    return 0;
-                }
+                return PlatformGetFreeSpace();
             }
         }
 
@@ -35,55 +36,86 @@ namespace Microsoft.Xna.Framework.Storage
         {
             get
             {
-                try
-                {
-                    return PlatformTotalSpace();
-                }
-                catch (Exception)
-                {
-                    return 0;
-                }
+                return PlatformGetTotalSpace();
             }
         }
 
-        public StorageDevice(PlayerIndex? player)
+        /// <summary>
+        /// Creates a storage device for saving game data.
+        /// </summary>
+        /// <param name="titleName">The name of the game title used on some platforms for naming save data.</param>
+        /// <param name="player">The optional player index.  TODO: Why?</param>
+        public StorageDevice(string titleName, PlayerIndex? player)
         {
+            // TODO: Validate titleName is Ascii.
+
+            _titleName = titleName;
+
+            // TODO: How do we go from a player index to an actual player on the console?  
             _player = player;
+
+            PlatformInitialize();
         }
 
-        public void DeleteContainerAsync(string containerName, CancellationToken cancellationToken)
+        ~StorageDevice()
         {
-            Task.Run(() => DeleteContainer(containerName), cancellationToken);
+            PlatformDispose();
         }
 
+        public void Dispose()
+        {
+            if (IsDisposed)
+                return;
+
+            PlatformDispose();
+            GC.SuppressFinalize(this);
+
+            IsDisposed = true;
+        }
+
+        /// <summary>
+        /// Deletes the named container if it exists.
+        /// </summary>
+        /// <param name="containerName">The name of the container.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void DeleteContainer(string containerName)
         {
             if (string.IsNullOrEmpty(containerName))
                 throw new ArgumentNullException("containerName", "A container name must be provided.");
 
-            // safely delete it.
             PlatformDeleteContainer(containerName);
         }
 
-        public Task<StorageContainer> OpenContainerAsync(string containerName, CancellationToken cancellationToken)
+        /// <summary>
+        /// Opens and existing container or creates one.
+        /// </summary>
+        /// <param name="containerName">The name of the container.</param>
+        /// <param name="requiredFreeBytes">On container creation we check for this available space or return null.</param>
+        /// <returns>The container or null if it could not be created.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public StorageContainer OpenContainer(string containerName, long requiredFreeBytes)
         {
-            return Task.Run(() => OpenContainer(containerName), cancellationToken);
-        }
+            // TODO: Should containerName be "displayName" like in old XNA?
 
-        public StorageContainer OpenContainer(string containerName)
-        {
             if (string.IsNullOrEmpty(containerName))
                 throw new ArgumentNullException("containerName", "A container name must be provided.");
 
+            // TODO: Validate containerName is valid for storage file path!
+            // I think this should be ASCII to support all platforms.
+
+
+            if (requiredFreeBytes <= 0)
+                throw new ArgumentOutOfRangeException("requiredFreeBytes", "Must be greater than 0.");
+
             try
             {
-                _storageContainer = PlatformOpenContainer(containerName);
-                if (_storageContainer == null)
+                var container = PlatformOpenContainer(containerName, requiredFreeBytes);
+                if (container == null)
                 {
                     Debug.WriteLine("Failed to open storage container: {0}", containerName);
                     return null;
                 }
-                return _storageContainer;
+                return container;
             }
             catch (Exception ex)
             {
