df9184735ac461362df30d0722787e9de9a7daf9
tomspilman <tom@sickheadgames.com>
2026-09-04 16:04:57 -0500

    Fix for #9522 where the baseInstance argument to DrawInstancedPrimitives was ignored on DX12 and Vulkan.

--- native/monogame/vulkan/MGG_Vulkan.cpp
+++ native/monogame/vulkan/MGG_Vulkan.cpp
@@ -3965,15 +3965,19 @@ void MGG_GraphicsDevice_DrawIndexedInstanced(
 	mgint primitiveCount,
 	mgint indexStart,
 	mgint vertexStart,
+	mgint baseInstance,
 	mgint instanceCount)
 {
 	assert(device != nullptr);
 	assert(primitiveCount >= 0);
 	assert(indexStart >= 0);
 	assert(vertexStart >= 0);
+	assert(baseInstance >= 0);
 	assert(instanceCount > 0);
 
 	if (primitiveCount <= 0)
+		return;
+	if (instanceCount <= 0)
 		return;
 
 	auto& frame = device->frames[device->frameIndex];
@@ -4002,7 +4006,7 @@ void MGG_GraphicsDevice_DrawIndexedInstanced(
 		instanceCount,    
 		indexStart,       
 		vertexStart,      
-		0);               
+		baseInstance);
 }
 
 inline mgint getMipScalar(mgint level, mgint value)
