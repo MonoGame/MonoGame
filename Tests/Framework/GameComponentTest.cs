613d26315f3f1d5eca3d8cba7767d76ab8975c34
Dean Ellis <dellis1972@googlemail.com>
2026-09-24 13:46:13 +0100

    Enable more tests on MacOS & Linux (#9395)
    Enable some more tests on MacOS. 
    Fix a few issues where tests need to run on the UI thread on MacOS
    
    ### Contributor Declaration
    
    - [x] I certify that no LLM's were used to generate any code or
    documentation in this contribution.

--- Tests/Framework/GameComponentTest.cs
+++ Tests/Framework/GameComponentTest.cs
@@ -12,9 +12,6 @@ namespace MonoGame.Tests.Framework
     public static class GameComponentTest
     {
         [Test]
-#if DESKTOPGL || DESKTOPGL4
-        [Ignore("This crashes inside SDL on Mac!")]
-#endif
         public static void InitializeOrderTest()
         {
             var game = new TestGameBase();
