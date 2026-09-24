c7335d5a06054e407f602873f768d707eac67364
Dean Ellis <dellis1972@googlemail.com>
2026-09-24 13:51:16 +0100

    Add ability to build on a docker linux image (#9420)
    Context #9412
    
    Uses the linux docker image built via
    https://github.com/MonoGame/steamrt-build-images.
    The reason for this is if we build using the latest linux images we end
    up linking to a recent version of glib. As a result users will be unable
    to use MonoGame or its pipeline on older versions of linux. We use the
    steam "sniper" docker image for maximum compatibility. All the native
    dependencies (MonoGame.Library.* and MonoGame.Tool.*) have been updated
    to use the same docker image for linux builder. These are bumped as part
    of #9502
    
    This image includes premake5, which is not available for linux arm64 in
    binary form so we add it to the image from source.
    
    Rename the premake custom option `--arch` to `--mg-arch` as newer
    versions of premake have an `--arch` argument which clashes with ours.
    
    Added building of the `mgpipeline` to the `BuildNativeTask` so its
    consistent. The code in the Content Pipeline csproj is still there for
    local builds, but on CI that route is disabled. This is because we need
    to build the linux native binaries in docker to make sure we have
    maximum compatability.
    
    
    ### Contributor Declaration
    
    - [x] I certify that no LLM's were used to generate any code or
    documentation in this contribution.

--- build/BuildToolsTasks/BuildContentPipelineTask.cs
+++ build/BuildToolsTasks/BuildContentPipelineTask.cs
@@ -10,6 +10,7 @@ public sealed class BuildContentPipelineTask : FrostingTask<BuildContext>
     {
         var builderPath = context.GetProjectPath(ProjectType.ContentPipeline);
         context.DotNetPackSettings.MSBuildSettings.WithProperty("DisableMonoGameToolAssets", "True");
+        context.DotNetPackSettings.MSBuildSettings.WithProperty("DisableNativeBuild", "True");
         context.DotNetPack(builderPath, context.DotNetPackSettings);
         context.DotNetPackSettings.MSBuildSettings.Properties.Remove("DisableMonoGameToolAssets");
 
