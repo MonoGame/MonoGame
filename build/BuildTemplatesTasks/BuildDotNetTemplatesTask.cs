e4580d0e4e5ca1e6056809994112f18475776bbf
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-09-24 15:57:35 -0400

    Pack templates from an artifact staging directory

--- build/BuildTemplatesTasks/BuildDotNetTemplatesTask.cs
+++ build/BuildTemplatesTasks/BuildDotNetTemplatesTask.cs
@@ -4,6 +4,18 @@ namespace BuildScripts;
 [TaskName("Build dotnet templates")]
 public sealed class BuildDotNetTemplatesTask : FrostingTask<BuildContext>
 {
+    private const string TemplatesSourceDirectory = "external/MonoGame.Templates/CSharp";
+    private const string TemplatesProjectName = "MonoGame.Templates.CSharp.csproj";
+
     public override void Run(BuildContext context)
-        => context.DotNetPack(context.GetProjectPath(ProjectType.Templates, "MonoGame.Templates.CSharp"), context.DotNetPackSettings);
+    {
+        string stagingDirectory = context.GetOutputPath("TemplatePackageSource");
+        if (context.DirectoryExists(stagingDirectory))
+        {
+            context.DeleteDirectory(stagingDirectory);
+        }
+
+        context.CopyDirectory(TemplatesSourceDirectory, stagingDirectory);
+        context.DotNetPack(System.IO.Path.Combine(stagingDirectory, TemplatesProjectName), context.DotNetPackSettings);
+    }
 }
